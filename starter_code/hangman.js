43b58168a83dc68765c4fca3df8b6eaf8f4a18f5
Camila Maya <camilamaya21@gmail.com>
2017-10-26 16:43:49 +0200

    First iteration: Game Logic DONE - Juan Vallejo y Camila Maya

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,34 +1,58 @@
 var hangman;
 
 function Hangman() {
-
+this.words = [""];
+this.secretWord = "";
+this.letters = [""];
+this.guessedLetter = "";
+this.errorsLeft = "";
 }
 
 Hangman.prototype._getWord = function () {
-
+return this.words[0];
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+ if (keyCode >= 65 && keyCode <= 90){
+   return true;
+ } else {
+    return false;
+ }
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
-
+if (this.letters.indexOf(key) === -1){
+   return true;
+ } else {
+   return false;
+ }
 };
 
 Hangman.prototype._addCorrectLetter = function(i){
-
+  this.guessedLetter=this.secretWord[i].toUpperCase();
 };
 
 Hangman.prototype._addWrongLetter = function (letter){
-
+ if (this.secretWord.indexOf(letter) === -1){
+   this.errorsLeft--;
+ }
 };
 
 Hangman.prototype._checkGameOver = function() {
-
+ if (this.errorsLeft == 0){
+   return true;
+ } else {
+   return false;
+ }
 };
 
 Hangman.prototype._checkWinner = function() {
+ if (this.secretWord.length == this.guessedLetter.length) {
+   return true;
+ } else {
+   return false;
+ }
+
 
 };
 
