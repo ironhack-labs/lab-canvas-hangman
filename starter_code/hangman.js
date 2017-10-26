b35f919907504bfbb1845881181acdb5cdcee70f
gemaderus <hi@gemaderus.com>
2017-10-26 17:10:40 +0200

    Test passed.

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,57 @@
 var hangman;
 
 function Hangman() {
-
+  this.words = ["hello"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "R";
+  this.errorsLeft = 7;
 }
 
 Hangman.prototype._getWord = function () {
-
+  return "";
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
+  if (typeof(keyCode)==='number' && ((keyCode>=65 && keyCode<=90) || (keyCode>=97 && keyCode<=122))) {
+    return true;
+  }else{
+    return false;
+  }
 
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
-
+  //this.checkIfLetter(key);
+    if(this.letters.indexOf(key) === -1 ){
+      return true;
+    } else {
+      return false;
+    }
 };
 
 Hangman.prototype._addCorrectLetter = function(i){
-
+  typeof(this.guessedLetter) === "string"
 };
 
 Hangman.prototype._addWrongLetter = function (letter){
-
+  this.errorsLeft -=1;
 };
 
 Hangman.prototype._checkGameOver = function() {
-
+  if (this.errorsLeft == 0){
+    return true;
+  }else{
+    return false;
+  }
 };
 
 Hangman.prototype._checkWinner = function() {
+  if(this.secretWord.length == this.guessedLetter.length){
+    return true;
+  }else{
+    return false;
+  }
 
-};
-
-document.getElementById("start-game-button").onclick = function(){
-  hangman = new Hangman();
-};
-
-
-document.onkeydown = function(e) {
 
 };
