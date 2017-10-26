c84512e1552ac3ff63be04696403bc8a8d630fc1
Alber Pedraza <alberpedrazaes@gmail.com>
2017-10-26 21:54:42 +0200

    MAD - PP - Alberto Pedraza y Jose Gaspar

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,35 +1,70 @@
 var hangman;
 
 function Hangman() {
-
+  this.words = [""];
+  this.secretWord="";
+  this.letters = [];
+  this.guessedLetter = [];
+  this.errorsLeft = 10;
 }
 
 Hangman.prototype._getWord = function () {
 
+  return "";
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
 
+if ((keyCode > 65) && (keyCode < 90)){
+
+  return true;
+
+}else{
+  return false;
+}
+  return keyCode;
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
 
+  if(this.letters.includes(key)) {
+    return false;
+
+  }else{
+    this.letters.push();
+    return true;
+  }
 };
 
 Hangman.prototype._addCorrectLetter = function(i){
-
+   var keySplit = this.secretWord.split('');
+  return this.guessedLetter = keySplit[i].toUpperCase();
 };
 
 Hangman.prototype._addWrongLetter = function (letter){
 
+this.errorsLeft--;
 };
 
 Hangman.prototype._checkGameOver = function() {
-
+  if (this.errorsLeft == 0){
+    return true;
+  }else{
+    return false;
+  }
 };
-
 Hangman.prototype._checkWinner = function() {
 
+  var varScret = this.secretWord.toUpperCase();
+ varScret = varScret.split('').sort;
+ varScret = this.guessedLetter.sort;
+  varScret = _.difference(varScret,this.guessedLetter);
+
+if (varScret.length == 1){
+return true;
+}else{
+return false;
+}
 };
 
 document.getElementById("start-game-button").onclick = function(){
