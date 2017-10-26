eab4013ebf5f412c802672c29d81b00663faadc2
Juan Cuadra <jcb8793@gmail.com>
2017-10-26 16:31:18 +0200

    Done

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,30 +1,29 @@
-
 //space is the width value where we will start drawing wrong letters
 var space = 700;
 
 //all the shapes and coordinates we need to draw the hangman
 var errorsDraw = [
-  { type: "triangle", x1: 50,  y1: 700, x2: 125, y2: 650, x3: 200, y3: 700 },
-  { type: "line",     x1: 125, y1: 650, x2: 125, y2: 50 },
-  { type: "line",     x1: 125, y1: 50,  x2: 500, y2: 50 },
-  { type: "line",     x1: 500, y1: 50,  x2: 500, y2: 100 },
-  { type: "circle",   x1: 550, y1: 150, x2: 500, y2: 150, r: 50, },
-  { type: "line",     x1: 500, y1: 200, x2: 500, y2: 400 },
-  { type: "line",     x1: 500, y1: 400, x2: 420, y2: 500 },
-  { type: "line",     x1: 500, y1: 400, x2: 580, y2: 500 },
-  { type: "line",     x1: 500, y1: 250, x2: 420, y2: 300 },
-  { type: "line",     x1: 500, y1: 250, x2: 580, y2: 300 }
+  { type: "triangle", x1: 50, y1: 700, x2: 125, y2: 650, x3: 200, y3: 700 },
+  { type: "line", x1: 125, y1: 650, x2: 125, y2: 50 },
+  { type: "line", x1: 125, y1: 50, x2: 500, y2: 50 },
+  { type: "line", x1: 500, y1: 50, x2: 500, y2: 100 },
+  { type: "circle", x1: 550, y1: 150, x2: 500, y2: 150, r: 50 },
+  { type: "line", x1: 500, y1: 200, x2: 500, y2: 400 },
+  { type: "line", x1: 500, y1: 400, x2: 420, y2: 500 },
+  { type: "line", x1: 500, y1: 400, x2: 580, y2: 500 },
+  { type: "line", x1: 500, y1: 250, x2: 420, y2: 300 },
+  { type: "line", x1: 500, y1: 250, x2: 580, y2: 300 }
 ];
 
 function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById("hangman").getContext('2d');
+  this.ctx = document.getElementById("hangman").getContext("2d");
   this.secretWord = secretWord;
   this._createBoard();
   this._drawLines();
 }
 
-HangmanCanvas.prototype._createBoard = function () {
-  this.ctx.clearRect(0,0,1200,800);
+HangmanCanvas.prototype._createBoard = function() {
+  this.ctx.clearRect(0, 0, 1200, 800);
   this.ctx.beginPath();
   this.ctx.lineWidth = 8;
 };
@@ -32,18 +31,18 @@ HangmanCanvas.prototype._createBoard = function () {
 HangmanCanvas.prototype._drawLines = function() {
   for (var i = 0; i < this.secretWord.length; i++) {
     this.ctx.lineWidth = 3;
-    this.ctx.moveTo(250 + (i * 70), 700);
-    this.ctx.lineTo(300 + (i * 70), 700);
+    this.ctx.moveTo(250 + i * 70, 700);
+    this.ctx.lineTo(300 + i * 70, 700);
     this.ctx.stroke();
   }
 };
 
-HangmanCanvas.prototype._writeCorrectLetter = function(i){
+HangmanCanvas.prototype._writeCorrectLetter = function(i) {
   this.ctx.font = "48px Open Sans, sans-serif";
-  this.ctx.fillText(this.secretWord[i].toUpperCase(), 260+(i*70), 680);
+  this.ctx.fillText(this.secretWord[i].toUpperCase(), 260 + i * 70, 680);
 };
 
-HangmanCanvas.prototype._writeWrongLetter = function (letter, errorsLeft){
+HangmanCanvas.prototype._writeWrongLetter = function(letter, errorsLeft) {
   this.ctx.font = "48px Open Sans, sans-serif";
   this.ctx.fillText(letter, space, 200);
   space += 45;
@@ -75,7 +74,7 @@ HangmanCanvas.prototype._gameOver = function() {
   img.onload = function() {
     that.ctx.drawImage(img, 400, 200);
   };
-  img.src = './images/gameover.png';
+  img.src = "./images/gameover.png";
 };
 
 HangmanCanvas.prototype._winner = function() {
@@ -84,5 +83,5 @@ HangmanCanvas.prototype._winner = function() {
   img.onload = function() {
     that.ctx.drawImage(img, 150, 0);
   };
-  img.src = './images/awesome.png';
+  img.src = "./images/awesome.png";
 };
