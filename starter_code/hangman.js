d392d650022919525d338299fcfd7a16f889c70f
Riftland <fran.mscr@gmail.com>
2017-10-26 17:06:10 +0200

    iteration 1 done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,53 @@
 var hangman;
 
 function Hangman() {
-
+  this.words = ['IRONHACK'];
+  this.secretWord = '';
+  this.letters = [];
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
 }
 
 Hangman.prototype._getWord = function () {
-
+  return '';
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+  if(keyCode > 65 && keyCode < 90){
+    return true;
+  } else{
+    return false;
+  }
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
-
+  if(this.letters.includes(key)){
+    return false;
+  } else{
+    return true;
+  }
 };
 
 Hangman.prototype._addCorrectLetter = function(i){
-
+  this.guessedLetter += this.secretWord.substr(i, 1).toUpperCase();
 };
 
 Hangman.prototype._addWrongLetter = function (letter){
-
+  this.errorsLeft--;
 };
 
 Hangman.prototype._checkGameOver = function() {
-
+  if(this.errorsLeft == 0){
+    return true;
+  } else{
+    return false;
+  }
 };
 
 Hangman.prototype._checkWinner = function() {
-
-};
-
-document.getElementById("start-game-button").onclick = function(){
-  hangman = new Hangman();
-};
-
-
-document.onkeydown = function(e) {
-
+  if(this.guessedLetter.split('').sort().join('') == this.secretWord.split('').sort().join('')){
+    return true;
+  } else{
+    return false;
+  }
 };
