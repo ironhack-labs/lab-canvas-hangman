53350fbde4bc8042074710063f56e9a79cfe4890
Alex Del Valle <amd407@gmail.com>
2017-10-27 15:30:45 -0400

    Didn't quite make it

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,15 +1,37 @@
 var hangman;
 
-function Hangman() {
+var words = [
+  "lunch",
+  "zebra",
+  "xylophone",
+  "dynamite",
+];
 
+var secretWord;
+var letters;
+var guessedLetter;
+var errorsLeft;
+
+function Hangman() {
+    this.words = words;
+    this.secretWord = this._getWord();
+    // this.letters =
+    // this.guessedLetter = guessedLetter;
+    // this.errorsLeft = errorsLeft;
 }
 
 Hangman.prototype._getWord = function () {
-
+    secretWord = this.words[Math.floor(Math.random() * this.words.length)];
+    return secretWord;
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
@@ -32,9 +54,9 @@ Hangman.prototype._checkWinner = function() {
 
 };
 
-document.getElementById("start-game-button").onclick = function(){
-  hangman = new Hangman();
-};
+// document.getElementById("start-game-button").onclick = function(){
+//   hangman = new Hangman();
+// };
 
 
 document.onkeydown = function(e) {
