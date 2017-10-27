d55ed84d7412512443b957f113f68169e66aa438
Alejandro Poggi <apoggi419@gmail.com>
2017-10-27 15:26:58 -0400

    almost got through the first iteration of this confusing exercise

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,35 +1,41 @@
-var hangman;
+var hangman = new Hangman();
 
 function Hangman() {
-
+  this.words = ['cat', 'hat', 'mouse'];
+  this.secretWord = this.words[Math.floor(Math.random()*(this.words.length-1))];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+  this.letters = [];
 }
 
 Hangman.prototype._getWord = function () {
-
+  return this.words[Math.floor(Math.random()*(this.words.length-1))];
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+  return  (keyCode > 64 && keyCode < 91);
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
-
+  return this.letters.indexOf(key) === -1;
 };
 
 Hangman.prototype._addCorrectLetter = function(i){
-
+  this.guessedLetter += letters[i];
+  this._checkWinner();
 };
 
 Hangman.prototype._addWrongLetter = function (letter){
-
+  this.errorsLeft --;
+  this._checkGameOver();
 };
 
 Hangman.prototype._checkGameOver = function() {
-
+  return this.errorsLeft === 0;
 };
 
 Hangman.prototype._checkWinner = function() {
-
+  return this.guessedLetter.length === this.secretWord.length;
 };
 
 document.getElementById("start-game-button").onclick = function(){
