6168743fa403947e1ba731384208641f92987533
Bart Smet <bartsmet30@gmail.com>
2017-11-22 19:53:16 +0100

    feat: Initial commit after class

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,23 +1,38 @@
 var hangman;
 
 function Hangman() {
-
+this.words = ["table","house","ironhack","meatballs","blitzkrieg"];
+this.secretWord = "";
+this.letters = [];
+this.guessedLetter = "";
+this.errorsLeft = 10;
 }
 
 Hangman.prototype._getWord = function () {
-
+  return this.words[Math.floor(Math.floor()*this.words.length)];
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+ if (keyCode === 43) {
+   return false;
+ } else {
+   return true;
+ }
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
-
+if (this.letters.includes(key)) {
+  return true;
+} else {
+  return false;
+}
 };
 
 Hangman.prototype._addCorrectLetter = function(i){
-
+if (this._checkClickedLetters(key) === false){
+  this.guessedLetter += i;
+}
+this._checkWinner();
 };
 
 Hangman.prototype._addWrongLetter = function (letter){
@@ -29,7 +44,9 @@ Hangman.prototype._checkGameOver = function() {
 };
 
 Hangman.prototype._checkWinner = function() {
-
+  if(this.secretWord.length === this.letters.length){
+    return true;
+  }
 };
 
 document.getElementById("start-game-button").onclick = function(){
