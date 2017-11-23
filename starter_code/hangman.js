e086ceb95ea9c23c224ae7361b3886adb845b1b3
Pablo Laso <plasocortabitarte@gmail.com>
2017-11-23 17:22:29 +0100

    test done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -34,19 +34,30 @@ Hangman.prototype._checkClickedLetters = function(key) {
 };
 
 Hangman.prototype._addCorrectLetter = function(i){
-  
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  this._checkWinner();
 };
 
 Hangman.prototype._addWrongLetter = function (letter){
-
+  this.errorsLeft--;
+  this._checkGameOver();
 };
 
 Hangman.prototype._checkGameOver = function() {
-
+  if (this.errorsLeft === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 Hangman.prototype._checkWinner = function() {
+  var secretWordArrayCompare = this.secretWord.split('').sort().join('');
+  var guessedLetterArrayCompare = this.guessedLetter.split('').sort().join('');
 
+  var winner = secretWordArrayCompare === guessedLetterArrayCompare ? true : false;
+
+  return winner;
 };
 
 document.getElementById("start-game-button").onclick = function(){
