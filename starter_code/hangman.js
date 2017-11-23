4596e309a3a2fa4ff4195f7217b20e47720f9cbb
sevi221 <sevilaura221@gmail.com>
2017-11-23 17:41:57 +0100

    second upload

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,19 +1,24 @@
 var hangman;
 
 function Hangman() {
-
+  this.words = ["perro", "gato", "amsterdam", "murcielago"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
 }
 
 Hangman.prototype._getWord = function () {
+   return this.words[Math.floor(Math.random() * this.words.length)];
 
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+  return (keyCode >= 60 && keyCode <= 90);
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
-
+  return !(this.letters.includes(key));
 };
 
 Hangman.prototype._addCorrectLetter = function(i){
