437dbbb43f1f31fa500fcb70458262a191e147ff
Enrique Hernández <l6cenrique@gmail.com>
2018-01-18 17:29:11 +0100

    hecho lo que nos ha dado tiempo

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,28 +1,55 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ['HOLA', 'IRONHACK', 'JAVASCRIPT'];
+  this.secretWord = '';
+  this.guessedLetter = '';
+  this.letters = [];
+  this.errorsLeft ;
+}
 
-// }
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  var indexWord = Math.floor(Math.random() * this.words.length);
+  this.secretWord += this.words[indexWord];
+  return this.secretWord;
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode > 65 && keyCode < 90) {
+    return true;
+  } else {
+    return false
+  }
 
-// };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.indexOf(key) === -1) {
+    this.letters.push(key);
+      return true;
+    } else {
+      return false;
+    }
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+};
 
-// };
+ Hangman.prototype.addCorrectLetter = function (i) {
+  var letraCap= this.secretWord[i].toUpperCase();
+     this.guessedLetter+=letraCap;
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+  }
 
-// };
+ ;
+
+ Hangman.prototype.addWrongLetter = function (letter) {
+  if( this.secretWord.indexOf(letter)==-1){
+    this.errorsLeft-1
+  }
+
+ };
 
 // Hangman.prototype.checkGameOver = function () {
 
