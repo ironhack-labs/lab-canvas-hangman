180e0d34a04522b38f2156a49aa558192f28bde0
Jesús Silva <hola@jesussilva.es>
2018-01-18 17:23:01 +0100

    Todos los test correctos menos uno

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,69 @@
-var hangman;
+var hangman = new Hangman();
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["hola", "adios", "bye"];
+  this.secretWord = "ironhack";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function() {
+  return this.words[Math.floor(Math.random() * this.words.length)];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function(keyCode) {
+  if (keyCode >= 65 && keyCode <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function(key) {
+  if ( this.checkIfLetter() ) {
+    if (this.letters.includes(key)) {
+      return false;
+    } else {
+      this.letters.push(key);
+      if ( this.secretWord.includes(key) ) {
+        var letterIndex = this.secretWord.indexOf(key);
+        this.addCorrectLetter(letterIndex);
+        return true;
+      }  
+    }
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function(i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  this.checkWinner();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function(letter) {
+  this.errorsLeft -= 1;
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function() {
+  if (this.errorsLeft === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.checkWinner = function() {
+  if (this.guessedLetter.length === this.secretWord.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
 };
 
-
-document.onkeydown = function (e) {
-
-};
+document.onkeydown = function(e) {};
