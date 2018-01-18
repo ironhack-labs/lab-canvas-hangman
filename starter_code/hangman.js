c7f79832d9283771837af20060b9c9362876be0c
Alvaro <amartinezfernandez.61@gmail.com>
2018-01-18 17:10:48 +0100

    No hemos llegado a la iteración 1

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,49 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ['a', 'b,', 'c'];
+  this.secretWord = '';
+  this.letters = [];
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  return this.words[Math.floor(Math.random()*this.words.length)];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if(((typeof(keyCode) == 'number') && (keyCode >= 65) && (keyCode <= 90))){
+    return true
+  } else { return false }
+};
+//Key Code de 65 a 90
+Hangman.prototype.checkClickedLetters = function (key) {
+   if(this.letters.includes(key)){
+     return false;
+   } return true;
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase()
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addWrongLetter = function (letter) {
+   
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+   if(this.letters >= this.errorsLeft){
+     return true;
+   } return false;
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkWinner = function () {
+  if(this.letters == this.errorsLeft){
+    return true;
+  } return false;
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
