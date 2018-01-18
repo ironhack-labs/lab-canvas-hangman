e8f280c0733d2d6bf6f683418f5beed4d5f3bd9a
Carlos San Martin <carlosanmartin@gmail.com>
2018-01-18 18:28:25 +0100

    done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,24 +1,40 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["JESUS", "JOSE", "MARIA", "BUEY", "MULA"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function() {
+  return this.words[
+    Math.floor(Math.random() * Math.floor(this.words.length - 1))
+  ];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function(keyCode) {
+  if (keyCode >= 65 && keyCode <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function(key) {
+  if (this.letters.includes(key)) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
-
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  if (this.secretWord.includes(this.secretWord[i])) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+  }
+};
 
 // Hangman.prototype.addWrongLetter = function (letter) {
 
@@ -32,11 +48,8 @@ var hangman;
 
 // };
 
-document.getElementById('start-game-button').onclick = function () {
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
 };
 
-
-document.onkeydown = function (e) {
-
-};
+document.onkeydown = function(e) {};
