683fcac8c3151a5bac60e5b413662c3068a90539
Myriam <bourbiamyriam@gmail.com>
2018-01-19 09:13:21 +0100

    Hangman Jean-Nicolas_Myriam

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,79 @@
 
 function HangmanCanvas(secretWord) {
+  this.canvas=document.getElementById('hangman');
   this.ctx = document.getElementById('hangman').getContext('2d');
+  this.secretWord=secretWord;
 }
 
 HangmanCanvas.prototype.createBoard = function () {
-
+  this.ctx.clearRect(0, 0, this.ctx.width, this.ctx.height);
 };
 
 HangmanCanvas.prototype.drawLines = function () {
-
+  this.ctx.beginPath();
+  this.ctx.moveTo(300, 700);
+  for (var i=0; i< this.secretWord.length; i++){
+    this.ctx.moveTo(300+i*75, 700); 
+    this.ctx.lineTo(350+i*75, 700);
+  }
+  
 };
-
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
+  
 };
 
 HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
+  
 };
 
 HangmanCanvas.prototype.drawHangman = function (shape) {
-
+  switch (this.errorsLeft){
+    case 9 :this.ctx.moveTo(75, 700);
+    this.ctx.lineTo(200, 700);
+    this.ctx.lineTo(137.5, 650);
+    this.ctx.lineTo(75, 700);
+    this.ctx.stroke();
+    break;
+    case 8 :this.ctx.moveTo(137.5, 650);
+    this.ctx.lineTo(137.5, 100);
+    break;
+    case 7 :this.ctx.moveTo(137.5, 100);
+    this.ctx.lineTo(450, 100);
+    break;
+    case 6:  this.ctx.moveTo(450, 100);
+    this.ctx.lineTo(450, 150);
+    break;
+    case 5: this.ctx.moveTo(480, 180);
+    this.ctx.arc(450, 180, 30, 0, Math.PI*2, true);
+    break;
+    case 4: this.ctx.moveTo(450, 210);
+    this.ctx.lineTo(450, 350);
+    break;
+    case 3: this.ctx.moveTo(450, 350);
+    this.ctx.lineTo(500, 420);
+    break;
+    case 2:  this.ctx.moveTo(450, 350);
+    this.ctx.lineTo(400, 420);
+    break;
+    case 1: this.ctx.moveTo(450, 250);
+    this.ctx.lineTo(500, 320);
+    break;
+    case 0:  this.ctx.moveTo(450, 250);
+    this.ctx.lineTo(400, 320);
+    break;
+  }
+  
 };
 
 HangmanCanvas.prototype.gameOver = function () {
-
+  
 };
 
 HangmanCanvas.prototype.winner = function () {
+
+   // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    var img = new Image();   
+    img.src = 'images/awesome.png'
+    this.ctx.drawImage(img,0,0, this.canvas.height, this.canvas.width)
 
 };
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,67 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words=["riri","fifi","mimi"];
+  this.secretWord="";
+  this.letters=[];
+  this.guessedLetter="";
+  this.errorsLeft=10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  return this.words[Math.floor(Math.random() * this.words.length)]
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (key) {
+  if (key>64 && key<91){
+    return true;
+  } else {return false;}
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (letter) {
+  return !this.letters.includes(letter);
+}
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+this.guessedLetter += this.secretWord[i].toUpperCase();
+ };
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+this.errorsLeft--;
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft==0){
+    return true
+  }
+  else {
+    return false
+  }
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  var test=true;
+  var i=0;
+  while(test && i< this.secretWord.length)
+  {
+    if (!this.guessedLetter.includes(this.secretWord[i])){
+      test=false;
+    }
+    else i++
+  }
+  return test 
+  
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
 };
 
-
+var key="";
+var letter = "";
 document.onkeydown = function (e) {
-
+  console.log(e.key);
+  key = e.keyCode;
+  letter = e.key;
 };
