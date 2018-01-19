be31b46628338d8347de91b2283647ccb16c511b
Luis Herrera <la.herrera457@gmail.com>
2018-01-19 15:58:42 +0100

    hangman logical iterations

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,51 @@
 var hangman;
 
-// function Hangman() {
 
-// }
+function Hangman() {
+  this.words = [];
+  this.secretWord = '';
+  this.letters = [];
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  this.secretWord = toString(this.words[Math.floor(Math.random*this.words.length)]);
+  return this.secretWord;
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode <= 43) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.includes(key) === true) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  // return this.guessedLetter + i;
+  console.log(i);
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addWrongLetter = function (letter) {
 
-// };
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkGameOver = function () {
 
-// };
+};
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+Hangman.prototype.checkWinner = function () {
 
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
