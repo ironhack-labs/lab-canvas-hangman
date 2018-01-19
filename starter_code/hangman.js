661447df2b0c8f1400e0a12d6798d61d9b838ea0
Alla Roman <alla.roman.mail@gmail.com>
2018-01-19 16:36:24 +0100

    Done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,20 +1,32 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  var self = this;
+  self.words = ['cheese', 'tomato', 'table'];
+  self.secretWord = '';
 
-// }
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () { 
+  var self = this;
+  var n = Math.floor(Math.random() * self.words.length);
+  choosenWord = self.words[n];
+  return choosenWord;
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+if (64 < keyCode < 91) {//keyCode.length === 1 && keyCode.match(/[a-z]/i)) {
+  return true;
+} else { 
+ return false; 
+}
 
-// };
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+ Hangman.prototype.checkClickedLetters = function (key) {
 
-// };
+};
 
 // Hangman.prototype.addCorrectLetter = function (i) {
 
