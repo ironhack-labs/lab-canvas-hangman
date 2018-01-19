0fad8459ee89f56e0367ebf9d1e37e4beb6b03a7
lindaluisa <lindaluisarosenthal@gmail.com>
2018-01-19 16:36:14 +0100

    Iteration 1 - addCorrectLetter

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,24 +1,46 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = [];
+  this.letters = [];
+  this.secretWord = "";
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  var self = this;
+  var randomWord = "" + self.words[Math.floor(Math.random() * self.words.length)];
+  return randomWord; 
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  var self = this;
+  if (keyCode >= 65 && keyCode < 91) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  var self = this;
+  var letterFound = self.letters.find(function (letter){
+    return key === letter;
+  });
+  console.log(letterFound);
+  if (letterFound === key) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
-
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  var self = this;
+  
+};
 
 // Hangman.prototype.addWrongLetter = function (letter) {
 
