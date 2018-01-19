c4036efa30a9125695044f63b4f66a28dd4ea5e1
Jana Stinn <jana-stinn@gmx.de>
2018-01-19 15:55:31 +0100

    first commit

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,40 @@
 
 function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+  var self = this;
+  self.ctx = document.getElementById('hangman').getContext('2d');
 }
 
 HangmanCanvas.prototype.createBoard = function () {
-
+  var self = this;
+  self.ctx.clearRect(0, 0, 1200, 800);
+  self.ctx.beginPath();
+  self.ctx.lineWidth = 8;
 };
 
 HangmanCanvas.prototype.drawLines = function () {
-
+  var self = this;
+  self.ctx.beginPath();
+  self.ctx.moveTo(500, 300);
+  self.ctx.lineTo(550, 300);
+  self.ctx.stroke();
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
+  var self = this;
 };
 
 HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
+  var self = this;
 };
 
 HangmanCanvas.prototype.drawHangman = function (shape) {
-
+  var self = this;
 };
 
 HangmanCanvas.prototype.gameOver = function () {
-
+  var self = this;
 };
 
 HangmanCanvas.prototype.winner = function () {
-
+  var self = this;
 };
