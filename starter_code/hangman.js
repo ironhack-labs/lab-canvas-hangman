cdda6deb5cb44157d7e36dcb26c2427906e7ea6b
Davinia Roca <daviniaroca@Davinias-MacBook-Air.local>
2018-02-02 09:13:07 -0600

    done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,50 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+   this.words=["hola","Debora","Davinia"];
+   this.secretWord=""; // palabra a adiviniar
+   this.letters=[]; //letras intentadas
+   this.guessedLetter=""; // letras adivinadas
+   this.errorsLeft=10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+    if (this.words.length>=2){
+      var ind = Math.floor(Math.random()*this.words.length);
+      return this.words[ind];
+    }
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+    return ((64<keyCode)&&(keyCode<91));
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+    var isnot= true;
+    this.letters.forEach(function(l){
+       if (l===key){
+         return false;
+       }
+    });
+    return isnot;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+    this.guessedLetter+=this.secretWord[i].toUpperCase();
+    this.checkWinner();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+    this.errorsLeft-=1;
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+    return (!this.errorsLeft===0)
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+    return this.guessedLetter.length===this.secretWord.length;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
