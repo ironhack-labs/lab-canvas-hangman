2977266fc6eab9f0de4789b529c63d4716a86fb2
Diego Vazquez <diurivj@icloud.com>
2018-04-12 14:33:53 -0500

    Only logic part

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,17 +1,27 @@
 
 function HangmanCanvas(secretWord) {
   this.ctx = document.getElementById('hangman').getContext('2d');
+  this.secretWord = secretWord;
 }
 
 HangmanCanvas.prototype.createBoard = function () {
-
+  this.ctx.clearRect(0,0, getElementById('hangman').width, getElementById('hangman').height);
 };
 
 HangmanCanvas.prototype.drawLines = function () {
-
+  var j = 0;
+  for(i = 0; i<this.secretWord.length; i++){
+    this.ctx.beginPath();
+    this.ctx.moveTo(50+j,50);
+    this.ctx.lineTo(60+j,50);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    j += 30;
+  }
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+  
 
 };
 
