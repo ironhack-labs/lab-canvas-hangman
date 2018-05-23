56660aaabf3fe5e7cae164075eb997afa4f04dda
Mike Nieva <miguelnieva@conexionew.com>
2018-05-23 18:30:36 -0500

    Primer avancesc on tests

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,51 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = [];
+  this.secretWord = "";
+  this.letters = [];
 
-// }
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  return '';
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (typeof keyCode == "number" && keyCode >= 65 && keyCode <=90){
+    return true
+  }else {
+    return false
+  }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  
+  if (typeof key == "string"){
+    console.log(this.letters);    
+    // if(key == this.letters.find(key)){
+    //   return false
+    // }
+    return true
+  }
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addCorrectLetter = function (i) {
 
-// };
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.addWrongLetter = function (letter) {
 
-// };
+};
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+Hangman.prototype.checkGameOver = function () {
 
-// };
+};
 
-// Hangman.prototype.checkGameOver = function () {
+Hangman.prototype.checkWinner = function () {
 
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
