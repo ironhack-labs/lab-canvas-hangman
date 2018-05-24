8f82062945163cd32a8bd3f5caf68b12fc10fd8e
Alekz7 <ing.aragon.ipn@gmail.com>
2018-05-24 18:20:30 -0500

    MEXPT-ALEJANDRO_ARAGON_ALVAREZ

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,10 +1,47 @@
 
 function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+  	this.ctx = document.getElementById('hangman').getContext('2d');
+  	console.log("Llamando las funciones para dibujar: " + secretWord);
+  	this.ctx.fillStyle = "black";
+  	this.ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+	console.log("obj");
+	
+	let anchoLetra = 50;
+	let x = 30;
+	let y = 80;
+	let espacioEntreLetras = 10;
+	this.ctx.beginPath();
+	for(var k = 0; k < secretWord.length; k++){
+
+		this.ctx.moveTo(x,y);
+		this.ctx.lineTo(x + anchoLetra ,y);
+		x = x + anchoLetra + espacioEntreLetras;
+	}		
+	this.ctx.strokeStyle = 'white';
+	this.ctx.stroke();
+
+	let xCabeza = 400;
+	let yCabeza = 200;
+	let anchoCabeza = 60;
+	let largoCuerpo = 200;
+	//HangMan Head
+	this.ctx.beginPath();
+	this.ctx.arc(xCabeza,yCabeza,anchoCabeza,0,2*Math.PI,false);
+	this.ctx.strokeStyle = 'white';
+	this.ctx.stroke();
+
+	//HangMan Body
+	this.ctx.beginPath();
+	this.ctx.moveTo(xCabeza , yCabeza + anchoCabeza );
+	this.ctx.lineTo(xCabeza , yCabeza + anchoCabeza + largoCuerpo);
+	this.ctx.strokeStyle = 'yellow';
+	this.ctx.stroke();
+
 }
 
-HangmanCanvas.prototype.createBoard = function () {
-
+HangmanCanvas.prototype.createBoard = function () {	
+	// this.ctx.fillRect(0,0,window.innerWidth,window.innerHeight);		
+	console.log("obj");
 };
 
 HangmanCanvas.prototype.drawLines = function () {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,72 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+	// An array where we will store all the words that the player need to guess. We will take one of them randomly.
+	this.words = ['node','ironhack','bootstrap','jasmine','flexbox','ubuntu'];	
+	this.secretWord = ""; // Here we will store the word chosen for each game
+	this.letters = []; // An array to store the letters the user already clicked, so we do not repeat them.
+	this.guessedLetter = ""; // A string to store the letters the user clicked and guessed. We will use this to know when the user wins.
+	this.errorsLeft = 10; // It should start at 10, and decrease every time a user clicks on a letter that is not in the word.
+}
 
-// }
+Hangman.prototype.getWord = function () {
+	let numeroAleatorio = Math.floor(Math.random() * this.words.length);
+	return this.words[numeroAleatorio];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+	// MAYUSCULAS A--Z::::::::::::minusculas a--z
+	if ( keyCode >= 65 && keyCode <= 90 || keyCode >= 97 && keyCode <= 122 ){
+		return true;
+	}
+	return false;
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+	if (this.letters.indexOf(key) == -1) {
+		// this.letters.push(String.fromCharCode(key).toUpperCase);
+		return true;		
+	}
+	return false;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (key) {
 
-// };
+	// This is not good yet
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+	for(var j = 0; j < this.secretWord.length;j++){
+		let letraSecreta = this.secretWord[j].toUpperCase;
+		let letraEscrita = String.fromCharCode(i).toUpperCase;
+		if (letraSecreta == letraEscrita){
+			this.guessedLetter[i] = letraSecreta[j];
+		}
+	}
 
-// };
+	if (this.secretWord == this.guessedLetter){
+		return true;
+	} else {
+		return false;
+	}
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.addWrongLetter = function (letter) {
+	this.errorsLeft--;
 
-// };
+};
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+Hangman.prototype.checkGameOver = function () {
+	if (this.errorsLeft == 0) return true; else return false;
+};
 
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+	if (this.guessedLetter == this.secretWord) return true; else return false;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  HangmanCanvas(hangman.getWord());
+  console.log(hangman.secretWord);
+  HangmanCanvas.createBoard;
 };
 
 
