57f306ff01816540bcb0f0171b497778a7e869a5
Miguel Angel Rodriguez <finkel218@gmail.com>
2018-05-26 10:08:37 -0500

    no termine

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,74 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+  this.words= ["perro", "gato","pollo","helado","Doctor"];
+  this.secretWord= "";
+  this.letters =[];
+  this.guessedLetter = "";
+  this.errorsLeft= 10;
 
-// }
+ }
 
-// Hangman.prototype.getWord = function () {
+ Hangman.prototype.getWord = function () {
+ return this.words[Math.floor(Math.random() * this.words.length)];
+ };
 
-// };
+ Hangman.prototype.checkIfLetter = function (keyCode) {
+  if( 65 <= keyCode && keyCode <= 90){
+    return true
+  } else {
+    return false;
+  } 
+ };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  Hangman.prototype.checkClickedLetters = function (key) {
+    return (this.letters.indexOf(key) == -1);
+  };
 
-// };
+  Hangman.prototype.addCorrectLetter = function (i) {
+    this.guessedLetter = (this.secretWord[i].toUpperCase());  
+  };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+  Hangman.prototype.addWrongLetter = function (letter) {
+    this.errorsLeft--;
+  };
 
-// };
+  Hangman.prototype.checkGameOver = function () {
+    if(this.errorsLeft === 0){
+      return true
+    }else{
+      return false;
+    }
+    
+  };
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+  Hangman.prototype.checkWinner = function () {
+    return (this.secretWord.length == this.guessedLetter.length);
+  };
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
-  hangman = new Hangman();
+  document.getElementById('start-game-button').onclick = function () {
+    hangman = new Hangman();
+    hangman.secretWord = hangman.getWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    hangmanCanvas.createBoard();
+    hangmanCanvas.drawLines();
+    console.log(hangmanCanvas);
 };
 
 
 document.onkeydown = function (e) {
-
+  if (hangman.checkIfLetter(e.keyCode)){
+    var upper = e.key.toUpperCase();
+    if (hangman.checkClickedLetters(upper)){
+      hangman.letters.push(upper);
+      if (hangman.secretWord.indexOf(upper)!==-1){
+        hangmanCanvas.writeCorrectLetter(hangman.secretWord.indexOf(upper));
+        hangman.addCorrectLetter(hangman.secretWord.indexOf(upper));
+        if(hangman.checkWinner()){hangmanCanvas.winner();}
+      } else {
+        hangman.addWrongLetter();
+        hangmanCanvas.writeWrongLetter(upper, hangman.errorsLeft);
+      }
+    }
+  }
 };
