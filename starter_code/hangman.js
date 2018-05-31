afc82e36def22562a4e7a82bcd8281cfab061b4c
spelavrtovec <spela@vrtovec.net>
2018-05-31 14:37:47 +0200

    We finished the functions, but didn't do the canvas part (yet)

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,58 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = [];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
 
-// }
 
-// Hangman.prototype.getWord = function () {
+}
 
-// };
+Hangman.prototype.getWord = function (words) {
+  return "" + this.words[Math.floor(Math.random*this.words.length)];
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90 ) {
+    return true
+  } else return false;
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+ if (this.letters.indexOf(key) > -1) {
+   return false
+ } else return true;
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+  // for (var i = 0; i < this.letters.length; i++) {        // -> So why does this not work? 
+  //   if (this.letters[i].indexOf(key) > -1) {
+  //     return false
+  //   } else return true;
+  // };
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  if (this.checkClickedLetters) {
+    return this.guessedLetter = this.guessedLetter + this.secretWord.charAt(i).toUpperCase();
+  } else return false
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft -= 1;
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft === 0) {
+    return true;
+  } else return false;
+};
 
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  if (this.guessedLetter.length == this.secretWord.length) {
+    return true;
+  } else return false;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
