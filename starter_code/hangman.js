d5ec6e723ab2653792a5943ff62cfcf22c1da47e
aparna pattathil <aparnapattathil@hotmail.com>
2018-06-01 18:16:41 +0200

    First Iteration

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,59 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+this.words = ['apple','banana', 'grapes', 'computer','map','fan','whiteboard'];
+this.secretWord = this.getWord();
+this.letters = [];
+this.guessedLetter = '';
+this.errorsLeft = 10;
 
-// }
 
-// Hangman.prototype.getWord = function () {
+}
 
-// };
+ Hangman.prototype.getWord = function () {
+  return this.words[Math.floor(Math.random()* this.words.length )];
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if((64 < keyCode && keyCode < 91) || (96 < keyCode && keyCode < 123)){
+  return true;
+}else{
+  return false;
+}
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  if(this.letters.includes(key)){
+    return false;
+  }else{
+    return true;
+  }
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+   
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+this.errorsLeft--;
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft === 0){
+    return true;
+  }else{
+    return false;
+  }
+};
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+if(this.guessedLetter.toUpperCase() === this.secretWord.toUpperCase()){
+  return true;
+}else{
+  return false;
+}
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
