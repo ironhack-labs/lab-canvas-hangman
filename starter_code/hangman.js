33885a6596aca2eee305ffe768d4b7becd246f7c
Marie Gillier <magillier@hotmail.fr>
2018-06-07 19:58:28 +0200

    first iteration done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,56 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["China","France","Japan","Korea","India", "Spain", "Brasil"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  var randomIndex = Math.floor(Math.random()*this.words.length);
+  var secretWord = this.words[randomIndex];
+  return secretWord;
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 64 && keyCode <= 91) {
+    return true;
+  } else {
+    return false;
+  }
+ };
 
-// };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.includes(key)){
+    return false
+  } else {
+    this.letters.push(key);
+    return true;
+  }
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    return this.guessedLetter;
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft-=1;
 
-// };
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkGameOver = function () {
+  return this.errorsLeft===0;
+};
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  if (this.guessedLetter.length===this.secretWord.length){
+  return true;
+} else return false;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
