f481d74880168a0624dce65e57b9eb53f56ec7f7
pabloturati <pablo.turati@gmail.com>
2018-06-21 17:31:37 -0500

    not done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,55 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["dog", "cat", "mouse"];
+  this.secretWord = "dog";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  var randomIndex = Math.floor(Math.random()*this.words.length);
+  var randomWord = this.words[randomIndex];
+  return randomWord;
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if(keyCode > 64 && keyCode < 91) return true;
+  return false;
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  var index = this.letters.indexOf(key.toUpperCase());
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  if(index<0){
+    return true;
+  }else{
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += i;
+  this.checkWinner();
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.letters.push(letter);
+  this.errorsLeft--;
 
-// };
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft == 0) return true;
+  return false;
+};
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  
+  if(this.guessedLetter.length === this.secretWord.length) return true;
+  return false;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
