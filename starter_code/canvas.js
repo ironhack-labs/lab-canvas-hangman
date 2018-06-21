bc668669f997a4348f2b634eb3ac7429d9532c81
Andrés Martin <monkeyandres@protonmail.com>
2018-06-21 20:25:32 +0200

    Clean Up

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,77 +1,30 @@
-
 var hangman = new Hangman();
 var secretWord = hangman.getWord();
 var startBtn = document.getElementById("start-game-button");
 
 function HangmanCanvas(secretWord) {
-  this.element = document.getElementById('hangman')
-  this.ctx = this.element.getContext('2d');
+  this.element = document.getElementById("hangman");
+  this.ctx = this.element.getContext("2d");
   this.ctx.font = "30px sans-serif";
-  this.ctx.lineWidth=6;
+  this.ctx.lineWidth = 6;
 }
 
-HangmanCanvas.prototype.createBoard = function () {
-  // this.ctx.beginPath();
-
-  // // Base
-  // this.ctx.moveTo(50, 780);
-  // this.ctx.lineTo(250 ,780);
-  // this.ctx.lineTo(150, 680); // Vertice arriba
-  // this.ctx.lineTo(50, 780);
-
-  // // Palo para arriba
-  // this.ctx.moveTo(150,680);
-  // this.ctx.lineTo(150,100);
-
-  // // Palo horizontal
-  // this.ctx.lineTo(400 ,100);
-
-  // // Cuerda horca
-  // this.ctx.lineTo(400, 160);
-
-  // // Cerrar dibujo
-  // this.ctx.stroke();
-  // this.ctx.closePath();
-  
-  // // Cabeza
-  // var pi = Math.PI*2
-  // this.ctx.beginPath();
-  // this.ctx.arc(400, 230, 70, 0, pi, true);
-  // this.ctx.stroke();
-  // this.ctx.closePath();
-
-  // // Cuerpo
-  // this.ctx.beginPath();
-  // this.ctx.moveTo(400, 300);
-  // this.ctx.lineTo(400, 540); // Fin cuerpo
-  
-  // // Piernas
-  // this.ctx.lineTo(320, 640); //Pierna Izquierda
-  // this.ctx.moveTo(400, 540);
-  // this.ctx.lineTo(480, 640); // Pierna Derecha
-
-  // // Brazos
-  // this.ctx.moveTo(400, 350);
-  // this.ctx.lineTo(320, 420); // Brazo izquierdo
-  // this.ctx.moveTo(400, 350);
-  // this.ctx.lineTo(480, 420); // Brazo derecho
-  // this.ctx.stroke();
-  // this.ctx.closePath();
+HangmanCanvas.prototype.createBoard = function() {
   this.cleanCanvas();
   this.drawLines(secretWord.length);
 };
 
-HangmanCanvas.prototype.cleanCanvas = function(){
+HangmanCanvas.prototype.cleanCanvas = function() {
   this.ctx.clearRect(0, 0, 1200, 800);
-}
+};
 
-HangmanCanvas.prototype.drawLines = function (numLines) {
+HangmanCanvas.prototype.drawLines = function(numLines) {
   this.ctx.beginPath();
 
   var positionX = 320;
   var positionY = 780;
 
-  for(var i = 0; i<numLines; i++){
+  for (var i = 0; i < numLines; i++) {
     positionX += 10;
     this.ctx.moveTo(positionX, positionY);
 
@@ -82,22 +35,22 @@ HangmanCanvas.prototype.drawLines = function (numLines) {
   this.ctx.closePath();
 };
 
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+HangmanCanvas.prototype.writeCorrectLetter = function(index) {
   var positionX = 340;
   var positionY = 770;
 
-  this.ctx.fillText(secretWord[index], positionX+(60*index),positionY);
+  this.ctx.fillText(secretWord[index], positionX + 60 * index, positionY);
 };
 
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
+HangmanCanvas.prototype.writeWrongLetter = function(letter, errorsLeft) {
   var positionX = 600;
   var positionY = 120;
 
-  this.ctx.fillText(letter, positionX+((10-errorsLeft)*35),positionY);
+  this.ctx.fillText(letter, positionX + (10 - errorsLeft) * 35, positionY);
 };
 
-HangmanCanvas.prototype.drawHangman = function (shape) {
-  switch(shape){
+HangmanCanvas.prototype.drawHangman = function(shape) {
+  switch (shape) {
     case 1:
       var newFun = printBase.bind(this);
       newFun();
@@ -108,8 +61,8 @@ HangmanCanvas.prototype.drawHangman = function (shape) {
       break;
     case 3:
       var newFun = printHorizontalStick.bind(this);
-    newFun();
-    break;
+      newFun();
+      break;
     case 4:
       var newFun = printGallow.bind(this);
       newFun();
@@ -141,59 +94,59 @@ HangmanCanvas.prototype.drawHangman = function (shape) {
   }
 };
 
-HangmanCanvas.prototype.gameOver = function () {
-  alert("Has Perdido")
-  this.cleanCanvas()
+HangmanCanvas.prototype.gameOver = function() {
+  alert("Has Perdido");
+  this.cleanCanvas();
 };
 
-HangmanCanvas.prototype.winner = function () {
-  alert("Has Ganado")
-  this.cleanCanvas()
+HangmanCanvas.prototype.winner = function() {
+  alert("Has Ganado");
+  this.cleanCanvas();
 };
 
-function printBase(){
+function printBase() {
   this.ctx.beginPath();
   this.ctx.moveTo(50, 780);
-  this.ctx.lineTo(250 ,780);
+  this.ctx.lineTo(250, 780);
   this.ctx.lineTo(150, 680); // Vertice arriba
   this.ctx.lineTo(50, 780);
   this.ctx.stroke();
   this.ctx.closePath();
 }
 
-function printStick(){
+function printStick() {
   this.ctx.beginPath();
-  this.ctx.moveTo(150,680);
-  this.ctx.lineTo(150,100);
+  this.ctx.moveTo(150, 680);
+  this.ctx.lineTo(150, 100);
   this.ctx.stroke();
   this.ctx.closePath();
 }
 
-function printHorizontalStick(){
+function printHorizontalStick() {
   this.ctx.beginPath();
-  this.ctx.moveTo(150,100);
-  this.ctx.lineTo(400 ,100);
+  this.ctx.moveTo(150, 100);
+  this.ctx.lineTo(400, 100);
   this.ctx.stroke();
   this.ctx.closePath();
 }
 
-function printGallow(){
+function printGallow() {
   this.ctx.beginPath();
-  this.ctx.moveTo(400 ,100);
+  this.ctx.moveTo(400, 100);
   this.ctx.lineTo(400, 160);
   this.ctx.stroke();
   this.ctx.closePath();
 }
 
-function printHead(){
-  var pi = Math.PI*2
+function printHead() {
+  var pi = Math.PI * 2;
   this.ctx.beginPath();
   this.ctx.arc(400, 230, 70, 0, pi, true);
   this.ctx.stroke();
   this.ctx.closePath();
 }
 
-function printBody(){
+function printBody() {
   this.ctx.beginPath();
   this.ctx.moveTo(400, 300);
   this.ctx.lineTo(400, 540); // Fin cuerpo
@@ -201,7 +154,7 @@ function printBody(){
   this.ctx.closePath();
 }
 
-function printLeftLeg(){
+function printLeftLeg() {
   this.ctx.beginPath();
   this.ctx.moveTo(400, 540);
   this.ctx.lineTo(320, 640); //Pierna Izquierda
@@ -209,7 +162,7 @@ function printLeftLeg(){
   this.ctx.closePath();
 }
 
-function printRightLeg(){
+function printRightLeg() {
   this.ctx.beginPath();
   this.ctx.moveTo(400, 540);
   this.ctx.lineTo(480, 640); // Pierna Derecha
@@ -217,7 +170,7 @@ function printRightLeg(){
   this.ctx.closePath();
 }
 
-function printLeftArm(){
+function printLeftArm() {
   this.ctx.beginPath();
   this.ctx.moveTo(400, 350);
   this.ctx.lineTo(320, 420); // Brazo izquierdo
@@ -225,7 +178,7 @@ function printLeftArm(){
   this.ctx.closePath();
 }
 
-function printRightArm(){
+function printRightArm() {
   this.ctx.beginPath();
   this.ctx.moveTo(400, 350);
   this.ctx.lineTo(480, 420); // Brazo derecho
@@ -233,46 +186,47 @@ function printRightArm(){
   this.ctx.closePath();
 }
 
-function addLetter(letter){
-  var isCorrect =  hangman.checkIfCorrect(letter);
-  
-  if(isCorrect){
+function addLetter(letter) {
+  var isCorrect = hangman.checkIfCorrect(letter);
+
+  if (isCorrect) {
     var win = hangman.addCorrectLetter(letter);
     printLetter(letter);
   } else {
     var lose = hangman.addWrongLetter(letter);
     var errorsLeft = hangman.errorsLeft;
-    game.drawHangman(10-errorsLeft);
+    game.drawHangman(10 - errorsLeft);
     game.writeWrongLetter(letter, errorsLeft);
   }
   checkStatus(win, lose);
 }
 
-function printLetter(letter){
-  for(var i = 0; i < secretWord.length; i++){
-    if(secretWord[i] == letter){
+function printLetter(letter) {
+  for (var i = 0; i < secretWord.length; i++) {
+    if (secretWord[i] == letter) {
       game.writeCorrectLetter(i);
     }
   }
 }
 
-function checkStatus(win, lose){
-  if(win){
+function checkStatus(win, lose) {
+  if (win) {
     game.winner();
-  } else if(lose){
+  } else if (lose) {
     game.gameOver();
   }
 }
 
 var game = new HangmanCanvas();
-startBtn.addEventListener('click', event => {
+
+startBtn.addEventListener("click", event => {
   game.createBoard();
 
-  window.addEventListener('keydown', event => {
+  window.addEventListener("keydown", event => {
     var letter = event.key;
     var isLetter = hangman.checkIfLetter(event.keyCode);
     var avalibleLetter = hangman.checkClickedLetters(letter);
-    if(isLetter && avalibleLetter){
+    if (isLetter && avalibleLetter) {
       addLetter(letter.toUpperCase());
     }
   });
