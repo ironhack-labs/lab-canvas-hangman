002d5e2dba244be492956b080a8b715dd3828f0c
Clement Sirvente <clement.sirvente@gmail.com>
2018-06-21 19:36:31 +0200

    iteration1

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,43 @@
-var hangman;
+var hangman; 
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["vodka","rum","gin","tequila"];
+  this.secretWord = "gin";
+  this.letters="";
+  this.guessedLetter=[];
+  this.errorsLeft=0;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+ return this.words[Math.floor(Math.random()*this.words.length)];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  return 65 < keyCode && 90 > keyCode;
+/*   return keyCode.length === 1 && keyCode.match(/[a-z]/i); */
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  return this.letters.indexOf(key) === -1; 
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    this.checkWinner();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft --;
+  this.checkGameOver();
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+  return this.errorsLeft === 0;
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  return this.guessedLetter.length === this.secretWord; //ansi number
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
