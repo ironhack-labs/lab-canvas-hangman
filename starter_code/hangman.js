ddea88083fc2f5edf720a7fbc301771afc74c7da
Paul de Barbeyrac <pauldebarbeyrac@gmail.com>
2018-06-21 19:02:49 +0200

    js part

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,42 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+	this.words = ["france", "perou", "coupe", "monde"];
+	this.secretWord = "coupe";
+	this.letters = [];
+	this.guessedLetter = "";
+	this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+	return this.words[Math.floor(Math.random()* this.words.length)]
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+	return 65 < keyCode && 90 > keyCode;
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+	return this.letters.indexOf(key) === -1;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+	this.guessedLetter += this.secretWord[i].toUpperCase();
+	this.checkWinner();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+	this.errorsLeft--;
+	this.checkGameOver();
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+	return this.errorsLeft === 0;
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+	return this.guessedLetter.length === this.secretWord.length;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
