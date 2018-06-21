751b8939a811777fd5ef4c9918410eb837372602
Hanane DUPOUY <hndup0908@gmail.com>
2018-06-21 19:46:03 +0200

    in progress

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -32,8 +32,65 @@ var hangman;
 
 // };
 
+// document.getElementById('start-game-button').onclick = function () {
+//   hangman = new Hangman();
+// };
+
+
+// document.onkeydown = function (e) {
+
+// };
+
+
+
+var hangman;
+
+function Hangman() {
+	this.words = ["france", "perou", "coupe", "monde"];
+	this.secretWord = "coupe";
+	this.letters = [];
+	this.guessedLetter = "";
+	this.errorsLeft = 10;
+}
+
+Hangman.prototype.getWord = function () {
+	return this.words[Math.floor(Math.random()* this.words.length)]
+};
+
+Hangman.prototype.checkIfLetter = function (keyCode) {
+	return 65 < keyCode && 90 > keyCode;
+};
+
+Hangman.prototype.checkClickedLetters = function (key) {
+	return this.letters.indexOf(key) === -1;
+};
+
+Hangman.prototype.addCorrectLetter = function (i) {
+	this.guessedLetter += this.secretWord[i].toUpperCase();
+	this.checkWinner();
+};
+
+Hangman.prototype.addWrongLetter = function (letter) {
+	this.errorsLeft--;
+	this.checkGameOver();
+};
+
+Hangman.prototype.checkGameOver = function () {
+	return this.errorsLeft === 0;
+};
+
+Hangman.prototype.checkWinner = function () {
+	return this.guessedLetter.length === this.secretWord.length;
+};
+
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  hangmanCtx = new HangmanCanvas(hangman.secretWord);
+  hangmanCtx.createBoard();
+  hangmanCtx.drawLines();
+  hangmanCtx.writeCorrectLetter(4);
+  hangmanCtx.writeWrongLetter("A", 9);
+  hangmanCtx.drawHangman(10);
 };
 
 
