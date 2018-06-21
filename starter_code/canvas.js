87d1b3f69c94ae6905a8628960736f1f61605d72
Niko <nkouuu@hotmail.com>
2018-06-21 19:54:44 +0200

    primer commit

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,50 @@
-
 function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+  this.ctx = document.getElementById("hangman").getContext("2d");
 }
 
-HangmanCanvas.prototype.createBoard = function () {
+HangmanCanvas.prototype.createBoard = function(width) {
+  ctx.clear();
+  ctx.lineWidth = width;
+};
+
+HangmanCanvas.prototype.drawLines = function() {
+  ctx.beginPath();
+  ctx.moveTo(200, 400);
+  var x = 200;
+  for (var i = 0; i < hangman.secretWord.length; i++) {
+    x += 25;
+    ctx.lineTo(x, 400);
+    ctx.stroke();
+    x += 5;
+    ctx.moveTo(x, 400);
+  }
+};
+
+HangmanCanvas.prototype.writeCorrectLetter = function(index) {
 
 };
 
-HangmanCanvas.prototype.drawLines = function () {
+HangmanCanvas.prototype.writeWrongLetter = function(letter, errorsLeft) {};
 
+HangmanCanvas.prototype.drawHangman = function(shape) {
+ctx.beginPath();
+ ctx.moveTo(175,400);
+var x=175;
+  
+ctx.lineTo(105,400);
+
+ctx.lineTo(140,370);
+ctx.closePath()
+
+ctx.stroke()
+ctx.beginPath()
+ctx.moveTo(140,370)
+ctx.lineTo(140,100)
+ctx.lineTo(300,100)
+ctx.lineTo(300,140)
+ctx.stroke()
 };
 
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+HangmanCanvas.prototype.gameOver = function() {};
 
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
+HangmanCanvas.prototype.winner = function() {};
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,56 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words=["ironhack","hangman","game"]
+  this.secretWord="";
+  this.letters=[]
+  this.guessedLetter=""
+  this.errorsLeft=10
+}
 
-// }
+Hangman.prototype.getWord = function () {
 
-// Hangman.prototype.getWord = function () {
+  var index=Math.floor(Math.random()*this.words.length)
+  this.secretWord=this.words[index]
+  return this.secretWord
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  return ( (keyCode>=65&&keyCode<=90)||(keyCode>=97&&keyCode<=122))
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
+  return !this.letters.includes(key)
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter=this.guessedLetter+this.secretWord[i].toUpperCase();
+  return this.checkWinner();
+  
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--;
+  return this.checkGameOver();
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+ Hangman.prototype.checkGameOver = function () {
+return this.errorsLeft==0;
+ };
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+ Hangman.prototype.checkWinner = function () {
+  for(var i=0;i<this.secretWord.length;i++){
+    if(!this.guessedLetter.includes(this.secretWord[i])){
+      return false;
+    }
+  }
+  return true;
+ };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  
 };
 
 
