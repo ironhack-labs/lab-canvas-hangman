831dac99fc98856058e8dfc965a5135a44d52637
Nicole Jelonek <nicolejelonek@gmail.com>
2018-08-09 15:34:32 +0200

    BER Dalina & Nicole

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,56 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words=["wework", "nuts", "computer","chartreuse",""];
+  this.secretWord = this.getWord();
+  this.letters = [];
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () { 
+  return this.words[Math.floor(Math.random()* this.words.length )];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90 || keyCode >= 97 && keyCode <= 122) {
+    return true
+  }else {
+    return false
+  }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  if ((this.letters).includes(key)) {
+    return false
+  }
+  return true
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter+=this.secretWord[i].toUpperCase();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.letters.push(letter);
+  this.errorsLeft--;
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft === 0) {
+    return true
+  } return false
+ };
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  console.log(this.guessedLetter);
+  console.log(this.secretWord);
+  var letterGuess = this.guessedLetter.split("").sort().join();
+  var bigSecret = this.secretWord.split("").sort().join();
+  if (letterGuess === bigSecret) {
+    return true
+    } return false
+  }
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
