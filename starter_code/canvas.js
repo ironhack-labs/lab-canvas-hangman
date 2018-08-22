e4375b5f2b06fc9f78cb3080b870d1c484b7231b
Adolfo Vela <vela@tekplanner.com>
2018-08-22 17:31:09 -0500

    Funciones para dibujar el mono

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,16 +1,22 @@
-
 function HangmanCanvas(secretWord) {
   this.canvas = document.getElementById('hangman');
   this.ctx = this.canvas.getContext('2d');
+
+  // Color para trabajar el canvas
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+
 HangmanCanvas.prototype.createBoard = function () {
-  ctx.clearRect(0,0,canvas.width, canvas.heigth);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
 HangmanCanvas.prototype.drawLines = function () {
-  
+
 };
+
+
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
 
@@ -31,3 +37,90 @@ HangmanCanvas.prototype.gameOver = function () {
 HangmanCanvas.prototype.winner = function () {
 
 };
+
+
+
+//-------------- Funciones para dibujar cada linea del Ahorcado----------
+HangmanCanvas("Hola") //Eliminar-- Lama al contexto para probar el dibujo
+
+var line01 = function () {
+  ctx.beginPath();
+  ctx.moveTo(100, 700);
+  ctx.lineTo(250, 700);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+var line02 = function () {
+  ctx.beginPath();
+  ctx.moveTo(100, 700);
+  ctx.lineTo(175, 650);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+var line03 = function () {
+  ctx.beginPath();
+  ctx.moveTo(250, 700);
+  ctx.lineTo(175, 650);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+var line04 = function () {
+  ctx.beginPath();
+  ctx.moveTo(175, 650);
+  ctx.lineTo(175, 100);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+var line05 = function () {
+  ctx.beginPath();
+  ctx.moveTo(175, 100);
+  ctx.lineTo(500, 100);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+var line06 = function () {
+  ctx.beginPath();
+  ctx.moveTo(500, 100);
+  ctx.lineTo(500, 150);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+// Cabeza
+var line07 = function () {
+  ctx.beginPath();
+  ctx.arc(500, 200, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+var line08 = function () {
+  ctx.beginPath();
+  ctx.moveTo(500, 250);
+  ctx.lineTo(500, 400);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+var line09 = function () {
+  ctx.beginPath();
+  ctx.moveTo(500, 400);
+  ctx.lineTo(450, 450);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+var line10 = function () {
+  ctx.beginPath();
+  ctx.moveTo(500, 400);
+  ctx.lineTo(550, 450);
+  ctx.lineWidth = 6;
+  ctx.stroke();
+}
+
+line01();
+line02();
+line03();
+line04();
+line05();
+line06();
+line07();
+line08();
+line09();
+line10();
+
