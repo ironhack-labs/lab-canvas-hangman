98967628851811bc15f1bfb1b73d085aba8fcde4
alexrojas <alex.rojas@agintechllc.com>
2018-08-24 10:36:00 -0400

    first commit

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,84 @@
-var hangman;
+// var hangman;
 
-// function Hangman() {
+class Hangman{
+  constructor(words){
+    this.words = words
+    this.secretWord = 'alex'
+    this.wrongletters = []
+    this.guessedLetter = []
+    this.errorsLeft = 10
+    this.newWord = []
+  }
 
-// }
+  getWord(){
+    return this.words[Math.floor(Math.random()*this.words.length)]
+  }
 
-// Hangman.prototype.getWord = function () {
+  checkIfLetter(pressed){
+    if(pressed >= 65 && pressed <= 90){
+    console.log("we are right");
+      return true
+    }else {
+      return false
+    }
+  }
 
-// };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  checkClickedLetters(pressed){
 
-// };
+      if(this.wrongletters.indexOf(pressed) >= 0){
+        console.log('letter is pressed')
+        alert ("You have already chosen this letter");
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+      }else{
 
-// };
+        if(this.secretWord.includes(pressed) === true){
+          this.guessedLetter.push(pressed)
+          this.wrongletters.push(pressed)
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+          console.log(`{this.guessedLetter}`)
+        }else{
+          this.wrongletters.push(pressed)
+          this.errorsLeft -=1
+          hangmanCanv.drawHangMan()
 
-// };
+        }
+    }
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+  }
 
-// };
 
-// Hangman.prototype.checkGameOver = function () {
+  checkGameOver(){
+    if(this.errorsLeft === 0 ){
+      alert("you lose!!!!")
+      return true
+    }
+  }
 
-// };
+  checkWinner(){
 
-// Hangman.prototype.checkWinner = function () {
+    if(this.guessedLetter.length === this.secretWord.length){
 
-// };
+      alert('you win')
+    }else{
+      return('keep playing')
+    }
+  }
+}
+
+
 
 document.getElementById('start-game-button').onclick = function () {
-  hangman = new Hangman();
+  hangman = new Hangman('alex');
+  hangmanCanv = new HangmanCanvas(hangman.secret)
 };
 
 
-document.onkeydown = function (e) {
+document.onkeydown = function(e) {
+  if(hangman.checkIfLetter(e.keyCode)){
+    console.log(e.key)
+    hangman.checkClickedLetters(e.key)
+    hangman.checkWinner()
+  }
 
 };
