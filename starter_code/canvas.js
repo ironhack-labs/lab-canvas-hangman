39ad980877040e034b51440ce21b12ce7f10d4d7
RaulG <guiaraul@gmail.com>
2018-08-30 18:11:35 +0200

    Not Done

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,13 +1,63 @@
+var errorsDraw = [
+  {
+    type: 'triangle', x1: 50,  y1: 500, x2: 125, y2: 475, x3: 200, y3: 500
+  },
+  {
+    type: 'line',     x1: 125, y1: 475, x2: 125, y2: 50
+  },
+  {
+    type: 'line',     x1: 125, y1: 50,  x2: 300, y2: 50
+  },
+  {
+    type: 'line',     x1: 300, y1: 50,  x2: 300, y2: 100
+  },
+  {
+    type: 'circle',   x1: 335, y1: 135, x2: 300, y2: 135, r: 35
+  },
+  {
+    type: 'line',     x1: 300, y1: 170, x2: 300, y2: 320
+  },
+  {
+    type: 'line',     x1: 300, y1: 320, x2: 250, y2: 380
+  },
+  {
+    type: 'line',     x1: 300, y1: 320, x2: 350, y2: 380
+  },
+  {
+    type: 'line',     x1: 300, y1: 200, x2: 265, y2: 250
+  },
+  {
+    type: 'line',     x1: 300, y1: 200, x2: 335, y2: 250
+  }
+];
+
 
 function HangmanCanvas(secretWord) {
   this.ctx = document.getElementById('hangman').getContext('2d');
 }
 
 HangmanCanvas.prototype.createBoard = function () {
+  //this.clearRect
+  this.drawLines();
 
 };
 
-HangmanCanvas.prototype.drawLines = function () {
+HangmanCanvas.prototype.drawLines = function () { console.log('entra1');
+  var j = 0;
+  for ( var i = 0; i < hangman.getWord().length; i++) {
+    console.log('entra');
+    this.ctx.beginPath();
+    this.ctx.moveTo(250+j, 500);
+    this.ctx.lineTo(300+j, 500); 
+    this.ctx.fill(); 
+    this.ctx.closePath();   
+    this.ctx.stroke();
+    j += 60;
+
+  }
+  
+  
+  
 
 };
 
@@ -30,3 +80,6 @@ HangmanCanvas.prototype.gameOver = function () {
 HangmanCanvas.prototype.winner = function () {
 
 };
+
+//var canvas = new HangmanCanvas();
+//canvas.drawLines();
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,90 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ['cadena', 'string', 'array'];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
 
-// }
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  this.secretWord = Math.floor(Math.random()*this.words.length);
+  return this.words[Math.floor(Math.random()*this.words.length)];
+  //this.secretWord = 
 
-// };
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode > 90 || keyCode < 65) {
+    return false;
+  } else return true;
+  /*
+  var re = new RegExp(/[^a-z]/, 'i');
+  var re2 = '/[^a-z]/i'
+  console.log( keyCode);
+  */
 
-// };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  
+  if (this.letters.includes(key)) {
+    return false;
+  } else return true;
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.addCorrectLetter = function (i) {
 
-// };
+  this.guessedLetter += this.secretWord.charAt(i).toUpperCase();
+  this.checkWinner();
+    //user wins
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--;
+  
+    this.checkGameOver();
+  
+};
 
-// Hangman.prototype.checkGameOver = function () {
+Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft == 0){
+    return true;
+  } else {return false;}
+};
 
-// };
+Hangman.prototype.checkWinner = function () {
+  //var cont = 0;
+  /*
+  for (var i = 0; i < this.guessedLetter.length; i++)
+  if (this.secretWord.includes(this.guessedLetter[i])){
+    cont++;
+  }
+  if (cont >= this.secretWord.length) {
+    return true;
+  } else {return false;}
+  */
+  var arraySecret = this.secretWord.split("").sort().join();
+  var arrayGuessed = this.guessedLetter.split("").sort().join();
 
-// Hangman.prototype.checkWinner = function () {
+  if (arraySecret === arrayGuessed){
+    return true;
+  } else {return false;}
 
-// };
+  
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  console.log(hangman.getWord());
+  var canvas = new HangmanCanvas(hangman.getWord());
+  console.log(canvas);
+  
+  canvas.drawLines();
 };
 
 
