63cb935d96db2e5006bc1ec2538b8b6ea77f917d
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-08-30 17:49:44 +0200

    in process

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,61 @@
-var hangman;
+function Hangman() {
+  this.words = ["coche", "manzana", "femur", "tobillo", "pelicula"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// function Hangman() {
+Hangman.prototype.getWord = function() {
+  var i = Math.floor(Math.random() * this.words.length);
+  this.secretWord = this.words[i];
+  return this.secretWord;
+};
 
-// }
+Hangman.prototype.checkIfLetter = function(keyCode) {
+  if (typeof keyCode == "number" && keyCode >= 65 && keyCode <= 90) {
+    return true;
+  } else {
+    var isLetter = typeof keyCode == "string";
+    return isLetter;
+  }
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkClickedLetters = function(key) {
+  if (!this.letters.includes(key)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function(i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addWrongLetter = function(letter) {
+  this.errorsLeft--;
+  this.checkGameOver();
+};
 
-// };
+Hangman.prototype.checkGameOver = function() {
+  if (this.errorsLeft == 0){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkWinner = function () {
+ if(this.guessedLetter.length == this.secretWord.length){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
 };
 
 
-document.onkeydown = function (e) {
-
-};
