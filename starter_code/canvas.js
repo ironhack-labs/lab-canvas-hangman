de7e40f11b5e5582152146d93a739e623056e249
duarte <dumejo92@gmail.com>
2018-08-30 16:57:55 -0500

    avance

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,93 @@
+function letras() {
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
-}
-
-HangmanCanvas.prototype.createBoard = function () {
-
-};
-
-HangmanCanvas.prototype.drawLines = function () {
-
-};
-
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
+  //Letras
+  ctx.beginPath()
+  ctx.moveTo(500,550)
+  ctx.lineTo(580,550)
+  ctx.stroke()
+  
+  ctx.beginPath()
+  ctx.moveTo(600,550)
+  ctx.lineTo(680,550)
+  ctx.stroke()
+  
+  ctx.beginPath()
+  ctx.moveTo(700,550)
+  ctx.lineTo(780,550)
+  ctx.stroke()
+  
+  ctx.beginPath()
+  ctx.moveTo(800,550)
+  ctx.lineTo(880,550)
+  ctx.stroke()
+  
+  ctx.beginPath()
+  ctx.moveTo(900,550)
+  ctx.lineTo(980,550)
+  ctx.stroke()
+  
+  ctx.beginPath()
+  ctx.moveTo(1000,550)
+  ctx.lineTo(1080,550)
+  ctx.stroke()
+  }
+  
+  letras ()
+  
+  //Triangulo
+  ctx.beginPath();
+  ctx.moveTo(375, 550);
+  ctx.lineTo(425, 500);
+  ctx.lineTo(470, 550);
+  ctx.fill();
+  
+  //LInea Arriba
+  ctx.beginPath()
+  ctx.moveTo(425,70)
+  ctx.lineTo(800,70)
+  ctx.stroke()
+  
+  // Linea Vertical
+  ctx.beginPath()
+  ctx.moveTo(425,70)
+  ctx.lineTo(425,550)
+  ctx.stroke()
+  
+  //LInea Vertical pequena
+  ctx.beginPath()
+  ctx.moveTo(800,70)
+  ctx.lineTo(800,130)
+  ctx.stroke()
+  
+  // Cabeza
+  ctx.beginPath()
+  ctx.arc(800,165,35,0,10,false)
+  ctx.stroke()
+  
+  //Cuerpo
+  ctx.beginPath()
+  ctx.moveTo(800,350)
+  ctx.lineTo(800,200)
+  ctx.stroke()
+  
+  //Brazos
+  ctx.beginPath()
+  ctx.moveTo(730,270)
+  ctx.lineTo(800,220)
+  ctx.stroke()
+  
+  ctx.beginPath()
+  ctx.moveTo(870,270)
+  ctx.lineTo(800,220)
+  ctx.stroke()
+  
+  //Piernas
+  ctx.beginPath()
+  ctx.moveTo(730,400)
+  ctx.lineTo(800,350)
+  ctx.stroke()
+  
+  ctx.beginPath()
+  ctx.moveTo(870,400)
+  ctx.lineTo(800,350)
+  ctx.stroke()
