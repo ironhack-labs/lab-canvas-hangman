582a4616ebac58d70ab0b60803bd977f029b5312
AdrianMelGon <adrianmelgon@gmail.com>
2018-08-31 08:34:45 +0200

    done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,78 @@
-var hangman;
+function Hangman() {
+  this.words = ["manzana", "perro", "gato"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 0;
+}
 
-// function Hangman() {
+// var hangman = new Hangman();
 
-// }
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  var randomIndex = Math.floor(Math.random() * (this.words.length - 1));
+  return this.words[randomIndex];
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
+  for (var i = 0; i < this.letters.length - 1; i++) {
+    if (this.letters.includes(key)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
 
-// };
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
 
-// };
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+ Hangman.prototype.addWrongLetter = function (letter) {
+   this.errorsLeft--;
+ };
 
-// };
+ Hangman.prototype.checkGameOver = function () {
+   if (this.errorsLeft == 0) {
+     return true;
+   } else {
+     return false;
+   }
+ };
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+ Hangman.prototype.checkWinner = function () {
+   var sWord = function() {
+  
+    var arr = this.secretWord.split("");
+    var arr1 = arr.sort();
+    var arr2 = arr1.join("");
+    return arr2.toUpperCase();
+ }.bind(this);
+ var gLetter = function() {
+   console.log(this.guessedLetter)
+  var arr3 = this.guessedLetter.split("");
+  var arr4 = arr3.sort();
+  var arr5 = arr4.join("");
+  return arr5
+}.bind(this);
 
-// };
+ if (sWord() == gLetter()) {
+   return true;
+ } else {
+   return false;
+ }
+ }
 
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
