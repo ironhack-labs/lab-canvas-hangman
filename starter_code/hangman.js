cfd2fbabfbbf0c9817b655c73771af0ac3e41ab7
Esperanza Amaya <eamayag@gmail.com>
2018-08-31 09:19:48 +0200

    JS and firts draw

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,59 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+this.words = ["HANG","MAN","IRONHACK"],
+this.secretWord = " ",
+this.letters = [],
+this.guessedLetter = "",
+this.errorsLeft = 10
+ }
 
-// }
+ Hangman.prototype.getWord = function () {
+  var indexWord = Math.floor(Math.random() * this.words.length);
+  return this.words[indexWord]
+ };
 
-// Hangman.prototype.getWord = function () {
+ Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90){
+    return true;
+  } else {
+    return false;
+  }
+ };
 
-// };
+ Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.includes(key)) {
+    return false;
+  } else {
+    return true;
+  }
+ };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+ Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  this.checkWinner();
+};
 
-// };
+ Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft -= 1;
+  this.checkGameOver();
+ };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+ Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft == 0) {
+    return true;
+  } else {
+    return false;
+  }
 
-// };
+ };
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+ Hangman.prototype.checkWinner = function () {
+  if (this.secretWord.length == this.guessedLetter.length) {
+       return true;
+     } else {
+   } return false;
+  };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
