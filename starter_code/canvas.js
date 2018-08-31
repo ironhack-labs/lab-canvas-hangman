2cc256eba870e5cc9f7ffc0f0fb78167bc5dfec4
Jhon Scarzo <jhon753951@gmail.com>
2018-08-31 07:13:03 +0200

    V.1

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,14 +1,98 @@
+var errorsDraw = [{
+    type: 'triangle',
+    x1: 50,
+    y1: 500,
+    x2: 125,
+    y2: 475,
+    x3: 200,
+    y3: 500
+  },
+  {
+    type: 'line',
+    x1: 125,
+    y1: 475,
+    x2: 125,
+    y2: 50
+  },
+  {
+    type: 'line',
+    x1: 125,
+    y1: 50,
+    x2: 300,
+    y2: 50
+  },
+  {
+    type: 'line',
+    x1: 300,
+    y1: 50,
+    x2: 300,
+    y2: 100
+  },
+  {
+    type: 'circle',
+    x1: 335,
+    y1: 135,
+    x2: 300,
+    y2: 135,
+    r: 35
+  },
+  {
+    type: 'line',
+    x1: 300,
+    y1: 170,
+    x2: 300,
+    y2: 320
+  },
+  {
+    type: 'line',
+    x1: 300,
+    y1: 320,
+    x2: 250,
+    y2: 380
+  },
+  {
+    type: 'line',
+    x1: 300,
+    y1: 320,
+    x2: 350,
+    y2: 380
+  },
+  {
+    type: 'line',
+    x1: 300,
+    y1: 200,
+    x2: 265,
+    y2: 250
+  },
+  {
+    type: 'line',
+    x1: 300,
+    y1: 200,
+    x2: 335,
+    y2: 250
+  }
+];
 
 function HangmanCanvas(secretWord) {
   this.ctx = document.getElementById('hangman').getContext('2d');
+  this.secretWord = secretWord;
+  this._createBoard(),
+    this._drawLines()
 }
 
 HangmanCanvas.prototype.createBoard = function () {
-
+  this.ctx.clearRect(0, 0, 1200, 800)
+  this.ctx.beginPath
+  this.ctx.linewidth
 };
 
 HangmanCanvas.prototype.drawLines = function () {
-
+  for (var i; i < this.secretWord.length; i++) {
+    this.ctx.linewidth = 5
+    this.ctx.moveTo(250 + (i *50), 500);
+    this.ctx.lineTo(300 + (i *50), 500);
+    this.ctx.stroke();
+  }
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
@@ -20,13 +104,5 @@ HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
 };
 
 HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
 
 };
