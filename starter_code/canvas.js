9d61b0ed2e9a0dc6065f87cfd24cb27a540e212c
Leticia Olábarri <leticiaolabarri@yahoo.es>
2018-10-04 18:22:13 +0200

    done

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -4,11 +4,25 @@ function HangmanCanvas(secretWord) {
 }
 
 HangmanCanvas.prototype.createBoard = function () {
-
+  this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  // Dibujar horca
+  ctx.moveTo(50,300);
+  ctx.beginPath();
+  ctx.lineTo(30,400);
+  ctx.lineTo(70,400);
+  ctx.lineTo(50,300);
+  ctx.lineTo(50,30);
+  ctx.lineTo(120,30);
+  ctx.lineTo(120,40);
+  ctx.closePath();
 };
 
-HangmanCanvas.prototype.drawLines = function () {
-
+HangmanCanvas.prototype.drawLines = function (xpos) {
+  ctx.moveTo(80,250);
+  this.ctx.beginPath();
+  this.ctx.moveTo(xpos, 250);
+  this.ctx.lineTo(xpos + 40, 250);
+  this.ctx.stroke();
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
@@ -20,7 +34,49 @@ HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
 };
 
 HangmanCanvas.prototype.drawHangman = function (shape) {
-
+  switch(shape) {
+  // Dibujar cabeza
+  ctx.moveTo(120,40);
+  ctx.beginPath();
+  var x = 120;
+  var y = 75;
+  var radius = 35;
+  var startAngle = 0;
+  var endAngle = Math.PI*2;
+  ctx.arc(x, y, radius, startAngle, endAngle, true);
+  closePath();
+  break;
+  // Dibujar cuerpo
+  ctx.moveTo(120,110);
+  ctx.beginPath();
+  ctx.lineTo(120,200);
+  ctxclosePath();
+  break;
+  // Dibujar brazo derecho
+  ctx.moveTo(120,130);
+  ctx.beginPath();
+  ctx.lineTo(140,160);
+  ctx.closePath;
+  break;
+  // Dibujar brazo izquierdo
+  ctx.moveTo(120,130);
+  beginPath();
+  lineTo(100,160);
+  ctx.closePath();
+  break;
+  // Dibujar pierna derecha
+  ctx.moveTo(120,200);
+  ctx.beginPath();
+  ctx.lineTo(140,250);
+  closePath();
+  break;
+  // Dibujar pierna izquierda
+  ctx.moveTo(120,200);
+  ctx.beginPath();
+  ctx.lineTo(100,250);
+  closePath();
+  break;
+  }
 };
 
 HangmanCanvas.prototype.gameOver = function () {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,96 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["JASMINE", "CANVAS", "JQUERY", "JAVASCRIPT", "UNDEFINED"],
+  this.secretWord = "",
+  this.letters = [],
+  this.guessedLetter = "",
+  this.errorsLeft = 10
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  var selectedWord = [Math.floor(Math.random()*this.words.length)];
+  return ("" + selectedWord + "");
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode > 64 && keyCode < 91) {
+    var clickedLetter = String.fromCharCode(keyCode);
+    this.letters.push(clickedLetter);
+    console.log(this.letters);
+    return true;
+  }
+  else {
+    return false;
+  };
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.includes(key)) {
+    return false;
+  }
+  else {
+    return true;
+  }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  var splittedSecretWord = this.secretWord.split("");
+  if (splittedSecretWord.includes(i)) {
+    var x = String.fromCharCode(i)
+    console.log(x)
+    this.guessedLetter + x;
+    return true;
+  }
+  else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  var splittedSecretWord = this.secretWord.split("");
+  if (splittedSecretWord.includes(letter)) {
+    return false;
+  }
+  else {
+    return this.errorsLeft --;
+  }
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft === 0) {
+    return true;
+  }
+  else {
+    return false;
+  };
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  // Hacer de secretWord un array en el que cada
+  // una de las letras sea un string
+  var splittedWord = this.secretWord.split("");
+  // Ordenar alfabéticamente las letras y quitar las que
+  // se repitan
+  var sortedWord = splittedWord.sort();
+  var removedDuplicates = sortedWord.filter(function(elem, index, self) {
+    return index == self.indexOf(elem);
+});
+  console.log(removedDuplicates);
+  // Hacer un array con las letras ya adivinadas en el
+  // que cada una de ellas sea un string
+  var splittedLetters = this.guessedLetter.split("");
+  // Ponerlas en orden alfabético
+  var sortedLetters = splittedLetters.sort();
+  console.log(sortedLetters)
+  if (sortedLetters === removedDuplicates) {
+    alert("YOU WIN!!!!!");
+    return true;
+  }
+  else {
+    return false;
+  }
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
