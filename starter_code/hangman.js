14701326a888784eb52b5b3f749a14db225a0f1e
Antoine Ironhack <antoineironhack@MacBook-Pro-de-Antoine.local>
2018-10-18 22:03:09 +0200

    canvas-push

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,56 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ['jb', 'frustration', 'lourd', 'ouais'];
+  this.secretWord = '';
+  this.letters = [];
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
 
-// }
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  return this.words[Math.floor(Math.random() * this.words.length)];
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90 || keyCode >= 97 && keyCode <= 122) {
+    return true ;
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+ Hangman.prototype.checkClickedLetters = function (key) {
+  return !this.letters.includes(key);
+ };
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  return this.guessedLetter += this.secretWord[i].toUpperCase();
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.letters.push(letter);
+  return this.errorsLeft -= 1;
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkGameOver = function () {
+ if (this.errorsLeft === 0) {
+   return true;
+ } else {
+   return false
+ }
+};
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+ for (i = 0; i < this.secretWord.length ; i++) {
+   if (this.guessedLetter.indexOf(this.secretWord[i]) === -1) {
+     return false;
+   }
+ }
+ return true;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
