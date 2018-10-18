064f8c265fed65d346457816062597557e1eaad8
justinezahiri <justinezahiri@gmail.com>
2018-10-18 21:32:53 +0200

    completed until checkClickedLetters

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,52 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["yoda", "rey", "luke", "leia"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10; //should decrease every time a user clicks on a letter that is not in the word.
+  //this.hangman.canvas = new HangmanCanvas(this.secretWord);
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  var y = Math.floor(Math.random() * this.words.length);
+  return this.words[y];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+ if (keyCode > 64 && keyCode < 91) {
+   return true;
+ }
+ else {
+   return false;
+ }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.indexOf(key) === -1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+//should add the guessed letters to this.secretWord
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+};
 
-// };
+Hangman.prototype.checkWinner = function () {
 
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
