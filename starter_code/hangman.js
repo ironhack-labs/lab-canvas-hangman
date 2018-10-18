4fa51c71ae95868e018866ab792edb285ccb46b3
Johanna <johanna.skuhra@gmail.com>
2018-10-18 22:05:30 +0200

    Done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,69 +1,118 @@
-class Hangman {
-  constructor() {
-    this.words = ['HELLO', 'AWESOME', 'WHATEVER', 'CHARTREUSE'];
-    this.secretWord = this.getWord();
+var hangman;
+
+class Hangman{
+  constructor(words, letter){
+    this.words = ['sdafc', 'adfqwerf', 'aefaw'];
+    this.secretWord = ('');
     this.letters = [];
-    this.guessedLetter = '';
+    this.guessedLetter = ('');
     this.errorsLeft = 10;
   }
   getWord() {
     return this.words[Math.floor(Math.random()*this.words.length)]
   }
-  checkIfLetter(letter) {
-    letter = letter.toUpperCase()
-    if(letter.length===1 && (letter>='A' && letter<='Z')) {
-      return true
-    } else {
-      // console.log('It\'s not a letter')
-      return false
-    } 
-  }
-  checkClickedLetters(letter) {
-    if(this.checkIfLetter(letter)==true) {
-      if(this.letters.includes(letter)) {
-        // this.guessedLetter += letter
-        return false
-      } else {
-        // this.errorsLeft--
+
+  checkIfLetter (str){
+    str = str.toUpperCase()
+      if (str.length===1 && str.match(/[A-Z]/i)){
+        //this.letters.push(str) wrong just keeping it to show the push function
+        
         return true
       }
-    }
-  }
-  checkGameOver(){
-    if(this.errorsLeft === 0) {
-      return true
-    } else {
-      return false
-    }
-  }
-  checkWinner(){
-    let secretWordArr = this.secretWord.split('')
-    let counter = 0
-    let test = this.guessedLetter
-    secretWordArr.forEach(function(el) {
-      if(test.includes(el)) {
-        counter++
+      else {
+        return false
       }
-    })
-    if(this.secretWord.length==counter){
-      return true
-    } else {
+  }
+  checkClickedLetters(letter){
+    if (this.letters.includes(letter)){
+      this.guessedLetter += letter
       return false
-    }  
+    }
+    else {
+      return true
+    }
   }
-  addCorrectLetter(letter) {
-    this.guessedLetter += letter
-    this.checkWinner()
+    
+  checkGameOver(){
+    if (this.errorsLeft === 0){
+      return true
+    }
+    else {
+      return false
+    }
+
   }
-  addWrongLetter() {
-    this.errorsLeft--
-    this.checkGameOver()
+  
+  checkWinner(){
+    //if (this.checkGameOver() > 0 && (this.secretWord === this.letter[i])
+    var secretArray = this.secretWord.split('');
+    for (var i = 0; i < secretArray.length; i++){
+      if (!this.guessedLetter.includes(secretArray[i])){
+        return false
+      }
+    }
+    return true
   }
 
+  //Adds to the guessedLetter variable the letter that was pressed. Also, it should check if the user wins
+  addCorrectLetter(secretWordPositon) {
+    this.guessedLetter = this.secretWord[secretWordPosition].toUpperCase();
+    this.checkWinner();
+  }
+  
+  //Should subtract one from the variable errorsLeft and check if the game is over.
+  addWrongLetter(letter) {
+    if (!this.secretWord.includes(letter)){
+      this.errorsLeft --;
+      this.checkGameOver();
+    }
+  }
+
+
+      
 }
 
+//var hangman = new Hangman ()
 
-var hangman = new Hangman();
+
+
+// Hangman.prototype.getWord = function () {
+
+// };
+
+// Hangman.prototype.checkIfLetter = function (keyCode) {
+
+// };
+
+// Hangman.prototype.checkClickedLetters = function (key) {
+
+// };
+
+// Hangman.prototype.addCorrectLetter = function (i) {
+
+// };
+
+// Hangman.prototype.addWrongLetter = function (letter) {
+
+// };
+
+// Hangman.prototype.checkGameOver = function () {
+
+// };
+
+// Hangman.prototype.checkWinner = function () {
+
+// };
+
+document.getElementById('start-game-button').onclick = function () {
+  hangman = new Hangman();
+};
+
+
+document.onkeydown = function (e) {
+
+};
+
 
 
 // Hangman.prototype.getWord = function () {
