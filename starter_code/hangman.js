31415136d3c684b04fafcebbf9be767af3158fc5
Arthur Houdebine <arthur.houdebine@gmail.com>
2018-10-18 22:01:17 +0200

    Passes all the tests

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,59 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ['anelka','lizarazu','barthez'];
+  this.secretWord = '';
+  this.letters = [];
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+    return this.words[Math.floor((Math.random()*this.words.length))];
+};
 
-// Hangman.prototype.getWord = function () {
 
-// };
+//chiffres de 65 et 122;
+Hangman.prototype.checkIfLetter = function (keyCode) {
+if (keyCode > 64 && keyCode < 92){
+  return true;
+}
+else return false;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
+//Si key est déjà présent dans l'array, return false;
+//Si key n'est pas présent, return true;
+for(var i=0;i<this.letters.length;i++)
+{
+    if(this.letters[i]===key){return false;}
+}
+return true;
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+this.guessedLetter += this.secretWord.charAt(i).toUpperCase();
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addWrongLetter = function (letter) {
+this.errorsLeft -= 1;
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+if (this.errorsLeft==0){
+  return true;
+}
+else if (this.errorsLeft>0){return false};
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkWinner = function () {
+  for(var i=0;i<this.guessedLetter.length;i++)
+  {
+    if(this.guessedLetter.indexOf(this.secretWord[i])==-1) {return false;}
+ ;}
+ return true;
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
