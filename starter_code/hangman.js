1877fc044860d5ae2f986956030bfb1ded095fc5
Sandra Dupont <ssduptcom@gmail.com>
2018-10-18 21:31:52 +0200

    stopped at AddcorrectLetter

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,60 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["chien", "biche", "chat"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  var x = Math.floor(Math.random()*this.words.length);
+  return this.secretWord = this.words[x];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 122 ) {return true;}
+  else {return false;}
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+   if (this.letters.indexOf(key.toString()) === -1) {return true;}
+   else {return false;}
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  var correctLetter = this.secretWord[i];
+  this.guessedLetter[i] = correctLetter;
+    
+  
+  return ""+this.guessedLetter;
 
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
+};
 
 // Hangman.prototype.addWrongLetter = function (letter) {
 
 // };
 
-// Hangman.prototype.checkGameOver = function () {
+Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft == 0) { return true;}
+  else {return false;}
+};
 
-// };
 
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+    if (this.secretWord.length == this.guessedLetter.length) {
+   
+      for (var i=0; i <= this.secretWord.length; i++) {
+   
+        if (this.secretWord.indexOf(this.guessedLetter[i]) == -1) { return false; }
+        else {return true;}
+      }
+   
+    }
+   
+    else {  return false; }
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
