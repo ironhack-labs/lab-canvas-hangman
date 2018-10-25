58db4746dae0c5870fe72ed4c0f47bdb7e99e3e3
Jeff Smith <jmstranslate@gmail.com>
2018-10-25 15:29:02 -0300

    not mine

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,122 @@
-
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+class HangmanCanvas{
+  constructor(secret) {
+  this.secret = secret;
+  this.ctx = document.getElementById('hangman').getContext('2d')
+  this.ctx.clearRect(0,0,1080,1080);
+  this.drawLines(secret);
+  }
+drawTriangle(){
+  // console.log(this)
+  this.ctx.beginPath();
+  this.ctx.moveTo(150, 550); //top
+  this.ctx.lineTo(200, 600); //right bottom
+  this.ctx.lineTo(100, 600); //left bottom
+  this.ctx.lineTo(150, 550); //top
+  this.ctx.stroke();
 }
 
-HangmanCanvas.prototype.createBoard = function () {
+drawPole(){
+  // console.log('pole')
+  this.ctx.beginPath();
+  this.ctx.moveTo(150, 550);
+  this.ctx.lineTo(150, 50);
+  this.ctx.lineTo(350, 50);
+  this.ctx.lineTo(350, 100);
+  this.ctx.stroke(); 
+}
 
-};
+drawHead() {
+  this.ctx.beginPath();
+  var x = 350; // x coordinate
+  var y = 150; // y coordinate
+  var radius = 50; // Arc radius
+  var startAngle = 0; // Starting point on circle
+  var endAngle = Math.PI*2; // End point on circle
+  this.ctx.arc(x, y, radius, startAngle, endAngle, true);
+  this.ctx.stroke();
+}
 
-HangmanCanvas.prototype.drawLines = function () {
+drawBody() {
+  this.ctx.beginPath();
+  this.ctx.moveTo(350, 200);
+  this.ctx.lineTo(350, 400);
+  this.ctx.stroke(); 
+}
 
-};
+drawArms(){
+  this.ctx.beginPath();
+  this.ctx.moveTo(300, 300);
+  this.ctx.lineTo(400, 300);
+  this.ctx.stroke(); 
+}
+drawLegs() {
+  this.ctx.beginPath();
+  this.ctx.moveTo(300, 500);
+  this.ctx.lineTo(350, 400);
+  this.ctx.lineTo(400, 500);
+  this.ctx.stroke(); 
+}
+drawIt(index){
+  let that=this;
+  let drawing = [this.drawTriangle.bind(that), this.drawPole.bind(that) ,this.drawHead.bind(that), this.drawBody.bind(that), this.drawArms.bind(that),this.drawLegs.bind(that)];
+  drawing[index]();
+}
+drawLines(word){
+  let numLines = word.length;
+  let x=410;
+  let y=600;
+  let lineLength=50;
+  let lineSeparation=70;
+    for (let i=0; i<word.length; i++){
+      this.ctx.beginPath();
+      this.ctx.moveTo(x+(i*lineSeparation), y);
+      this.ctx.lineTo(x+lineLength+(lineSeparation*i), y);
+      this.ctx.lineWidth=4;
+      this.ctx.stroke(); 
+    }
+  }
+  drawText(letter,index){
+    let lineSeparation=70;
+    this.ctx.font = '48px serif';
+    this.ctx.fillStyle = 'blue';
+    this.ctx.fillText(letter, 420+(index*lineSeparation), 580);
+  }
+  drawWrongLetters(letter,index){
+    let lineSeparation=50;
+    this.ctx.font = '48px serif';
+    this.ctx.fillStyle = 'red';
+    this.ctx.fillText(letter, 500+(lineSeparation*index), 380);
+  }
+} // end of HangmanCanvas
 
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+// function HangmanCanvas(secretWord) {
+//   this.ctx = document.getElementById('hangman').getContext('2d');
+// }
 
-};
+// HangmanCanvas.prototype.createBoard = function () {
 
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
+// };
 
-};
+// HangmanCanvas.prototype.drawLines = function () {
 
-HangmanCanvas.prototype.drawHangman = function (shape) {
+// };
 
-};
+// HangmanCanvas.prototype.writeCorrectLetter = function (index) {
 
-HangmanCanvas.prototype.gameOver = function () {
+// };
 
-};
+// HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
 
-HangmanCanvas.prototype.winner = function () {
+// };
 
-};
+// HangmanCanvas.prototype.drawHangman = function (shape) {
+
+// };
+
+// HangmanCanvas.prototype.gameOver = function () {
+
+// };
+
+// HangmanCanvas.prototype.winner = function () {
+
+// };
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,8 +1,151 @@
-var hangman;
+// NOT MINE, BUT REALLY INTERESTING
+var words = ["Italy", "France", "Spain", "Brazil", "Argentina"];
 
-// function Hangman() {
+class HangmanGame{
+  constructor(arrayOfWords){
+    this.secretWord = "";
+    this.clickedLetters = [];
+    this.guessedLetters=[];
+    this.letterArray=[]
+    this.errorCount = 0;
+    this.getWord(arrayOfWords);
+    this.thecanvas = {};
+  }
+  getWord(wordArray){
+    this.secretWord =wordArray[Math.floor(Math.random()*(wordArray.length))];
+    this.secretWord = this.secretWord.toLowerCase();
+    this.letterArray =this.secretWord.split('');
+    console.log(this.secretWord);
+  } // end of constructor
+  
+  checkIfLetter(keyCode){
+    if (keyCode >= 65 && keyCode <= 90) {
+    return true;
+    }
+    //add check if letter function
+    // return true or false
+  }
+  checkClickedLetters(key){
+    if (!this.clickedLetters.includes(key)) {
+      this.clickedLetters.push(key);
+      if (this.secretWord.includes(key)) {
+      this.addCorrectLetter(key)
+      // return true
+      } else {
+        this.addWrongLetter(key)
+        console.log('the letter ' +key + ' is incorrect')
+        // return false
+      }
+    }
+    //check if clicked letter is correct
+  }
+  addCorrectLetter(keyToAdd) {
+    this.letterArray.forEach((thisLetter, index)=>{
+      if (thisLetter==keyToAdd) {
+        this.guessedLetters.push(keyToAdd);
+        this.thecanvas.drawText(thisLetter,index);
+        this.checkWinner()
+      }
+    })
+  }
 
-// }
+  addWrongLetter(theLetter) {
+    this.errorCount++;
+    this.checkGameOver();
+    this.thecanvas.drawIt(this.errorCount-1)
+    this.thecanvas.drawWrongLetters(theLetter,(this.errorCount))
+  }
+
+  checkGameOver() {
+    if (this.errorCount == 6){
+      setTimeout(function(){
+        var myDrawing = $('canvas');
+        myDrawing.hide()
+        var gameOver = $('.game-over');
+        gameOver.show()
+        setTimeout(function(){
+          alert('Game Over')
+          gameOver.hide()
+          var startLogo = $('.start');
+          startLogo.show()
+          }, 2000);
+        }, 2000);
+    }
+  }
+  checkWinner() {
+    if (this.guessedLetters.length == this.letterArray.length){
+      setTimeout(function(){
+        var wonGame = $('.you-won');
+        wonGame.show()
+        var myDrawing = $('canvas');
+        myDrawing.hide()
+        setTimeout(function(){
+          wonGame.hide()
+          var startLogo = $('.start');
+          startLogo.show()
+          alert('Congratulations! YOU WON!!!!!')
+          }, 2000);
+        }, 2000);
+    }
+  }
+  
+} // end of HangmanGame
+
+// var hangman;
+// function Hangman() {}
+// Hangman.prototype.getWord = function () {};
+// Hangman.prototype.checkIfLetter = function (keyCode) {};
+// Hangman.prototype.checkClickedLetters = function (key) {};
+// Hangman.prototype.addCorrectLetter = function (i) {};
+// Hangman.prototype.addWrongLetter = function (letter) {};
+// Hangman.prototype.checkGameOver = function () {};
+// Hangman.prototype.checkWinner = function () {};
+
+document.getElementById('start-game-button').onclick = function () {
+  hangman = new HangmanGame(words);
+  hangCanv = new HangmanCanvas(hangman.secretWord)
+  hangman.thecanvas = hangCanv;
+  var startLogo = $('.start');
+  startLogo.hide()
+  var myDrawing = $('canvas');
+  myDrawing.show()
+};
+
+ 
+document.onkeydown = function (event) {
+  var keyPressed= event.key;
+  var codeOfKey=event.keyCode;
+  if (hangman.checkIfLetter(codeOfKey)) {
+    hangman.checkClickedLetters(keyPressed)
+  }
+  
+};
+
+
+
+/*
+    hangman.prototype.checkIfLetter = function(keyCode) {
+        // 
+    }
+
+    hangman.prototype.checkedClickedLetters = function(key) {
+      if(!this.clickedLetters.includes(key)) {
+        this.clickedLetters.push(key);
+        if(this.secretWord.includes(key)) {
+          this.addCorrectLetter(key);
+          console.log(true);
+        } else {
+          this.addWrongLetter(key)
+          console.log('the letter ' + key + ' is incorrect');
+        }
+      }
+    }
+
+
+
+
+
+
 
 // Hangman.prototype.getWord = function () {
 
@@ -40,3 +183,4 @@ document.getElementById('start-game-button').onclick = function () {
 document.onkeydown = function (e) {
 
 };
+*/
