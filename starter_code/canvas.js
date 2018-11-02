0d05c69afc2385c7919d5e62b70607bc4ff162a3
Yoli Chaviano <yolichvn@gmail.com>
2018-11-02 11:16:35 -0400

    HUGE STRUGGLE!  Missing canvas, when tried to follow Nick's inctructions the game broke.

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,17 @@
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+class GameAnimator{
+  constructor(){
+    this.ctx = document.getElementById('game').getContext('2d');
+  }
+
+  drawLines(){
+    let numberOfLines = this.secretWord.length;
+    this.ctx.lineWidth = 8;
+    this.ctx.beginPath();
+    this.ctx.moveTo(500, 500);
+    this.ctx.lieneTo(550, 500);
+    this.ctx.stroke;
+  }
 }
+// when trying to copy Nick's canva instructions my game broke :(  
 
-HangmanCanvas.prototype.createBoard = function () {
-
-};
-
-HangmanCanvas.prototype.drawLines = function () {
-
-};
-
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,81 @@
-var hangman;
+let game;
 
-// function Hangman() {
+class Hangman {
+  constructor(){
+    this.words = ["jurist", "daredevil", "scrupulous", "erudite", "contrarian", "university"];
+    this.secretWord = '';
+    this.errorsLeft = 10;
+    this.lettersUsed = new Array();
+    this.lettersGuessed = new Array();
+    this.letterHeld = new Array();
+  }
 
-// }
 
-// Hangman.prototype.getWord = function () {
+  // Pick random word from array - store in secretWord variable
+  getWord() {
+  let randomNumber = Math.floor(Math.random()*this.words.length);
+  this.secretWord = this.words[randomNumber];
+}
 
-// };
+// check if pressed key is a letter
+checkClickedLetters(event){
+  let pressedKey = String.fromCharCode(event.which);
+    if (pressedKey.match(/[a-zA-Z\.]/)){
+      let newLetter = pressedKey;
+//check if correct letter, and if letter has been used before
+      if(game.lettersUsed.indexOf(newLetter) !== -1 && game.secretWord.indexOf(newLetter) == -1){
+        alert('wrong, try again');
+        this.errorsLeft += 1;
+      } else if(game.lettersUsed.indexOf(newLetter) !== -1){ 
+          alert('this letter has already been used');
+        } else if(game.secretWord.indexOf(newLetter) !== -1){
+          game.lettersUsed.push(newLetter);
+          game.lettersGuessed.push(newLetter);
+          } else {
+            game.lettersUsed.push(newLetter);
+            };
+    } else {
+        alert('Only Letters Please');
+        this.errorsLeft += 1;
+        return false;
+      };
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+//deduct point from counter if wrong letter and if won/lost
+addWrongLetter(event) {
+  if(this.secretWord.indexOf(event.key) == -1){
+    this.errorsLeft -= 1;
+  };
+  if(this.errorsLeft == 0) {
+    alert('You lost your head, click button to try again');
+    game = new Hangman();
+    game.getWord();
+    console.log(game.secretWord);
+  }
+};
+checkWinner() {
+  for(let i = 0; i < this.secretWord.length; i++){
+    if(this.letterHeld.indexOf(this.secretWord[i]) == -1){
+      this.letterHeld.push(this.secretWord[i]);
+    };
+  };
+  if(game.lettersGuessed.length === this.letterHeld.length){
+    alert('WINNER');
+  };
+};
+};
 
-// };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
+//Start game on click
 document.getElementById('start-game-button').onclick = function () {
-  hangman = new Hangman();
+game = new Hangman();
+game.getWord();
+console.log(game.secretWord);
 };
 
-
-document.onkeydown = function (e) {
-
-};
+$(document).keypress(function(e){
+game.checkClickedLetters(e);
+game.checkWinner();
+game.addWrongLetter(e);
+console.log(game.errorsLeft);
+});
