f9f55899e430a33cb15721cb10228ff024fef2bf
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-08 12:47:02 -0600

    Iteration 1 done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,54 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["JAVASCRIPT","DOM","IRONHACK"]
+  this.secretWord = "IRONHACK"
+  this.letters = []
+  this.guessedLetter = ""
+  this.errorsLeft = 10
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  return "hola"
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if(64 < keyCode && keyCode < 90)
+    return true
+  else 
+    return false
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  var clicked = this.letters.indexOf(key)
+    if(clicked === -1)
+      return true
+    else 
+      return false
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  var letter = this.secretWord.substr(i,1).toUpperCase()
+  this.guessedLetter += letter
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft === 0)
+    return true
+  else 
+    return false
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  if(this.secretWord.length === this.guessedLetter.length)
+    return true
+  else
+    return false
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
