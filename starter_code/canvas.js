24d2ff6167ef998675803d3bd12081b02c1bec0e
Pily01 <40185631+Pily01@users.noreply.github.com>
2018-11-08 23:51:05 -0600

    SANTI Y PILY MEX DAY#8 PAIR PROGRAMMING

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,135 @@
 
+
+var juego = new HangmanCanvas()
+var hangmanOne = new Hangman()
+
+
 function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+  this.canvas = document.getElementById('hangman');
+  this.ctx = hangman.getContext('2d');
+  this.secretWord = secretWord;
+  this.createBoard()
+  this.drawLines()
 }
 
-HangmanCanvas.prototype.createBoard = function () {
 
+var button = document.getElementById("start-game-button")
+
+button.addEventListener("click", function(){
+  juego.createBoard()
+  juego.drawLines()
+})
+
+HangmanCanvas.prototype.createBoard = function () {
+  console.log("hols")
+
+ 
+  this.ctx.clearRect(0,0,800,1200)
+  this.ctx.beginPath()
 };
 
+  var monito = {
+    cuerda: this.ctx.moveTo(100,100)
+    this.ctx.lineTo(100,150)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+  
+    paloHorizontal: 
+    this.ctx.beginPath()
+    this.ctx.moveTo(100,100)
+    this.ctx.lineTo(30,100)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+
+    paloVertical: this.ctx.beginPath()
+    this.ctx.moveTo(30,100)
+    this.ctx.lineTo(30,500)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+
+    base: this.ctx.beginPath()
+    this.ctx.moveTo(70,500)
+    this.ctx.lineTo(10,500)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+
+    cabeza: this.ctx.beginPath()
+    this.ctx.arc(100,175,25,0,Math.PI*2,false)
+    this.ctx.strokeStyle = "black"
+    this.ctx.stroke()
+    this.ctx.closePath(),
+
+    cuerpo: this.ctx.beginPath()
+    this.ctx.moveTo(100,200)
+    this.ctx.lineTo(100,300)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+    
+    piernaIzq : this.ctx.beginPath()
+    this.ctx.moveTo(100,300)
+    this.ctx.lineTo(80,350)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+
+    piernaDer:  this.ctx.beginPath()
+    this.ctx.moveTo(100,300)
+    this.ctx.lineTo(120,350)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+
+    manoDerecha:  this.ctx.beginPath()
+    this.ctx.moveTo(100,250)
+    this.ctx.lineTo(120,200)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+
+    manoIzquierda:   this.ctx.beginPath()
+    this.ctx.moveTo(100,250)
+    this.ctx.lineTo(80,300)
+    this.ctx.stroke()
+    this.ctx.closePath(),
+
+
+  }
+
+  
 HangmanCanvas.prototype.drawLines = function () {
+  console.log("pepe")
+  console.log(this.secretWord)
+  for( i = 0; i < this.secretWord.length; i++){
+
+    this.ctx.moveTo(100 + (i*50),500)
+    this.ctx.lineTo(120 + (i*50),500)
+    this.ctx.stroke()
+  }
 
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+  this.ctx.font(this.secretWord[index], 100 + (i*50),500)
+  this.ctx.fillText()
+
+
 
 };
 
 HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
+  this.ctx.font(letter, 500 + i,500 + i)
+  this.ctx.fillText()
 
 };
 
 HangmanCanvas.prototype.drawHangman = function (shape) {
+  this.ctx.monito.shape
 
 };
 
 HangmanCanvas.prototype.gameOver = function () {
-
+  this.ctx.
 };
 
 HangmanCanvas.prototype.winner = function () {
 
 };
+
+
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,84 @@
+
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["code", "perro", "gato", "laptop", "Ironhack"]
+  this.secretWord = this.getWord()
+  this.letters = []
+  this.guessedLetter = ""
+  this.errorsLeft = 10
 
-// }
+}
 
-// Hangman.prototype.getWord = function () {
 
-// };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+var button = document.getElementById("start-game-button")
 
-// };
+button.onclick = function(){
+  console.log("HELLO")
+  hangman = new Hangman
+  document.getElementById("title").style.display = "none"
+  document.getElementById("game").style.display = "block"
+}
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.getWord = function () {
+  return this.words[Math.floor(Math.random()*MimeTypeArray.length)]
+};
 
-// };
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+    if(keyCode > 64 &&  keyCode < 91){
+      this.letters.push(keyCode)
+      return true
+    }else{
+      return false
+    }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+    var result = this.letters.includes(key)
+    if (result){
+      return false
+    }else{
+      return true
+    }
 
-// Hangman.prototype.checkGameOver = function () {
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
 
-// Hangman.prototype.checkWinner = function () {
+  this.guessedLetter += this.secretWord[i].toUpperCase()
+  if(this.guessedLetter === this.secretWord){
+    return true
+  }else{
+    return false
+  }
 
-// };
+};
+
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft -= 1
+};
+
+Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft === 0){
+    return true
+  }else{
+    return false
+  }
+
+};
+
+Hangman.prototype.checkWinner = function () {
+  if (this.guessedLetter.length === this.secretWord.length){
+    return true
+  }else{
+    return false
+  }
+
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
