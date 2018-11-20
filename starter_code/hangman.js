00ae41f0b211cf1885e9f744be862de0bed8c6e6
JZunigaV <j_zuniga4@hotmail.com>
2018-11-20 15:58:44 -0600

    avance

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,24 +1,42 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
 
-// }
+  this.words = ["Perro", "gato", "Alien"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
 
-// };
+  //Regresa un valor aleatorio del array de palabras
+  var randomWord = this.words[Math.floor(Math.random() * this.words.length)];
+  return randomWord;
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+  if (isNaN(keyCode)) {
+    //Si keycode no es numero
+    return true;
+  } else {
+    //Si keycode es un numero
+    return false;
+  }
 
-// };
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkClickedLetters = function (key) {
 
-// };
+};
+
+Hangman.prototype.addCorrectLetter = function (i) {
+  
+
+};
 
 // Hangman.prototype.addWrongLetter = function (letter) {
 
