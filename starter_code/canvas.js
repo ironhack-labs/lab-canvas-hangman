bb89c9f150865fe44afeaed776a691ed7dfd8970
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2018-11-20 22:03:55 -0600

    Avance parcial

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -4,11 +4,37 @@ function HangmanCanvas(secretWord) {
 }
 
 HangmanCanvas.prototype.createBoard = function () {
-
+  this.ctx.strokeRect(0, 0, 1200, 800);
+  this.drawLines();
 };
 
 HangmanCanvas.prototype.drawLines = function () {
+  //Las de las letras
+  // this.ctx.beginPath();
+  // this.ctx.arc(400, 400, 200, 0, Math.PI*2);
+  // this.ctx.fill();
+  //Las del patíbulo
+  let i = 100;
+  let j = 750;
+  let topPole = 100;
+  let radius = 50;
+  //Gallow
+  this.ctx.beginPath();
+  this.ctx.lineWidth = "3";
+  this.ctx.moveTo(i+75, j-50);
+  this.ctx.lineTo(i, j);
+  this.ctx.lineTo(i+150, j);
+  this.ctx.lineTo(i+75, j-50);
+  this.ctx.lineTo(i+75, topPole);
+  this.ctx.lineTo(i+450, topPole);
+  this.ctx.lineTo(i+450, topPole+radius);
+  this.ctx.stroke();
+  this.ctx.beginPath();
 
+  //Vato
+  this.ctx.arc(i+450, topPole + radius*2, radius, 0, Math.PI*2);
+  this.ctx.stroke();
+  // this.ctx.fill();
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,58 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["Chilaquiles", "Tacos", "Pizza"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  let i = Math.floor(Math.random()*this.words.length);
+  let word = this.words[i];
+  return word;
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if ((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)) {
+    return true; 
+  }
+  else return false;
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.indexOf(key) === -1) {
+    return true;
+  }
+  else return false;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter = this.guessedLetter + this.secretWord[i].toUpperCase();
+  this.checkWinner();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--;
+  this.checkGameOver();
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft <= 0) return true;
+  else return false;
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+//Letras repetidas??????
+Hangman.prototype.checkWinner = function () {
+  if (this.guessedLetter.length === this.secretWord.length) return true;
+  else return false;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  hangmanCanvas = new HangmanCanvas();
+  hangmanCanvas.createBoard();
 };
 
 
