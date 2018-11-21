fe6335fff16b804d7f76e26f616ea4914f69bddf
JZunigaV <j_zuniga4@hotmail.com>
2018-11-21 17:50:17 -0600

    Solo falta pintar

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,6 +1,9 @@
-
 function HangmanCanvas(secretWord) {
+
   this.ctx = document.getElementById('hangman').getContext('2d');
+  this.secretWord = secretWord;
+  this.coords = [];
+
 }
 
 HangmanCanvas.prototype.createBoard = function () {
@@ -8,6 +11,31 @@ HangmanCanvas.prototype.createBoard = function () {
 };
 
 HangmanCanvas.prototype.drawLines = function () {
+
+  //La posición inicial para las lineas donde aparecerán las letras
+  var initialX = 300;
+  var initialY = 700;
+
+  this.ctx.beginPath();
+  this.ctx.moveTo(initialX, initialY);
+  this.ctx.lineTo(initialX + 100, initialY);
+  this.ctx.closePath();
+  this.ctx.stroke();
+
+  for (var i = 0; i < this.secretWord.length - 1; i++) {
+
+    //Aumentamos eje de las x para comenzar a dibujar otra line adelante de la anterior
+    initialX += 150;
+    
+    this.coords.push(initialX);
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(initialX, initialY);
+    this.ctx.lineTo(initialX + 100, initialY);
+    this.ctx.closePath();
+    this.ctx.stroke();
+
+  }
 
 };
 
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,60 +1,112 @@
 var hangman;
+var canvas;
 
 function Hangman() {
 
-  this.words = ["Perro", "gato", "Alien"];
+  this.words = ["hola", "Iphone"];
   this.secretWord = "";
   this.letters = [];
   this.guessedLetter = "";
   this.errorsLeft = 10;
+
 }
 
 Hangman.prototype.getWord = function () {
-
-  //Regresa un valor aleatorio del array de palabras
-  var randomWord = this.words[Math.floor(Math.random() * this.words.length)];
-  return randomWord;
-
+  var randomNumber = Math.floor(Math.random() * this.words.length);
+  this.secretWord = this.words[randomNumber];
+  return this.secretWord;
 };
 
 Hangman.prototype.checkIfLetter = function (keyCode) {
-
-  if (isNaN(keyCode)) {
-    //Si keycode no es numero
+  if (keyCode >= 65 && keyCode <= 90) {
     return true;
   } else {
-    //Si keycode es un numero
     return false;
   }
-
 };
 
 Hangman.prototype.checkClickedLetters = function (key) {
-
+  if (this.letters.includes(key)) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 Hangman.prototype.addCorrectLetter = function (i) {
-  
-
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  this.checkWinner();
+  return this.guessedLetter;
 };
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.letters.push(letter);
+  this.errorsLeft--;
+  this.checkGameOver();
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+  if (this.errorsLeft === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.checkGameOver = function () {
 
-// };
+Hangman.prototype.verify = function (letter) {
+  return this.secretWord.includes(letter);
+};
 
-// Hangman.prototype.checkWinner = function () {
 
-// };
+
+Hangman.prototype.checkWinner = function () {
+  for (var i = 0; i < this.secretWord.length; i++) {
+    if (this.guessedLetter.includes(this.secretWord[i])) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
-};
+  canvas = new HangmanCanvas(hangman.getWord());
+  canvas.drawLines();
 
+};
 
 document.onkeydown = function (e) {
 
+
+
+  //Si la tecla que se presionó no es una letra, da una alerta
+  if (!hangman.checkIfLetter(e.keyCode)) {
+    alert("Escribe una letra");
+    return 0;
+  }
+
+  //Si es una letra verifica que no haya sido presionada con anterioridad
+  if (!hangman.checkClickedLetters(e.keyCode)) {
+    alert("ya presionaste esa tecla!!!");
+  } else {
+    //Ingresamos la letra presionada al array letters
+    hangman.letters.push(e.keyCode);
+
+    //Si la tecla que presionamos corresponde a Secret Word la pintamos en el Lugar que le corresponde
+    if (hangman.verify(e.key)) {
+      for (var i = 0; i < hangman.secretWord.length; i++) {
+        if (hangman.secretWord[i] === e.key) {
+         
+          canvas.writeCorrectLetter(i);
+          hangman.addCorrectLetter(i);
+          if (hangman.checkWinner()) {
+            alert("Winner!");
+          }
+        }
+      }
+    }
+  }
 };
