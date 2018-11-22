ce04397988a5ec147bd85c8f8b7967791158f448
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2018-11-22 00:21:54 -0600

    Almost finished

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,52 +1,138 @@
 
 function HangmanCanvas(secretWord) {
+  this.secretWord = secretWord;
   this.ctx = document.getElementById('hangman').getContext('2d');
+  this.width = 1200; // Ancho del canvas
+  this.height = 800; //Alto del canvas
+  this.leftPadding = 100; // Padding a la izq
+  this.bottomPadding = 50; // Padding entre inferior del canvas y base de la horca
+  this.radius = 50; // Radio de la cabeza del monito
+  this.topPole = 100; //Coord Y de la cima del poste
+  this.letterLength = 75; //En pixeles, la longitud de los espacios de letras
+  this.letterPadding = 12; //En pixeles, el padding entre los espacios de letras
 }
 
 HangmanCanvas.prototype.createBoard = function () {
-  this.ctx.strokeRect(0, 0, 1200, 800);
+  this.ctx.strokeRect(0, 0, this.width, this.height);
   this.drawLines();
 };
 
 HangmanCanvas.prototype.drawLines = function () {
-  //Las de las letras
-  // this.ctx.beginPath();
-  // this.ctx.arc(400, 400, 200, 0, Math.PI*2);
-  // this.ctx.fill();
-  //Las del patíbulo
-  let i = 100;
-  let j = 750;
-  let topPole = 100;
-  let radius = 50;
-  //Gallow
-  this.ctx.beginPath();
+  let i = this.leftPadding;
+  let j = this.height - this.bottomPadding;
+  let lettersXOrigin = i + 200; //La base no está parametrizada y mide 150px
+  let lettersYOrigin = j;
+
   this.ctx.lineWidth = "3";
+
+  //Las de las letras
+  let cant = this.secretWord.length;
+  for (let k = 0; k < cant; k++) {
+    init = lettersXOrigin + k*this.letterLength;
+    fin = lettersXOrigin + (k+1)*this.letterLength - this.letterPadding;
+    this.ctx.beginPath();
+    this.ctx.moveTo(init, lettersYOrigin);
+    this.ctx.lineTo(fin, lettersYOrigin);
+    this.ctx.stroke();
+  }
+
+  //Las del gallow
+  this.ctx.beginPath();
   this.ctx.moveTo(i+75, j-50);
   this.ctx.lineTo(i, j);
   this.ctx.lineTo(i+150, j);
   this.ctx.lineTo(i+75, j-50);
-  this.ctx.lineTo(i+75, topPole);
-  this.ctx.lineTo(i+450, topPole);
-  this.ctx.lineTo(i+450, topPole+radius);
+  this.ctx.lineTo(i+75, this.topPole);
+  this.ctx.lineTo(i+450, this.topPole);
+  this.ctx.lineTo(i+450, this.topPole+this.radius);
   this.ctx.stroke();
-  this.ctx.beginPath();
-
-  //Vato
-  this.ctx.arc(i+450, topPole + radius*2, radius, 0, Math.PI*2);
-  this.ctx.stroke();
-  // this.ctx.fill();
+  
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
+  let lettersXOrigin = this.leftPadding + 200;
+  let x = lettersXOrigin + index*this.letterLength;
+  let y = this.height-this.bottomPadding - 5; //5 es un mini padding
+  this.ctx.font = "75px sans-serif";
+  this.ctx.fillText(this.secretWord[index], x, y);
 };
 
 HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
+  let gridXOrigin = this.leftPadding + 600;
+  let gridYOrigin = this.topPole + 200;
+  let gridLength = 5;
 
+  posX = gridXOrigin + ((10 - errorsLeft) % gridLength)*this.letterLength;
+  posY = gridYOrigin + Math.floor((10-errorsLeft)/gridLength)*this.letterLength;
+  this.ctx.font = "75px sans-serif";
+  this.ctx.fillText(letter, posX, posY);
 };
 
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
+HangmanCanvas.prototype.drawHangman = function (errorsLeft) {
+  let i = this.leftPadding;
+  this.ctx.lineWidth = "3";
+  switch (errorsLeft) {
+    case 10: //Cabeza
+      this.ctx.beginPath();
+      this.ctx.arc(i+450, this.topPole + this.radius*2, this.radius, 0, Math.PI*2);
+      this.ctx.stroke();
+      break;
+    case 9: //Cuello
+      this.ctx.beginPath();
+      this.ctx.moveTo(i+450, this.topPole + this.radius*3);
+      this.ctx.lineTo(i+450, this.topPole + this.radius*3 + 25);
+      this.ctx.stroke();
+      break;
+    case 8: //Torso 
+      this.ctx.beginPath();
+      this.ctx.strokeRect(i+375, this.topPole + this.radius*3 + 25, 150, 200);
+      break;
+    case 7: //Hombro izquierdo
+      this.ctx.beginPath();
+      this.ctx.strokeRect(i+345, this.topPole+ this.radius*3 + 25, 30, 50);
+      break;
+    case 6: //Brazo izquierdo
+      this.ctx.beginPath();
+      this.ctx.strokeRect(i+350, this.topPole+ this.radius*3 + 75, 20, 100);
+      break;
+    case 5: //Hombro derecho
+      this.ctx.beginPath();
+      this.ctx.strokeRect(i+525, this.topPole+ this.radius*3 + 25, 30, 50);
+      break;
+    case 4: //Brazo derecho
+      this.ctx.beginPath();
+      this.ctx.strokeRect(i+530, this.topPole+ this.radius*3 + 75, 20, 100);
+      break;
+    case 3: //Pierna izquierda
+      this.ctx.beginPath();
+      this.ctx.strokeRect(i+400, this.topPole+ this.radius*3 + 225, 45, 120);
+      break;
+    case 2: //Pierna derecha
+      this.ctx.beginPath();
+      this.ctx.strokeRect(i+460, this.topPole+ this.radius*3 + 225, 45, 120);
+      break;
+    case 1: // Cara
+      this.ctx.beginPath(); //Ojo izq
+      this.ctx.arc(i+450-15, this.topPole + this.radius*2 - 20, this.radius/6, 0, Math.PI*2);
+      this.ctx.stroke();
+      this.ctx.fill();
+      //Ojo derecho
+      this.ctx.beginPath(); //Ojo izq
+      this.ctx.arc(i+450+15, this.topPole + this.radius*2 - 20, this.radius/6, 0, Math.PI*2);
+      this.ctx.stroke();
+      this.ctx.fill();
+      //nariz
+      this.ctx.beginPath();
+      this.ctx.moveTo(i+450, this.topPole + this.radius*2 - 10);
+      this.ctx.lineTo(i+435, this.topPole + this.radius*2);
+      this.ctx.lineTo(i+450, this.topPole + this.radius*2 + 5);
+      this.ctx.stroke();
+      //Boca triste
+      this.ctx.beginPath();
+      this.ctx.arc(i+450, this.topPole + this.radius*2 + 35, 14, -0.85*Math.PI, -0.15*Math.PI, false);
+      this.ctx.stroke();
+      break;
+  }
 };
 
 HangmanCanvas.prototype.gameOver = function () {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,16 +1,19 @@
 var hangman;
+let running = false;
 
 function Hangman() {
-  this.words = ["Chilaquiles", "Tacos", "Pizza"];
+  //Pendiente: corregir lógica para que permita palabras con letras repetidas
+  this.words = ["Tacos", "Nuevo", "Cebra", "Vuelta", "Tango", "Chanfle", "Goles"];
   this.secretWord = "";
   this.letters = [];
   this.guessedLetter = "";
   this.errorsLeft = 10;
+  this.finished = false;
 }
 
 Hangman.prototype.getWord = function () {
   let i = Math.floor(Math.random()*this.words.length);
-  let word = this.words[i];
+  let word = this.words[i].toUpperCase();
   return word;
 };
 
@@ -30,32 +33,58 @@ Hangman.prototype.checkClickedLetters = function (key) {
 
 Hangman.prototype.addCorrectLetter = function (i) {
   this.guessedLetter = this.guessedLetter + this.secretWord[i].toUpperCase();
-  this.checkWinner();
+  if (this.checkWinner()) {
+    alert("Congratulations! You won!");
+    this.finished = true;
+  }
 };
 
 Hangman.prototype.addWrongLetter = function (letter) {
   this.errorsLeft--;
-  this.checkGameOver();
+  if (this.checkGameOver()) {
+    alert("Game over! You lost!");
+    this.finished = true;
+  }
 };
 
 Hangman.prototype.checkGameOver = function () {
-  if (this.errorsLeft <= 0) return true;
-  else return false;
+  return (this.errorsLeft <= 0) ? true : false;
 };
 
 //Letras repetidas??????
 Hangman.prototype.checkWinner = function () {
-  if (this.guessedLetter.length === this.secretWord.length) return true;
-  else return false;
+  return (this.guessedLetter.length === this.secretWord.length) ? true : false;
 };
 
 document.getElementById('start-game-button').onclick = function () {
+  running = true;
   hangman = new Hangman();
-  hangmanCanvas = new HangmanCanvas();
+  hangman.secretWord = hangman.getWord();
+  hangmanCanvas = new HangmanCanvas(hangman.secretWord);
   hangmanCanvas.createBoard();
 };
 
-
+//¿Cómo le hago para que ya no siga corriendo cuando ya terminó?
 document.onkeydown = function (e) {
-
+  if (running && !(hangman.finished)) {
+    // e = e.key.toUpperCase();
+    keycode = e.keyCode;
+    key = e.key.toUpperCase();
+    if (hangman.checkIfLetter(keycode) && hangman.checkClickedLetters(key)) {
+      hangman.letters.push(key); //Agregar la letra al arreglo que contiene las que ya se hicieron
+      if (hangman.secretWord.indexOf(key) === -1) {
+        hangmanCanvas.writeWrongLetter(key, hangman.errorsLeft); //Primero pinta
+        hangmanCanvas.drawHangman(hangman.errorsLeft);
+        hangman.addWrongLetter(key); //Luego ejecuta la lógica
+      } 
+      else {
+        //Aguas con los duplicados
+        hangmanCanvas.writeCorrectLetter(hangman.secretWord.indexOf(key));
+        hangman.addCorrectLetter(hangman.secretWord.indexOf(key));
+      }
+    }
+    else if (!(hangman.checkIfLetter(keycode))){
+      alert("Please press an actual letter this time!");
+    }
+  }
 };
