b9be52ea8a4c32d277fb26376e6746b84954e6eb
hobglobin27 <hobglobin27@gmail.com>
2018-11-22 12:18:46 -0600

    Version completa Hangman

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,84 +1,149 @@
 
 function HangmanCanvas(secretWord) {
   this.ctx = document.getElementById('hangman').getContext('2d');
+  this.secretWord=secretWord;
 }
-
-function Hangman(){
-	this.words=['a','l','g','o'];
-	this.secretWord='';
-	this.letters=[];
-	this.guessedLetter='';
-	this.errorsLeft= 10;
-}
-
-Hangman.prototype.getWord= function (){
-	var word=this.words[Math.floor(Math.random()*this.words.length)]
-	return word;
-}
-Hangman.prototype.checkIfLetter= function (letter){
-	return (letter>=65 && letter<=90 || letter>=97 && letter<=122);
-}
-
-Hangman.prototype.checkClickedLetters= function (letter){ 
-	var existe= this.letters.find(function(element) {
-  				return element == letter;
-  			});
-	return existe?false:true;
-}
-
-Hangman.prototype.addCorrectLetter= function (pos){
-	this.guessedLetter+=this.secretWord.substring(pos,pos+1).toUpperCase();
-}
-
-Hangman.prototype.addWrongLetter= function (pos){
-	this.errorsLeft--;
-}
-
-
-Hangman.prototype.checkGameOver= function (pos){
-	return (this.errorsLeft>0?false:true);
-}
-
-Hangman.prototype.checkWinner= function (){
-	var contadorWin=0;
-	for(var i=0;i<this.secretWord.length;i++){
-		for(var j=0;j<this.guessedLetter.length;j++){
-			if(this.guessedLetter[j] == this.secretWord[i]){
-				this.guessedLetter[j]=null;
-				contadorWin++;
-				break;
-			}
-		}
-	}
-	return contadorWin==this.secretWord.length;
-}
-
 
 HangmanCanvas.prototype.createBoard = function () {
-
+  this.ctx.clearRect(0,0,this.ctx.canvas.width,this.ctx.canvas.height);
+  document.getElementById("boton").className='boton-true';
 };
 
-HangmanCanvas.prototype.drawLines = function () {
-
+HangmanCanvas.prototype.drawLines = function (wordLength) {
+  var inicio=200;
+  var sumaInicio = 15;
+  var longitud=30;
+  var nuevoInicio;
+  this.ctx.beginPath();
+  this.ctx.strokeStyle="#000000";
+  this.ctx.moveTo(inicio, 450);
+  for(let i=0; i<wordLength; i++){
+    this.ctx.lineTo(inicio+longitud, 450);
+    this.ctx.stroke();
+    inicio=inicio+longitud+sumaInicio;
+    this.ctx.moveTo(inicio, 450);
+  }
 };
 
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+HangmanCanvas.prototype.writeCorrectLetter = function (letra) {
+  var inicio=200;
+  var sumaInicio = 15;
+  var longitud=30;
+  var nuevoInicio;
+  this.ctx.beginPath();
+  this.ctx.strokeStyle="#000000";
+  this.ctx.font = '38px serif';
+  this.ctx.moveTo(inicio, 445);
 
+  for(let i=0; i<this.secretWord.length; i++)  {
+    if(letra.toUpperCase() === this.secretWord[i].toUpperCase()){
+      this.ctx.fillText(letra.toUpperCase(), inicio, 445);
+
+    }
+    inicio=inicio+longitud+sumaInicio;
+    this.ctx.moveTo(inicio, 445);
+  }
 };
 
 HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
+  var inicio=390;
+  var sumaInicio = 35;
+  this.ctx.beginPath();
+  this.ctx.strokeStyle="#000000";
+  this.ctx.font = '38px serif';
+  inicio= inicio+sumaInicio*errorsLeft;
+  this.ctx.moveTo(inicio, 110);
 
+  this.ctx.fillText(letter.toUpperCase(), inicio, 110);
 };
 
 HangmanCanvas.prototype.drawHangman = function (shape) {
+  this.ctx.beginPath();
+  this.ctx.strokeStyle="#000000";
+  switch(shape){
 
+  case 1:
+  ////////Triangulo
+    this.ctx.moveTo(100, 400);
+    this.ctx.lineTo(50, 450);
+    this.ctx.lineTo(150, 450);
+    this.ctx.lineTo(100, 400);
+  break;
+
+  case 2:
+    /////////Mastil
+    this.ctx.moveTo(100, 400);
+    this.ctx.lineTo(100, 0);
+  break;
+
+  case 3:
+    /////////Barra
+    this.ctx.moveTo(100, 0);
+    this.ctx.lineTo(350, 0);
+  break;
+
+  case 4:
+    /////////Soga
+    this.ctx.moveTo(350, 0);
+    this.ctx.lineTo(350, 70);
+  break;
+
+  case 5:
+    /////////cabeza
+    this.ctx.moveTo(390, 110);
+    this.ctx.arc(350, 110, 40, 0, Math.PI*2, true);
+  break;
+
+  case 6:
+    /////////torso
+    this.ctx.moveTo(350, 150);
+    this.ctx.lineTo(350, 220);
+  break;
+
+  case 7:
+    /////////pierna izquierda
+    this.ctx.moveTo(350, 220);
+    this.ctx.lineTo(310, 290);
+  break;
+
+  case 8:
+    /////////pierna derecha
+    this.ctx.moveTo(350, 220);
+    this.ctx.lineTo(390, 290);
+  break;
+
+  case 9:
+    /////////brazo izquierdo
+    this.ctx.moveTo(350, 170);
+    this.ctx.lineTo(310, 210);
+  break;
+
+  case 10:
+    /////////brazo derecho
+    this.ctx.moveTo(350, 170);
+    this.ctx.lineTo(390, 210);
+  break;
+  }
+  this.ctx.stroke();
 
 };
 
 HangmanCanvas.prototype.gameOver = function () {
-
+  this.ctx.clearRect(0,0,this.ctx.canvas.width,this.ctx.canvas.height);
+  var img = new Image();
+  imgScale = 4;
+  img.onload = function() {
+    hangmanCanvas.ctx.drawImage(img,300, 0,150*imgScale,300);
+  };
+  img.src = './images/gameover.png';
 };
 
 HangmanCanvas.prototype.winner = function () {
-
+  this.ctx.clearRect(0,0,this.ctx.canvas.width,this.ctx.canvas.height);
+  var img = new Image();
+  imgScale = 4;
+    img.onload = function() {
+      hangmanCanvas.ctx.drawImage(img,300, 0,150*imgScale,350);
+    };
+    img.src = './images/awesome.png';
 };
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,112 @@
 var hangman;
+var hangmanCanvas;
+function Hangman(){
+	this.words=['PATO','LEON','ORNITORRINCO','FILIPINAS','ALEMANIA',
+	'BRASIL','MANZANA','TORONJA','AUTOMOVIL','MOTOCICLETA','CIRCO',
+	'FERROCARRIL','MEDICO','ABOGADO','INGENIERO','AVION','DELEGADO',
+	'FUTBOL','GOLF','TENIS','BASQUETBOL','BASURA','ESCRITORIO','MESA',
+	'REFRIGERADOR','COCINA','ALCOBA','REGADERA','JARDIN','ARBOL','PLANTA',
+	'TIERRA','MARTES','DOMINGO','CAMPO','CIUDAD','PECES','TIBURON',
+	'TORTUGA','BALLENA','PULPO','CARACOL','PANTERA','JIRAFA','ELEFANTE'];
+	this.secretWord='';
+	this.letters=[];
+	this.guessedLetter='';
+	this.errorsLeft= 10;
+}
 
-// function Hangman() {
+Hangman.prototype.getWord= function (){
+	var word=this.words[Math.floor(Math.random()*this.words.length)]
+	return word;
+}
+Hangman.prototype.checkIfLetter= function (letter){
+	return (letter>=65 && letter<=90 || letter>=97 && letter<=122);
+}
 
-// }
+Hangman.prototype.checkClickedLetters= function (letter){
+	var existe= this.letters.find(function(element) {
+  				return element == letter;
+  			});
+	return existe?false:true;
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.addCorrectLetter= function (letter){
+	for(let i=0; i<this.secretWord.length; i++)
+    if(letter.toUpperCase() === this.secretWord[i].toUpperCase())
+			this.guessedLetter+=this.secretWord.substring(i,i+1).toUpperCase();
+}
 
-// };
+Hangman.prototype.addWrongLetter= function (){
+	this.errorsLeft--;
+}
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// };
+Hangman.prototype.checkGameOver= function (){
+	return (this.errorsLeft>0?false:true);
+}
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkWinner= function (){
+	var contadorWin=0;
+	for(var i=0;i<this.secretWord.length;i++){
+		for(var j=0;j<this.guessedLetter.length;j++){
+			if(this.guessedLetter[j] == this.secretWord[i]){
+				this.guessedLetter[j]=null;
+				contadorWin++;
+				break;
+			}
+		}
+	}
+	return contadorWin==this.secretWord.length;
+}
 
-// };
+Hangman.prototype.existeEnSecreta= function (letter){
+	var existe= this.secretWord.indexOf(letter);
+	if(existe!=-1)
+		return true;
+	return false;
+}
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+	hangmanCanvas = new HangmanCanvas(hangman.getWord());
+	hangman.secretWord=hangmanCanvas.secretWord;
+	hangmanCanvas.createBoard();
+	hangmanCanvas.drawLines(hangmanCanvas.secretWord.length);
+	console.log(hangmanCanvas.secretWord);
+	document.getElementById("boton").className='boton-false';
 };
 
 
 document.onkeydown = function (e) {
+	if(document.getElementById("boton").className==="boton-false"){
+		tecla=e.keyCode;
+		letra=String.fromCharCode(e.keyCode);
 
+		if(!hangman.checkIfLetter(tecla))
+			alert("Solo se aceptan letras");
+		else{
+			if(!hangman.checkClickedLetters(letra))
+				alert("La letra pulsada ya fue seleccionada anteriormente. Seleccione otra letra");
+			else{
+				hangman.letters.push(letra);
+				if(hangman.existeEnSecreta(letra)){
+					hangman.addCorrectLetter(letra);
+					hangmanCanvas.writeCorrectLetter(letra);
+					if(hangman.checkWinner()){
+						document.getElementById("boton").className='boton-true';
+						hangmanCanvas.winner();
+					}
+				}
+				else{
+					hangman.addWrongLetter();
+					hangmanCanvas.writeWrongLetter(letra,10-hangman.errorsLeft);
+					hangmanCanvas.drawHangman(10-hangman.errorsLeft);
+					if(hangman.checkGameOver()){
+						document.getElementById("boton").className='boton-true';
+						hangmanCanvas.gameOver();
+					}
+				}
+			}
+		}
+	}
 };
