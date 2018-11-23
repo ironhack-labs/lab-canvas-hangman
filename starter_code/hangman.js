7b464e6fc7c9cfae6b31f69f70df378771d9eaf1
jose-martinez-albarran <jose.jesus.martz@gmail.com>
2018-11-22 19:12:32 -0600

    Pending canvas side by Jose Martinez

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,58 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+  this.words = ['IRONHACK','BOOTCAMP'];
+  this.secretWord = '';
+  this.letters = []; 
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
+ }
 
-// }
+Hangman.prototype.getWord = function () {
+  var randWords = this.words[Math.floor(Math.random()*this.words.length)];
+  return this.secretWord = randWords;
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+    if(this.letters.includes(key)){
+      this.letters.push(key);
+      return false;
+    } else {
+      return true;
+    }
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+    if(this.secretWord.indexOf(this.guessedLetter) === i){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft === 0){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkWinner = function () {
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
