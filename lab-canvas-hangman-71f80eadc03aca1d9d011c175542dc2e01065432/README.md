891d1d84f2627181c5507897d150fc786618fe3b
dcarbajalc <43658183+dcarbajalc@users.noreply.github.com>
2019-02-13 19:47:45 -0600

    DibujaLetra

---
+++ lab-canvas-hangman-71f80eadc03aca1d9d011c175542dc2e01065432/README.md
@@ -0,0 +1,92 @@
+![Ironhack Logo](https://i.imgur.com/1QgrNNw.png)
+
+# JS | Canvas Hangman
+
+## Introduction
+
+In this exercise, we are going to create the classic game [Hangman](https://en.wikipedia.org/wiki/Hangman_(game)). In this game, we have to figure out a secret word by guessing one letter at a time, with each error, the man will get one step closer to death! The goal is to successfully guess the word before the man gets hanged.
+
+![](https://i.imgur.com/wrQrY1T.png)
+
+As we have learned, we can't confront the whole game without splitting it up into smaller steps. We will split it up into three different parts: structure, logic, and game layout. This technique is known as **incremental build.**
+
+We will separate the game logic from the iteration with the `canvas`. We should be able to play from the console first and then add the graphics.
+
+## Requirements
+
+- Fork this repo
+- Clone this repo
+
+## Submission
+
+Upon completion, run the following commands:
+```
+$ git add .
+$ git commit -m "done"
+$ git push origin master
+```
+Create Pull Request so your TAs can check up your work.
+
+## 1. First iteration: Game Logic
+
+:::info
+In order to do the game logic, we add some **Jasmine** tests to help you. Navigate to:
+
+```bash
+starter_code
+  |___ jasmine
+```
+Open the `SpecRunner.html` to check them.
+:::
+
+### Hangman Constructor
+
+First, of at all let's create our Hangman constructor. It should have the following properties:
+
+- **words**. An array where we will store all the words that the player need to guess. We will take one of them randomly.
+- **secretWord**. Here we will store the word chosen for each game.
+- **letters**. An array to store the letters the user already clicked, so we do not repeat them.
+- **guessedLetter**. A string to store the letters the user clicked and guessed. We will use this to know when the user wins.
+- **errorsLeft**. It should start at 10, and decrease every time a user clicks on a letter that is not in the word.
+
+### Prototypes properties
+
+- `getWord()`. Returns a random word from our array `words`.
+- `checkIfLetter`. This function should check if the key the user has typed is a letter.
+- `checkClickedLetters`. Checks if the pressed letter has already been pressed and returns true if it was not or false in the opposite case.
+- `checkGameOver`. Returns a boolean value, `true` if the users lose, and `false` in any other case.
+- `checkWinner`. Check if the users win and return a boolean value.
+- `addCorrectLetter`. Adds to the `guessedLetter` variable the letter that was pressed. Also, it should check if the user wins.
+- `addWrongLetter`. Should subtract one from the variable `errorsLeft` and check if the game is over.
+
+## 2. Second Iteration: Draw in Canvas
+
+![](https://s3-eu-west-1.amazonaws.com/ih-materials/uploads/upload_3e1e1919b29ba77e77cdcec2ed7b92c5.png)
+
+Now we need to start drawing the hangman in order to finish our game! As we said, we will do it in a different file separating it from the logic.
+
+Let´s create a constructor `HangmanCanvas` and store in a variable our `canvas`. After storing it, we should get the context. Remember `getContext('2d')`. We also want to store in a variable the secret word.
+
+### Creating the board
+
+First, we need to draw the board. We should clear the `canvas`, so every time we start the game we have a clean one. We can also set up here the `width` we want to set to our lines.
+
+### Draw the Secret Word Lines
+
+We know the secret word so we should put one line for each of the letters the user has to guess.
+
+### Write the Guessed Letters and the Wrong Ones
+
+Every time a user clicks the keyboard, after checking if the letter was not already clicked, we should write it on our board. If the secret word includes the letter we should write it in the position it corresponds, and if is not included in the secret word, we will write it on the top right so the user knows which ones he already clicked.
+
+### Draw the Hangman
+
+The last task, we need to draw **THE HANGMAN**. You can notice the drawing is composed of lines and one circle. Go ahead and think how can we do it :wink:
+
+## Bonus Iteration
+
+Your game is finished! Anybody can play it, but we need to show them something when they win or lose, so go ahead and create two functions `gameOver` and `winner`, to display the images available on the `starter_code > images` folder.
+
+![](https://s3-eu-west-1.amazonaws.com/ih-materials/uploads/upload_1dc0d7772d204da800d078c153c12e47.png)
+
+Happy coding! ❤️
---
+++ lab-canvas-hangman-71f80eadc03aca1d9d011c175542dc2e01065432/starter_code/jasmine/spec/Hangman.js
@@ -0,0 +1,149 @@
+describe('Hangman Game', function () {
+  var hangman;
+  beforeEach(function () {
+    hangman = new Hangman();
+  });
+  describe('Words to Pick', function () {
+    it('There should be an array of words', function () {
+      expect(hangman.words).toBeDefined();
+    });
+    it('There should be at least one word to pick', function () {
+      expect(hangman.words.length).toBeGreaterThan(2);
+    });
+  });
+
+  describe('Secret Word', function () {
+    it('secretWord should exist', function () {
+      expect(hangman.secretWord).toBeDefined();
+    });
+
+    it('getWord should be a function', function () {
+      expect(typeof (hangman.getWord)).toBe('function');
+    });
+
+    it('getWord should return a string', function () {
+      expect(typeof (hangman.getWord())).toBe('string');
+    });
+
+    it('secretWord should be a string', function () {
+      expect(typeof (hangman.secretWord)).toBe('string');
+    });
+  });
+
+  describe('Check if is a letter', function () {
+    it('checkIfLetter should be a function', function () {
+      expect(typeof (hangman.checkIfLetter)).toBe('function');
+    });
+
+    it('checkIfLetter should receive a number', function () {
+      expect(typeof (hangman.checkIfLetter)).toBe('function');
+    });
+
+    it('checkIfLetter should return a boolean', function () {
+      var keyCode = 43;
+      hangman.checkIfLetter(keyCode);
+      expect(typeof (keyCode)).toBe('number');
+    });
+
+    it('checkIfLetter should return false', function () {
+      expect(hangman.checkIfLetter(43)).toEqual(false);
+    });
+
+    it('checkIfLetter should return true', function () {
+      expect(hangman.checkIfLetter(76)).toEqual(true);
+    });
+  });
+
+  describe('Check if the letter was already clicked', function () {
+    it('checkClickedLetters should be a function', function () {
+      expect(typeof (hangman.checkClickedLetters)).toBe('function');
+    });
+    it('checkClickedLetters should receive a string', function () {
+      var key = 'P';
+      hangman.checkClickedLetters(key);
+      expect(typeof (key)).toBe('string');
+    });
+    it('checkClickedLetters should return a boolean', function () {
+      hangman.letters.push('I');
+      expect(typeof (hangman.checkIfLetter('N'))).toBe('boolean');
+    });
+
+    it('checkClickedLetters should return true', function () {
+      hangman.letters.push('I', 'R', 'P');
+      expect(hangman.checkClickedLetters('F')).toEqual(true);
+    });
+
+    it('checkIfLetter should return false', function () {
+      hangman.letters.push('I', 'R', 'P');
+      expect(hangman.checkClickedLetters('R')).toEqual(false);
+    });
+  });
+
+  describe('Add correct letters', function () {
+    it('addCorrectLetter should be a function', function () {
+      expect(typeof (hangman.addCorrectLetter)).toBe('function');
+    });
+    it('addCorrectLetter should receive a number', function () {
+      var key = 'N';
+      hangman.checkClickedLetters(key);
+      expect(typeof (key)).toBe('string');
+    });
+    it('addCorrectLetter should add letters to guessedLetter string', function () {
+      hangman.secretWord = 'Ironhack';
+      hangman.addCorrectLetter(1);
+      expect(hangman.guessedLetter).toEqual('R');
+    });
+  });
+
+  describe('Wrong letters', function () {
+    it('addWrongLetter should be a function', function () {
+      expect(typeof (hangman.addWrongLetter)).toBe('function');
+    });
+    it('addWrongLetter should receive a string', function () {
+      var letter = 'P';
+      hangman.addWrongLetter(letter);
+      expect(typeof (letter)).toBe('string');
+    });
+    it('addWrongLetter should discount the amount of errors left', function () {
+      hangman.errorsLeft = 7;
+      hangman.addWrongLetter('P');
+      expect(hangman.errorsLeft).toEqual(6);
+    });
+  });
+
+  describe('Check if the game is over', function () {
+    it('checkGameOver should be a function', function () {
+      expect(typeof (hangman.checkGameOver)).toBe('function');
+    });
+    it('checkGameOver should return a boolean', function () {
+      expect(typeof (hangman.checkGameOver())).toBe('boolean');
+    });
+    it('checkGameOver should return false if the errorsLeft is 0', function () {
+      hangman.errorsLeft = 0;
+      expect(hangman.checkGameOver()).toEqual(true);
+    });
+    it('checkGameOver should return false if the errorsLeft is 0', function () {
+      hangman.errorsLeft = 5;
+      expect(hangman.checkGameOver()).toEqual(false);
+    });
+  });
+
+  describe('Check if we win', function () {
+    it('checkWinner should be a function', function () {
+      expect(typeof (hangman.checkWinner)).toBe('function');
+    });
+    it('checkWinner should return a boolean', function () {
+      expect(typeof (hangman.checkWinner())).toBe('boolean');
+    });
+    it('checkWinner should return true if we guess all letters', function () {
+      hangman.secretWord = 'IRONHACK';
+      hangman.guessedLetter = 'KHARCNIO';
+      expect(hangman.checkWinner()).toEqual(true);
+    });
+    it('checkWinner should return true if we guess all letters', function () {
+      hangman.secretWord = 'IRONHACK';
+      hangman.guessedLetter = 'KHARCN';
+      expect(hangman.checkWinner()).toEqual(false);
+    });
+  });
+});
