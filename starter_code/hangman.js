20cc9c8b7d1d17845033cb9c1b6e4989c533a73c
Guille LA <guillermolucena90@gmail.com>
2019-02-19 18:53:24 +0100

    first iteration done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,43 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words = ["coding", "enjoy", "basketball", "wizard", "beer", "barman", "parties", "lesson", "codewars", "downhill", "forest", "function", "friends", "athlete", "design", "canvas", "compass", "html", "background", "limitless", "infinite", "foodie", "springroll", "summer", "pumpkin", "science", "nature", "parachute"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  var word = Math.floor(Math.random() * this.words.length)
+  return this.words[word];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  return (keyCode >= 65 && keyCode <= 90) ? true : false
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  return (this.letters.includes(key) === true) ? false : true
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  return this.checkGameOver;
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--;
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkGameOver = function () {
+  return (this.errorsLeft === 0) ? true : false
+};
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  var letterSet = new Set(this.secretWord);
+  return (letterSet.size === this.guessedLetter.length) ? true : false
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
