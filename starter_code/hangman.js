56206096fd1517f09af2ff192202f56aadefba34
Alvaro Garcia <agd3101@gmail.com>
2019-02-19 20:17:39 +0100

    [MAD PT] Alvaro Garcia

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,64 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words=["PERRO","GATO","ZAPATO","ZARIGUELLA"];
+  this.secretWord="";
+  this.letters=[];
+  this.guessedLetter="";
+  this.errorsLeft=10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  return this.words[Math.floor(Math.random())*this.words.length];
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// };
+if (keyCode>64 && keyCode<91){
+  return true;
+} else{
+  return false;
+}
+ };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
+   if(this.letters.includes(key)){
+      return false;
+    }else{
+      return true;
+    }
+};
 
-// };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+//Adds to the guessedLetter variable the letter that was pressed. 
+//Also, it should check if the user wins.
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  this.checkWinner();
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+//Should subtract one from the variable errorsLeft and check if the game is over.
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--;
+  this.checkGameOver();
+};
+//Returns a boolean value, true if the users lose, and false in any other case.
+Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft==0){
+    return true;
+  }else{
+    return false;
+  }
+};
+//Check if the users win and return a boolean value
+Hangman.prototype.checkWinner = function () {
+  if(this.secretWord.length==this.guessedLetter.length){
+    return true;
+  }else{
+    return false;
+  }
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
