d524c329def4404358d8659603f9676b46557e73
David Gallego Gomez <davizs@hotmail.com>
2019-02-20 00:17:47 +0100

    Se pasan los tests de Jasmine

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -4,7 +4,7 @@ function Hangman() {
   this.words = ['piña', 'pitufo', 'mollete', 'viena'];
   this.secretWord = this.words[0];
   this.letters = [];
-  this.guessedLetter;
+  this.guessedLetter = '';
   this.errorsLeft = 10;
 }
 
@@ -18,25 +18,41 @@ Hangman.prototype.checkIfLetter = function (keyCode) {
   return ( ( keyCode > 64 && keyCode < 91 ) || keyCode === 186 ); // 186 es el keyCode de la letra 'ñ'
 };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkClickedLetters = function (key) {
+  const result = this.letters.filter( letter => letter === key );
+  return result.length === 0;
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  return this.checkWinner(); // ???
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.letters.push(letter);
+  this.errorsLeft --;
+  return this.checkGameOver(); // ???
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+  return ( this.errorsLeft === 0 );
+};
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+function differentLetters ( word ) {
+  var str = word.toUpperCase();
+  var uniql = "";
+  for (var x=0;x < str.length;x++) {
+    if(uniql.indexOf(str.charAt(x)) == -1) { uniql += str[x]; }
+  }
+  var sortLetter = uniql.split('').sort().join('');
+  return sortLetter;
+}
 
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  var sortGuesses = this.guessedLetter.toUpperCase().split('').sort().join(''); // Pasamos a mayúsculas y orden alfabético
+  var sortSecretWord = differentLetters ( this.secretWord );
+  return (!this.checkGameOver() && sortGuesses.localeCompare(sortSecretWord) === 0);
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
