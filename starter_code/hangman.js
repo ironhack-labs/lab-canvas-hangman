68ec1dcb27026a60a4f38a72afe739f44a84951b
Javi L <jlopezro@gmail.com>
2019-02-19 20:03:31 +0100

    Realizado hasta prototype getWord()

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,16 +1,64 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+this.words = ["JavaScript", "kata", "Ironhack", "iteration", "Madrid", "Miami", "Barcelona", "Berlin", "Sao Paolo", "Mexico DF", "Paris"];
+this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
+//this.secretWord = secretWord;
 
-// }
+/*$("input").keyup(function(e){
+  var letters = $("input").val()
+  //console.log(inputValue)
+  //var sayHi = "Hello " + inputValue;
+  //$("h1").html(sayHi)
+}) */
 
-// Hangman.prototype.getWord = function () {
+this.letters = [];
+//this.guessedLetter = letters.toString();
+this.guessedLetter = '';
+//this.error = error;
+//this.total = 10;
+this.errorsLeft = 10;
+ }
 
-// };
+/*for (this.errorsLeft = 10; errorsLeft > 0; errorsLeft--) {
+  console.log(errorsLeft);
+}*/
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+ Hangman.prototype.getWord = function () {
+ //this.words[Math.floor(Math.random() * this.words.length)];
 
-// };
+ //this.secretWord = this.secretWord;
+ //return this.secretWord;
+
+ secretWord = this.secretWord;
+
+ return secretWord;
+
+};
+
+
+
+
+
+
+
+Hangman.prototype.checkIfLetter = function (keyCode) {
+
+ var keyCode = prompt("Please Type a letter");
+
+ /* $("prompt").keyup(function(e){
+    keyCode = $("prompt").val()
+    console.log(keyCode) */
+
+    if (keyCode >= 65 && keyCode <= 90){
+ //    if (keyCode = /[a-z]\D/) {
+      true;
+    } else {
+     // alert("Please type a letter");
+     false;
+    }
+
+ };
 
 // Hangman.prototype.checkClickedLetters = function (key) {
 
