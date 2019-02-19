9525d26fad0ba3e695abb01dd047f148c4b631d3
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-02-19 23:11:16 +0100

    first iteration fixed

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,64 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+  this.words = ['COCHE', 'AVIÓN', 'CAMIONETA', 'ELEFANTE'];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+ }
 
-// }
+ Hangman.prototype.getWord = function () {
+   // Returns a random word from our array 
+  this.words[Math.floor(Math.random())*this.words.length];
+  return 'undefined';
+ };
 
-// Hangman.prototype.getWord = function () {
+ Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 64 && keyCode <= 91){
+    return true;
+  } else {
+    return false;
+  }
+ };
 
-// };
+ Hangman.prototype.checkClickedLetters = function (key) {
+  if(this.letters.includes(key)){
+    return false;
+  }else{
+    return true;
+  }
+ };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+ Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  this.checkWinner();
+ };
 
-// };
+ Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--;
+  //llamada a game over
+  this.checkGameOver();
+ };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+ Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft == 0){
+    return true;
+  }else{
+    return false;
+  }
+ };
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+ Hangman.prototype.checkWinner = function () {
+ //Compruebe si los usuarios ganan y devuelven un valor booleano.
+  if(this.secretWord.length == this.guessedLetter.length){
+    //return true if we guess all letters
+    return true;
+  } else {
+    //return true if we guess all letters
+    return false;
+  }
+ };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
