71de1ecfad0cb62ca954c5e005500520c190cd0e
eskalona17 <eskalona_17@hotmail.com>
2019-02-20 18:47:35 +0100

    first

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,66 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words= ['ADIDAS', 'NIKE', 'PUMA', 'REEBOK'];
+  this.secretWord= "";
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorsLeft = 10;
+}
 
-// }
+Hangman.prototype.getWord = function () {
+  var indexRandom = Math.floor(Math.random()*this.words.length)
+    console.log(indexRandom)
+    this.secretWord= String(this.words[indexRandom])
+    return String(this.words[indexRandom])
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (64 < keycode && keycode < 123){
+    return true
+  }
+  return false
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  var key = letter.charCodeAt();
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (this.checkIfLetter(key)){
+    for(var i=0; i<= this.letters.length; i++){
+      if (letter == this.letters[i]){
+        return false
+      }
+  }    
+  }
+  return true
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[index].toUpperCase()
+  this.checkWinner()
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--;
+  this.checkGameOver()
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft=== 0){
+    return true
+  }
+  return false 
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  var guessedLetterArr = this.guessedLetter.toLowerCase().split('')
+  var secretWordArr = this.secretWord.toLowerCase().split('')
+  
+  if (secretWordArr.sort().toString() === guessedLetterArr.sort().toString()){
+    return true
+  }
+  return false
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
