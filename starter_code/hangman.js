c33b649f76aa3d1185fd41f23ce3c4319c4f94c5
kellyjoany <kelly.joany@gmail.com>
2019-04-05 20:18:28 -0300

    done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,64 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+   this.words = ['kelly', 'arthur', 'rodrigo'];
+   this.secretWord = '' //this.getWord;
+   this.letters = [];
+   this.guessedLetter = '';
+   this.errorsLeft = 10;
+ }
 
-// }
+ Hangman.prototype.getWord = function () {
+  let word = this.words[(Math.floor(Math.random() * this.words.length))];
+  return word;
+ };
 
-// Hangman.prototype.getWord = function () {
+ Hangman.prototype.checkIfLetter = function (keyCode) {
+   let result = keyCode;
+   if((result >= 65 && result <=90)){
+      return true;
+   }
+  return false; 
+ };
 
-// };
+ Hangman.prototype.checkClickedLetters = function (key) {
+  if(this.letters.indexOf(key) === -1) {
+      return true;
+    } 
+  return false;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+ Hangman.prototype.addCorrectLetter = function (i) {
+  this.letters.push(i);
+  let correct = this.secretWord[i];
+  this.guessedLetter += correct.toUpperCase(); 
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.letters.push(letter);
+  this.errorsLeft -= 1;
+  this.checkGameOver();
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+ Hangman.prototype.checkGameOver = function () {
+    if(this.errorsLeft === 0){
+      return true;
+    }
+  return false;  
+ };
 
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+    let secret = this.secretWord.split('');
+    secret = secret.toUpperCase;
+    //if(this.guessedLetter.indexOf(secret[i]) === -1){
+    if(this.guessedLetter.indexOf(secret !== -1)){
+      return true;
+    }
+/*  if(this.guessedLetter == this.secretWord){
+      return true;
+    }*/
+  return false;   
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
