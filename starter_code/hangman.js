35b6cbf8db98e18435461bfb530ab3bf5080fbb2
Karen Ota <karenkota@yahoo.com.br>
2019-04-06 11:27:35 -0300

    incomplete

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,58 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+    this.words = [ 'saxofone', 'piano', 'reco-reco', 'harmonic', 'panderinho'];
+    // transformar objeto, em string:
+    this.secretWord = wordRandom.toString('');
+    this.letters = [] ;
+    this.guessedLetter ='';
+    this.errorsLeft = 10;
+}
+  let wordRandom = ('');
+  Hangman.prototype.getWord = function () {
+  wordRandom = this.words[Math.floor(this.words.length * Math.random ())];
+};
 
-// }
+// codeKey = ref. > ascii table
+let codeKey = Hangman.prototype.checkIfLetter = function (keyCode) {
+  if(keyCode >= 64 && keyCode <= 123) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkClickedLetters = function (key) {
+    // this.words.localeCompare(this.secretWord){
+    if (key === codeKey){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  for (i = 0; i < wordRandom.length; i += 1) {
+    if (wordRandom[i] === guessedLetter){
+      return this.guessedLetter = wordRandom[i] + guessedLetter;
+    } else {
+      return false
+    }
+  }
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+};
 
-// };
+Hangman.prototype.checkWinner = function () {
 
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
