72ecf7e3eb73dcd1c6c19532bda9d3cbcf60f914
davidh <iq.davidh@gmail.com>
2019-05-23 18:28:44 -0500

    ya se tienen 2 errores

--- starter_code/ProDrawAhorcado.js
+++ starter_code/ProDrawAhorcado.js
@@ -23,12 +23,21 @@ let AnimacionInterfaz = {
 
 
 let DrawAhorcado = {
-    centroHead:{x:100,y:75},
+    centroHead: {x: 100, y: 75},
+    cuerpo: {
+        radioHead: 40,
+        torso: 50
+    },
     ctx: null,
+    configStroke: function () {
+        this.getCtx().lineWidth = "8";
+        this.getCtx().strokeStyle = "green";
+    },
     getCtx: function () {
 
         if (this.ctx === null) {
             var c = document.getElementById("hangman");
+
             this.ctx = c.getContext("2d");
         }
 
@@ -36,15 +45,27 @@ let DrawAhorcado = {
     },
     error1: function () {
         //La cabeza
-        let ctx=this.getCtx();
+        let ctx = this.getCtx();
+
         ctx.beginPath();
-        ctx.arc(this.centroHead.x,this.centroHead.y,50,0,2*Math.PI);
-       // ctx.endPath();
+        this.configStroke();
+        ctx.arc(this.centroHead.x, this.centroHead.y, this.cuerpo.radioHead, 0, 2 * Math.PI);
+        ctx.closePath();
         ctx.stroke();
         return true;
 
     },
     error2: function () {
+        //cuello
+        let ctx = this.getCtx();
+
+
+        this.configStroke();
+        ctx.moveTo(this.centroHead.x, this.centroHead.y + this.cuerpo.radioHead);
+        let largo = this.centroHead.y + this.cuerpo.radioHead + this.cuerpo.torso;
+        ctx.lineTo(this.centroHead.x, largo);
+        ctx.stroke(); // Draw it
+        return true;
 
     },
     error3: function () {
--- starter_code/ProDrawAhorcado_test.js
+++ starter_code/ProDrawAhorcado_test.js
@@ -5,3 +5,9 @@
  */
 
 DrawAhorcado.error1();
+DrawAhorcado.error2();
+DrawAhorcado.error3();
+DrawAhorcado.error4();
+DrawAhorcado.error5();
+DrawAhorcado.error6();
+DrawAhorcado.error7();
