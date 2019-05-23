6db302547900f4e5ec63efc917b3a50128bc2775
David H <iq.davidh@gmail.com>
2019-05-22 19:33:20 -0500

    animacion lista

--- starter_code/app.js
+++ starter_code/app.js
@@ -4,25 +4,82 @@
  * Created by David on 21/05/2019.
  */
 
+let AnimacionInterfaz = {
+    esperaLong: async function () {
+
+
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('1');
+                resolve();
+            }, 1500)
+        });
+
+        await promise; // wait till the promise resolves (*)
+        return true;
+    }
+};
 
 const app = new Vue({
     el: '#app',
     data: {
-        juego:null
+        juego: null,
+        juego_estado: ConfigJuego.estados.before_play,
+        isShowStar: true,
+        cssClassBotonStart: ''
     },
     methods: {
-        onClickStart(){
+        onClickStart() {
             //incializar el juego
-            let palabra= ConfigJuego.getRandomPalabra();
-            this.juego=new Juego(palabra);
+            let palabra = ConfigJuego.getRandomPalabra();
+            this.juego = new Juego(palabra);
+        },
+        onKeyPress(event) {
+            console.log(event);
+        },
+        async start() {
+
+            let palabra = ConfigJuego.getRandomPalabra();
+            this.cssClassBotonStart = 'salidaBoton';
+
+
+
+            await  AnimacionInterfaz.esperaLong();
+
+            app.cssClassBotonStart = 'noDisplay';
+
+
+
+            console.log('2');
+
+
+            //
+            // await setTimeout( ()=>{
+            //     app.cssClassBotonStart='noDisplay';
+            //     return new Promise();
+            //     console.log(1);
+            // },1500) ;
+
+
+
         }
     },
-    computed:{
-        getLista
+    computed: {
+        isShowCmdStart() {
+            return this.juego_estado === ConfigJuego.estados.before_play;
+        },
+        isShowCanvas() {
+
+        },
+
+        getCssClassBotonStart() {
+            return this.cssClassBotonStart;
+        }
+
     }
     ,
     mounted() {
-
+        this.cssClassBotonStart = 'entradaBoton';
 
     }
 });
