3255d058768f2bf2f757c9b13dcede95dbc745ee
geguiluz <memo.eguiluz@gmail.com>
2019-05-24 23:47:19 -0500

    Lògica bàsica funcionando

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,79 @@
 
-function HangmanCanvas(secretWord) {
+let createBoard = () => {
   this.ctx = document.getElementById('hangman').getContext('2d');
+  renderWord(hangman.secretArray);
+};
+
+
+let drawHangman = (errorCount) => {
+  switch (errorCount){
+    case 1:{
+      // Draw base line
+
+      break;
+    }
+    case 2:{
+      //Draw pole
+
+      break;
+    }
+    case 3:{
+      //Draw horizontal beam
+
+      break;
+    }
+    case 4:{
+      //Draw rope
+
+      break;
+    }
+    case 5:{
+      //Draw head
+
+      break;
+    }
+    case 6:{
+      //Draw body
+
+      break;
+    }
+    case 7:{
+      //Draw ledt hand
+
+      break;
+    }
+    case 8:{
+      //Draw right hand
+
+      break;
+    }
+    case 9:{
+      //Draw right foot
+
+      break;
+    }
+    case 10:{
+      //Draw left foot
+      
+      break;
+    }
+  }
+};
+
+let renderWord = (wordArray) => {
+  // Spit every element of the array
+};
+
+let gameOver = () => {
+
+};
+
+let winner = () => {
+
+};
+
+createBoard(wordArray);
+for (let i = 1; i < wordArray.length; i ++){
+  drawHangman(i);
+
 }
-
-HangmanCanvas.prototype.createBoard = function () {
-
-};
-
-HangmanCanvas.prototype.drawLines = function () {
-
-};
-
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -5,45 +5,52 @@ class Hangman {
   constructor() {
     this.guessedLetter = '';
     this.errorsLeft = 10;
-    this.words = ['perro', 'gato', 'elefante','ballena'];
+    // this.words = ['PERRO', 'GATO', 'ELEFANTE','BALLENA'];
+    this.words = ['PERRO'];
     this.secretWord = '';
+    this.secretArray = [];
     this.letters = [];
   }
   getWord() {
     // Returns a random word from our array words
      this.secretWord = this.words[Math.floor(Math.random()*this.words.length)];
+     // Store our secret word in an array, so we can use it later in comparissons and such
+     this.secretArray = this.secretWord.split('');
+     // Set an empty array in letters, so we can render it on screen later
+     this.secretArray.map(e=>this.letters.push('_'));
+     console.log("secretArray", this.secretArray);
+     console.log("letters", this.letters);
      return this.secretWord;
   }
  
   checkGameOver(){
     // checkGameOver. Returns a boolean value, true if the users lose, and false in any other case.
     if(this.errorsLeft === 0){
+      console.log('Game Over');
       return true;
     } else {
       return false;
     }
 
   }
-  checkIfLetter(input, checktype){
+  checkIfLetter(input, checktype = 'abc'){
     let universe = ''
     // Checks if entered value is a letter form A to Z and returns a boolean
-    if(checktype = 'abc'){
+    if(checktype === 'abc'){
       universe = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    } 
-    if (checktype = 'pressed'){
+    }
+    if (checktype === 'pressed'){
       universe = this.guessedLetter;
     }
-    if (checktype = 'word'){
+    if (checktype ===  'secret'){
       universe = this.secretWord;
     }
     let arr = universe.split('');
-    arr.indexOf(input) > -1 ? false : true;
+    return arr.includes(input);
   }
   checkWinner(){
     // checkWinner. Check if the users win and return a boolean value.
-    // First let's turn our secretWord into an array for comparisson purposes
-    let secretArray = this.secretWord.split('');
-    if(secretArray.every( e => this.letters.includes(e) )){
+    if(this.secretArray.every( e => this.letters.includes(e) )){
       // Compare letters with secretWord. If they're the same, the player wins the game
       return true;
     } else{
@@ -54,33 +61,42 @@ class Hangman {
 
   addCorrectLetter(input){
     // addCorrectLetter. Adds to the guessedLetter variable the letter that was pressed. Also, it should check if the user wins.
-    this.guessedLetter+=input;
-    this.drawPositions();
-    
+    this.guessedLetter += input;
+    // Add the letter in all places where it belongs to. Store it in the letters array the exact way it will be rendered on screen
+    // Let's cicle through our secretArray, so we can insert our input letter in the space it belongs to in the letters array
+    this.secretArray.map((e, index)=>e === input ? this.letters[index] = this.secretArray[index] : this.letters[index]);
+    console.log('this.letters', this.letters)
+    return this.checkWinner();
   }
   addWrongLetter(){
     // addWrongLetter. Should subtract one from the variable errorsLeft and check if the game is over.
     this.errorsLeft --;
-    this.checkGameOver();
+    return this.checkGameOver();
   }
   checkClickedLetters(input){
     // checkClickedLetters. Checks if the pressed letter has already been pressed and returns true if it was not or false in the opposite case.
-    input = input.toUppercase();
+    // input = input.toUppercase;
     // First check if entered value is even a valid letter
     if (this.checkIfLetter(input, 'abc')){
       // Check if letter input has already been pressed
-      if (this.checkIfLetter(input, 'pressed')){
+      if (!this.checkIfLetter(input, 'pressed')){
         // Input letter has not been pressed before, so let's check if our input letter exists inside the secretWord
         if (this.checkIfLetter(input, 'secret')){
           // Input letter does exist; return true and call addCorrectLetter
-          // Place letter in an array, so that it can be drawn on screen
+          if(this.addCorrectLetter(input)){
+            // addCorrectLetter checks if user won, in which case returns a true
+            console.log('User has won');
+            return true;
+          }
         } else {
-          // Input letter does not exist in the secretWord; return false and call addWrongLetter
-         return false; 
+          // Input letter does not exist in the secretWord; call addWrongLetter
+          console.log(`letter ${input} not in word ${this.secretWord}` )   
+          return this.addWrongLetter();       
         }
 
       } else {
         //Return false if input letter has already been pressed
+        console.log(`letter ${input} has already been entered ${this.secretWord}` )  
         return false;
       }
     }
@@ -89,20 +105,12 @@ class Hangman {
 
 }
 
-
-  hangman = new Hangman();
-  hangman.getWord();
-  hangman.checkClickedLetters();
-
-
-
-
-
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  hangman.getWord();
 };
 
-
 document.onkeydown = function (e) {
-
+  let character = String.fromCharCode(e.keyCode);
+  hangman.checkClickedLetters(character);
 };
