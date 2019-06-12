0d4d4549a873450be9281a7942e545f630743570
Dago Rodriguez <dagor@Dagos-MacBook-Pro.local>
2019-06-12 18:33:33 -0500

    finished the jasmine iterations but everything was confusing because tehre wasnt enough specifications in the structions. The last one was hard to get

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,75 @@
 var hangman;
 
-// function Hangman() {
+class Hangman {
+  constructor() {
+    this.words = ["platos", "dagoberto", "ironhack", "Mexico"];
+    this.secretWord = "ironhack";
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+  }
+}
+Hangman.prototype.getWord = function() {
+  return this.words[
+    Math.floor(Math.random() * this.words.length)
+  ].toUpperCase();
+};
 
-// }
+Hangman.prototype.checkIfLetter = function(keyCode) {
+  if ((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkClickedLetters = function(key) {
+  return this.letters.indexOf(key) === -1;
+};
+Hangman.prototype.addCorrectLetter = function(i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+};
 
-// };
+Hangman.prototype.addWrongLetter = function(letter) {
+  this.errorsLeft -= 1;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkGameOver = function() {
+  if (this.errorsLeft <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.checkWinner = function() {
+  let guess = this.guessedLetter;
+  let secret = this.secretWord;
+  let newStr = "";
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+  for (i = 0; i < guess.length; i++) {
+    newStr = this.secretWord.replace(this.guessedLetter[i], "");
+    if (newStr === secret) {
+      return false;
+    }
+    guess = secret;
+  }
+  // If all the characters have been found in the second string, then return true.
+  return true;
 
-// };
+  // return Array.prototype.every.call(
+  //   this.secretWord,
+  //   function(c) {
+  //     return this.guessedLetter.indexOf(c) > -1;
+  //   },
+  //   this
+  // );
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
+  hangman.secretWord = hangman.getWord();
+  canvas = new HangmanCanvas(hangman.secretWord);
 };
 
-
-document.onkeydown = function (e) {
-
-};
+document.onkeydown = function(e) {};
