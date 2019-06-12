0a8d41c45a2fe3af79ea0e3327d96ab2f38468aa
bryanmej <bryanmej@gmail.com>
2019-06-12 18:55:54 -0500

    first part completed still working

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,70 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+this.words = ['hola' , 'jello', 'ornitorrinco']
+this.secretWord = this.words[2]
+this.letters=[]
+this.guessedLetter = ""
+this.errorsLeft = 10
+}
 
-// }
+Hangman.prototype.getWord = function () {
+return this.secretWord
+};
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+if(keyCode >= 61 && keyCode <= 172){
+return true
+}else{
+return false
+}
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+return this.letters.indexOf(key) < 0;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.addCorrectLetter = function (i) {
+this.guessedLetter += this.secretWord[i].toUpperCase()
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+if(this.guessedLetter != letter) {
+this.errorsLeft--
+}
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+};
 
-// };
+Hangman.prototype.checkGameOver = function (errorsLeft) {
+if(this.errorsLeft == 0) {
+return true
+}else{
+return false
+}
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+};
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+let guess = this.guessedLetter;
+let secret = this.secretWord;
+let Str = "";
+for (i = 0; i < guess.length; i++) {
+Str = this.secretWord.replace(this.guessedLetter[i], "");
+if (Str === secret) {
+return false;
+}
+guess = secret;
+}
+// If all the characters have been found in the second string, then return true.
+return true;
+};
 
 document.getElementById('start-game-button').onclick = function () {
-  hangman = new Hangman();
+hangman = new Hangman();
 };
 
 
 document.onkeydown = function (e) {
+
 
 };
