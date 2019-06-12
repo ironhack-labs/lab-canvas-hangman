7d7bc2587a095a0b7d5557c4edd0fe9b1c234161
bichous <bichous07@gmail.com>
2019-06-12 15:16:37 -0500

    Estamos en la funcion de checar si la letra ya fue escrita

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,20 +1,31 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+ this.words = ["cero", "uno", "dos"];
+ this.secretWord = "";
+ this.letters = [];
+ this.guessedLetter = ""
+ this.errorsLeft = () => {}
 
-// }
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function  ()  {
+rnd = Math.floor(Math.random() * this.words.length)
+//console.log(rnd)
+return this.words[rnd]
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
+if (keyCode > 64 && keyCode < 91) {
+  return false
+}else {
+  return true
+}
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
 
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
+ };
 
 // Hangman.prototype.addCorrectLetter = function (i) {
 
@@ -33,7 +44,7 @@ var hangman;
 // };
 
 document.getElementById('start-game-button').onclick = function () {
-  hangman = new Hangman();
+ hangman = new Hangman();
 };
 
 
