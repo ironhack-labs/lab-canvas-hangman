8c71a2fd1cc710cedd1ad2e3d31daa072d8f8bea
Daniel Orio <danielislas3@live.com>
2019-06-12 15:17:36 -0500

    terminado hasta addCorrectLetter

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,32 +1,50 @@
 var hangman;
 
-// function Hangman() {
+class Hangman {
+constructor(){
 
-// }
 
-// Hangman.prototype.getWord = function () {
+    this.words=["Mango","Sandia","Aasdad"],
+    this.letters=[],
+    this.secretWord="Limon",
+    this.guessedLetter="",
+    this. errorsLeft=10
+  }
+  }
+ 
+Hangman.prototype.getWord = function () {
+ return this.words[Math.floor(Math.random() * this.words.length)].toUpperCase() 
+};
 
-// };
+ Hangman.prototype.checkIfLetter = function (keyCode) {
+  if((keyCode>=65&&keyCode<=90)||(keyCode>=97&&keyCode<=122))return true
+    return false
+ };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
+  if(this.letters.includes(key)) return false
+  this.letters.push(key)
+  return true
+ };
+////////////////////// 
+Hangman.prototype.addCorrectLetter = function (i) {
+ // this.guessedLetter += this.secretWord[i].toUpperCase()
+ let ltUp = i.toUpperCase()
+  this.guessedLetter=this.guessedLetter + this.secretWord.push(i).toUpperCase()
+ // this.guessedLetter.push(i)
+  // if(this.guessedLetter.includes(i)) return true
 
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
+  return false
+  
+ };
 
 // Hangman.prototype.addWrongLetter = function (letter) {
 
 // };
 
-// Hangman.prototype.checkGameOver = function () {
+ Hangman.prototype.checkGameOver = function () {
 
-// };
+ };
 
 // Hangman.prototype.checkWinner = function () {
 
