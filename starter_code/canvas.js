ba7a3cde02bc5c5c44bf26a5e8e447509c9f6708
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2019-06-12 17:52:36 -0500

    PP-FT-MX Diego Garza - Jorge Martinez Jasmine completo falta canvas

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,15 +1,26 @@
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+class HangmanCanvas {
+  constructor(secretWord){
+    this.canvas = document.getElementById('hangman')
+    this.ctx = this.canvas.getContext('2d');
+  }
+  createBoard = () => {
+    this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height)
+    
+  }
+  drawLines = (secretWord) => {
+    let x= 400
+    for(let i = 0; i<secretWord.length; i++){
+        this.ctx.beginPath()
+        this.ctx.moveTo(x, 400)
+        x += 20
+        this.ctx.lineTo(x,400)
+        this.ctx.lineWidth=10
+        x += 10
+        this.ctx.stroke()
+    }
+  }
 }
-
-HangmanCanvas.prototype.createBoard = function () {
-
-};
-
-HangmanCanvas.prototype.drawLines = function () {
-
-};
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
 
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,76 @@
 var hangman;
-
-// function Hangman() {
-
-// }
-
-// Hangman.prototype.getWord = function () {
-
-// };
-
-// Hangman.prototype.checkIfLetter = function (keyCode) {
-
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
+class Hangman {
+constructor(){
+  this.words=['Ironhack','Octocat','Lead Teacher','Teacher Assitant','Ironbeers']
+  this.secretWord= this.words[Math.floor(Math.random()*this.words.length)]
+  this.letters =[]
+  this.guessedLetter = ''
+  this.errorsLeft = 10
+ }
+ getWord = () => {
+  return this.secretWord
+}
+checkIfLetter = (keyCode) =>{
+  if(keyCode > 64 && keyCode < 91 ){
+    return true
+  }
+  else{
+    return false
+  }
+}
+checkClickedLetters = (key) => {
+  let valor = true
+  for(let i = 0; i < this.letters.length; i++){
+    if(this.letters[i]== key){
+      valor = false
+    }
+  }
+  return valor
+}
+checkGameOver = () => {
+  if(this.errorsLeft<=0){
+    return true
+  }
+  else{
+    return false
+  }
+}
+checkWinner = () => {
+  let correctlength = this.secretWord
+  let user = this.guessedLetter
+  let final= 0
+  for (let i = 0; i< user.length ; i++ ){
+    for ( let j = 0 ; j < correctlength.length ; j++){
+      if ( user[i] == this.secretWord[j]){
+          final++
+          correctlength = correctlength.replace(/`${checar}`/g, "")
+          console.log(correctlength)
+      }
+    }
+  }
+  if ( final == correctlength.length){
+    return true
+  }
+  else{
+    return false
+  }
+}
+addCorrectLetter = (i) => {
+  let valor = this.secretWord[i]
+  valor = valor.toLocaleUpperCase()
+  this.guessedLetter = this.guessedLetter + valor
+  this.checkWinner(this.guessedLetter)
+}
+addWrongLetter = (letter) => {
+  this.errorsLeft--
+}
+}
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  let a = hangman.secretWord
+  let b = new HangmanCanvas()
+  b.createBoard()
+  b.drawLines(hangman.secretWord)
 };
 
 
