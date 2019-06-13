17f905937394836547c124ef6e9d49e2fe96615c
bichous <bichous07@gmail.com>
2019-06-12 23:19:37 -0500

    all done whit Jasmine, begin workin on the canvas

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -5,7 +5,7 @@ function Hangman() {
  this.secretWord = "";
  this.letters = [];
  this.guessedLetter = ""
- this.errorsLeft = () => {}
+ this.errorsLeft = 10
 
 }
 
@@ -17,31 +17,43 @@ return this.words[rnd]
 
 Hangman.prototype.checkIfLetter = function (keyCode) {
 if (keyCode > 64 && keyCode < 91) {
-  return false
-}else {
   return true
+}else {
+  return false
 }
 };
 
 Hangman.prototype.checkClickedLetters = function (key) {
-
+  return this.letters.indexOf(key) === -1
  };
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.addCorrectLetter = function (i) {
+return this.guessedLetter += this.secretWord[i].toUpperCase()
+ };
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+if (!this.secretWord.includes(letter)){
+return this.errorsLeft --
+}
+};
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+ Hangman.prototype.checkGameOver = function () {
+ if (this.errorsLeft <= 0){
+   return true
+ } else {
+   return false
+ }
+ };
 
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+//let final = ''
+  for (let i = 0; i < this.secretWord.length; i++) {
+  if (!this.guessedLetter.includes(this.secretWord[i])) {
+    return false
+  }
+    }
+    return true
+ };
 
 document.getElementById('start-game-button').onclick = function () {
  hangman = new Hangman();
