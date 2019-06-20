ce12803231835fe433f1dba371b441e44d7b103f
daxoliveira <dax-oliveira-br@gmail.com>
2019-06-20 16:36:15 -0300

    hangman first iteration completed

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,14 +1,33 @@
-
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+class HangmanCanvas {
+  constructor(secretWord) {
+    this.word = secretWord;
+    this.ctx = document.getElementById('hangman').getContext('2d');
+  }
 }
 
 HangmanCanvas.prototype.createBoard = function () {
+  this.ctx.beginPath();
+  this.ctx.lineWidth = 5;
+  this.ctx.moveTo(0, 790);
+  this.ctx.lineTo(200, 790);
+  this.ctx.lineTo(100, 740);
+  this.ctx.closePath();
+  this.ctx.stroke();
 
-};
+  this.ctx.moveTo(100, 740);
+  this.ctx.lineTo(100, 5);
+  this.ctx.lineTo(500, 5);
+  this.ctx.lineTo(500, 105);
+  this.ctx.stroke();
 
-HangmanCanvas.prototype.drawLines = function () {
-
+  this.ctx.moveTo(250, 790);
+  let posicao = 250;
+  for (let i = 0; i < this.word.length; i += 1) {
+    this.ctx.lineTo(posicao + 80, 790);
+    this.ctx.stroke();
+    posicao += 100;
+    this.ctx.moveTo(posicao, 790);    
+  }
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
@@ -24,7 +43,7 @@ HangmanCanvas.prototype.drawHangman = function (shape) {
 };
 
 HangmanCanvas.prototype.gameOver = function () {
-
+  this.alert('You Lose');
 };
 
 HangmanCanvas.prototype.winner = function () {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,78 @@
 var hangman;
+let canvas;
 
-// function Hangman() {
-
-// }
-
-// Hangman.prototype.getWord = function () {
-
-// };
-
-// Hangman.prototype.checkIfLetter = function (keyCode) {
-
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+class Hangman {
+  constructor () {
+      this.words = ['APPLE', 'BANANA', 'RIDICULO'];
+      this.secretWord = this.getWord();
+      this.letters = [];
+      this.guessedLetter = '';
+      this.errorsLeft = 10;
+  }
+  getWord () {
+    let idx = Math.floor(Math.random() * this.words.length);
+    this.secretWord = this.words[idx];
+    return this.secretWord;
+  }
+  checkIfLetter (key) {
+    console.log(`Voce digitou a ${key}`);
+    if (key >= 65 && key <= 90) {
+      let letra = String.fromCharCode(key).toLocaleUpperCase();
+      this.checkClickedLetters(letra);
+      return true;
+    }
+    return false;
+  }
+  checkClickedLetters (letra) {
+    if (this.letters.includes(letra)) {
+      return false;
+    }
+    if (this.secretWord.includes(letra)) {
+      let posicao = this.secretWord.indexOf(letra);
+      this.addCorrectLetter(posicao);
+      return true;
+    } else {
+      let posicao = this.secretWord.indexOf(letra);
+      this.addWrongLetter(posicao);
+      return true;
+    }
+  }
+  checkWinner(){
+    let wordArr = this.secretWord.split('');
+    let rightArr = this.guessedLetter.split('');
+    for (let i = 0; i < wordArr.length; i += 1) {
+      if (!rightArr.includes(wordArr[i]))
+        return false;
+    }
+    return true;
+  }
+  checkGameOver() {
+    if (!this.errorsLeft > 0) {
+      canvas.gameOver();
+      return true;
+    }
+    return false;
+  }
+  addCorrectLetter(key) {
+    let letraConvertida = this.secretWord[key].toUpperCase();
+    this.letters.push(letraConvertida);
+    this.guessedLetter += letraConvertida;
+    this.checkWinner();
+  }
+  addWrongLetter(key) {
+    let letraConvertida = String.fromCharCode(key);
+    this.letters.push(letraConvertida);
+    if (!this.checkGameOver())
+      this.errorsLeft -= 1;
+  }
+}
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  canvas = new HangmanCanvas(hangman.secretWord);
+  canvas.createBoard();
 };
 
-
 document.onkeydown = function (e) {
-
+  hangman.checkIfLetter(e.keyCode);
 };
