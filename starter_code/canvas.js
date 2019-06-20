5fce3af90a21de9e829553911b76ad96805434f3
Stefanie Cespedes <stefanie.davilaa@gmail.com>
2019-06-20 20:20:40 -0300

    done

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,26 +1,151 @@
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+class HangmanCanvas {
+  constructor(secretWord) {
+   this.cx = document.getElementById('hangman').getContext('2d');
+   this.secretWord = 'dinheiro';
+  }
 }
 
-HangmanCanvas.prototype.createBoard = function () {
+// const connect = new Hangman();
 
+HangmanCanvas.prototype.createBoard = function () {
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(140, 600);
+  this.cx.lineTo(240, 600);
+  this.cx.stroke();
+  this.cx.moveTo(140, 600);
+  this.cx.lineTo(190, 540);
+  this.cx.moveTo(240, 600);
+  this.cx.lineTo(190, 540);
+  this.cx.moveTo(190, 540);
+  this.cx.lineTo(190, 100);
+  this.cx.moveTo(190, 100);
+  this.cx.lineTo(450, 100);
+  this.cx.moveTo(450, 100);
+  this.cx.lineTo(450, 150);
+  this.cx.stroke();
+  this.cx.closePath();
 };
 
 HangmanCanvas.prototype.drawLines = function () {
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(290, 600);
+  this.cx.lineTo(335, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+  
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(350, 600);
+  this.cx.lineTo(395, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(410, 600);
+  this.cx.lineTo(455, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(470, 600);
+  this.cx.lineTo(515, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(530, 600);
+  this.cx.lineTo(575, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(590, 600);
+  this.cx.lineTo(635, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(650, 600);
+  this.cx.lineTo(695, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(710, 600);
+  this.cx.lineTo(755, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(770, 600);
+  this.cx.lineTo(815, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.lineWidth = 2;
+  this.cx.moveTo(830, 600);
+  this.cx.lineTo(875, 600);
+  this.cx.stroke()
+  this.cx.closePath();
+  
 
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
+  // connect.addCorrectLetter(index)
+  // this.cx.textFill()
 };
 
 HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
 
 };
 
-HangmanCanvas.prototype.drawHangman = function (shape) {
+HangmanCanvas.prototype.drawHangman = function () {
+  this.cx.beginPath();
+  this.cx.arc(450, 200, 50, 90, (Math.PI/180) * 90, true);
+  this.cx.stroke();
+  this.cx.closePath();
 
+  this.cx.beginPath();
+  this.cx.moveTo(450, 250);
+  this.cx.lineTo(450, 380);
+  this.cx.stroke();
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.moveTo(450, 250);
+  this.cx.lineTo(400, 300);
+  this.cx.stroke();
+  this.cx.closePath();
+  
+  this.cx.beginPath();
+  this.cx.moveTo(450, 250);
+  this.cx.lineTo(500, 300);
+  this.cx.stroke();
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.moveTo(450, 380);
+  this.cx.lineTo(490, 480);
+  this.cx.stroke();
+  this.cx.closePath();
+
+  this.cx.beginPath();
+  this.cx.moveTo(450, 380);
+  this.cx.lineTo(420, 480);
+  this.cx.stroke();
+  this.cx.closePath();
 };
 
 HangmanCanvas.prototype.gameOver = function () {
@@ -30,3 +155,4 @@ HangmanCanvas.prototype.gameOver = function () {
 HangmanCanvas.prototype.winner = function () {
 
 };
+
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,71 @@
-var hangman;
+class Hangman {
+  constructor (words, secretWord, letters, guessedLetter, errorsLeft) {
+    this.words = ['cachorro', 'gato', 'rato'];
+    this.secretWord = 'dinheiro';
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+  }
 
-// function Hangman() {
+  getWord () {
+    let randomWord = this.words[Math.floor(Math.random()) * this.words.length];
+    return randomWord;
+  }
 
-// }
+  checkIfLetter (num) {
+    if (num >= 65 && num <= 90) {
+      return true;
+    }
+    return false;
+  }
 
-// Hangman.prototype.getWord = function () {
+  checkClickedLetters (key) {
+    if (this.secretWord.includes(key)) {
+      addCorrectLetter(this.secretWord.indexOf(key));
+    }
+    if (this.letters.includes(key)) {
+      return false
+    } else {
+      return true
+    }
+  }
 
-// };
+  addCorrectLetter (i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase()
+  }
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  addWrongLetter (letter) {
+    this.letters.push(letter);
+    return this.errorsLeft -= 1;
+  }
 
-// };
+  checkGameOver () {
+    if (this.errorsLeft === 0) {
+      return true
+    } 
+    return false
+  }
 
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+  checkWinner () {
+    if (this.secretWord.split('').sort().toString() === this.guessedLetter.split('').sort().toString()) {
+      return true
+    }
+    return false
+  }
+}
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  const canvas = new HangmanCanvas();
+  canvas.createBoard();
+  canvas.drawLines();
+  canvas.writeCorrectLetter();
+  canvas.drawHangman();
+  canvas.gameOver();
+  canvas.winner();
 };
 
 
 document.onkeydown = function (e) {
-
+  console.log(hangman.checkIfLetter(e.keyCode));
 };
