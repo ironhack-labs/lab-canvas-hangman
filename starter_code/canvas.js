c4a82f0f56bad22b2230d29a3167c0f7d57fec3b
confidenti-al <steve@confidenti.al>
2019-06-21 10:12:57 -0400

    Sending code before I change it to how Nick is showing it.

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,53 @@
 
-function HangmanCanvas(secretWord) {
+class HangmanCanvas{
+  constructor(secretWord) {
   this.ctx = document.getElementById('hangman').getContext('2d');
+  }
+  
+  createBoard() {
+    
+  };
+  
+  drawLines() {
+    // close the path
+    this.ctx.beginPath();
+ 
+    // BOTTOM TRIANGLE
+    this.ctx.moveTo(200, 400);
+    this.ctx.lineTo(400, 400);
+    this.ctx.stroke();
+    this.ctx.lineTo(300, 350);
+    this.ctx.stroke();
+    this.ctx.lineTo(200, 400);
+    this.ctx.stroke();
+    
+    this.ctx.moveTo(350, 400);
+    this.ctx.lineTo(350, 0);
+    this.ctx.stroke();
+  
+    // close the path
+    this.ctx.closePath();
+    
+  };
+
+writeCorrectLetter(guessedLetter) {
+
+};
+
+writeWrongLetter(letter, errorsLeft) {
+
+};
+
+drawHangman(shape) {
+
+};
+
+gameOver() {
+
+};
+
+winner() {
+
+};
+
 }
-
-HangmanCanvas.prototype.createBoard = function () {
-
-};
-
-HangmanCanvas.prototype.drawLines = function () {
-
-};
-
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
