2b655d2c1db5042320a10c4a2d8cc02d250e33a8
Julia Ramos <juliaramosguedes@gmail.com>
2019-07-14 16:42:06 -0300

    done hangman class

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,49 @@
-var hangman;
+let hangman;
 
-// function Hangman() {
+class Hangman {
+  constructor() {
+    this.words = ['teste', 'teto', 'cachorro', 'dinheiro', 'treta'];
+    this.secretWord = '';
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+  }
 
-// }
+  getWord() {
+    this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
+    return this.secretWord;
+  }
 
-// Hangman.prototype.getWord = function () {
+  checkIfLetter(letter) {
+    return letter > 64 && letter < 91 ? true : false;
+  }
 
-// };
+  // checkClickedLetters. Checks if the pressed letter has already been pressed and returns true if it was not or false in the opposite case.
+  checkClickedLetters(letter) {
+    return !this.letters.includes(letter) ? true : false;
+  }
+  
+  addCorrectLetter(letter) {
+    this.guessedLetter += this.secretWord[letter].toUpperCase()
+    this.checkGameOver()
+  }
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  // addWrongLetter. Should subtract one from the variable errorsLeft and check if the game is over.
+  addWrongLetter() {
+    this.errorsLeft--
+    this.checkGameOver()
+  }
 
-// };
+  // checkGameOver. Returns a boolean value, true if the users lose, and false in any other case.
+  checkGameOver() {
+    return this.errorsLeft === 0 ? true : false
+  }
 
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+// checkWinner. Check if the users win and return a boolean value.
+  checkWinner() {
+    return this.secretWord.length === this.guessedLetter.length ? true : false
+  }
+}
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
@@ -40,3 +53,7 @@ document.getElementById('start-game-button').onclick = function () {
 document.onkeydown = function (e) {
 
 };
+
+// const teste = new Hangman()
+// let result = teste.checkIfLetter('d')
+// console.log(result)
