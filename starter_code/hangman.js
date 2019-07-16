56f19f516187e8332528da744f9434fe4d25ac49
Julia Ramos <juliaramosguedes@gmail.com>
2019-07-16 07:59:42 -0300

    done start game button function

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,4 +1,4 @@
-let hangman;
+let hangman, canvas;
 
 class Hangman {
   constructor() {
@@ -8,16 +8,16 @@ class Hangman {
     this.guessedLetter = '';
     this.errorsLeft = 10;
   }
-
+  
   getWord() {
     this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
     return this.secretWord;
   }
-
+  
   checkIfLetter(letter) {
     return letter > 64 && letter < 91 ? true : false;
   }
-
+  
   // checkClickedLetters. Checks if the pressed letter has already been pressed and returns true if it was not or false in the opposite case.
   checkClickedLetters(letter) {
     const notInclude = !this.letters.includes(letter)
@@ -29,13 +29,13 @@ class Hangman {
     this.guessedLetter += this.secretWord[letter].toUpperCase()
     this.checkGameOver()
   }
-
+  
   // addWrongLetter. Should subtract one from the variable errorsLeft and check if the game is over.
   addWrongLetter() {
     this.errorsLeft--
     this.checkGameOver()
   }
-
+  
   // checkGameOver. Returns a boolean value, true if the users lose, and false in any other case.
   checkGameOver() {
     return this.errorsLeft === 0 ? true : false
@@ -49,11 +49,29 @@ class Hangman {
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  canvas = new HangmanCanvas(hangman.getWord())
+  canvas.createBoard()
+  canvas.drawLines()
 };
 
 
 document.onkeydown = function (e) {
+  const letterClicked = e.key
+  const letterKeyCode = e.which
+  
+  if (hangman.checkIfLetter(letterKeyCode)) {
+    if (hangman.checkClickedLetters()) {
 
+    } else {
+      alert('Repeated letter. Enter a diferent letter.')
+    }
+
+  } 
+  // else {
+  //   alert('Enter a letter.')
+  // }
+
+  console.log()
 };
 
 // const teste = new Hangman()
