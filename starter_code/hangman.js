8504190326901d30d9d209cd9c7d343ed8406845
Dayana Mick <dayanamick@gmail.com>
2019-07-20 15:58:40 +0200

    First Iteration

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,65 @@
-var hangman;
+let hangman;
 
-// function Hangman() {
 
-// }
 
-// Hangman.prototype.getWord = function () {
+class Hangman {
+  constructor(words) {
+    this.words = ['test', 'test2', 'test3']
+    this.secretWord = ''
+    this.letters = []
+    this.guessedLetter = ''
+    this.errorsLeft = 10
+  }
 
-// };
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)]
+  }
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  checkIfLetter(keyCode) {
+    if (keyCode > 64 && keyCode < 91) {
+      return true
+    } else {
+      return false
+    }
+  }
 
-// };
+  checkClickedLetters(letter) {
+    if (this.letters.includes(letter)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+  addCorrectLetter(positionInWord) {
+    this.guessedLetter += this.secretWord[positionInWord].toUpperCase()
+  }
 
-// };
+  checkWinner() {
+    return this.guessedLetter.split('').sort().join('') === this.secretWord.split('').sort().join('')
+  }
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+  addWrongLetter() {
+    this.errorsLeft -= 1
+  }
 
-// };
+  checkGameOver() {
+    if (this.errorsLeft > 0) {
+      return false
+    } else {
+      return true
+    }
+  }
+}
 
-// Hangman.prototype.addWrongLetter = function (letter) {
 
-// };
 
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
 };
 
 
-document.onkeydown = function (e) {
+document.onkeydown = function (event) {
 
 };
