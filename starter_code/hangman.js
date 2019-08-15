ec6568870b9f08de6438467b1915952f6211f27d
angelmc32 <angel.melendez.c@gmail.com>
2019-08-15 06:54:49 -0500

    Finished game logic

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -2,13 +2,16 @@ var hangman;
 
 class Hangman {
   constructor() {
-    this.words = ["filler", "crap", "bullshit"];
-    this.secretWord = "shit";
+    this.words = ["archaeopteryx", "renaissance", "encapsulation", "javascript", "retrospective"];
+    this.secretWord = "";
     this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
   }
-
+  /*
   getWord() {
-    return "hola";
+    let random = Math.floor(Math.random()*this.words.length);
+    return this.words[random];
   }
 
   checkIfLetter(keyCode) {
@@ -22,35 +25,91 @@ class Hangman {
     }
     return true;
   }
+
+  addCorrectLetter(key){
+    return this.guessedLetter = this.guessedLetter.concat(this.secretWord[key].toUpperCase());
+  }
+
+  addWrongLetter(letter) {
+    this.errorsLeft--
+  }
+
+  checkGameOver() {
+    if( this.errorsLeft === 0 ) return true;
+    return false;
+  }
+
+  checkWinner() {
+    let uniqueLetters = this.uniqueCharacters(this.secretWord);
+    for ( let i = 0 ; i < this.guessedLetter.length ; i++ ) {
+      if ( !this.isPresent( this.guessedLetter[i] , this.secretWord ) ) return false;
+    }
+    if ( uniqueLetters.length !== this.guessedLetter.length ) return false;
+    return true;
+  }
+
+  uniqueCharacters(array) {
+    return array.split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('');
+  }
+
+  isPresent(letter, array) {
+    for ( let i = 0 ; i < array.length ; i++ ) {
+      if ( letter === array[i] ) return true;
+    }
+    return false;
+  }
+  */
 }
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  let random = Math.floor(Math.random()*this.words.length);
+  return this.words[random];
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode > 64 && keyCode < 91) return true;
+  return false;
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) {
+  for (let i = 0; i < this.letters.length; i++) {
+    if (this.letters[i] === key) return false;
+  }
+  return true;
+};
 
-// };
+Hangman.prototype.addCorrectLetter = function (i) {
+  return this.guessedLetter = this.guessedLetter.concat(this.secretWord[i].toUpperCase());
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addWrongLetter = function (letter) {
+  this.errorsLeft--
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+  if( this.errorsLeft === 0 ) return true;
+  return false;
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkWinner = function () {
+  let uniqueLetters = this.uniqueCharacters(this.secretWord);
+  for ( let i = 0 ; i < this.guessedLetter.length ; i++ ) {
+    if ( !this.isPresent( this.guessedLetter[i] , this.secretWord ) ) return false;
+  }
+  if ( uniqueLetters.length !== this.guessedLetter.length ) return false;
+  return true;
+};
 
-// };
+Hangman.prototype.uniqueCharacters = function(array) {
+  return array.split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('');
+}
 
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.isPresent = function(letter, array) {
+  for ( let i = 0 ; i < array.length ; i++ ) {
+    if ( letter === array[i] ) return true;
+  }
+  return false;
+}
 
 document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
