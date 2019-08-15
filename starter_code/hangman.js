9b2b2ba56ebc9fba742a6fec4a479f4a4edd0c13
Ernesto <02ernesto.rodriguez@gmail.com>
2019-08-15 09:36:08 -0500

    Rodrigo ahi va el avance, ojala alcances a verlo :)

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,53 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+  this.words = ['ayuntamiento', 'murcielago', 'telefono', 'pizza', 'camion'];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = '';
+  this.errorsLeft = 10;
+ }
 
-// }
+Hangman.prototype.getWord = function () {
+var maximo = this.words.length;
 
-// Hangman.prototype.getWord = function () {
+	var randomWordIndex = Math.floor(Math.random() * maximo);
+	this.secretWord = this.words[randomWordIndex];
+	return this.secretWord;
+};
 
-// };
+ Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90 || keyCode >= 97 && keyCode <= 122) {
+     return true;
+  } else {
+      return false;
+  }
+};
+ Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.indexOf(key) !== -1) {
+		return false;
+	} else {
+		return true;
+	}
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+ };
 
-// };
+ Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i].toUpperCase();
+  
+ };
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+ Hangman.prototype.addWrongLetter = function (letter) {
 
-// };
+ };
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+ Hangman.prototype.checkGameOver = function () {
 
-// };
+ };
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+ Hangman.prototype.checkWinner = function () {
 
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+ };
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
