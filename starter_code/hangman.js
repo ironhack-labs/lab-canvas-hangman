0fbbe6b5e259e9fd49a4ec52e0870169586d09e4
Giseliux <giseliuxgasca@gmail.com>
2019-08-22 01:13:23 -0500

    Aun estoy interpretando el còdigo y el objetivo de cada una de las funciones del juego

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,36 +1,38 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
 
-// }
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
 
-// };
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkIfLetter = function (keyCode) {
 
-// };
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.checkClickedLetters = function (key) {
 
-// };
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.addCorrectLetter = function (i) {
 
-// };
+};
 
-// Hangman.prototype.addWrongLetter = function (letter) {
+Hangman.prototype.addWrongLetter = function (letter) {
 
-// };
+};
 
-// Hangman.prototype.checkGameOver = function () {
+Hangman.prototype.checkGameOver = function () {
 
-// };
+};
 
-// Hangman.prototype.checkWinner = function () {
+Hangman.prototype.checkWinner = function () {
 
-// };
+  return true
+
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
