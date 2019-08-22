5d4012d14f26c1b2dcc18545d30add4bb2ec9285
Daniel Garcia <jdanielink@gmail.com>
2019-08-22 00:49:45 -0500

    done... kinda

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,107 @@
-var hangman;
+let hangman;
+const words = [
+  'response',
+  'excavate',
+  'minority',
+  'describe',
+  'buttocks',
+  'campaign',
+  'proclaim',
+  'question',
+  'displace',
+  'slippery',
+  'necklace',
+  'complete',
+  'sandwich',
+  'generate',
+  'economic',
+  'detector',
+  'feminist',
+  'particle',
+  'baseball',
+  'property'
+];
 
-// function Hangman() {
+class Hangman {
+  constructor() {
+    this.words = words;
+    this.secretWord = '';
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+  }
 
-// }
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)].toUpperCase();
+  }
 
-// Hangman.prototype.getWord = function () {
+  checkIfLetter(keyCode) {
+    if ((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)) return true;
+    return false;
+  }
 
-// };
+  checkClickedLetters(key) {
+    return !this.letters.includes(key);
+  }
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    this.checkWinner();
+  }
 
-// };
+  addWrongLetter(letter) {
+    this.errorsLeft--;
+    this.checkGameOver();
+  }
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+  checkGameOver() {
+    if (this.errorsLeft === 0) return true;
+    return false;
+  }
 
-// };
+  checkWinner() {
+    for (var j = 0; j < this.secretWord.length; j++) {
+      if (this.guessedLetter.indexOf(this.secretWord[j]) === -1) return false;
+    }
+    return true;
+  }
+}
 
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
+document.getElementById('start-game-button').onclick = function() {
   hangman = new Hangman();
+  hangman.secretWord = hangman.getWord();
+  hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  hangmanCanvas.createBoard();
+  hangmanCanvas.drawLines();
 };
 
+document.onkeydown = function(e) {
+  if (hangman) {
+    if (hangman.checkIfLetter(e.keyCode)) {
+      let up = e.key.toUpperCase();
+      if (hangman.checkClickedLetters(up)) {
+        hangman.letters.push(up);
+        if (hangman.secretWord.indexOf(up) >= 0) {
+          hangmanCanvas.writeCorrectLetter(hangman.secretWord.indexOf(up));
+          hangman.addCorrectLetter(hangman.secretWord.indexOf(up));
+          if (hangman.checkWinner()) {
+            hangmanCanvas.winner();
+          }
+        } else {
+          if (hangman.checkGameOver()) {
+            return hangmanCanvas.gameOver();
+          }
+          hangman.addWrongLetter();
+          hangmanCanvas.writeWrongLetter(up, hangman.errorsLeft);
+        }
+      }
+    }
+  }
+};
 
-document.onkeydown = function (e) {
-
+document.onclick = () => {
+  if (hangman.checkGameOver() || hangman.checkWinner()) {
+    document.getElementById('starting').style.display = '';
+    document.getElementById('game').style.display = 'none';
+  }
 };
