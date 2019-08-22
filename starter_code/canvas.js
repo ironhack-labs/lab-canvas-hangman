beb01ec19091d69d1ff7de0c1e1bda0edc67aba1
Tania Marin <nicte.tania@gmail.com>
2019-08-22 09:05:20 -0500

    update

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -6,33 +6,46 @@ function HangmanCanvas(secretWord) {
 }
 
 HangmanCanvas.prototype.createBoard = function () {
-  this.ctx.clearRect(0, 0, 1200, 800)
+  this.ctx.clearRect(0, 0, 600, 400)
   this.ctx.lineWidth = 4
   this.ctx.strokeStyle = 'black'
 
 };
 
 HangmanCanvas.prototype.drawLines = function () {
-  let yPosition = 450
-  let xPosition = 50
+  let yPosition = 300
+  let xPosition = 25
 
   for  (i = 0; i < this.secretWord.length; i++) {
    this.ctx.beginPath()
    this.ctx.moveTo(xPosition, yPosition) 
-   this.ctx.lineTo(xPosition+25, yPosition)   
+   this.ctx.lineTo(xPosition+30, yPosition)   
    this.ctx.stroke()
    this.ctx.closePath()
 
-   xPosition +=30
+   xPosition +=35
   }
 
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
+  let yPosition = 290
+  let xPosition = 25+(index*35) 
+   this.ctx.fillStyle = 'black'
+   this.ctx.font = '40px Courier'
+   this.ctx.fillText(this.secretWord[index], xPosition, yPosition)
+  
 };
 
 HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
+  let yPosition = 30
+  let xPosition = 180+(155-(errorsLeft*35)) 
+
+   this.ctx.fillStyle = 'red'
+   this.ctx.font = '40px Courier'
+   this.ctx.fillText(letter, xPosition, yPosition)
+   yPosition = 70
+   this.ctx.fillText(errorsLeft, xPosition, yPosition)
 
 };
 
@@ -41,9 +54,15 @@ HangmanCanvas.prototype.drawHangman = function (shape) {
 };
 
 HangmanCanvas.prototype.gameOver = function () {
-
+  let looserI = new Image();
+  looserI.src = './images/gameover.png'
+  this.ctx.clearRect(0, 0, 600, 400)
+  this.ctx.drawImage(looserI, 50, 0, 500, 400)
 };
 
 HangmanCanvas.prototype.winner = function () {
-
+  let winnerI = new Image();
+  winnerI.src = './images/awesome.png'
+  this.ctx.clearRect(0, 0, 600, 400)
+  this.ctx.drawImage(winnerI, 50, 0, 500, 400)
 };
