cd7bc0e9560988ed44e19c4687b30751783c3258
Larry Nobles <larrynobles@Larrys-MacBook-Pro.local>
2019-08-22 06:34:23 -0500

    hangman code iteration 1

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,14 +1,32 @@
 
 function HangmanCanvas(secretWord) {
   this.ctx = document.getElementById('hangman').getContext('2d');
+  this.secretWord = secretWord
 }
 
 HangmanCanvas.prototype.createBoard = function () {
+  console.log('Clear Board')
+  this.ctx.clearRect(0, 0, 1200, 800)
+  this.ctx.lineWidth = 8;
+  this.ctx.strokeStyle = 'black';
 
-};
+}
 
 HangmanCanvas.prototype.drawLines = function () {
+  console.log('Draw Lines')
+let x = 100
+let y = 700
 
+
+for(let i = 0; i <this.secretWord.length; i++)  {
+  this.ctx.beginPath()
+  this.ctx.moveTo(x , y)
+  this.ctx.lineTo(x + 60 , y)
+  this.ctx.stroke()
+  this.ctx.closePath()
+  x = x + 85 
+ // console.log(x,y)
+}
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,99 @@
 var hangman;
 
-// function Hangman() {
+ function Hangman() {
+   this.words = ['today', 'banana', 'universe'];
+   this.secretWord = '';
+   this.letters = [];
+   this.guessedLetter = '';
+   this.errorsLeft = 10;
 
-// }
+ }
 
-// Hangman.prototype.getWord = function () {
+// Returns a random word from an array
+Hangman.prototype.getWord = function () {
+  return this.words[Math.floor(Math.random() * this.words.length)].toUpperCase()
+ };
 
-// };
+ //This function should check if the key (via keyCode) the user has typed is a letter.  Returns true if it is a letter.
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  if (keyCode >= 65 && keyCode <= 90) {
+    this.letters.push(keyCode)
+    return true
+  } else {
+    return false
+  }
+ };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+ //This function checks if the pressed letter has already been pressed and returns true if the was not or false in the opposite case.
+Hangman.prototype.checkClickedLetters = function (key) {
+  if (this.letters.includes(key)) {
+    return false
+  } else {
+    return true
+  }
+};
 
-// };
+//This function adds to the guessedLetter variable letter that was pressed.  Also, it should check if the user winns.
+Hangman.prototype.addCorrectLetter = function (i) {
+  this.guessedLetter += this.secretWord[i]
+  this.checkWinner()
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addWrongLetter = function (letter) {
+    this.errorsLeft--;
+    this.checkGameOver()
+};
 
-// };
+Hangman.prototype.checkGameOver = function () {
+  if(this.errorsLeft === 0){
+    return true
+  } else {
+    return false
+  }
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkWinner = function () {
+  if (this.guessedLetter.length === this.secretWord.length ) {
+    return true
+  } else {
+    return false
+  }
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+};
 
 document.getElementById('start-game-button').onclick = function () {
+  console.log('Hello')
   hangman = new Hangman();
+  hangman.secretWord = hangman.getWord();
+  hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  hangmanCanvas.createBoard()
+  hangmanCanvas.drawLines()
+  console.log(hangman)
+  console.log(hangmanCanvas)
 };
 
 
 document.onkeydown = function (e) {
-
+ // console.log(e,e.keyCode)
+  if (hangman.checkIfLetter(e.keyCode)) {
+    let upCase = e.key.toUpperCase()
+ //   console.log(upCase)
+    if (hangman.checkClickedLetters(upCase)) {
+      hangman.letters.push(upCase)
+      console.log(hangman.letters)
+      if (hangman.secretWord.indexOf(upCase) >= 0) {
+        console.log('You Done Good')
+        hangman.checkWinner()
+        console.log(hangman)
+      } else {
+        hangman.addWrongLetter
+        console.log('Wrong Letter')
+        console.log(hangman.errorsLeft)
+      }
+    } else {
+      e = prompt('The letter has been selected please select another alphabet key')
+    }
+  } else {
+    e = prompt('Please select another alphabet key')
+  }
 };
