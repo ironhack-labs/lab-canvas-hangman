57f9ef241f63d7c1a250a1284b274b760db45b09
Tania Marin <nicte.tania@gmail.com>
2019-08-21 22:58:28 -0500

    mini avance jaja :(

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,13 +1,30 @@
 
 function HangmanCanvas(secretWord) {
   this.ctx = document.getElementById('hangman').getContext('2d');
+  this.secretWord = secretWord
+  this.start = 0
 }
 
 HangmanCanvas.prototype.createBoard = function () {
+  this.ctx.clearRect(0, 0, 1200, 800)
+  this.ctx.lineWidth = 4
+  this.ctx.strokeStyle = 'black'
 
 };
 
 HangmanCanvas.prototype.drawLines = function () {
+  let yPosition = 450
+  let xPosition = 50
+
+  for  (i = 0; i < this.secretWord.length; i++) {
+   this.ctx.beginPath()
+   this.ctx.moveTo(xPosition, yPosition) 
+   this.ctx.lineTo(xPosition+25, yPosition)   
+   this.ctx.stroke()
+   this.ctx.closePath()
+
+   xPosition +=30
+  }
 
 };
 
