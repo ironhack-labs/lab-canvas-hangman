7bfbb2e24e460f9a2b50d3cd4cfd7109d35909a4
Jose Octavio de Castro Neves Jr <joc@fullsquad.com.br>
2019-09-12 15:25:30 -0300

    es5 prototype to es6 class syntax sugar

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,36 @@
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+class HangmanCanvas {
+  constructor(secretWord) {
+    this.ctx = document.getElementById('hangman').getContext('2d');
+    
+  }
+
+  createBoard() {
+
+  }
+
+  drawLines() {
+
+  }
+
+  writeCorrectLetter(index) {
+
+  }
+
+  writeWrongLetter(letter, errorsLeft) {
+
+  }
+
+  drawHangman(shape) {
+
+  }
+
+  gameOver() {
+
+  }
+
+  winner() {
+
+  }
+
 }
-
-HangmanCanvas.prototype.createBoard = function () {
-
-};
-
-HangmanCanvas.prototype.drawLines = function () {
-
-};
-
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
