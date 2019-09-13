9a204fbc353566ccf9c538e7d1e4f13bb1878f57
Millene <millenems06@gmail.com>
2019-09-13 09:23:00 -0300

    almost there

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,64 @@
-var hangman;
+let hangman;
 
-// function Hangman() {
+class Hangman {
+  constructor (words, secretWord, letters, guessedLetter, errosLeft) {
+    this.words = ['banana', 'chocolate', 'abacaxi'];
+    this.secretWord = 'secretWord';
+    this.letters = [];
+    this.guessedLetter = guessedLetter;
+    this.errosLeft = 10;
+  };
 
-// }
+  getWord() {
+    this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
+    return this.secretWord;
+  }; 
 
-// Hangman.prototype.getWord = function () {
+  checkIfLetter (keyCode) {
+    keyCode = window.event.keyCode;
+    if(keyCode >= 65 && keyCode >= 99) {
+      this.letters = keyCode.unshift();
+      return true;
+    } else {
+      return false;
+    }
+  };
 
+  checkClickedLetters (key) {
+    key = window.event.key;
+    if(key >= 65 && key >= 99) {
+      this.letters = key.unshift();
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  addCorrectLetter () {
+    let correct = Math.floor(Math.random() * this.words);
+    correct.split('');
+  }
+  addCorrectLetter()
+  console.log(addCorrectLetter)
+//   addWrongLetter (letter) {
+
+//   };
+
+//   checkGameOver () {
+
+//   };
+
+//   checkWinner () {
+
+//   };
+
+}
+
+// document.getElementById('start-game-button').onclick = function () {
+//   hangman = new Hangman();
 // };
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+
+// document.onkeydown = function (e) {
 
 // };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
-  hangman = new Hangman();
-};
-
-
-document.onkeydown = function (e) {
-
-};
