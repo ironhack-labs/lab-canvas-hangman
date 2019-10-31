15e9cdadf7ec43d9d92016183dab4d3c289b53f7
pabloPeralta <paps21.gol@gmail.com>
2019-10-31 02:25:47 -0600

    not done =(

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,20 +1,34 @@
 
+let ctx  = document.getElementById('hangman').getContext('2d');
+
+let hangman = new Hangman();
+
 class HangmanCanvas {
   constructor(secretWord) {
-    this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    //this.ctx = document.getElementById('hangman').getContext('2d');
   }
 
   createBoard() {
-
+    this.cxt.clearRect(0, 0, this.canvas.width, this.canvas.height)
   }
 
-  drawLines() {
+  drawLines() { 
 
+    ctx.beginPath();
+    for (var y = 0; y < Hangman.addCorrectLetter(i); y += 1) {
+        addLineSubPath(ctx, y);
+    }
+    ctx.stroke();
+
+function addLineSubPath(ctx, y) {
+    ctx.moveTo(y*30, 0);
+    ctx.lineTo(y*30+25, 0);
+}
+    
   }
 
   writeCorrectLetter(index) {
-
+    
   }
 
   writeWrongLetter(letter, errorsLeft) {
@@ -22,7 +36,71 @@ class HangmanCanvas {
   }
 
   drawHangman(shape) {
+       //base triangulo
+ctx.beginPath();
+ctx.moveTo(290, 650);
+ctx.lineTo(400, 750);
+ctx.lineTo(180, 750);
+ctx.lineTo(290, 650);
+ctx.stroke();
+ctx.closePath();
 
+//linea 
+ctx.beginPath();
+ctx.moveTo(290, 650);
+ctx.lineTo(290, 200);
+ctx.stroke();
+ctx.closePath();
+
+//linea horizontal 
+ctx.beginPath();
+ctx.moveTo(290, 200);
+ctx.lineTo(500, 200);
+ctx.stroke();
+ctx.closePath();
+
+///vertical
+ctx.beginPath();
+ctx.moveTo(500, 200);
+ctx.lineTo(500, 250);
+ctx.stroke();
+ctx.closePath();
+
+//cabeza
+ctx.beginPath();
+ctx.arc(500, 300, 50, 0, 2 * Math.PI, true);
+ctx.stroke();
+ctx.closePath();
+//cuerpo
+ctx.beginPath();
+ctx.moveTo(500, 350);
+ctx.lineTo(500, 550);
+ctx.stroke();
+ctx.closePath();
+//brazoIzq
+ctx.beginPath();
+ctx.moveTo(500, 380);
+ctx.lineTo(450, 480);
+ctx.stroke();
+ctx.closePath();
+//brazoDer
+ctx.beginPath();
+ctx.moveTo(500, 380);
+ctx.lineTo(550, 480);
+ctx.stroke();
+ctx.closePath();
+//piernaIzq
+ctx.beginPath();
+ctx.moveTo(500, 550);
+ctx.lineTo(450, 680);
+ctx.stroke();
+ctx.closePath();
+//piernaDer
+ctx.beginPath();
+ctx.moveTo(500, 550);
+ctx.lineTo(550, 680);
+ctx.stroke();
+ctx.closePath();
   }
 
   gameOver() {
@@ -34,3 +112,7 @@ class HangmanCanvas {
   }
 
 }
+
+
+
+
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,63 @@
-let hangman;
+ class Hangman {
+   constructor(words, secretWord, letters, guessedLetter, errorsLeft) {
+    this.words = words;
+    this.secretWord = '' ;
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+    this.words  = ['casa', 'arbol', 'agua', 'caja'];
+   }
 
-// class Hangman {
-//   constructor() {
+   getWord() {
+    this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
+    //let separacion = rand.split('');
+    return this.secretWord;
+   }
 
-//   }
+   checkIfLetter(keyCode) {
+    /*lo busqué en stack =( no sabia que como validar el keycode*/
+    if(keyCode >= 65 && keyCode <= 90 || keyCode>=97 && keyCode<=122){
+      return true;
+    }else{
+      return false;
+    }
+   }
 
-//   getWord() {
+   checkClickedLetters(key) {
+    if(this.letters.includes(key)){
+      return false;
+    }else {
+      return true;
+    }
+   }
 
-//   }
+   addCorrectLetter(i) {
+    //addCorrectLetter. Adds to the guessedLetter variable the letter that was pressed. Also, it should check if the user wins.
+    let separacion = this.secretWord.split('');
+    this.guessedLetter += separacion[i].toLocaleUpperCase() ;
+    
+   }
 
-//   checkIfLetter(keyCode) {
+   addWrongLetter(letter) {
+    //Should subtract one from the variable errorsLeft and check if the game is over.
+    this.errorsLeft -= 1;
+    
+   }
 
-//   }
-
-//   checkClickedLetters(key) {
-
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
+   checkGameOver() {
+    ///¡¡?????
+    if(this.errorsLeft === 0){
+      return true;
+    }else {
+      return "perdiste";
+    }
+   }
 
 //   checkWinner() {
-
+      ////?????¡¡¡¡¡
 //   }
 
-// }
+ }
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
