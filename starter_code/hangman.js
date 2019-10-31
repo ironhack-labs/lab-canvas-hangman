1174ca59cf9df6a477087deaedcab474de3865ad
RicardoSalas17 <ricardosalas.1716@gmail.com>
2019-10-31 00:10:11 -0600

    incompleto

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,8 +1,46 @@
 let hangman;
+function Hangman() {
+  this.words = ["HOLA", "JAVASCRIPT", "RICARDO", "BATMAN", "IRONHACK", "HACK"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetter = [];
+  this.errorsLeft = 5;
+}
+Hangman.prototype.getWord = function() {
+  var word = this.words[Math.floor(Math.random() * this.words.length)];
+  return word;
+};
+Hangman.prototype.checkIfLetter = function(keyCode) {
+  if (keyCode >= 65 && keyCode <= 90) {
+    return true;
+  } else {
+    return false;
+  }
+};
+Hangman.prototype.checkClickedLetters = function(key) {
+  var clicked = this.letters.indexOf(key);
+  if (clicked === -1) return true;
+  else return false;
+};
+Hangman.prototype.addLetter = function(key) {
+  if (this.checkClickedLetters(key)) {
+    this.letters.push(key);
+  }
+};
 
+addCorrectLetter(i) {
+  
+
+}
 // class Hangman {
-//   constructor() {
-
+//   // constructor() {
+//   function Hangman() {
+//     this.words = ["HOLA", "JAVASCRIPT", "RICARDO", "BATMAN", "IRONHACK", "HACK"];
+//     this.secretWord = "";
+//     this.letters = [];
+//     this.guessedLetter = [];
+//     this.errorsLeft = 5;
+//   }
 //   }
 
 //   getWord() {
@@ -17,9 +55,6 @@ let hangman;
 
 //   }
 
-//   addCorrectLetter(i) {
-
-//   }
 
 //   addWrongLetter(letter) {
 
@@ -35,10 +70,10 @@ let hangman;
 
 // }
 
-document.getElementById('start-game-button').onclick = () => {
-  hangman = new Hangman();
-};
+// document.getElementById('start-game-button').onclick = () => {
+//   hangman = new Hangman();
+// };
 
-document.onkeydown = (e) => {
+// document.onkeydown = (e) => {
 
-};
+// };
