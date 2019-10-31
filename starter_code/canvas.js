eef856b1a68c8d324f8dc4798aebd47134709120
EChColorado <edxz7c@gmail.com>
2019-10-31 04:04:24 -0600

    Jasmine Done, the GUI done, 65% on the game logic

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,18 +2,45 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
+    this.secretWord = secretWord;
     
   }
 
   createBoard() {
-
+    let canvas = document.querySelector('#hangman')
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
 
   drawLines() {
-
+    let nLines = this.secretWord.length;
+    let shift = 20;
+    let lineWidth = 50
+    let start = 400;
+    this.ctx.beginPath(); 
+    for(let i=0; i<= nLines; i++){ 
+      this.ctx.moveTo(start , 700);
+      this.ctx.lineTo(start + lineWidth, 700);
+      start += shift + lineWidth
+    }
+    this.ctx.stroke();
+    this.ctx.closePath();
+    
   }
 
   writeCorrectLetter(index) {
+    let nLines = this.secretWord.length;
+    let shift = 20;
+    let lineWidth = 50
+    let start = 400;
+    this.ctx.font = "50px Arial";
+    this.ctx.fillStyle ='black'
+    for(let i=0; i<= nLines; i++){ 
+      if (this.secretWord[i] === "s"){
+        this.ctx.fillText('A', start + 10, 680);
+      }
+      start += shift + lineWidth
+    }
 
   }
 
@@ -22,15 +49,92 @@ class HangmanCanvas {
   }
 
   drawHangman(shape) {
+    //base
+    this.ctx.beginPath(); 
+    this.ctx.moveTo(225, 650);
+    this.ctx.lineTo(150, 700);
+    this.ctx.lineTo(300, 700);
+    this.ctx.lineTo(225, 650);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //main post
+    this.ctx.beginPath(); 
+    this.ctx.lineTo(225, 650);
+    this.ctx.lineTo(225, 100);
+
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //main rigth post
+    this.ctx.beginPath();
+    this.ctx.lineTo(225, 100); 
+    this.ctx.lineTo(600, 100);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //down post
+    this.ctx.beginPath();
+    this.ctx.lineTo(600, 100); 
+    this.ctx.lineTo(600, 200);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //head
+    this.ctx.beginPath();
+    this.ctx.arc(600, 250, 50, 0, 2 * Math.PI, true);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //body
+    this.ctx.beginPath();
+    this.ctx.lineTo(600, 300); 
+    this.ctx.lineTo(600, 500);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //Rigth leg
+    this.ctx.beginPath();
+    this.ctx.lineTo(600, 500); 
+    this.ctx.lineTo(700, 600);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //Left leg
+    this.ctx.beginPath();
+    this.ctx.lineTo(600, 500); 
+    this.ctx.lineTo(500, 600);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //Rigth arm
+    this.ctx.beginPath();
+    this.ctx.lineTo(600, 350); 
+    this.ctx.lineTo(700, 400);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //Left arm
+    this.ctx.beginPath();
+    this.ctx.lineTo(600, 350); 
+    this.ctx.lineTo(500, 400);
+    this.ctx.stroke();
+    this.ctx.closePath();
 
   }
 
   gameOver() {
-
+    let canvas = document.querySelector('#hangman')
+    const img = new Image();
+    img.src = 'images/gameover.png'
+    this.ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
   }
 
   winner() {
-
+    let canvas = document.querySelector('#hangman')
+    const img = new Image();
+    img.src = 'images/awesome.png'
+    this.ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
   }
 
 }
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,71 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words=['dog','cat','eggs','plateau'];           // An array where we will store all the words that the player need to guess. We will take one of them randomly.
+    this.secretWord = '';    //Here we will store the word chosen for each game.
+    this.letters = [];       //An array to store the letters the user already clicked, so we do not repeat them.
+    this.guessedLetter = ''; //A string to store the letters the user clicked and guessed. We will use this to know when the user wins.
+    this.errorsLeft = 10;    //It should start at 10, and decrease every time a user clicks on a letter that is not in the word.
+  }
 
-//   }
+  getWord() {
+    //Returns a random word from our array words.
+    return this.words[Math.floor(Math.random()*this.words.length)]
+  }
+  
+  checkIfLetter(keyCode) {
+    //This function should check if the key the user has typed is a letter.
+    if(typeof(keyCode)==='number'){
+      return (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)
+    }
+    return false
+  }
 
-//   getWord() {
+  checkClickedLetters(key) {
+    //Checks if the pressed letter has already been pressed and returns true if it was not or false in the opposite case.
+    return !this.letters.includes(key)
+  }
 
-//   }
+  addCorrectLetter(i) {
+    //Adds to the guessedLetter variable the letter that was pressed. Also, it should check if the user wins.
+    let secretWordSplit = this.secretWord.split('');
+    this.guessedLetter += secretWordSplit[i].toUpperCase();
+  }
 
-//   checkIfLetter(keyCode) {
+  addWrongLetter(letter) {
+    //Should subtract one from the variable errorsLeft and check if the game is over.
+    let ch = String.fromCharCode(letter)
+    if(!this.secretWord.includes(ch) && !this.letters.includes(ch) ){
+      this.errorsLeft--;
+      this.letters.push(ch)
+    }
+  }
 
-//   }
+  checkGameOver() {
+    //Returns a boolean value, true if the users lose, and false in any other case
+    return this.errorsLeft === 0;
+  }
 
-//   checkClickedLetters(key) {
+  checkWinner() {
+    //Check if the users win and return a boolean value
+    return this.secretWord.length === this.guessedLetter.length 
+  }
 
-//   }
+}
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
-document.getElementById('start-game-button').onclick = () => {
-  hangman = new Hangman();
+document.getElementById('start-game-button').onclick = (e) => {
+  let secretWord = 'Ironhack'
+  let hangman = new Hangman();
+  let hangmanCanvas = new HangmanCanvas(secretWord);
+  let key = e;
+  console.log(key)
+  hangmanCanvas.createBoard()
+  hangmanCanvas.drawHangman()
+  hangmanCanvas.drawLines()
+  hangmanCanvas.drawLines()
 };
 
 document.onkeydown = (e) => {
-
+  console.log(e.target)
 };
