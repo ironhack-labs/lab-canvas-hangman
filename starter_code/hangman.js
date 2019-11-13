f393ad8dcc5ae4eda9d2c17b8e64e3089e07c4ff
Ardash <56517452+ardache@users.noreply.github.com>
2019-11-13 16:07:05 -0600

    Avance Mañana
    adicion a checkIfLetter & avance en addCorrectLetter(i)

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -7,6 +7,7 @@ class Hangman {
     this.letters = [];
     this.guessedLetter = "";
     this.errorsLeft = 10;
+  
    }
 
   getWord() {
@@ -18,7 +19,7 @@ class Hangman {
 
   checkIfLetter(keyCode) {
     `Checks if the key the user has typed is a letter.`
-    if (keyCode >= 65 && keyCode <= 90){
+    if ((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)){
       return true
     }
     return false
@@ -37,14 +38,15 @@ class Hangman {
     and returns true if it was not or false in the opposite case.`
 
     if (this.letters.indexOf(key) != -1){
+      this.letters.push(String.fromCharCode(key))
       return false;
     }
     return true;
   }
 
-//   addCorrectLetter(i) {
-
-//   }
+   addCorrectLetter(i) {
+    this.guessedLetter += this.letters[i]
+   }
 
 //   addWrongLetter(letter) {
 
