7a3de37b0e0415f7410c3976bf6598ee3652c9da
Yunnuenmontes <yunnuenmontesym@gmail.com>
2019-11-13 19:37:05 -0600

    done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,89 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words =['tiesto','armin','DJfernando'];
+    this.secretWord = '';
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
 
-//   }
+  }
 
-//   getWord() {
+  getWord() {
+    return this.words[Math.floor(Math.random()*this.words.length)]
+  }
 
-//   }
+  checkIfLetter(keyCode) {
 
-//   checkIfLetter(keyCode) {
+    return (keyCode >= 65 && keyCode <=90);
 
-//   }
+  }
 
-//   checkClickedLetters(key) {
+  checkClickedLetters(key) {
 
-//   }
+    console.log(key)
 
-//   addCorrectLetter(i) {
+    if(this.letters.indexOf(key)!=-1){
+      return false;
 
-//   }
 
-//   addWrongLetter(letter) {
+    }
+    else {
+      return true;
+    }
+  }
 
-//   }
+  addCorrectLetter(i) {
 
-//   checkGameOver() {
+    this.guessedLetter += this.secretWord[i].toUpperCase()
+    if(this.checkWinner()){
+      return true;
 
-//   }
+    }else {
+      return false;
+    }
 
-//   checkWinner() {
 
-//   }
 
-// }
+  }
+
+  addWrongLetter(letter) {
+   this.errorsLeft -= 1
+
+  }
+
+  checkGameOver() {
+
+      if(this.errorsLeft<=0){
+
+      return true;
+
+    }else{
+
+      return false;
+    }
+
+  }
+
+  checkWinner() {
+    if(this.guessedLetter.length == this.secretWord.length){
+      return true;
+
+    }else {
+      return false;
+    }
+
+  }
+
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
 };
 
 document.onkeydown = (e) => {
-
+  // if(hangman.checkIfletter(e.keyCode)){
+  //   if(hangman.letters.indexOf(keyCode)){}
+  // }
 };
