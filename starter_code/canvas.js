70c4be8951a200229b4c3405d14742667ebb04e6
Edgar Zea <cesszea@icloud.com>
2019-11-14 18:13:29 -0600

    Done by Edgar and Gustavo

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,19 +2,26 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.secretWord = secretWord;
   }
 
   createBoard() {
-
+    this.ctx.clearReact(0,0,1200,800);
+    this.ctx.lineWidth = 5;
   }
 
   drawLines() {
-
+    let lines = this.secretWord.length * 100 + 100;
+    for (let i = 100; i < lines; i += 100) {
+      ctx.beginPath()
+      ctx.moveTo(i, 750);
+      ctx.lineTo(i + 50, 750)
+      ctx.stroke()
+    }
   }
 
   writeCorrectLetter(index) {
-
+    
   }
 
   writeWrongLetter(letter, errorsLeft) {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,40 +1,50 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ['STRING','CLASS','JAVASCRIPT','CONDITIONALS','ARRAYS','OBJECTS'];
+    this.secretWord = '';
+    this.letters = [''];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
 
-//   }
+  }
 
-//   getWord() {
+  getWord() {
+    return this.words[Math.floor(Math.random()*this.words.length)];
+  }
 
-//   }
+  checkIfLetter(keyCode) {
+    if (keyCode >= 65 && keyCode <= 90) {
+      return true;
+    }
+    return false
+  }
 
-//   checkIfLetter(keyCode) {
+  checkClickedLetters(key) {
+    return !this.letters.includes(key)
+  }
 
-//   }
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    this.checkWinner()
+  }
 
-//   checkClickedLetters(key) {
+  addWrongLetter(letter) {
+    this.errorsLeft-=1;
+  }
 
-//   }
+  checkGameOver() {
+    if (this.errorsLeft == 0) {
+      return true;
+    }
+    return false;
+  }
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
+  checkWinner() {
+    return this.guessedLetter.split('').sort().join('') === this.secretWord.split('').sort().join('')
+  }
+}
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
 };
