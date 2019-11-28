a72cf423c34f8a273141528769886347e9c010e9
Strapak, Sergej <strapak@mediamarktsaturn.com>
2019-11-28 13:25:32 +0100

    working on addWrongLetter function

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,40 +1,56 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+constructor() {
+  this.words = ["foo", "bar", "muh"];
+  this.secretWord = '';
+  this.letters = [];
+  this.guessedLetter = "";
+  this.errorLeft = 10;
+  }
 
+   getWord(){
+    let secretIndex = Math.floor(Math.random()*this.words.length);
+    this.SecretWord = this.words[secretIndex];
+    return this.secretWord;
+  }
 
-//   }
+   checkIfLetter(keyCode) {
+    if (keyCode >= 65 && keyCode <= 90 || keyCode >= 97 && keyCode <= 122){
+      return true;
+      }
+      else{
+        return false;
+      }
+    }
 
-//   getWord() {
+   checkClickedLetters(key) {
+    let newLetter=key;
+    if (this.letters.includes(key)){
+      return false;
+    }
+    else{
+      return true;
+    }
+   }
 
-//   }
+   addCorrectLetter(i) {
+    this.guessedLetter = this.guessedLetter + this.secretWord[i].toUpperCase();
+   }
 
-//   checkIfLetter(keyCode) {
+   addWrongLetter(letter) {
+    this.errorLeft = this.errorLeft - 1
+   }
 
-//   }
+   checkGameOver() {
 
-//   checkClickedLetters(key) {
+   }
 
-//   }
+   checkWinner() {
 
-//   addCorrectLetter(i) {
+   }
 
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+ }
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
