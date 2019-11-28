2eb27dabaecf5209d7832708da699dde18a1ac73
Rochowski <rochowskim@media-saturn.com>
2019-11-28 15:06:09 +0000

    create board and draw lines completed

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,36 +1,51 @@
+let h = 540;
+let v = 600;
 
 class HangmanCanvas {
   constructor(secretWord) {
-    this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.ctx = document.getElementById("hangman").getContext("2d");
+    this.secretWord = secretWord;
   }
 
   createBoard() {
-
+    this.ctx.clearRect(0, 0, 1200, 800);
+    this.ctx.lineWidth = 2.0;
   }
 
   drawLines() {
+    let i = 0;
+    let step = 60;
 
+    for (i=0; i<this.secretWord.length; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(500 + step * i, 600);
+      this.ctx.lineTo(530 + step * i, 600);
+      this.ctx.closePath();
+      this.ctx.strokeStyle = "black";
+      this.ctx.stroke();
+      console.log("hallo");
+    }
+      
+      /* this.ctx.beginPath();
+      this.ctx.moveTo(530 + step * i, 600);
+      this.ctx.lineTo(560 + step * i, 600);
+      this.ctx.closePath();
+      this.ctx.strokeStyle = "red";
+      this.ctx.stroke(); */
   }
 
-  writeCorrectLetter(index) {
+  writeCorrectLetter(index) {}
 
-  }
+  writeWrongLetter(letter, errorsLeft) {}
 
-  writeWrongLetter(letter, errorsLeft) {
+  drawHangman(shape) {}
 
-  }
+  gameOver() {}
 
-  drawHangman(shape) {
-
-  }
-
-  gameOver() {
-
-  }
-
-  winner() {
-
-  }
-
+  winner() {}
 }
+
+let hangman = new HangmanCanvas("Word");
+
+hangman.createBoard();
+hangman.drawLines();
