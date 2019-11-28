7813f72c8b66b21b2b9118b4a29e48fd2bca030a
Rochowski <rochowskim@media-saturn.com>
2019-11-28 11:03:00 +0000

    addCorrectLetter completed

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,47 @@
-let hangman;
+class Hangman {
+  constructor() {
+    this.words = ["word", "test", "other"];
+    this.secretWord = this.getWord();
+    this.letters = [];
+    this.guessedLetter = [];
+    this.errorsLeft = 10;
+}
 
-// class Hangman {
-//   constructor() {
 
-//   }
+   getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)]; 
+   }
 
-//   getWord() {
 
-//   }
+   checkIfLetter(keyCode) {
+    if ((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)) {
+      this.letters.push(String.fromCharCode(keyCode));
+      return true;
+    } 
+    else return false;
+   }
 
-//   checkIfLetter(keyCode) {
+   checkClickedLetters(key) {
+     return !(this.letters.includes(key));
+   }
 
-//   }
+   addCorrectLetter(i) {
+ this.guessedLetter.push(this.secretWord[i].toUpperCase());
+   } 
 
-//   checkClickedLetters(key) {
+   addWrongLetter(letter) {
+   }
+   checkGameOver() {
+   }
+   checkWinner() {
+   }
+ }
 
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
-document.getElementById('start-game-button').onclick = () => {
+/* document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
 };
 
 document.onkeydown = (e) => {
 
 };
+let hangman; */
