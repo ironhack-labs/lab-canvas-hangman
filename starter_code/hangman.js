51212a6d38bf7a5c989ab296d5f4005bebaf03e5
kilianal <kilianal@mediamarktsaturn.com>
2019-11-28 15:00:37 +0100

    Iteration 1

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,69 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+constructor() {
+  this.words =["hallo", "Wort", "wort"];
+  this.secretWord="";
+  this.letters=[];
+  this.guessedLetter="";
+  this.errorsLeft=10;
+  }
+  getWord() {
+    let randomWordIndex=Math.floor(Math.random()* this.words.length);
+    let randomWord=this.words[randomWordIndex];
+    this.secretWord=randomWord;
+   return randomWord;
+  }
+ checkIfLetter(keyCode) {
+   //man könnte es auch mit RegEX machen
+   
+   let pressedKey=keyCode;
+    let keyCodeArray=[87,69,82,84,90,85,73,79,80,186,65,83,68,70,71,72,74,75,76,192,222,89,88,67,86,66,78,77];
+    let wahr=keyCodeArray.includes(pressedKey);
+    return wahr;
+    
+ }
+  checkClickedLetters(key) {
+    let vorhanden =this.letters.includes(key);
+    return !vorhanden;
+   
+   }
+  addCorrectLetter(i) {
+    this.guessedLetter+=this.secretWord[i].toUpperCase();
 
-//   }
+  }
+  addWrongLetter(letter) {
+    if(!this.secretWord.includes(letter)){
+      this.errorsLeft-=1;
+    }
+  }
+  checkGameOver() {
+    if(this.errorsLeft==0){
+      return true
+      }
+      else{
+        return false;
+      }
 
-//   getWord() {
+  }
+  checkWinner() {
+    return(this.guessedLetter.length==this.secretWord.length)
+      
+    
 
-//   }
-
-//   checkIfLetter(keyCode) {
-
-//   }
-
-//   checkClickedLetters(key) {
-
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+  }
+}
 
 document.getElementById('start-game-button').onclick = () => {
+  
   hangman = new Hangman();
+  hangman.getWord();
+  
 };
 
 document.onkeydown = (e) => {
-
+ hangman.checkIfLetter(e.keyCode);
+ hangman.checkClickedLetters(e.key);
+ 
+  
 };
