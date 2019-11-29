613a9b802276d7305cd2f396d7b9bd32163a29d9
Malte Schenkling <schenkling@mediamarktsaturn.com>
2019-11-29 09:18:10 +0100

    work state

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,20 +1,70 @@
+let drawLinesCounter = 0;
 
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.secretWord = secretWord; 
   }
 
   createBoard() {
-
+    this.ctx.fillStyle = "white";
+    this.ctx.fillRect(0, 0, 800, 1200);
   }
 
-  drawLines() {
+  drawLines() { //works
+    //800x1200
+    let widthOfCanvas = 1200;
+    let heightOfCanvas = 800;
+    let canvasCenterX = widthOfCanvas/2;
+    //console.log("Center: " + canvasCenterX);
+    let LineY = 600;
+    let lineWidth = 20;
+    let spaceWidth = 10;
+    //console.log("Secret: " + this.secretWord);
+    let lineNoCount = this.secretWord.split("").length;
+    //console.log("lineCount: " + lineNoCount);
 
+    let fillLineLengthInclSpaces = (lineNoCount * (lineWidth+spaceWidth))-spaceWidth;
+    //console.log("fullLen: " + fillLineLengthInclSpaces);
+    let lineStartPosition = canvasCenterX - fillLineLengthInclSpaces/2;
+    //console.log("lineStart: " + lineStartPosition);
+    let lineEndPosition = lineStartPosition + lineWidth;
+    //console.log("lineEnd: " + lineEndPosition);
+
+    //draw a line for every char of the secret word
+    this.ctx.lineWidth = 5;
+    this.ctx.strokeStyle = "black";
+
+    this.ctx.beginPath();
+    for (let letter of this.secretWord) {
+      console.log("StartX: ", lineStartPosition, " EndX: ", lineEndPosition, "constY:", LineY);
+      this.ctx.moveTo(lineStartPosition, LineY);
+      this.ctx.lineTo(lineEndPosition, LineY);
+      this.ctx.stroke();
+      lineStartPosition = lineEndPosition + spaceWidth
+      lineEndPosition = lineStartPosition + lineWidth;
+    }
   }
 
   writeCorrectLetter(index) {
 
+    console.log();
+    this.ctx.font = "25px Arial";
+    this.ctx.fillStyle = "black";
+    //--> identify "key pressed" number of chars in secret
+    if (!(this.secretWord.indexOf(//add char) === this.secretWord.lastIndexOf(//add char))) {
+      for () {
+
+      }
+    }
+    
+
+
+
+
+    console.log(this.secretWord);
+    console.log(this.secretWord.charAt(index));
+    this.ctx.fillText(`${this.secretWord.charAt(index)}`,10,40);
   }
 
   writeWrongLetter(letter, errorsLeft) {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,103 @@
-let hangman;
+let secretWordHandover = "";
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ["javascript", "canvas", "document"];
+    this.secretWord = "";
+    this.letters = []; //constains all entered-in letters
+    this.guessedLetter = ""; //contains just the correct letters which are yet guessed
+    this.errorsLeft = 10;
+  }
 
-//   }
+  getWord() {
+    let randomWordId = Math.floor( Math.random() * this.words.length );
+    this.secretWord += this.words[randomWordId];
+    return this.secretWord;
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    return (keyCode>=65 && keyCode<=90);
+  }
+  
+  //validates whether the char is entered first (true) or one next time (false)
+  //returns wrong boolean state as required by tests
+  checkClickedLetters(key) {
+    let charAlreadyPresent = !this.letters.includes(key);
+    if (this.letters.includes(key) === false) {
+      this.letters.push(key);
+      //console.log(this.letters)
+    }
+    //console.log("CharPresent?: ", charAlreadyPresent);
+    return charAlreadyPresent;
+  }
 
-//   }
+  //adds letter to guessedLetter
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    if (this.checkWinner() === true) {
+      //do someting - won
+      console.log("won");
+    }
+  }
 
-//   checkIfLetter(keyCode) {
+  addWrongLetter(letter) {
+    this.errorsLeft--; //decrease try count for the wrong guess
+    if (this.checkGameOver === true) {
+      //do someting - gameover
+      console.log("gameover");
+    }
+  }
 
-//   }
+  checkGameOver() {
+    return this.errorsLeft === 0;
+  }
 
-//   checkClickedLetters(key) {
-
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+  checkWinner() {
+    let secretWordArray = this.secretWord.split("");
+    let guessedLetterArray = this.guessedLetter.split("");
+    return secretWordArray.reduce( (tot, val) => {
+      //console.log( "tot:", tot, " val: ", val, " comp: ", guessedLetterArray.includes(val));
+      return ( tot && guessedLetterArray.includes(val) );
+    }, true);
+  }
+}
 
 document.getElementById('start-game-button').onclick = () => {
-  hangman = new Hangman();
+  let hangman = new Hangman();
+  hangman.getWord();
+  let hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  document.onkeydown = (e) => {
+    if (hangman.checkIfLetter(e.keyCode) === true) {
+      //console.log("checkIfLetter: ", hangman.checkIfLetter(e.keyCode), " keyCode: ", e.keyCode);
+      if (hangman.checkClickedLetters(e.key) === true) { //key entered first (true)
+        console.log("new char entered in");
+        //console.log("corrLet: ", hangman.secretWord.indexOf(e.key));
+        if (hangman.secretWord.includes(e.key) === true) { //key exists in secret
+          hangman.addCorrectLetter(hangman.secretWord.indexOf(e.key));
+          hangmanCanvas.writeCorrectLetter(hangman.secretWord.indexOf(e.key));
+        }
+      }
+      else if (hangman.checkClickedLetters(e.key) === false) { //key entered 2nd + time (false)
+        console.log("existing char entered in");
+        console.log("charInSecWord?: ", hangman.secretWord.includes(e.key));
+        hangman.addWrongLetter(e.key);
+      }
+    }
+    console.log("words: ", hangman.words);
+    console.log("secretWord: ", hangman.secretWord);
+    console.log("letters: ", hangman.letters);
+    console.log("guessedLetter: ", hangman.guessedLetter);
+    console.log("errorsLeft: ", hangman.errorsLeft);
+  }
+  hangmanCanvas.createBoard();
+  hangmanCanvas.drawLines();
 };
 
-document.onkeydown = (e) => {
-
-};
+/*
+function updateCanvas() {
+  clearCanvas(hangmanCanvas.ctx);
+  if (drawLinesCounter === 0) {
+    hangmanCanvas.drawLines();
+    drawLinesCounter = 1;
+  }
+}*/
