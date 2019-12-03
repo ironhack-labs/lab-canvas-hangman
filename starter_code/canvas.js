59422f1095d60e4bbf5940f200fd10b3c7565a96
J. A. Hernández <joseanher81@gmail.com>
2019-12-03 21:22:25 +0100

    Logic implemented

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,15 +2,28 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.secretWord = secretWord;
   }
 
   createBoard() {
-
+    
+    this.ctx.clearRect(0, 0, 1200, 800) ;
+    this.drawLines();
   }
 
   drawLines() {
-
+    console.log("Secret word is", this.secretWord);
+    var x = 250;
+    var y = 600;
+    for(var i = 0; i < this.secretWord.length; i++) {
+      console.log("drawing line");
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, y);
+      this.ctx.lineTo(x + 50, y);
+      this.ctx.stroke();
+      this.ctx.closePath();
+      x += 70;
+    } 
   }
 
   writeCorrectLetter(index) {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,58 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ["perro", "gato", "ahorcado", "castillo", "ponzoña"];
+    this.secretWord = '';
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+  }
 
-//   }
+   getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)];;
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    return (keyCode > 64 && keyCode < 91);
+  }
 
-//   }
+  checkClickedLetters(key) {
+    return this.letters.indexOf(key) == -1;
+  }
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    this.checkWinner();
+  }
 
-//   }
+  addWrongLetter(letter) {
+    this.errorsLeft--;
+    this.checkGameOver();
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    return this.errorsLeft == 0;
+  }
 
-//   }
+  checkWinner() {
+    return this.secretWord.length == this.guessedLetter.length;
+  }
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
+  let hCanvas = new HangmanCanvas(hangman.getWord());
+  hCanvas.createBoard();
 };
 
 document.onkeydown = (e) => {
+  if(hangman != undefined) {
+    console.log("Code", e.keyCode)
+    if(hangman.checkIfLetter(e.keyCode)) {
+      console.log("letter");
+    }
+  }
 
 };
