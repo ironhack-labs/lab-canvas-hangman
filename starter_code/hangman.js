fd9632fa1800f64bce6341dd94e349fcf97bd5ab
pelinski <teresapelinski@gmail.com>
2019-12-03 13:06:18 +0100

    constructor + getWord method

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,55 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = [
+      "elbow",
+      "steward",
+      "incongruous",
+      "proper",
+      "suspect",
+      "support",
+      "person",
+      "hut",
+      "cultivate",
+      "fail",
+      "row",
+      "rape",
+      "culture",
+      "mirror",
+      "dictionary",
+      "widen",
+      "suspicion",
+      "despair",
+      "presence",
+      "explain"
+    ];
+    this.secretWord = "";
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsleft = 10;
+  }
 
-//   }
+  getWord() {
+    this.secretWord = this.words[Math.floor(Math.random()*this.words.length)]
+    return this.secretWord;
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {}
 
-//   }
+  checkClickedLetters(key) {}
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {}
 
-//   }
+  addWrongLetter(letter) {}
 
-//   checkClickedLetters(key) {
+  checkGameOver() {}
 
-//   }
+  checkWinner() {}
+}
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
-document.getElementById('start-game-button').onclick = () => {
+document.getElementById("start-game-button").onclick = () => {
   hangman = new Hangman();
 };
 
-document.onkeydown = (e) => {
-
-};
+document.onkeydown = e => {};
