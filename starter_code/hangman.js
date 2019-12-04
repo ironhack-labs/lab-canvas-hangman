b95fed2504c54b6cce9aed2250ea4a64b5d5d6ff
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-12-04 12:22:55 +0100

    iteration-1 upload with one failled

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,56 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ['capucha','foca','laboratorio','termómetro','ambulancia','ambulancia', 'reputación', 'cultura', 'boca', 'cereales','terremoto', 'contrato', 'jamón'];
+    this.secretWord = this.getWord();
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+  }
 
-//   }
+  getWord() {
+    return this.words[Math.floor(Math.random()*this.words.length)]
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    if(keyCode >= 65 && keyCode <= 90) return true;
+    return false;
+  }
 
-//   }
+  checkClickedLetters(key) {
+    if(this.letters.includes(key)) return false;
+    return true;
+  }
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {
+    if(this.secretWord.includes(i)) this.guessedLetter += i;
+    this.checkWinner();
+  }
 
-//   }
+  addWrongLetter(letter) {
+    this.letters.push(letter);
+    this.errorsLeft -= 1;
+    this.checkGameOver()
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    if(this.errorsLeft == 0) return true;
+    return false;
+  }
 
-//   }
+  checkWinner() {
+    let counter = 0;
+    for(let i = 0; i < this.secretWord.length; i++){
+      if(this.guessedLetter.includes(this.secretWord[i])) counter++;
+    }
+   
+    if(counter === this.secretWord.length) return true;
 
-//   addCorrectLetter(i) {
+    return false
+  }
 
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
