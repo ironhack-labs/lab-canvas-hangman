349885f54cedb6bf08f492940e3601ba0a2f87f5
Fernando Lima Borrelli <flborrelli@gmail.com>
2019-12-05 20:39:53 -0300

    In progress

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,3 +1,13 @@
+// const canvas = document.getElementById('hangman');
+// const ctx = canvas.getContext('2d');
+
+
+// //Middle Circle
+// ctx.beginPath();
+// ctx.arc(500, 300, 75, 0, Math.PI * 2);
+// ctx.fillStyle = "peru";
+// ctx.fill();
+// ctx.closePath();
 
 class HangmanCanvas {
   constructor(secretWord) {
@@ -6,10 +16,60 @@ class HangmanCanvas {
   }
 
   createBoard() {
+    // this.ctx.beginPath();
+    // this.ctx.arc(600, 400, 75, 0, Math.PI * 2);
+    // this.ctx.fillStyle = "peru";
+    // this.ctx.fill();
+    // this.ctx.closePath();
+    this.ctx.strokeRect(0, 0, 1200, 800);
+    // this.ctx.fillRect = 'ligth-blue';
+    // this.ctx.closePath()
 
+    // this.ctx.beginPath()
+    // this.ctx.moveTo(360,600)
+    // this.ctx.lineTo(360,200)
+    // this.ctx.stroke()
+    // this.ctx.fillStyle('black')
+    // this.ctx.closePath()
+    
   }
 
   drawLines() {
+    //FORCA
+    this.ctx.beginPath()
+    this.ctx.moveTo(240,600)
+    this.ctx.lineTo(240,200)
+    this.ctx.lineTo(540,200)
+    this.ctx.lineTo(540,270)
+    this.ctx.moveTo(240,600)
+    this.ctx.lineTo(170,690)
+    this.ctx.moveTo(240,600)
+    this.ctx.lineTo(310,690)
+    this.ctx.lineTo(170,690)
+
+    //LINHAS ABAIXO DA FORCA
+    this.ctx.moveTo(360, 690)
+    this.ctx.lineTo(460, 690)
+    this.ctx.moveTo(510, 690)
+    this.ctx.lineTo(610, 690)
+    this.ctx.moveTo(660, 690)
+    this.ctx.lineTo(760, 690)
+    this.ctx.moveTo(810, 690)
+    this.ctx.lineTo(910, 690)
+    this.ctx.moveTo(960, 690)
+    this.ctx.lineTo(1060, 690)
+    this.ctx.stroke()
+    this.ctx.closePath()
+
+    //HANGMAN
+    this.ctx.beginPath();
+    // this.ctx.moveTo(540,270) //ponto inicial forca
+    this.ctx.arc(540, 340, 70, 0, Math.PI * 2);
+    this.ctx.fillStyle = "black";
+    this.ctx.fill()
+    this.ctx.closePath();
+
+
 
   }
 
@@ -33,4 +93,6 @@ class HangmanCanvas {
 
   }
 
+  
 }
+
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,84 @@
 let hangman;
+let hangmanCanvas;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ['carro', 'fruta', 'casa', 'bruxa', 'ironhack', 'Heitor', 'JOC'];
+    this.secretWord = Math.floor(Math.random() * this.words.length);
+    this.letters =[]
+    this.guessedLetter = ""
+    this.errorsLeft = 10
+}
 
-//   }
+  getWord() {
+    return "";
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    if(typeof keyCode === "number" && keyCode >= 65 && keyCode <= 90){
+      return true;
+    } else{
+      return false;
+    }
+  
+  }
 
-//   }
+  checkClickedLetters(key) {
+    if(typeof key === 'string' &&  !this.letters.includes(key)){
+      return true
+    } else {
+      return false
+    }
+  }
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase(); 
+  }
 
-//   }
+  addWrongLetter(letter) {
+    if(!this.secretWord.includes(letter)){
+      this.errorsLeft -= 1;
+    }
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    if(this.errorsLeft === 0){
+      return true
+    } else {
+      return false
+    }
+  }
 
-//   }
+  checkWinner() {
+    let test = "";
+    for(let i = 0; i < this.secretWord.length; i += 1){
+      for(let j = 0; j < this.guessedLetter.length; j += 1){
+        if(this.secretWord[i] === this.guessedLetter[j]){
+          test += this.secretWord[i];
+        }
+      }
+    }
+    if(test === this.secretWord){
+      return true;
+    } else{
+      return false;
+    }
+    }
 
-//   addCorrectLetter(i) {
+  }
 
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
+  hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  hangmanCanvas.createBoard();
+  hangmanCanvas.drawLines()
 };
+
+
 
 document.onkeydown = (e) => {
 
 };
+
+
