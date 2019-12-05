bd3c56f1754d494418c35db17fa12ae05587b958
Ana Segarra <anasegarraber@gmail.com>
2019-12-05 11:41:44 +0100

    Add draw hangman and fix writeCorrectLetters

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -14,25 +14,79 @@ class HangmanCanvas {
 		let x = 300;
 		for (let i = 0; i < this.secretWord.length; i++) {
 			if (this.secretWord[i] === ' ') {
-				this.ctx.fillStyle = 'red';
-				this.ctx.fillRect(x, this.ctx.canvas.height / 2, 30, 2);
+				this.ctx.fillStyle = 'white';
+				this.ctx.fillRect(x, 400, 30, 2);
 			} else {
 				this.ctx.fillStyle = 'black';
-				this.ctx.fillRect(x, this.ctx.canvas.height / 2, 25, 2);
+				this.ctx.fillRect(x, 400, 25, 2);
 			}
 			this.letterPosition.push(x);
 			x += 40;
 		}
 	}
-
 	writeCorrectLetter(index) {
-		this.ctx.font = '25px Arial';
-		this.ctx.fillText(this.secretWord[index], this.letterPosition[index], this.ctx.canvas.height / 2 - 2);
+		let instances = [];
+		for (let i = 0; i < this.secretWord.length; i++) {
+			if (this.secretWord[index] === this.secretWord[i]) {
+				instances.push(i);
+				// console.log(i, this.secretWord[i]);
+			}
+		}
+		instances.forEach(el => {
+			this.ctx.font = '25px Arial';
+			this.ctx.fillText(this.secretWord[el].toUpperCase(), this.letterPosition[el] + 3, 398);
+		});
 	}
 
-	writeWrongLetter(letter, errorsLeft) {}
+	writeWrongLetter(letter, errorsLeft) {
+		this.ctx.font = '25px Arial';
+		this.ctx.fillText(letter.toUpperCase(), firstPosition, 50);
+		this.drawHangman(errorsLeft);
+	}
 
-	drawHangman(shape) {}
+	drawHangman(shape) {
+		switch (shape) {
+			case 9:
+				this.ctx.moveTo(180, 360);
+				this.ctx.lineTo(260, 400);
+				this.ctx.lineTo(100, 400);
+				this.ctx.lineTo(180, 360);
+				break;
+			case 8:
+				this.ctx.lineTo(180, 10);
+				break;
+			case 7:
+				this.ctx.lineTo(390, 10);
+				break;
+			case 6:
+				this.ctx.lineTo(390, 30);
+				break;
+			case 5:
+				this.ctx.beginPath();
+				this.ctx.arc(390, 60, 30, Math.PI * 2, 0);
+				break;
+			case 4:
+				this.ctx.moveTo(390, 90);
+				this.ctx.lineTo(390, 240);
+				break;
+			case 3:
+				this.ctx.lineTo(440, 270);
+				break;
+			case 2:
+				this.ctx.moveTo(390, 240);
+				this.ctx.lineTo(340, 270);
+				break;
+			case 1:
+				this.ctx.moveTo(390, 160);
+				this.ctx.lineTo(340, 120);
+				break;
+			case 0:
+				this.ctx.moveTo(390, 160);
+				this.ctx.lineTo(440, 120);
+		}
+		this.ctx.lineWidth = 2;
+		this.ctx.stroke();
+	}
 
 	gameOver() {}
 
