ffa821669958455f06b9a4f9f4634a4a4782d103
Nykolle Malone <nykolle.malone@hotmail.com>
2019-12-05 19:28:46 -0300

    in progress

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,17 +1,35 @@
 
+//Let´s create a constructor HangmanCanvas and store in a variable our canvas. After storing it, we should get the context. Remember getContext('2d'). We also want to store in a variable the secret word.
+
+let axisX = document.getElementById('hangman').width;
+let axisY = document.getElementById('hangman').height;
+
+console.log(axisY);
+
 class HangmanCanvas {
   constructor(secretWord) {
-    this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.ctx = document.getElementById('hangman').getContext('2d'); 
   }
 
   createBoard() {
-
+    this.ctx.clearRect(0,0,axisX, axisY);
+    this.ctx.fillStyle = 'light-blue';
+    this.ctx.fillRect(0, 0, axisX, axisY); 
+    console.log('fro')
   }
 
   drawLines() {
-
+    let distLine = (600/secretWord.length);
+    for(let i = 0; i <= secretWord.length; i += 1) {
+      distLine = ((distLine-20) * i);
+      this.ctx.beginPath();
+      this.ctx.moveTo(500 + distLine, 680);
+      this.ctx.lineTo(520 + distLine, 680);
+      this.ctx.stroke();
+    }
   }
+
+
 
   writeCorrectLetter(index) {
 
@@ -22,7 +40,69 @@ class HangmanCanvas {
   }
 
   drawHangman(shape) {
+    
+    //BASE
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = 'black';
+    this.ctx.moveTo(180, 680);
+    this.ctx.lineTo(300, 600);
+    this.ctx.lineTo(420, 680);
+    this.ctx.fill();
+    this.ctx.stroke();
 
+    //LINHA VERTICAL
+    this.ctx.beginPath();
+    this.ctx.moveTo(300, 600);
+    this.ctx.lineTo(300, 200);
+    this.ctx.stroke();
+
+    //LINHA HORIZONTAL
+    this.ctx.beginPath();
+    this.ctx.moveTo(300, 200);
+    this.ctx.lineTo(600, 200);
+    this.ctx.stroke();
+
+    //VERTICAL MENOR
+    this.ctx.beginPath();
+    this.ctx.moveTo(600, 200);
+    this.ctx.lineTo(600, 280);
+    this.ctx.stroke();
+
+    //CABEÇA
+    this.ctx.beginPath();
+    this.ctx.arc(600, 360, 40, 0, Math.PI * 2);
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    //CORPO
+    this.ctx.beginPath();
+    this.ctx.moveTo(600, 400);
+    this.ctx.lineTo(600, 560);
+    this.ctx.stroke();
+
+    // PERNA ESQUERDA 
+    this.ctx.beginPath();
+    this.ctx.moveTo(600, 560);
+    this.ctx.lineTo(480, 640);
+    this.ctx.stroke();
+
+    //PERNA DIREITA 
+    this.ctx.beginPath();
+    this.ctx.moveTo(600, 560);
+    this.ctx.lineTo(720, 640);
+    this.ctx.stroke();
+
+    //BRAÇO ESQUERDO 
+    this.ctx.beginPath();
+    this.ctx.moveTo(600, 440);
+    this.ctx.lineTo(480, 520);
+    this.ctx.stroke();
+
+    //BRAÇO DIREITO 
+    this.ctx.beginPath();
+    this.ctx.moveTo(600, 440);
+    this.ctx.lineTo(720, 520);
+    this.ctx.stroke();
   }
 
   gameOver() {
@@ -34,3 +114,4 @@ class HangmanCanvas {
   }
 
 }
+
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,51 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ['cafe', 'bolacha', 'biscoito'];
+    this.secretWord = '';
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10; 
+}
 
-//   }
+  getWord() {
+    let randomNumber = (Math.floor(Math.random() * this.words.length));
+    return this.words[randomNumber]; 
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    if (keyCode >= 65 && keyCode <= 91) {
+      return true
+    } 
+    return false
+  } 
 
-//   }
+  checkClickedLetters(key) {
+    return !this.letters.includes(key);
+  }
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+  }
 
-//   }
+  addWrongLetter(letter) {
+     this.errorsLeft -= 1;
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    return this.errorsLeft <= 0;
+  }
 
-//   }
+  checkWinner() {
+    return this.guessedLetter.length === this.secretWord.length;
+  }
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
+  new HangmanCanvas(hangman.secretWord)
 };
 
 document.onkeydown = (e) => {
