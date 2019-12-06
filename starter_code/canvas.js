665b02add13c74f58e9f7e3a7f3842f104f1179a
Javi L <jlopezro@gmail.com>
2019-12-06 23:35:15 +0100

    board and secret word lines done. Write the letters in progress

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,36 +1,50 @@
+let ctx = document.getElementById("hangman").getContext("2d");
 
 class HangmanCanvas {
   constructor(secretWord) {
-    this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    // this.startPos = [secretWord[0], secretWord[1]];
+    this.secretWord = secretWord;
+    // this.startPos = [pos[0],pos[1]];
+    // this.pos = pos
+    // this.size = [50,50];
+    // this.speed = [pxSec[0],pxSec[1]];
   }
 
   createBoard() {
-
+    ctx.clearRect = (0, 0, 1200, 800);
   }
 
   drawLines() {
-
+    ctx.setLineDash([32, 12]);
+    ctx.beginPath();
+    ctx.moveTo(275, 700);
+    ctx.lineTo(575, 700);
+    ctx.stroke();
   }
 
   writeCorrectLetter(index) {
-
+   if (hangman.checkClickedLetters(index) === false) {
+    console.log("hola");
+   }
+    // this.secretWord.forEach(function(e){
+    //   if(e === index) {
+    //     console.log("hola");
+    //   }
+    // })
   }
 
-  writeWrongLetter(letter, errorsLeft) {
+  writeWrongLetter(letter, errorsLeft) {}
 
-  }
+  drawHangman(shape) {}
 
-  drawHangman(shape) {
+  gameOver() {}
 
-  }
-
-  gameOver() {
-
-  }
-
-  winner() {
-
-  }
-
+  winner() {}
 }
+
+let line = new HangmanCanvas();
+
+// function drawLines(){line.drawLines()}
+line.drawLines();
+
+// window.addEventListener("DOMContentLoaded", drawLines);
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -2,11 +2,12 @@ let hangman;
 
 class Hangman {
   constructor() {
-    this.words = ["Hangman", "game", "Ironhack"];
-    this.secretWord = "Hangman";
+    this.words = ["HANGMAN", "GAME", "IRONHACK"];
+    this.secretWord = "HANGMAN";
     this.letters = [];
     this.guessedLetter = "";
     this.errorsLeft = 10;
+    console.log("nuevo juego");
   }
 
   getWord() {
@@ -14,22 +15,38 @@ class Hangman {
   }
 
   checkIfLetter(keyCode) {
+    console.log("chequea si es letra")
+    console.log(keyCode);
     if (keyCode >= 65 && keyCode <= 90) {
+      console.log("es letra")
+      this.checkClickedLetters(keyCode)
       return true;
     } else {
+      console.log("no es letra")
       return false;
     }
   }
 
   checkClickedLetters(key) {
+    console.log(key,"<==")
     if (this.letters.includes(key)) {
+      console.log("false")
       return false;
     } else {
+      this.letters.push(key)
+      console.log(String.fromCharCode(key), "la letra")
+      let idx = this.secretWord.indexOf(String.fromCharCode(key))
+      console.log(this.secretWord, "palabra secreta")
+      console.log(idx, "el indice")
+      // this.addCorrectLetter(idx)
+      console.log(this.letters)
+      console.log("true")
       return true;
     }
   }
 
   addCorrectLetter(i) {
+    // if (this.secretWord.includes(i));
     this.guessedLetter += this.secretWord[i].toUpperCase();
     this.checkWinner;
   }
@@ -58,6 +75,10 @@ class Hangman {
 
 document.getElementById("start-game-button").onclick = () => {
   hangman = new Hangman();
+  console.log("hi");
 };
 
-document.onkeydown = e => {};
+document.onkeydown = e => {
+  console.log("tecla presionada", e);
+  hangman.checkIfLetter(e.keyCode);
+};
