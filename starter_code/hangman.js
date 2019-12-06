ed5e0ce79ec03a2e811c9dca36ce2521e86ab924
Gonzalo Hernández <gonzalohernandez@icloud.com>
2019-12-06 01:58:28 +0100

    game logic

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,51 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ["COMADREJA", "TABANO", "ALMENDRA"];
+    this.secretWord = this.getWord();
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+  }
 
-//   }
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)];
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    let regExp = /[A-Za-z]/;
+    let letter = String.fromCharCode(keyCode);
+    return regExp.test(letter);
+  }
 
-//   }
+  checkClickedLetters(key) {
+    return !this.letters.includes(key);
+  }
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    this.checkWinner();
+  }
 
-//   }
+  addWrongLetter(letter) {
+    this.errorsLeft--;
+    return this.checkGameOver();
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    return this.errorsLeft === 0;
+  }
 
-//   }
+  checkWinner() {
+    return this.secretWord.length === this.guessedLetter.length;
+  }
+}
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
-document.getElementById('start-game-button').onclick = () => {
+/*
+document.getElementById("start-game-button").onclick = () => {
   hangman = new Hangman();
 };
 
-document.onkeydown = (e) => {
-
-};
+document.onkeydown = e => {};
+*/
