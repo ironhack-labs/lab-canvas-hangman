0bc8599556e7db842f5687e0570903b6b956fc3b
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2019-12-06 19:20:38 +0100

    iteration1finish

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,70 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ["Alberto", "aa"];
+    this.secretWord = this.getWord();
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+  }
 
-//   }
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)];
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    if ((keyCode <= 90) && (keyCode >= 65)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
-//   }
+  checkClickedLetters(key) {
+    if (this.letters.indexOf(key) !== -1) {
+      return false;
+    } else {
+      return true;
+    }
+  };
 
-//   checkIfLetter(keyCode) {
 
-//   }
+  addCorrectLetter(i) {
+this.letters.push(this.secretWord[i]);
+this.guessedLetter += this.secretWord[i].toUpperCase();
+this.checkWinner;
+  }
 
-//   checkClickedLetters(key) {
+  addWrongLetter(letter) {
+this.letters.push(letter);
+this.errorsLeft -=1;
+this.checkGameOver();
+  }
 
-//   }
+  checkGameOver() {
+return (this.errorsLeft === 0);
+  }
 
-//   addCorrectLetter(i) {
+  checkWinner() {
+if (this.guessedLetter.length !== this.secretWord.length)
+return false;
+else{
+  for ( var i= 0 ; i < this.secretWord.length ;i++){
+    if (this.guessedLetter.toLowerCase().indexOf(this.secretWord[i]) !== -1){
+    return false;
+    }
+  }
+  return true;
+}
+  };
+}
 
-//   }
 
-//   addWrongLetter(letter) {
 
-//   }
 
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
-document.getElementById('start-game-button').onclick = () => {
+document.getElementById("start-game-button").onclick = () => {
   hangman = new Hangman();
 };
 
-document.onkeydown = (e) => {
-
-};
+document.onkeydown = e => {};
