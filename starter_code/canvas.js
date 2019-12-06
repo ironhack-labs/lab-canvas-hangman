0a62d07b3901fdeb785520c0455edb485ea981b2
Luismi Feijoo <luism.feijoo26@gmail.com>
2019-12-06 16:02:44 +0100

    create the HangmanCanvas class

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,11 +2,19 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.secretWord = secretWord;
   }
 
   createBoard() {
-
+    this.ctx.clearRect(0,0,1200,800);
+    this.ctx.lineWidth = 10;
+    this.ctx.moveTo(125,675);
+    this.ctx.lineTo(100,700);
+    this.ctx.lineTo(150,700);
+    this.ctx.lineTo(125,675);
+    this.ctx.lineto(125,200);
+    this.ctx.lineto(300,200);
+    this.ctx.lineto(300,250);
   }
 
   drawLines() {
