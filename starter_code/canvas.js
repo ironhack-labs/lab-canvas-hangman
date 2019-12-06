9acf24433f18f3454be30cd46a9110f843e69ef0
pelinski <teresapelinski@gmail.com>
2019-12-06 18:10:47 +0100

    creating the board + draw the secret word lines

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,36 +1,37 @@
-
 class HangmanCanvas {
   constructor(secretWord) {
-    this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.ctx = document.getElementById("hangman").getContext("2d");
+    this.numberOfLetters = secretWord.length;
   }
 
   createBoard() {
-
+    this.ctx.clearRect(0, 0, 500, 500);
   }
 
   drawLines() {
+    let startX = 25;
+    let linesWidth = 60;
+    let endX = startX + linesWidth;
+    let fixedY = 25;
+    let space = 10;
 
+    for (let i = 0; i < this.numberOfLetters; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(startX, fixedY);
+      this.ctx.lineTo(startX + linesWidth, fixedY);
+      this.ctx.stroke();
+      startX = endX + space;
+      endX = startX + linesWidth;
+    }
   }
 
-  writeCorrectLetter(index) {
+  writeCorrectLetter(index) {}
 
-  }
+  writeWrongLetter(letter, errorsLeft) {}
 
-  writeWrongLetter(letter, errorsLeft) {
+  drawHangman(shape) {}
 
-  }
+  gameOver() {}
 
-  drawHangman(shape) {
-
-  }
-
-  gameOver() {
-
-  }
-
-  winner() {
-
-  }
-
+  winner() {}
 }
