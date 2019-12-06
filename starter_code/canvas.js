b3fa1e10821f6d3877268415094be3de4ccc131b
Borja Domínguez <borja.d.atienza@gmail.com>
2019-12-06 10:10:49 +0100

    1º Interation DONE

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,15 +2,23 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
+    this.secretWord = hangman.getWord()
     
   }
 
   createBoard() {
-
+    
+    this.ctx.clearRect(0, 0, 800, 1200);
+    hangman.getWord()
+    this.drawLines(hangman.secretWord)
+    
   }
 
-  drawLines() {
+  drawLines(e) {
 
+    e.length
+    this.ctx.beginPath
+    
   }
 
   writeCorrectLetter(index) {
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,41 +1,73 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ["papelina", "periquito", "toledano"]
+    this.secretWord = ""
+    this.letters = []
+    this.guessedLetter = ""
+    this.errorsLeft = 10
+  }
 
-//   }
+  getWord() {
 
-//   getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)]
 
-//   }
+  }
 
-//   checkIfLetter(keyCode) {
+  checkIfLetter(keyCode) {
+    let letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z"];
 
-//   }
+    return letter.includes(String.fromCharCode(keyCode).toLowerCase())
 
-//   checkClickedLetters(key) {
+  }
 
-//   }
+  checkClickedLetters(key) {
 
-//   addCorrectLetter(i) {
+    if (this.letters.includes(key)) return false
 
-//   }
+    return true
 
-//   addWrongLetter(letter) {
+  }
 
-//   }
+  addCorrectLetter(i) {
 
-//   checkGameOver() {
+    if (this.secretWord.includes(this.secretWord[i]))
+      this.guessedLetter += this.secretWord[i].toUpperCase()
+    this.checkWinner()
 
-//   }
+  }
 
-//   checkWinner() {
+  addWrongLetter(letter) {
+    if (!this.secretWord.includes(letter))
+      this.errorsLeft--
+    this.checkGameOver()
 
-//   }
+  }
+  checkGameOver() {
 
-// }
+    return this.errorsLeft === 0
+
+  }
+
+  checkWinner() {
+    
+    // let sWord = this.secretWord.split("")
+    // let gLetter = this.secretWord.split("")
+
+    // if(sWord.length ==)
+    
+    // for(let i = 0; i = this.secretWord.length; i++){
+
+    // }
+    return this.guessedLetter.length === this.secretWord.length
+
+  }
+
+}
 
 document.getElementById('start-game-button').onclick = () => {
+  console.log("hola")
   hangman = new Hangman();
 };
 
