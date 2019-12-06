d98d38a7ce2ab5063c93a59e9d15bdb57b6b61a5
Ivan Esteban <ivan.estebanisabel@gmail.com>
2019-12-06 15:27:20 +0100

    iteration 1 - end

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,25 +1,114 @@
 let hangman;
 
 class Hangman {
-  constructor() {}
+  constructor() {
+    this.words = ["knowledge", "ironhack", "javascript"];
+    this.secretWord = "";
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+  }
 
-  getWord() {}
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)];
+  }
 
-  checkIfLetter(keyCode) {}
+  checkIfLetter(keyCode) {
+    let keys = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z"
+    ];
 
-  checkClickedLetters(key) {}
+    return keys.includes(String.fromCharCode(keyCode).toLowerCase());
+  }
 
-  addCorrectLetter(i) {}
+  checkClickedLetters(key) {
+    if (!this.letters.includes(key)) {
+      return true;
+    }
+    return false;
+  }
 
-  addWrongLetter(letter) {}
+  addCorrectLetter(i) {
+    if (this.secretWord.includes(this.secretWord[i])) {
+      this.guessedLetter += this.secretWord[i].toUpperCase();
+      this.checkWinner();
+    }
+  }
 
-  checkGameOver() {}
+  addWrongLetter(letter) {
+    if (!this.secretWord.includes(letter)) {
+      this.errorsLeft--;
+      this.checkGameOver();
+    }
+  }
 
-  checkWinner() {}
+  checkGameOver() {
+    return this.errorsLeft === 0;
+  }
+
+  checkWinner() {
+    if (
+      this.secretWord.localeCompare(this.guessedLetter) &&
+      this.secretWord.length === this.guessedLetter.length
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
 
 document.getElementById("start-game-button").onclick = () => {
   hangman = new Hangman();
 };
-
-document.onkeydown = e => {};
+[
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+ent.onkeydown = e => {};
