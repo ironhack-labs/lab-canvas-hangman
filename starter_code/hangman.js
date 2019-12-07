4daf9becb740eeff2343215e3a23bbdb3dcf2af6
zante <dza.devandcomputing@gmail.com>
2019-12-07 04:21:13 +0100

    first iteration completed_game logic

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,61 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  
+  constructor() {
+    this.words = ["casa", "perro", "constancia", "marino", "animal", "adivinanza", "javascript"];
+    this.secretWord = "";
+    this.letters = ['C','A','O'];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+  }
 
-//   }
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)];
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    return keyCode >= 65 && keyCode <= 90 ?  true : false;
+  }
 
-//   }
+  checkClickedLetters(key) {
+    // this.letters.forEach( letter =>{
+    //   console.log(letter);
+    //   return letter === key ? false : true;
+    // })
+    return this.letters.includes(key) ? false : true;
+  }
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+    return this.checkWinner();
+  }
 
-//   }
+  addWrongLetter(letter) {
+    this.errorsLeft --;
+    return this.checkGameOver();
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    return this.errorsLeft === 0 ? true : false;
+  }
 
-//   }
+  checkWinner() {
+    return this.guessedLetter.length === this.secretWord.length;
+  }
 
-//   addCorrectLetter(i) {
+}
 
-//   }
+hangman = new Hangman();
 
-//   addWrongLetter(letter) {
+console.log(hangman.checkClickedLetters('C'));
+console.log(hangman.checkWinner());
+//console.log();
 
-//   }
+// document.getElementById('start-game-button').onclick = () => {
+//   hangman = new Hangman();
+// };
 
-//   checkGameOver() {
+// document.onkeydown = (e) => {
 
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
-document.getElementById('start-game-button').onclick = () => {
-  hangman = new Hangman();
-};
-
-document.onkeydown = (e) => {
-
-};
+// };
