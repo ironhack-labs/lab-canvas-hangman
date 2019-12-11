1bcbffb8500d10eae1b9a3a425034af5ef0aa831
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-12-11 18:42:43 -0500

    not started

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -66,3 +66,26 @@ document.onkeydown = e => {
 // document.addEventListener('keydown', function(event) {
 //   console.log(event.which);
 // });
+
+// document.addEventListener('keydown', function(event) {
+//   hangman.checkIfLetter(event.keyCode);
+//   hangman.checkClickedLetters(`${event.key}`);
+//   console.log(event);
+
+//   document.body.innerHTML = `
+//     &nbsp;&nbsp;&nbsp;
+//     <b>which: ${event.which}</b>
+//     <br>&nbsp;
+//     <b>keyCode:</b> ${event.keyCode}
+//     <br>&nbsp;&nbsp;&nbsp;
+//     <b>shiftKey:</b> ${event.shiftKey}
+//     </br>&nbsp;&nbsp;&nbsp;&nbsp;
+//     <b>altKey:</b> ${event.altKey}
+//     <br>&nbsp;&nbsp;&nbsp;
+//     <b>ctrlKey:</b> ${event.ctrlKey}
+//     <br>&nbsp;&nbsp;
+//     <b>metaKey:</b> ${event.metaKey}
+//     <br>&nbsp;&nbsp;
+//     <b>key:</b> ${event.key}
+//   `;
+// });
