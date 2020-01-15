20b8f2a2eebfb8d274ab1715b7225886be425942
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-01-15 15:10:54 -0600

    1. First iteration: Game Logic

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,57 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ['javascript', 'ironhack','world']
+    this.secretWord = ""
+    this.letters = [] 
+    this.guessedLetter = ""
+    this.errorsLeft = 10
+  }
 
-//   }
+  getWord() {
+    const wordIndex = Math.floor( Math.random() * this.words.length)
+    return this.words[wordIndex]
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    const isValidKey = ( (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122) )
+    return isValidKey
+  }
 
-//   }
+  checkClickedLetters(key) {
+      if( !this.letters.includes(key) ){
+        this.letters.push(key.key)
+        if( this.secretWord.includes(key) ) this.addCorrectLetter(key)
+        return true
+      } 
+    return false
+  }
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toLocaleUpperCase()
+    return this.checkWinner()
+  }
 
-//   }
+  addWrongLetter(letter) {
+    this.errorsLeft--
+    return this.checkGameOver()
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    return (this.errorsLeft === 0)
+  }
 
-//   }
+  checkWinner() {
+    return (this.secretWord.length === this.guessedLetter.length)
+  }
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
 };
 
 document.onkeydown = (e) => {
-
+  hangman.checkClickedLetters( e )
 };
