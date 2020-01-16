6bed1d2bd52941cafcda3fdafd8f7d4e705048c9
America Zurita <americazurita@gmail.com>
2020-01-16 05:56:18 -0600

    primera iteración completada

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,69 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+ class Hangman {
+  constructor() {
+    this.words = ["patitos", "gatitos", "perritos"]
+    this.secretWord = ""
+    this.letters = []
+    this.guessedLetter = ""
+    this.errorsLeft = 10
+   }
 
-//   }
+  getWord() {
+    const index = Math.floor(Math.random()* this.words.length)
+      this.secretWord = this.words[index]
+      return this.secretWord
+   }
 
-//   getWord() {
+  checkIfLetter(keyCode) {
+    if (typeof keyCode === "number"){
+      if(keyCode >= 65 && keyCode <= 90) {
+         return true
+        } else {
+          return false
+        }
+      } else {
+        return false
+      }
+  }
+  
 
-//   }
+  checkClickedLetters(key) {
+   if (this.letters.indexOf(key)>= 0){
+     return false
+   } else {
+     return true
+   }
+  }
+  //why is it backwards?
+   
 
-//   checkIfLetter(keyCode) {
+    addCorrectLetter(i) {  
+      this.guessedLetter = this.guessedLetter + this.secretWord[i].toUpperCase()
+  }
 
-//   }
+   addWrongLetter(letter) {
+     this.errorsLeft--
+     this.checkGameOver()
+   }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+      if (this.errorsLeft === 0){
+        return true
+      } else {
+        return false
+      }
+   }
 
-//   }
+  checkWinner() {
+      if (this.guessedLetter.length === this.secretWord.length){
+        return true
+      } else {
+        return false
+      }
+    }
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+ }
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
