fec865f5267ff13cecafc634c4867db7d3867ba7
Víctor Peña Romero <vic.pero@icloud.com>
2020-01-15 21:19:47 -0600

    add first pull

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,14 +2,28 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
+    this.secretWord = secretWord
     
   }
 
   createBoard() {
+    this.ctx.clearRect(0, 0, 1200, 800)
 
   }
 
   drawLines() {
+
+    this.ctx.beginPath()
+
+    for (let i = 0; i < this.secretWord.length; i++) {
+      this.ctx.moveTo(300 + 100 * i, 600)
+      this.ctx.strokeStyle = 'purple'
+      this.ctx.lineWidth = 3
+      this.ctx.lineTo((300 + 100 * i) + 75,600)
+      this.ctx.stroke()
+    }
+
+    this.ctx.closePath()
 
   }
 
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,74 @@
 let hangman;
+let hangmanCanvas
 
-// class Hangman {
-//   constructor() {
+class Hangman {
 
-//   }
+  constructor() {
+    this.words = ['perro', 'gato', 'jirafa']
+    this.secretWord = this.getWord()
+    this.letters = []
+    this.guessedLetter = ''
+    this.errorsLeft = 10
+  }
 
-//   getWord() {
+  getWord() {
+    const i = Math.floor(Math.random() * this.words.length)
+    return this.words[i]
+  }
 
-//   }
+  checkIfLetter(keyCode) {
+    if (keyCode >= 65 && keyCode <= 90) {
+      return true
+    } else {
+      return false
+    }
+  }
 
-//   checkIfLetter(keyCode) {
+  checkClickedLetters(key) {
+    if (this.letters.includes(key)) {
+      return false
+    } else {
+      return true
+    }
+  }
 
-//   }
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase()
+    this.checkWinner()
+  }
 
-//   checkClickedLetters(key) {
+  addWrongLetter(letter) {
+    if (this.errorsLeft > 0) {
+      this.errorsLeft--
+      this.checkGameOver()
+    }
+  }
 
-//   }
+  checkGameOver() {
+    if (this.errorsLeft === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
 
-//   addCorrectLetter(i) {
+  checkWinner() {
+    if (this.secretWord.length === this.guessedLetter.length) {
+      return true
+    } else {
+      return false
+    }
+  }
 
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
+  hangmanCanvas = new HangmanCanvas(hangman.getWord())
+  hangmanCanvas.createBoard()
+  hangmanCanvas.drawLines()
 };
 
 document.onkeydown = (e) => {
-
+  //hangmanCanvas.createBoard()
 };
