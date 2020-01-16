f0540dd04bcf5c1bf7e4646ccf44fed25d6e0334
Karlish Vázquez <karlishvazquez@Karlishs-MacBook-Pro-748.local>
2020-01-16 00:10:17 -0600

    done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,40 +1,62 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+ class Hangman {
+   constructor() {
+    this.words = ['hola','caracol','burbuja'];
+    this.secretWord = '';
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+   }
+  getWord() {
+    this.secretWord = this.words[Math.floor(Math.random() *
+      this.words.length)];
+      return this.secretWord;
+  }
+ 
+  checkIfLetter(keyCode) { 
+    if (keyCode >= 65 && keyCode <= 90 ){
+      return true
+    } else{
+      return false
+  }
+} 
 
-//   }
+  checkClickedLetters(key) {
+    if(this.letters.includes(key)){
+    return false;
+  } else {
+    return true;
+  }
+} 
 
-//   getWord() {
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord.charAt(i).toUpperCase();
+    return this.checkWinner();
+  }
 
-//   }
+  addWrongLetter(letter) {
+    this.errorsLeft--;
+    return this.checkGameOver();
+  }
 
-//   checkIfLetter(keyCode) {
+   checkGameOver() {
+    if(this.errorsLeft <= 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-//   }
+  checkWinner() {
+    if(this.guessedLetter.length === this.secretWord.length){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-//   checkClickedLetters(key) {
-
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
+} 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
 };
