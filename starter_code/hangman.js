d5b1c8eef716822875ff21266f4fe5c2d4627339
Charly <cgtright@gmail.com>
2020-01-16 07:28:41 -0600

    no terminado

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,39 +1,55 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+   constructor() {
+      this.words = [
+        'PERRO',
+        'GORRA',
+        'ACEITE',
+        'IRONHACK',
+        'COMPUTADORA'
+      ];
+      this.secretWord = '';
+      this.lettersClick = [];
+      this.guesedLetter = '';
+      this.errorsLeft = 10;
+   }
+   getWord() {
+    let wordsIndex = Math.floor(Math.random() * this.words.length);
+    this.secretWord = this.words[wordsIndex]
+    return this.secretWord
+  }
+    checkIfLetter(keyCode) {
+    const isLetter = ( (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122) )
+    return isLetter
+   }
+   checkClickedLetters(key) {
+    
+  }
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i]
+  }
+  addWrongLetter(letter) {
 
-//   }
+  }
 
-//   getWord() {
+  checkGameOver() {
+    return (this.errorsLeft === 0)
+  }
 
-//   }
+  checkWinner() {
+    if(this.secretWord.length === this.guesedLetter.length){
+      return true;
+    } else {
+      return false;
+    }
 
-//   checkIfLetter(keyCode) {
+  }
+}
 
-//   }
 
-//   checkClickedLetters(key) {
+  
 
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
