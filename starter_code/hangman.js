e856112510aa36a79f8fa367c46570665c7af34b
CarlaOnate <carlaonategardella@gmail.com>
2020-01-15 22:14:22 -0600

    AddCorrectLetter listo

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,25 +1,32 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ['mochon','lolaxo','fridonguis','bonguis'],
+    this.secretWord = '',
+    this.letters = [],
+    this.guessedLetter = '',
+    this.errorsLeft = 10
+}
+getWord() {
+    this.secretWord = this.words[Math.floor(Math.random() * this.words.length)]
+    return this.secretWord
+}
 
-//   }
+  checkIfLetter(keyCode) {
+    this.letters.push(keyCode)
+    if (keyCode >= 65 || keyCode <= 91){ //de a a Z, sin ñ
+      return true
+    } else return false
+  }
 
-//   getWord() {
+  checkClickedLetters(key) {
+    return !this.letters.includes(key) //puse ! porque me marcaba jasmine
+  }
 
-//   }
-
-//   checkIfLetter(keyCode) {
-
-//   }
-
-//   checkClickedLetters(key) {
-
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
+  addCorrectLetter(i) {
+    this.guessedLetter+=i //espero que se concatenen por ser strings
+  }
 
 //   addWrongLetter(letter) {
 
@@ -33,7 +40,7 @@ let hangman;
 
 //   }
 
-// }
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
