5f2c8dc579c983375d32a7eefab631f501b5a8e2
oljudi <oljudimark@gmail.com>
2020-01-16 01:11:32 -0600

    done con un bug

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,36 +1,89 @@
-
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.secretWord = secretWord
+    this.index = []
   }
 
   createBoard() {
-
+    this.ctx.clearRect(0, 0, 1200, 800);
+    this.ctx.lineWidth = 5;
   }
 
   drawLines() {
-
+    let x = 300
+    const y = 600
+    const number = this.secretWord.length
+    this.ctx.font = "20px Georgia"
+    this.ctx.fillText(`Intentos Restantes: `, 900, 50)
+    for(let i = 0; i < number; i++){
+      this.ctx.fillRect(x,y,50,10)
+      x = x + 80
+      this.index.push(x-70)
+    }
   }
 
-  writeCorrectLetter(index) {
-
+  writeCorrectLetter(i, letter) {
+    this.ctx.font = "20px Georgia" //estilo mamalon para que la letra se vea grande
+    this.ctx.fillText(letter.toUpperCase(), this.index[i], 580)
   }
 
-  writeWrongLetter(letter, errorsLeft) {
-
+  writeWrongLetter(letter, intentos) {
+    let y = 50
+    this.ctx.font = "20px Georgia"
+    this.ctx.clearRect(1080, 20, 50, 50)
+    for(let i = 0; i < letter.length; i++){
+      this.ctx.fillText(intentos, 1080, 50)
+      y = y + 20
+      this.ctx.fillText(letter[i].toUpperCase(), 1000, y) //ya imprime en la posicion correcta
+    }
+    this.drawHangman(intentos)
   }
-
   drawHangman(shape) {
-
+    switch (shape) { 
+      case 0: // brazo derecho
+      this.ctx.fillRect(145, 395, 30, 5)
+      break;
+      case 1:// brazo izquierdo
+      this.ctx.fillRect(175, 395, 30, 5)
+      break;
+      case 2:// pierna derecha
+      this.ctx.fillRect(145, 445, 30, 5)
+      break;
+      case 3:// pierna izquierda
+      this.ctx.fillRect(175, 445, 30, 5)
+      break;
+      case 4: // cuerpo
+      this.ctx.fillRect(175, 365, 5, 80)
+      break;
+      case 5: // cabeza aka cubo
+      this.ctx.strokeRect(155, 325, 40, 40)
+      break;
+      case 6: // corte hacia el ahorcado 
+      this.ctx.fillRect(170, 300, 10, 25)
+      break;
+      case 7: // corte del triangulo 
+      this.ctx.fillRect(70, 300, 100, 10)
+      break;
+      case 8:// palo del triangulo
+      this.ctx.fillRect(70, 300, 10, 250)
+      break;
+      case 9:// cuadrado aka base
+      this.ctx.fillRect(50, 550, 50,50)
+      break;
+    }
   }
 
   gameOver() {
-
+    let img = new Image()
+    img.src = './images/gameover.png'
+    this.ctx.drawImage(img, 0, 0)
   }
 
   winner() {
-
+    let img = new Image()
+    img.src = "./images/awesome.png"
+    this.ctx.drawImage(img, 0, 0)
   }
 
 }
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,84 @@
 let hangman;
+let hangmanCanvas
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = [
+      'javascript', 'diego', 'ironhack'
+    ]
+    this.secretWord = ''
+    this.letters = []
+    this.guessedLetter = ''
+    this.errorsLeft = 10
+  }
 
-//   }
+  getWord() {
+    let randomNumber = Math.floor(Math.random() * this.words.length)
+    this.secretWord = this.words[randomNumber]
+    return this.words[randomNumber]
+  }
 
-//   getWord() {
+  checkIfLetter(keyCode) { //Checar si lo clicekado fue una letra
+    if( keyCode >= 65 && keyCode <= 90 ) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
 
-//   }
+  checkClickedLetters(key) {
+    return !this.letters.includes(key)
+  }
 
-//   checkIfLetter(keyCode) {
+  addCorrectLetter(i) {
+    let letter = this.secretWord.split('')
+    this.guessedLetter += letter[i].toUpperCase()
+  }
 
-//   }
+  addWrongLetter(letter) {
+    this.errorsLeft --
+    if(!this.letters.includes(letter)) this.letters.push(letter)
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    if(this.errorsLeft <= 0) return true
+    return false
+  }
 
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+  checkWinner() { 
+    if(this.secretWord.length === this.guessedLetter.length) return true
+    return false
+  }
+}
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
+  hangmanCanvas = new HangmanCanvas(hangman.getWord());
+  hangmanCanvas.createBoard()
+  hangmanCanvas.drawLines()
+  console.log(hangman.secretWord);
+  
+  document.onkeydown = (e) => {
+    if(hangman.checkIfLetter(e.keyCode)){               //Checar si lo clicekado fue una letra
+      if(hangman.secretWord.includes(e.key)){           //Checar si la letra esta en la palabra
+        let index = hangman.secretWord.indexOf(e.key)   // sacar la posicion donde esta esa letra
+        hangmanCanvas.writeCorrectLetter(index, e.key)
+        hangman.addCorrectLetter(index)                 //Agregarla al string guessedLetter
+        console.log(hangman.checkWinner());
+        if(hangman.checkWinner()){
+          console.log('Ganador');
+          hangmanCanvas.winner()
+        }
+      } else {
+        hangman.addWrongLetter(e.key)                   //Agregarla al arreglo global de letras
+        hangmanCanvas.writeWrongLetter(hangman.letters, hangman.errorsLeft)
+        if(hangman.checkGameOver()){ // Checar si ya perdiste
+          console.log('Perdiste') //llamar a gameover
+          hangmanCanvas.gameOver()
+        } 
+      }
+    }
+  };
 };
 
-document.onkeydown = (e) => {
-
-};
