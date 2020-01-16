d2bf9ff7ffdd9972c6630c260048cede53bb5aea
Edith LE <edith.lop.estrada@gmail.com>
2020-01-16 00:13:06 -0600

    sigo trabajando en canvas

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,19 +1,32 @@
 
+
+
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.secretWord = secretWord
+    this.guessedLetter = guessedLetter
   }
 
   createBoard() {
-
+    this.ctx.clearRect(0, 0, 1200, 800)
+    
   }
 
   drawLines() {
-
+    this.ctx.beginPath()
+    for(let i = 0; i< this.secretWord.length; i++){
+      this.ctx.moveTo(300 + 100 * i, 600)
+      this.ctx.strokeStyle = "black"
+      this.ctx.lineWidth = 3
+      this.ctx.lineTo((300 + 100 * i)+ 60, 600)
+      this.ctx.stroke()
+      this.ctx.closePath()
+    } 
   }
 
   writeCorrectLetter(index) {
+
 
   }
 
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,86 @@
 let hangman;
 
-// class Hangman {
-//   constructor() {
+class Hangman {
+  constructor() {
+    this.words = ['hello', 'word', 'panchita']
+    this.secretWord = this.getWord()
+    this.letters = []
+    this.guessedLetter = ''
+    this.errorsLeft = 10
 
-//   }
 
-//   getWord() {
+   }
 
-//   }
+    getWord() {
+      const i = Math.floor(Math.random() * this.words.length)
+      return this.words[i]
 
-//   checkIfLetter(keyCode) {
+    }
 
-//   }
+    checkIfLetter(keyCode) {
+      if(keyCode>=65 && keyCode<= 90){
+        return true
+      }else{
+        return false
+      }
 
-//   checkClickedLetters(key) {
+   }
 
-//   }
+    checkClickedLetters(key) {
+      if(this.letters.includes(key)){
+        return false
+      }else{
+        return true
+      }
+   }
 
-//   addCorrectLetter(i) {
+    addCorrectLetter(i) {
+      this.guessedLetter += this.secretWord[i].toUpperCase()
+      this.checkWinner()
+   }
 
-//   }
+   addWrongLetter(letter) {
+     if(this.errorsLeft > 0)
+     return this.errorsLeft-- 
+     this.checkGameOver()
+    }
 
-//   addWrongLetter(letter) {
+    checkGameOver() {
+      if(this.errorsLeft === 0){
+        return true
+      }else{
+        return false
+      }
+      
+  }
 
-//   }
+     checkWinner() {
+       if(this.secretWord.length === this.guessedLetter.length){
+         return true
+       }else{
+         return false
+       }
 
-//   checkGameOver() {
+   }
 
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
+ }
 
 document.getElementById('start-game-button').onclick = () => {
   hangman = new Hangman();
+  hangmanCanvas = new HangmanCanvas(hangman.getWord())
+  hangmanCanvas.createBoard()
+  hangmanCanvas.drawLines()
 };
 
 document.onkeydown = (e) => {
+  if(hangman.checkIfLetter(e.keyCode)){  //esta checa si hay una letra 
+    let letter = e.key.toUpperCase() //y si hay una letra, almacenarla en una variable que es letter y hacerla a mayúculas
+    if(hangman.checkClickedLetters(letter)){ //
+      if(hangman.secretWord.includes(letter)){
+        
+      }
+    }
+  }
+
 
 };
