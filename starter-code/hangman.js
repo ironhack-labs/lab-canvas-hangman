7dc253f44a585b70c10083009857c31585e78406
sandrabosk <sandraboskovic@hotmail.com>
2020-02-04 17:23:28 -0500

    improve guidelines, add hints in .js files

---
+++ starter-code/hangman.js
@@ -0,0 +1,28 @@
+let hangman;
+class Hangman {
+  constructor() {}
+
+  getWord() {}
+
+  checkIfLetter(keyCode) {}
+
+  checkClickedLetters(letter) {}
+
+  addCorrectLetter(index) {}
+
+  checkWinner() {}
+
+  addWrongLetter(letter) {}
+
+  checkGameOver() {}
+}
+
+document.getElementById('start-game-button').onclick = () => {
+  hangman = new Hangman();
+
+  // HINTS (uncomment when start working on the canvas portion of the lab)
+  // hangman.secretWord = hangman.getWord();
+  // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+};
+
+document.onkeydown = e => {};
