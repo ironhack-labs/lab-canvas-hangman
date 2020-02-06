43f29a70f3732ec24d5b93dfca9a32c35a7bfa57
Friso <fhoman@gmail.com>
2020-02-06 18:32:58 +0100

    done

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,93 @@
-let hangman;
+class Hangman {
+  constructor() {
+    this.words = ['olifant', 'nijlpaard', 'gorilla'];
+    this.secretWord = this.getWord();
+    this.letters = []
+    this.guessedLetter = [];
+    this.errorsLeft = 10;
+  }
 
-// class Hangman {
-//   constructor() {
+  getWord() {
+    return this.words[Math.floor(Math.random(this.words.length) * this.words.length)]
+  }
 
-//   }
+  checkIfLetter(e) {
+    if (e.keyCode > 64 && e.keyCode < 91) {
+      return true
+    } else {
+      return false;
+    }
+  }
 
-//   getWord() {
+  checkClickedLetters(key) {
+    if (this.letters.includes(key)) {
+      return false;
+    } else {
+      this.letters.push(key);
+      return true;
+    }
+  }
 
-//   }
+  addCorrectLetter(letter) {
+    let arrIndex = [];
+    for (var i = 0; i < this.secretWord.length; i++) {
+      if (letter === this.secretWord[i]) {
+        this.guessedLetter[i] = this.secretWord[i];
+        arrIndex.push(i)
+      }
+    }
+    return arrIndex;
+  }
 
-//   checkIfLetter(keyCode) {
 
-//   }
+  addWrongLetter(letter) {
+    if (!this.secretWord.includes(letter)) {
+      this.errorsLeft--
+      this.checkGameOver()
+    }
+  }
 
-//   checkClickedLetters(key) {
+  checkGameOver() {
+    if (this.errorsLeft < 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-//   }
+  checkWinner() {
+    if (this.guessedLetter.length === this.secretWord.length) {
+      return true
+    } else {
+      return false;
+    }
+  }
+}
 
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
-// }
-
+const hangman = new Hangman();
+const hangmanCanvas = new HangmanCanvas(hangman.secretWord)
 document.getElementById('start-game-button').onclick = () => {
-  hangman = new Hangman();
+  document.getElementById('start-container').style.display = 'none';
+  hangmanCanvas.createBoard()
+  hangmanCanvas.drawLines()
 };
 
 document.onkeydown = (e) => {
+  if (hangman.checkIfLetter(e)) {
+    if (hangman.checkClickedLetters(e.key)) {
+      hangman.checkClickedLetters(e.key)
+      hangman.addCorrectLetter(e.key)
+      hangmanCanvas.writeCorrectLetter(hangman.guessedLetter)
+      hangman.addWrongLetter(e.key);
+      hangmanCanvas.writeWrongLetter(hangman.letters);
+      hangmanCanvas.drawHangman(hangman.errorsLeft);
+      if (hangman.checkGameOver()) {
+        hangmanCanvas.gameOver()
+      }
+      if (hangman.checkWinner()) {
+        hangmanCanvas.winner()
+      }
+    }
+  }
 
-};
+}
