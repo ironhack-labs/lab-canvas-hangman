42fa6bb79d37599a79b809da4533d3e69d811f03
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-03-06 09:53:11 -0300

    add constructor, createBoard and drawLines

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,34 +1,43 @@
 class HangmanCanvas {
-  constructor(secretWord) {
-    this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
-  }
+	constructor(secretWord) {
+		this.context = document.getElementById("hangman").getContext("2d");
+		this.secretWord = secretWord;
+	}
 
-  createBoard() {
-    // ... your code goes here
-  }
+	createBoard() {
+		this.context.clearRect(0, 0, 1200, 800);
+		this.drawLines();
+	}
 
-  drawLines() {
-    // ... your code goes here
-  }
+	drawLines() {
+        // this.context.strokeRect(10, 10, 200, 200);
+        console.log(this.secretWord)
+		for (let i = 0; i < this.secretWord.length; i++) {
+			this.context.lineWidth = 2;
+			this.context.beginPath();
+			this.context.moveTo(20 + 100 * i, 120);
+			this.context.lineTo(80 + 100 * i, 120);
+			this.context.stroke();
+		}
+	}
 
-  writeCorrectLetter(index) {
-    // ... your code goes here
-  }
+	writeCorrectLetter(index) {
+        
+	}
 
-  writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
-  }
+	writeWrongLetter(letter, errorsLeft) {
+		// ... your code goes here
+	}
 
-  drawHangman(errorsLeft) {
-    // ... your code goes here
-  }
+	drawHangman(errorsLeft) {
+		// ... your code goes here
+	}
 
-  gameOver() {
-    // ... your code goes here
-  }
+	gameOver() {
+		// ... your code goes here
+	}
 
-  winner() {
-    // ... your code goes here
-  }
+	winner() {
+		// ... your code goes here
+	}
 }
