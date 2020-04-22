07b22a9e5b305ab7ac7fac3780b3f9b8f56b1b96
Alba Olveros <x8a.018@gmail.com>
2020-04-22 17:23:07 +0200

    Logic of the game finished.

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,60 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 10;
   }
 
   pickWord() {
-    // ... your code goes here
+    return this.words[Math.floor(Math.random() * this.words.length)];
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    if(keyCode >= 65 && keyCode <= 90) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+    if(!this.letters.includes(letter)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+    this.guessedLetters += letter;
+    this.checkWinner();
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+    this.errorsLeft--;
+    this.letters.push(letter);
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if(this.errorsLeft == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkWinner() {
-    // ... your code goes here
+
+    let guessedOrdered = [...this.guessedLetters].sort().join("");
+    let secretOrdered = [...this.secretWord].sort().join("");
+
+    if(guessedOrdered == secretOrdered) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
