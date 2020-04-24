44b422d6b6cc7f8c59e55e17d3ac370782eff993
irene villanueva <irens.vm@gmail.com>
2020-04-24 17:02:17 +0200

    starting

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,3 +1,5 @@
+// lo visual
+
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
@@ -5,7 +7,9 @@ class HangmanCanvas {
   }
 
   createBoard() {
-    // ... your code goes here
+    this.context.beginPath()
+    this.context.clearRect(0, 0, 1200, 800);
+    this.drawLines();
   }
 
   drawLines() {
@@ -21,14 +25,89 @@ class HangmanCanvas {
   }
 
   drawHangman(errorsLeft) {
-    // ... your code goes here
+
+    if (errorsLeft <= 9) {
+      this.context.beginPath();
+      this.context.arc(230, 90, 50, 0, Math.PI * 2);
+      this.context.stroke();
+    }
+    if (errorsLeft <= 8) {
+      this.context.beginPath();
+      this.context.moveTo(230, 140);
+      this.context.lineTo(230, 300);
+      this.context.stroke();
+    }
+    if (errorsLeft <= 7) {
+      this.context.beginPath();
+      this.context.moveTo(230, 300);
+      this.context.lineTo(260, 420);
+      this.context.stroke();
+    }
+    if (errorsLeft <= 6) {
+      this.context.beginPath();
+      this.context.moveTo(230, 300);
+      this.context.lineTo(200, 420);
+      this.context.stroke();
+      if (errorsLeft <= 5) {
+        this.context.beginPath();
+        this.context.moveTo(230, 160);
+        this.context.lineTo(260, 280);
+        this.context.stroke();
+      }
+      if (errorsLeft <= 4) {
+        this.context.beginPath();
+        this.context.moveTo(230, 160);
+        this.context.lineTo(200, 280);
+
+        this.context.stroke();
+      }
+      if (errorsLeft <= 3) {
+
+      }
+      if (errorsLeft <= 2) {
+
+      }
+      if (errorsLeft <= 1) {
+
+      }
+      if (errorsLeft <= 0) {
+      }
+    }
+
+
+
+    // canvas.addEventListener('click', function(event){
+    //
+    // 	console.dir(event);
+    // 	this.context.fillRect(event.offsetX, event.offsetY, 10, 10);
+    // 	console.log('hello');
+    //
+    // 	functionArray[counter]();
+    // 	counter++;
+    // });
+
   }
 
+
+
+  // ... your code goes here
+
+  // funciones hagman por partes
+
+
   gameOver() {
-    // ... your code goes here
+    let fail = new Image();
+    fail.scr= '/images/gameover.png';
+    fail.addEventListener('load', e => {
+      this.context.drawImage(fail, 340, 110);
+    })
+
   }
 
   winner() {
-    // ... your code goes here
-  }
+    let youwin = new Image();
+    youwin.src='/images/awesome.png'
+    youwin.addEventListener('load', e => {
+      this.context.drawImage(fail, 340, 110);
+    })  }
 }
