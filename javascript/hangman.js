b0bdb6f0464637efbd2f80f1357839cb29eba36b
Kevin Hernandez <cobra40@gmail.com>
2020-06-30 16:19:27 -0400

    added comments and testing

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,37 +1,37 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];;
-    this.letters = [];
-    this.guessedLetters = '';
-    this.errorsLeft = 10;
+    this.secretWord = ''; // word to guess
+    this.letters = []; //wrong guesses
+    this.guessedLetters = ''; //correct guesses
+    this.errorsLeft = 10; //chances to guess wrong
   }
-
+  //pick the word to guess
   pickWord() {
-    return this.secretWord;
+    return this.words[Math.floor(Math.random() * this.words.length)];
   }
-
+  //check if key event is a letter
   checkIfLetter(keyCode) {
     return keyCode <= 90 && keyCode >= 65;
   }
-
+  //check if key event is a past wrong guess
   checkClickedLetters(letter) {
     return !this.letters.includes(letter);
   }
-
+  //add the key event to a string of correct guesses
   addCorrectLetter(letter) {
-    this.guessedLetters += letter;
+    if(this.secretWord.includes(letter)) this.guessedLetters += letter;
   }
-
+  //add key event to the array of wrong guesses and lower the attempts by one
   addWrongLetter(letter) {
     this.letters.push(letter);
     this.errorsLeft--;
   }
-
+  //check if there are any more attempts left
   checkGameOver() {
     return this.errorsLeft === 0;
   }
-
+  //check is the word was guessed correctly
   checkWinner() {
     let secrectWord = this.secretWord.split('').sort();
     let guessedWord = this.guessedLetters.split('').sort();
@@ -40,6 +40,7 @@ class Hangman {
 }
 
 let hangman;
+let keyEvent;
 
 const startGameButton = document.getElementById('start-game-button');
 
@@ -49,23 +50,23 @@ if (startGameButton) {
 
     hangman.secretWord = hangman.pickWord();
     hangmanCanvas = new HangmanCanvas(hangman.secretWord);
-    hangmanCanvas.createBoard();
-
+    console.log(hangman.secretWord);
+    if(hangman.secretWord.includes(keyEvent)) {
+      hangman.addCorrectLetter(keyEvent);
+      console.log('correct', keyEvent);
+    } 
+    if(!hangman.secretWord.includes(keyEvent)){
+      hangman.addWrongLetter(keyEvent);
+      console.log('wrong', keyEvent);
+    }
+    hangmanCanvas.createBoard();    
   });
 }
 
 document.addEventListener('keydown', event => {
   // React to user pressing a key
-  let theWord = hangman.secrectWord;
-  if (hangman.checkIfLetter(event.keyCode) && hangman.checkClickedLetters(event.key)) {
-    
-    if(theWord.includes(event.key)){
-      console.log(event.key, event.keyCode);
-    //   hangman.addCorrectLetter(event.key);
-      // hangmanCanvas.writeCorrectLetter(hangman.secrectWord.indexOf(eventKey));
-    }//else{
-    //   hangman.addWrongLetter(event.key);
-    // }
-  }
-    
+  if (hangman.checkIfLetter(event.keyCode) && hangman.checkClickedLetters(event.key)){
+    keyEvent = event.key;
+    console.log('key', keyEvent);
+  }    
 });
