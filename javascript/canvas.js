8f4b8212c341aaacc7a6db76fe8c5f731ec7b249
Raquel Nascimento <raquel.fm.nascimento@gmail.com>
2020-07-01 22:04:57 +0100

    done until The HangmanCanvas methods

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,22 +1,44 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
+    this.secretWord = secretWord
+
     // ... your code goes here
   }
 
   createBoard() {
-    // ... your code goes here
+    this.context.clearRect(0,0,800,1200);
+    this.drawLines()
   }
 
   drawLines() {
-    // ... your code goes here
+       var x = 100;
+       var y = 600;
+    this.context.beginPath();
+     for (let i=0; i<this.secretWord.length; i++) {
+       this.context.moveTo(x,y);
+       this.context.lineTo(x+30,y);
+       x+=45;
+     }
+     this.context.stroke();
   }
 
   writeCorrectLetter(index) {
-    // ... your code goes here
+    this.context.fillRect(10, 10, 280, 280);
+    var x = 100;
+    var y = 600;
+    let letter = this.secretWord[index];
+    for (let i=0; i<this.secretWord.length; i++) {
+      if (letter===this.secretWord[i]) {
+        this.context.fillText(letter,x+(i*45),y-20)
+      } else {
+        console.log ("error")
+      }
+    }
   }
 
   writeWrongLetter(letter, errorsLeft) {
+    this.context.fillRect(10, 10, 280, 280);
     // ... your code goes here
   }
 
