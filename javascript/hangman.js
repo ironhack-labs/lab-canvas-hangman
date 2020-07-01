7d4476753aa5d2718459b0d0d126d3a1a9fce059
Mikayla Castro <mikaylacastro97@gmail.com>
2020-07-01 13:34:49 -0400

    iteration 1 minus checkWinner

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,31 +1,49 @@
 class Hangman {
   constructor(words) {
     this.words = words;
+    this.secretWord = this.words[Math.floor(Math.random() * this.words.length)];
+    this.letters = [];
+    this.guessedLetters = '';
+    this.errorsLeft = 10;
     // ... your code goes here
   }
 
   pickWord() {
     // ... your code goes here
+     return this.secretWord; 
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if(keyCode >= 65 && keyCode <= 90){
+      return true;
+    } return false;
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    if(this.letters.includes(letter)){
+      return false;
+    } return true;
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    if (!this.guessedLetters.includes(letter)) {
+      this.guessedLetters += letter;
+    }
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft--;
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft>0){
+      return false;
+    } return true;
   }
 
   checkWinner() {
