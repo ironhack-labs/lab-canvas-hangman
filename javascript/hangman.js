829a32b06c10a4405cb94801780a8bd05d639234
allecramarcella <allecramarcella.dev@gmail.com>
2020-07-28 21:18:10 +0200

    part 1

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,72 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = this.pickWord()
+    this.letters = []
+    this.guessedLetters = ''
+    this.errorsLeft = 10
   }
 
   pickWord() {
-    // ... your code goes here
+    const pickedWord = this.words[Math.floor(Math.random() * this.words.length)]
+    return pickedWord
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if(keyCode >= 65 && keyCode <= 90) {
+      return true
+    } else {
+      return false
+    }
   }
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+    if(this.letters.includes(letter)) {
+      return false
+    } else {
+      return true
+    }
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+    this.guessedLetters = letter
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+    this.errorsLeft -= 1
+    if(!this.letters.includes(letter)){
+      this.letters.push(letter)
+    }
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if(this.errorsLeft > 0) {
+      return false
+    } else {
+      return true
+    }
   }
 
   checkWinner() {
-    // ... your code goes here
+    function alphabet_order(str) {
+      return str.split('').sort().join('');
+    }
+
+    const secretWordAlpha = alphabet_order(this.secretWord)
+    const guessedLettersAlpha = alphabet_order(this.guessedLetters)
+
+    // console.log(test)
+    // console.log(test2)
+
+    // const gameOver = this.checkGameOver()
+
+    if(secretWordAlpha === guessedLettersAlpha) {
+      return true
+    } else {
+      return false
+    }
+  
   }
 }
 
