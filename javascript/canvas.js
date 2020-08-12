743b2574ed0b5f796a1167023cfc62622438441e
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2020-08-12 15:59:30 -0500

    Todavia no está completo

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,19 +1,33 @@
 class HangmanCanvas {
   constructor(secretWord) {
-    this.context = document.getElementById('hangman').getContext('2d');
+    this.canvas = document.getElementById('hangman')
+    this.context = this.canvas.getContext('2d');
     // ... your code goes here
+    this.secretWord = secretWord
   }
 
   createBoard() {
     // ... your code goes here
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.drawLines()
   }
 
   drawLines() {
     // ... your code goes here
+    let numberOfLines = this.secretWord.length
+    this.context.strokeStyle = 'black'
+    this.context.beginPath()
+    for(let i = 0; i < numberOfLines; i++){
+      this.context.moveTo(60 + (i+1)*40, 760)
+      this.context.lineTo(60 + (i+1)*40 + 35, 760)
+    }
+    this.context.stroke()
+    this.context.closePath()
   }
 
   writeCorrectLetter(index) {
     // ... your code goes here
+    
   }
 
   writeWrongLetter(letter, errorsLeft) {
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,66 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = words[(Math.ceil(Math.random()*(words.length - 1)))];
+    this.letters = [],
+    this.guessedLetters = '',
+    this.errorsLeft = 10
   }
 
   pickWord() {
     // ... your code goes here
+    return this.words[(Math.ceil(Math.random()*(this.words.length - 1)))];
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if(keyCode>=65 && keyCode<=90){
+      return true
+    } else {
+      return false
+    }
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    let clickedLetter = true
+    this.letters.forEach(element => {
+      if(element === letter){
+        clickedLetter = false
+      }
+    })
+     return clickedLetter
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    if(this.checkClickedLetters(letter)===true){
+      this.guessedLetters += letter
+      this.letters.push(letter)
+    }
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    if(this.checkClickedLetters(letter)===true){
+      this.letters.push(letter)
+      this.errorsLeft --
+    }
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft > 0){
+      return false
+    } else {
+      return true
+    }
   }
 
   checkWinner() {
     // ... your code goes here
+    let secretWordArray = this.secretWord.split('')
+    return (this.guessedLetters).includes(...secretWordArray)
   }
 }
 
@@ -42,10 +74,12 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    // Cómo hace hangman,js para leer la clase HangmanCanvas, dentro del archivo CanvasGradient.js?
 
     // ... your code goes here
+    hangmanCanvas.drawLines()
   });
 }
 
