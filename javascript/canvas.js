2b836d663942a035e8df2656231bbd56c70c65f5
Mai Yamawaki <joooooy031006@icloud.com>
2020-08-16 15:44:07 -0500

    terminamos con bonus

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,34 +1,152 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
-  }
-
-  createBoard() {
-    // ... your code goes here
-  }
-
-  drawLines() {
-    // ... your code goes here
-  }
-
-  writeCorrectLetter(index) {
-    // ... your code goes here
-  }
-
-  writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
-  }
-
-  drawHangman(errorsLeft) {
-    // ... your code goes here
-  }
-
-  gameOver() {
-    // ... your code goes here
-  }
-
-  winner() {
-    // ... your code goes here
-  }
+    this.secretWord = secretWord
+    this.writeCorrectLetter = this.writeCorrectLetter
+    this.writeWrongLetter = this.writeWrongLetter
+    this.drawHangman = this.drawHangman
+        // ... your code goes here
 }
+
+createBoard() {
+    this.context.clearRect()
+    this.drawLines()
+        // ... your code goes here
+}
+
+drawLines() {
+    let counter = 0;
+
+    while (counter < this.secretWord.length) {
+        this.context.beginPath();
+        this.context.moveTo(counter * 30 + 20, 500);
+        this.context.lineTo(counter * 30, 500)
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+
+        counter++
+    }
+}
+
+writeCorrectLetter(index) {
+    // ... your code goes here
+    this.context.font = "30px Arial";
+    console.log('correct', this.secretWord.split('')[index])
+
+    return this.context.fillText(this.secretWord.split('')[index], index * 30, 480)
+}
+
+writeWrongLetter(letter, errorsLeft) {
+    // ... your code goes here
+    this.context.font = "30px Arial";
+    console.log('errorsleft', errorsLeft)
+
+    return this.context.fillText(letter, 500 - (errorsLeft * 25), 50)
+}
+
+drawHangman(errorsLeft) {
+    // ... your code goes here
+    if (errorsLeft === 9) {
+        this.context.beginPath();
+        this.context.moveTo(20, 400);
+        this.context.lineTo(100, 400)
+        this.context.lineTo(60, 350)
+        this.context.closePath()
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+
+    if (errorsLeft === 8) {
+        this.context.beginPath();
+        this.context.moveTo(60, 350);
+        this.context.lineTo(60, 100)
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    if (errorsLeft === 7) {
+        this.context.beginPath();
+        this.context.moveTo(60, 100);
+        this.context.lineTo(300, 100)
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    if (errorsLeft === 6) {
+        this.context.beginPath();
+        this.context.moveTo(300, 100);
+        this.context.lineTo(300, 150)
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    if (errorsLeft === 5) {
+        this.context.beginPath();
+        this.context.moveTo(300, 150);
+        this.context.arc(300, 180, 30, 0, Math.PI * 2)
+        this.context.fill()
+        this.context.lineWidth = 5;
+        // this.context.stroke();
+        this.context.closePath();
+    }
+
+    if (errorsLeft === 4) {
+        this.context.beginPath();
+        this.context.moveTo(300, 210);
+        this.context.lineTo(300, 245)
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    if (errorsLeft === 3) {
+        this.context.beginPath();
+        this.context.moveTo(300, 245);
+        this.context.lineTo(300, 300)
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    if (errorsLeft === 2) {
+        this.context.beginPath();
+        this.context.moveTo(300, 300);
+        this.context.lineTo(270, 340)
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    if (errorsLeft === 1) {
+        this.context.beginPath();
+        this.context.moveTo(300, 300);
+        this.context.lineTo(330, 340)
+        this.context.lineWidth = 5;
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+}
+
+gameOver() {
+    // ... your code goes here
+    this.context.font = "60px Arial";
+    this.context.fillStyle="red"
+    this.context.fillText("GAME OVER", 200, 250)
+}
+
+winner(index) {
+    // ... your code goes here
+    if(this.secretWord.includes(this.secretWord.split('')[index])){
+        this.context.font = "60px Arial";
+        this.context.fillStyle="skyblue"
+        this.context.fillText("YOU WIN", 200, 250)
+    }
+}
+} 
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,55 +1,91 @@
-class Hangman {
+class Hangman{
   constructor(words) {
     this.words = words;
-    // ... your code goes here
-  }
+    this.secretWord = (this.words[Math.floor(Math.random() * this.words.length)]);
+    this.letters = [];
+    this.guessedLetters = '';
+    this.errorsLeft = 10;
+}
 
-  pickWord() {
+pickWord() {
     // ... your code goes here
-  }
+    return this.words[Math.floor(Math.random() * this.words.length)]
+}
 
-  checkIfLetter(keyCode) {
+checkIfLetter(keyCode) {
     // ... your code goes here
-  }
+    if (keyCode >= 65 && keyCode <= 90) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-  checkClickedLetters(letter) {
-    // ... your code goes here
-  }
+checkClickedLetters(letter) {
+    return !this.letters.includes(letter);
+}
 
-  addCorrectLetter(letter) {
-    // ... your code goes here
-  }
+addCorrectLetter(keyCode) {
+    this.guessedLetters = keyCode;
+}
 
-  addWrongLetter(letter) {
-    // ... your code goes here
-  }
+addWrongLetter(letter) {
+    this.errorsLeft--;
+    this.letters.push(letter)
+}
 
-  checkGameOver() {
-    // ... your code goes here
-  }
+checkGameOver() {
+    if (this.errorsLeft > 0) {
+        return false
+    } else {
+        return true
+    }
+}
 
-  checkWinner() {
-    // ... your code goes here
-  }
+checkWinner() {
+    for (let i = 0; i < this.secretWord.length; i++) {
+        if (this.guessedLetters.includes(this.secretWord[i])) {
+            return true
+        } else {
+            return false;
+        }
+    }
+}
 }
 
 let hangman;
+let hangmanCanvas;
 
 const startGameButton = document.getElementById('start-game-button');
 
 if (startGameButton) {
-  startGameButton.addEventListener('click', event => {
-    hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
-
-    // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
-
-    // ... your code goes here
+    startGameButton.addEventListener('click', event => {
+        hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
+        hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+        hangman.secretWord = hangman.pickWord();
+        hangmanCanvas.drawLines()
   });
 }
 
+
+
 document.addEventListener('keydown', event => {
-  // React to user pressing a key
-  // ... your code goes here
+    const index = hangmanCanvas.secretWord.split('').indexOf(event.key);
+    console.log(hangmanCanvas.secretWord)
+
+    if (index === -1) {
+        hangman.errorsLeft = hangman.errorsLeft - 1;
+        console.log('errors left', hangman.errorsLeft)
+        hangmanCanvas.drawHangman(hangman.errorsLeft)
+        hangmanCanvas.writeWrongLetter(event.key, hangman.errorsLeft - 1)
+        if(hangman.errorsLeft===0){
+          hangmanCanvas.gameOver()
+        }
+        } else {
+        hangmanCanvas.writeCorrectLetter(index)
+        hangman.addCorrectLetter(index)
+        if(hangman.guessedLetters===hangmanCanvas.secretWord){
+            hangmanCanvas.winner();
+        }
+      }
 });
