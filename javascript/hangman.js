7c768259a5a0ab47f59f4dc497f9463f88bfa3e7
Ute Wiemer <upwiemer@gmail.com>
2020-09-23 19:39:48 +0200

    iteration 1

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,37 +1,59 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 10;
   }
 
+
+
+
   pickWord() {
-    // ... your code goes here
+    let number = Math.floor(Math.random() * this.words.length)
+    return this.words[number]
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    if (keyCode <= 90 && keyCode >= 65) { return true }
+    else { return false }
   }
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+    if (this.letters.includes(letter)) { return false }
+    else { return true }
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+    return this.guessedLetters = letter + this.guessedLetters
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+
+    if (this.letters.includes(letter)) { }
+    else {
+      this.letters.push(letter)
+      return this.errorsLeft = this.errorsLeft - 1
+    }
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if (this.errorsLeft > 0) { return false }
+    else { return true }
   }
 
   checkWinner() {
-    // ... your code goes here
+    let newGuessedLetters = this.guessedLetters.split('')
+    let newSecretWord = this.secretWord.split('')
+    for (let i = 0; i < newGuessedLetters.length; i++) {
+      if (!newGuessedLetters.includes(newSecretWord[i])) { return false } // if it does NOT include
+    }
+    return true
   }
 }
+
+
 
 let hangman;
 
@@ -42,8 +64,8 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  hangman.secretWord = hangman.pickWord();
+  hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
     // ... your code goes here
   });
