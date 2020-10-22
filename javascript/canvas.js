2e803b5f05bdc6a8a23658ac117eb4bcb3128aa0
CarlosRiojas <carlos.riojas88@gmail.com>
2020-10-21 20:56:51 -0500

    WD FT OCT2020

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,15 +1,40 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
-  }
 
+    // ... your code goes here
+    // document.querySelector()
+    //onClick = callback
+    //addEventListener('click', callback )
+    //setInterval(callback,milisecs)
+
+    this.secretWord = secretWord;
+  }
+  // the method that should clear the canvas, so every time we start the game we have a clean one.
+  //This method also should call the next one we will define, the drawLines().
   createBoard() {
     // ... your code goes here
+    this.context.clearRect(0,0, this.context.width, this.context.height)
+    this.drawLines()
   }
-
+  // the method that should draw one line for each letter of the secret word.
+  //At this point we know the secret word the user has to guess.
   drawLines() {
     // ... your code goes here
+    // lineTo(1, 1);
+    // this.context.fillStyle = 'purple';
+    // this.context.fillRect(260, 260, 30, 30);
+    let moveX = 200
+    let y = 400
+    for(let i = 0; i < this.secretWord.length ;i++){
+      this.context.beginPath()
+      this.context.moveTo(moveX,y)
+      this.context.lineTo(moveX+40, y)
+      this.context.closePath()
+      this.context.stroke()
+      moveX+=50
+    }
+
   }
 
   writeCorrectLetter(index) {
@@ -32,3 +57,9 @@ class HangmanCanvas {
     // ... your code goes here
   }
 }
+
+hangmanCanvas = new HangmanCanvas("hangmanSecretWord");
+
+hangmanCanvas.createBoard()
+debugger
+
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,54 +1,60 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = this.pickWord(); //returs a random strring of lettterrs
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 10;
   }
-
   pickWord() {
-    // ... your code goes here
+    let random = Math.floor(Math.random() * this.words.length);
+    return this.words[random];
   }
-
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    return keyCode >= 65 && keyCode <= 90
+  }
+  checkClickedLetters(letter) {
+    return !this.letters.includes(letter)
   }
 
-  checkClickedLetters(letter) {
-    // ... your code goes here
-  }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+
+    return this.guessedLetters += letter;
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
-  }
+    this.errorsLeft -= 1 //should subtract one from the variable errorsLeft.
+    if (!this.letters.includes(letter)) { //It also should push this letter in the array of letters if the letter is not there already.
+      this.letters.push(letter)
+    }
 
+  }
   checkGameOver() {
     // ... your code goes here
+    return !this.errorsLeft > 0;
   }
-
+  //a method that should check if the user won and return the corresponding boolean value.
   checkWinner() {
-    // ... your code goes here
-  }
+      if (this.secretWord.length===this.guessedLetters.length){
+        return true
+      } else{
+        return false
+      }
+    }
 }
-
 let hangman;
-
 const startGameButton = document.getElementById('start-game-button');
-
 if (startGameButton) {
   startGameButton.addEventListener('click', event => {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
-
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
-
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
     // ... your code goes here
   });
 }
-
 document.addEventListener('keydown', event => {
   // React to user pressing a key
   // ... your code goes here
