31c807be3ac5ad23f32d62090233dac141b58ea1
alison <alisonadriel@hotmail.com>
2020-11-24 18:30:21 -0300

    complete

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,34 +1,130 @@
 class HangmanCanvas {
   constructor(secretWord) {
+    this.secretWord = secretWord;
     this.context = document.getElementById('hangman').getContext('2d');
     // ... your code goes here
   }
 
   createBoard() {
-    // ... your code goes here
+    this.context.clearRect(0, 0, 800, 1200);
+
+
   }
 
   drawLines() {
-    // ... your code goes here
+    this.context.beginPath();
+    for (let i = 0; i < this.secretWord.length; i += 1) {
+      this.context.moveTo(200 + i * 30, 380); //100 130
+      this.context.lineTo(220 + i * 30, 380); //120  150  
+    }
+    this.context.stroke();
+    this.context.closePath();
   }
 
   writeCorrectLetter(index) {
-    // ... your code goes here
+
+    this.context.font = '20px arial';
+    this.context.lineWidth = 5;
+    this.context.fillStyle = 'blue';
+
+    for (let i = 0; i < this.secretWord.length; i += 1) {
+      if (this.secretWord[i] == index) {
+        this.context.fillText(index, 200 + i * 30, 370);
+      }
+    }
   }
 
   writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
+
+    console.log('estou aqui!!!!');
+    console.log(hangman.letters);
+    this.context.font = '20px arial';
+    this.context.lineWidth = 5;
+    this.context.fillStyle = 'blue';
+    this.context.fillText(letter, 500 - errorsLeft * 30, 50);
+    this.drawHangman(errorsLeft);
   }
 
   drawHangman(errorsLeft) {
-    // ... your code goes here
+    console.log('estou aqui agora')
+    console.log(errorsLeft);
+
+    this.context.beginPath();
+    this.context.moveTo(50, 380);
+    if (errorsLeft == 9) {
+      this.context.lineTo(150, 380);
+      this.context.stroke();
+    }
+    this.context.moveTo(150, 380);
+    if (errorsLeft == 8) {
+      this.context.lineTo(100, 300);
+      this.context.stroke();
+    }
+    this.context.moveTo(100, 300);
+    if (errorsLeft == 7) {
+      this.context.lineTo(50, 380);
+      this.context.stroke();
+    }
+    this.context.moveTo(100, 300);
+    if (errorsLeft == 6) {
+      this.context.lineTo(100, 100);
+      this.context.stroke();
+    }
+    this.context.moveTo(100, 100);
+    if (errorsLeft == 5) {
+      this.context.lineTo(250, 100);
+      this.context.stroke();
+    }
+    this.context.moveTo(250, 100);
+    if (errorsLeft == 4) {
+      this.context.lineTo(250, 120);
+      this.context.stroke();
+    }
+    this.context.moveTo(250, 120);
+
+    if (errorsLeft == 3) {
+      this.context.arc(250, 140, 20, 0, Math.PI * 2);
+      this.context.stroke();
+    }
+    this.context.moveTo(250, 155);
+    if (errorsLeft == 2) {
+      this.context.lineTo(250, 260);
+      this.context.stroke();
+    }
+    this.context.moveTo(250, 260);
+    if (errorsLeft == 1) {
+      this.context.lineTo(230, 300);
+      this.context.stroke();
+    }
+    this.context.moveTo(250, 260);
+    if (errorsLeft == 0) {
+      this.context.lineTo(270, 300);
+      this.context.stroke();
+    }
   }
 
+  drawImage() {
+    this.context.clearRect(0, 0, 800, 1200);
+    const img = new Image();
+    img.src = './images/awesome.png'
+    this.context.drawImage(img, 100, 0, 300, 400);
+
+  }
   gameOver() {
-    // ... your code goes here
+
+    let gameOver = document.getElementById('game');
+
+    gameOver.innerHTML = 'Game Over';
+
+    this.createBoard();
+
   }
 
   winner() {
-    // ... your code goes here
+
+    let winner = document.getElementById('game');
+
+    winner.innerHTML = 'Winner';
+    this.drawImage();
   }
 }
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,38 +1,77 @@
+
+
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters = '';
+    this.errorsLeft = 10;
   }
-
   pickWord() {
-    // ... your code goes here
+    return this.words[Math.floor(Math.random() * this.words.length)]
   }
-
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    if (65 <= keyCode && keyCode <= 90) {
+      return true;
+    }
+    console.log('voce não digitou uma letra');
+    return false;
   }
-
   checkClickedLetters(letter) {
-    // ... your code goes here
-  }
+    console.log('entrei checkClikedLetters');
+    if (this.letters.includes(letter)) {
 
+      /* let li = document.createElement('li');
+       li.innerHTML = 'Você já digitou a letra' + ' '+ letter;
+       let parent = document.getElementsByTagName('ul')[0];
+       parent.appendChild(li);*/
+
+      console.log('Você já digitou essa letra');
+      return false;
+    }
+    this.letters.push(letter);
+    if (this.secretWord.includes(letter)) {
+      this.addCorrectLetter(letter);
+    } else {
+      this.addWrongLetter(letter);
+    }
+
+    return true;
+  }
   addCorrectLetter(letter) {
-    // ... your code goes here
+    console.log('entrei no addCorrectLtter');
+    this.guessedLetters += letter;
+    console.log(this.guessedLetters);
+    this.checkWinner();
+    hangmanCanvas.writeCorrectLetter(letter);
   }
-
   addWrongLetter(letter) {
-    // ... your code goes here
+    console.log('entrei no addWrongLetter');
+    this.errorsLeft -= 1
+    this.checkGameOver();
+    hangmanCanvas.writeWrongLetter(letter, this.errorsLeft);
   }
 
   checkGameOver() {
-    // ... your code goes here
-  }
+    console.log('entrei no game over')
 
+    if (this.errorsLeft > 0) return false;
+    hangmanCanvas.gameOver();
+    return true;
+  }
   checkWinner() {
-    // ... your code goes here
+    let check = [];
+    for (let i = 0; i < this.secretWord.length; i += 1) {
+      check.push(this.guessedLetters.includes(this.secretWord[i]));
+    }
+    console.log(check);
+
+    if (check.includes(false)) return false;
+    hangmanCanvas.winner();
+    return true;
   }
 }
-
 let hangman;
 
 const startGameButton = document.getElementById('start-game-button');
@@ -42,14 +81,21 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    let clearGame = document.getElementById('game');
+    clearGame.innerHTML = '';
+    hangman.secretWord = hangman.pickWord();
+    console.log(hangman.secretWord);
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    hangmanCanvas.createBoard();
+    hangmanCanvas.drawLines();
 
-    // ... your code goes here
   });
 }
-
 document.addEventListener('keydown', event => {
   // React to user pressing a key
-  // ... your code goes here
+  let keyCode = event.which;
+  let keyName = event.key;
+  hangman.checkIfLetter(keyCode);
+  hangman.checkClickedLetters(keyName);
+
 });
