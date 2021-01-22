765fdff5c4395c56fec508c1aac351827ad2c837
Alejandro Silva <a.s.cebreros@gmail.com>
2021-01-21 23:58:26 -0600

    ---

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,11 +1,15 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = ``
+    this.letters = []
+    this.guessedLetters = ``
+    this.errorsLeft = 10
   }
 
   pickWord() {
-    // ... your code goes here
+    return ``
+  // ... your code goes here
   }
 
   checkIfLetter(keyCode) {
