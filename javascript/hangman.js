5998e64078fdf3c2a88fa69a3b074d70d556373b
Rob TC <roberto.taboada@gmail.com>
2021-02-14 22:52:36 -0600

    Iteration 1: the game logic

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,72 +1,55 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    this.secretWord = "";
-    this.letters = [];
-    this.guessedLetters = "";
-    this.errorsLeft = 10;
-    
+    this.secretWord=""
+    this.errorsLeft=10
+    this.guessedLetters=""
+    this.letters=[]
   }
 
+  pickWord() {
+    let index = Math.floor(Math.random()*this.words.length)
 
-  pickWord(words) {
-    //a method that returns a random word from the array of words
-    
-    let randomIndex = Math.floor(Math.random()*words.length)
-
-    let wordPicked = words[randomIndex]
-    return String(wordPicked);
-
+    let wordPicked = words[index]
+    return wordPicked.toString()
   }
 
   checkIfLetter(keyCode) {
-//a method that returns true or false depending on the keyCode of the key pressed by the user: if the keyCode corresponds to a character from a-z, it should return true, otherwise, it should return false. You can use keycode.info to find out which codes refer to each key.
-   
-    let keyCheck = keyCode;
-    if (keyCheck >63 && keyCheck <91) {
-      return true;
-    } else {
-      return false; 
-    }
+      if(keyCode >=65 && keyCode<=90){
+        return true
+      } else {return false}
   }
 
   checkClickedLetters(letter) {
-    // a method that should check if the letter passed as an argument has already been pressed. It should return true if it was not or false in the opposite case.
-
-    let letterClicked = letter
-
-    for(let i=0;i<this.letters.length;i++) {
-       if (this.letters[i] === letterClicked) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-
-
+    if(this.letters.includes(letter)){
+      return false
+    } else {return true}
   }
-
 
   addCorrectLetter(letter) {
-    //a method that should add the passed letter to the guessedLetters property. This could be a good place to check if the user won.
-    this.guessedLetters += letter;
-    
-  }
+    this.guessedLetters+=letter
 
+  }
 
   addWrongLetter(letter) {
-    //a method that should subtract one from the variable errorsLeft. It also should push this letter in the array of letters if the letter is not there already.
-    this.errorsLeft -=1
-    
-    // ... your code goes here
+  this.letters.push(letter)
+  this.errorsLeft-=1
   }
-/*
+
   checkGameOver() {
-    // ... your code goes here
+    if(this.errorsLeft>0){
+      return false
+    } else {return true}
   }
 
   checkWinner() {
-    // ... your code goes here
+    let sortedSecretWord = this.secretWord.split('').sort().join('')  
+    let sortedGuessedLetters = this.guessedLetters.split('').sort().join('')  
+
+    if(sortedGuessedLetters===sortedSecretWord) {
+      return true
+    } else {return false}
+  
   }
 }
 
@@ -84,12 +67,9 @@ if (startGameButton) {
 
     // ... your code goes here
   });
-
-  */
 }
-/*
+
 document.addEventListener('keydown', event => {
   // React to user pressing a key
   // ... your code goes here
 });
-*/
