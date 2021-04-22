6445e1dbfb74106e237334d6884d4f66d5e69a37
danpinon <danielpformoso@gmail.com>
2021-04-22 12:51:42 -0500

    lab hangman incomplete

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,27 +1,97 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
+    this.secretWord = secretWord
+
   }
 
   createBoard() {
     // ... your code goes here
+    this.context.clearRect(0,0,1200,800)
+    this.drawLines()
   }
 
   drawLines() {
     // ... your code goes here
+    for(let i = 0; i < this.secretWord.length; i++){
+      this.context.beginPath()
+      this.context.lineWidth = 2
+      this.context.moveTo(500 + 70 * i, 700)
+      this.context.lineTo(550 + 70 * i, 700)
+      this.context.strokeStyle = "black"
+      this.context.stroke()
+      this.context.closePath()
+    }
   }
+  
 
   writeCorrectLetter(index) {
     // ... your code goes here
+    if(index != -1){
+      this.context.font = "60px Arial"
+      this.context.fillText(this.secretWord[index],500 + 70 * [index], 700)
+    }
+  
   }
 
   writeWrongLetter(letter, errorsLeft) {
     // ... your code goes here
+    //
+    if(errorsLeft > 0){
+      let index = 10 - errorsLeft
+      let width = index % 5
+      let height = Math.floor(index/5)
+      this.context.font = "60px Arial"
+      this.context.fillText(letter,500 + 100 * width, height * 100 + 100)
+    }
+    
+    
   }
+  
 
   drawHangman(errorsLeft) {
     // ... your code goes here
+    //pintar ahorado
+    //cada vez que hay un error debe pintarse una linea
+    //cada pintada depende del error left que yo tenga
+    //switch
+//path1
+    //9
+    this.context.beginPath()
+      this.context.moveTo(50, 700)
+      this.context.lineTo(200, 700)
+      this.context.lineTo(200-75, 650)
+      this.context.lineTo(50, 700)
+      this.context.stroke()
+    this.context.closePath()
+
+    //8
+    this.context.beginPath()
+      this.context.moveTo(200-75, 650)
+      this.context.lineTo(200-75, 100)
+      this.context.stroke()
+    this.context.closePath()
+
+    //7
+    this.context.beginPath()
+      this.context.moveTo(200-75, 100)
+      this.context.lineTo(300, 100)
+      this.context.stroke()
+    this.context.closePath()
+
+    //6
+    this.context.beginPath()
+      this.context.moveTo(300, 100)
+      this.context.lineTo(300, 200)
+      this.context.stroke()
+    this.context.closePath()
+
+    //5
+    this.context.beginPath()
+      this.context.arc(300, 300, 50, 0, 3.14* 2)
+      this.context.stroke()
+    this.context.closePath()
+
   }
 
   gameOver() {
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,88 @@
 class Hangman {
   constructor(words) {
-    this.words = words;
+
+    this.words = words; // ["mexico", "javascript", "agua"]
     // ... your code goes here
+    this.secretWord = this.pickWord()
+    this.letters = []
+    this.guessedLetters = ""
+    this.errorsLeft = 10
   }
 
   pickWord() {
     // ... your code goes here
+    // 1. TENGO UN ARREGLO
+    // 2. DE ESE ARREGLO TENGO QUE ELEGIR UNA PALABRA ALEATORIA
+    // 0            1           2
+    // ["mexico", "javascript", "agua"] - Random
+
+    let randomNumber = Math.floor(Math.random() * this.words.length)
+    return this.words[randomNumber]
   }
+
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if(keyCode >= 65 && keyCode <= 90){
+      return true
+    } else {
+      return false
+    }
   }
+
+
+  
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    return !this.letters.includes(letter)
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+      this.guessedLetters += letter
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft--
   }
 
   checkGameOver() {
     // ... your code goes here
+    return this.errorsLeft === 0? true : false
   }
 
   checkWinner() {
     // ... your code goes here
+    // todas las letras de secret word === a las de guessedLetters
+    // cuando idex of no encuentra nada regresa un -1
+
+    // if(this.guessedLetters.indexOf(letter) === -1){
+        //   return false
+        // } else {
+        //   return true
+        // }
+
+
+    // for(let letter of this.secretWord ){
+      
+    //   return this.guessedLetters.indexOf(letter) != -1? true : false
+        
+    // }
+
+    const secretArray = Array.from(this.secretWord)
+    const guessedArray = Array.from(this.guessedLetters)
+
+    let matches = 0
+
+    secretArray.forEach((letter) => {
+      if(guessedArray.includes(letter)){
+        matches++
+      }
+    })
+
+return matches === secretArray.length
   }
 }
 
@@ -42,8 +95,14 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    console.log(hangman.secretWord)
+    hangmanCanvas.createBoard()
+   
+    
+
+    hangmanCanvas.drawHangman(hangmanCanvas.errorsLeft)
 
     // ... your code goes here
   });
@@ -52,4 +111,28 @@ if (startGameButton) {
 document.addEventListener('keydown', event => {
   // React to user pressing a key
   // ... your code goes here
+  hangman
+  hangmanCanvas
+  console.log(hangman.secretWord)
+  console.log(event.key)
+  correctLetter(event.key)
+  addIncorrect(event.key, hangman.errorsLeft)
+  // hangmanCanvas.writeCorrectLetter(hangman.secretWord.indexOf(event.key))
+  console.log(hangman.secretWord.indexOf(event.key))
 });
+
+function correctLetter(key){
+  for( let i = 0; i < hangman.secretWord.length; i++){
+    if(hangman.secretWord[i] == key){
+      hangmanCanvas.writeCorrectLetter(i)
+    }
+  }
+}
+
+function addIncorrect(key, errorsLeft){
+  if(hangman.secretWord.indexOf(key) === -1){
+    hangmanCanvas.writeWrongLetter(key, errorsLeft)
+    hangman.addWrongLetter(key)
+    console.log(`bien ${key} ${errorsLeft}`)
+  }
+}
