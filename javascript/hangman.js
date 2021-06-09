22e696fe6b78a5a32e56a62b82fb5a977210cfc8
Hannes Polte <hannes.polte@gmx.de>
2021-06-09 19:42:46 +0200

    started 1st iteration

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,19 +1,25 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = '';
+    this.letters = [];
+    this.guessedLetters = '';
+    this.errorsLeft = 10;
+
   }
 
   pickWord() {
-    // ... your code goes here
+    let random = this.words[Math.floor(Math.random() * this.words.length)]
+    this.secretWord = random;
+    return random;
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    return keyCode >= 65 && keyCode <= 90 ? true : false;
   }
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+    return this.letters.includes(letter) ? false : true;
   }
 
   addCorrectLetter(letter) {
@@ -33,6 +39,7 @@ class Hangman {
   }
 }
 
+
 let hangman;
 
 const startGameButton = document.getElementById('start-game-button');
@@ -40,6 +47,7 @@ const startGameButton = document.getElementById('start-game-button');
 if (startGameButton) {
   startGameButton.addEventListener('click', event => {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
+    hangman.pickWord();
 
     // HINT (uncomment when start working on the canvas portion of the lab)
     // hangman.secretWord = hangman.pickWord();
@@ -50,6 +58,19 @@ if (startGameButton) {
 }
 
 document.addEventListener('keydown', event => {
-  // React to user pressing a key
-  // ... your code goes here
-});
+  if (hangman.checkIfLetter(event.keyCode)) {
+    if (hangman.checkClickedLetters(event.key)) {
+      hangman.letters.push(event.key);
+    }
+
+
+    console.log(hangman.letters)
+    // React to user pressing a key
+    // ... your code goes here
+  }
+
+}
+)
+  ;
+
+
