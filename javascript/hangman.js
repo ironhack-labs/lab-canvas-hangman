fc4114efa8f8db8ea1008ea3400a4c867ab88d61
Julia Häußler <julia@haeuszler.de>
2021-06-15 22:01:33 +0200

    first iteration

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,59 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 10;
   }
 
   pickWord() {
-    // ... your code goes here
+    return this.words[Math.floor(Math.random()*this.words.length)];
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    if (keyCode >= 65 && keyCode <= 90) {
+      return true
+    }
+    return false
   }
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+    if (this.letters.includes(letter)) {
+      return false
+    } else {
+      return true
+    }
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+    this.guessedLetters += letter;
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+    this.errorsLeft --;
+    this.letters.push(letter);
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if (this.errorsLeft > 0) {
+      return false 
+    } else {
+      return true
+    }
   }
 
   checkWinner() {
-    // ... your code goes here
+    // let solvedWord = "";
+    // for (let i=0; i < this.secretWord; i++) {
+    //   solvedWord = this.guessedLetters.replace(secretWord[i], "");
+    //   if (solvedWord === this.guessedLetters){
+    //     return true
+    //   }
+    // }
+    
+    
+
   }
 }
 
@@ -42,10 +66,10 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
-    // ... your code goes here
+    //... your code goes here
   });
 }
 
