768bd81befdb3117829da16d8502ec98d447e06e
Mark Caldwell <caldweme@yahoo.com>
2021-06-17 14:21:59 +0200

    frustrating

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,19 +1,33 @@
 class HangmanCanvas {
   constructor(secretWord) {
-    this.context = document.getElementById('hangman').getContext('2d');
+    this.context = document.getElementById("hangman").getContext("2d");
+    this.secretWord = hangman.pickWord();
     // ... your code goes here
   }
 
   createBoard() {
+    //context.clearRect(0, 0, canvas.width, canvas.height);
+    this.drawLines();
     // ... your code goes here
   }
 
   drawLines() {
-    // ... your code goes here
+    for (let i = 0; i < this.secretWord.length; i++) {
+      this.context.beginPath();
+      this.context.moveTo(250 + i * 80, 500);
+      this.context.lineTo(270 + i * 80, 500);
+      this.context.stroke();
+      this.context.closePath();
+    }
   }
 
   writeCorrectLetter(index) {
     // ... your code goes here
+    let correctLetter = "";
+    let position = this.secretWord(index);
+    for (let i = 0; i <= correctLetter.length; i++) {
+      this.context.fillText(index, 250 + i * 80, 500);
+    }
   }
 
   writeWrongLetter(letter, errorsLeft) {
