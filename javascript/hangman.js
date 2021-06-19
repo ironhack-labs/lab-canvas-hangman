92c5c41bc17af44d07b817408913e64bd2fe92a6
Renan <reluiz@leroymerlin.com.br>
2021-06-19 01:14:42 -0300

    solved lab

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,39 +2,66 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = this.pickWord();
+    this.letters = [] ;
+    this.guessedLetters = '';
+    this.errorsLeft = 10;
   }
 
   pickWord() {
     // ... your code goes here
+    let indexRandom = Math.floor(Math.random() * this.words.length);
+    let randomWord = this.words[indexRandom];
+    return randomWord;
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
-  }
+    if (keyCode >= 65 && keyCode <= 90 ){
+      return true;
+    }else{
+      return false;
+    };
+  };
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    if(this.letters.includes(letter))return false
+    else return true    
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+      this.guessedLetters += letter
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft -= 1
+    this.letters.push(letter)
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft === 0){
+      return true
+    }else{
+      return false
+    }
   }
 
   checkWinner() {
     // ... your code goes here
+    if(this.guessedLetters.length === this.secretWord.length){
+      return true
+    }else{
+      return false
+    }
   }
 }
 
 let hangman;
-
+let hangmanCanvas;
 const startGameButton = document.getElementById('start-game-button');
 
 if (startGameButton) {
@@ -42,9 +69,9 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
-
+     hangman.secretWord = hangman.pickWord();
+     hangmanCanvas = new HangmanCanvas(hangman.secretWord)
+     hangmanCanvas.createBoard();
     // ... your code goes here
   });
 }
@@ -52,4 +79,30 @@ if (startGameButton) {
 document.addEventListener('keydown', event => {
   // React to user pressing a key
   // ... your code goes here
+  let tecla = event.key
+  //console.log(tecla)
+  if (hangman.checkIfLetter(event.keyCode)){
+    if(hangman.checkClickedLetters(tecla)){
+      if(hangman.secretWord.includes(tecla)){
+        hangman.addCorrectLetter(tecla)
+        hangmanCanvas.writeCorrectLetter(tecla)
+      }else{
+       hangman.addWrongLetter(tecla)  
+       hangmanCanvas.writeWrongLetter(hangman.letters, hangman.errorsLeft)
+       hangmanCanvas.drawHangman(hangman.errorsLeft)
+      }
+    }else{
+      return window.alert(`you have clicked to "${tecla}"" before, please chose other!`)
+    }
+  }else {
+    return window.alert(`You didn't choose a letter, you chose "${tecla}", please chose a letter`)
+  }
+  if (hangman.checkGameOver()){
+    window.alert('You lose, please play again. Click in "START GAME" ')
+     hangmanCanvas.gameOver()
+  };
+  if (hangman.checkWinner()){
+    window.alert('You won, congratulations')
+    hangmanCanvas.winner()
+  }
 });
