5ad5abdacce0d688a93df4ade89666c7fdea5384
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-08-21 15:36:43 +0200

    hangman dibujado

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,34 +1,106 @@
 class HangmanCanvas {
-  constructor(secretWord) {
-    this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
-  }
+	constructor(secretWord) {
+		this.context = document.getElementById('hangman').getContext('2d');
+		// ... your code goes here
+		this.secretWord = secretWord;
+		//width 1200 / height 800
+	}
 
-  createBoard() {
-    // ... your code goes here
-  }
+	createBoard() {
+		// ... your code goes here
+		this.context.clearRect(0, 0, 1200, 800);
+		//alert('test');
 
-  drawLines() {
-    // ... your code goes here
-  }
+		this.drawLines();
+	}
 
-  writeCorrectLetter(index) {
-    // ... your code goes here
-  }
+	drawLines() {
+		// ... your code goes here
 
-  writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
-  }
+		//Triangulo base
+		this.context.beginPath();
+		this.context.moveTo(75, 750);
+		this.context.lineTo(200, 750);
+		this.context.lineTo(140, 650);
+		this.context.fill();
 
-  drawHangman(errorsLeft) {
-    // ... your code goes here
-  }
+		//Palo 1
+		this.context.beginPath();
+		this.context.moveTo(140, 650);
+		this.context.lineTo(140, 150);
+		this.context.stroke();
 
-  gameOver() {
-    // ... your code goes here
-  }
+		//Palo 2
+		this.context.beginPath();
+		this.context.moveTo(140, 150);
+		this.context.lineTo(500, 150);
+		this.context.stroke();
 
-  winner() {
-    // ... your code goes here
-  }
+		//Palo hanging
+		this.context.beginPath();
+		this.context.moveTo(500, 150);
+		this.context.lineTo(500, 250);
+		this.context.stroke();
+
+		//Cabeza
+		this.context.beginPath();
+		this.context.arc(500, 250, 35, 0, Math.PI * 2);
+		this.context.stroke();
+		this.context.fill();
+
+		//Cuerpo lineal
+		this.context.beginPath();
+		this.context.moveTo(500, 285);
+		this.context.lineTo(500, 430);
+		this.context.stroke();
+		this.context.fill();
+
+		//Brazo1
+		this.context.beginPath();
+		this.context.moveTo(500, 300);
+		this.context.lineTo(450, 350);
+		this.context.stroke();
+		this.context.fill();
+
+		//brazo2
+		this.context.beginPath();
+		this.context.moveTo(500, 300);
+		this.context.lineTo(550, 350);
+		this.context.stroke();
+		this.context.fill();
+
+		//pata1
+		this.context.beginPath();
+		this.context.moveTo(500, 430);
+		this.context.lineTo(450, 480);
+		this.context.stroke();
+		this.context.fill();
+
+		//pata2
+		this.context.beginPath();
+		this.context.moveTo(500, 430);
+		this.context.lineTo(550, 480);
+		this.context.stroke();
+		this.context.fill();
+	}
+
+	writeCorrectLetter(index) {
+		// ... your code goes here
+	}
+
+	writeWrongLetter(letter, errorsLeft) {
+		// ... your code goes here
+	}
+
+	drawHangman(errorsLeft) {
+		// ... your code goes here
+	}
+
+	gameOver() {
+		// ... your code goes here
+	}
+
+	winner() {
+		// ... your code goes here
+	}
 }
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -67,10 +67,12 @@ if (startGameButton) {
 		hangman = new Hangman([ 'node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa' ]);
 
 		// HINT (uncomment when start working on the canvas portion of the lab)
-		// hangman.secretWord = hangman.pickWord();
-		// hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+		hangman.secretWord = hangman.pickWord();
+		hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
 		// ... your code goes here
+		//console.log(hangman.secretWord);
+		hangmanCanvas.createBoard();
 	});
 }
 
