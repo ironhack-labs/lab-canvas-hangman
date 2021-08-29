39c07398ace730c0a74caf0eb7c7ba7aa5c7c6d6
jdavesa <86001040+jdavesa@users.noreply.github.com>
2021-08-29 15:07:42 +0200

    Lab Completo - Falta Bonus "winner"

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,32 +1,306 @@
 class HangmanCanvas {
   constructor(secretWord) {
-    this.context = document.getElementById('hangman').getContext('2d');
+    this.ctx = document.getElementById('hangman').getContext('2d');
+    this.secretWord = secretWord
     // ... your code goes here
   }
 
   createBoard() {
+    this.ctx.clearRect(0,0,1200,800)
+    this.drawLines()
     // ... your code goes here
   }
 
   drawLines() {
+
+    for(let i = 1; i <= this.secretWord.length; i++){
+      
+      this.ctx.beginPath();
+      this.ctx.moveTo(100*i, 200);
+      this.ctx.lineTo((100*i)+80, 200);
+      this.ctx.stroke();
+    }
     // ... your code goes here
   }
 
   writeCorrectLetter(index) {
+
+    this.ctx.font = "60px Arial"
+    this.ctx.fillText(`${this.secretWord[index].toUpperCase()}`, 100*(index+1)+20 ,150)
+    console.log(`the letter is : ${this.secretWord[index]}, and the index is : ${index}`)
+    
     // ... your code goes here
   }
 
+  getAndWriteIndex(letter){
+    for(let i = 0; i < this.secretWord.length; i++){
+      if(this.secretWord[i] === letter){
+        this.writeCorrectLetter(i)
+
+      }
+    }
+  }
+  
+
   writeWrongLetter(letter, errorsLeft) {
+    if(errorsLeft >= 0)
+    this.ctx.font = "30px Arial"
+    this.ctx.fillText(letter, 100,700)
+    
     // ... your code goes here
   }
 
   drawHangman(errorsLeft) {
     // ... your code goes here
+    switch(errorsLeft){
+      case 9:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        break;
+      case 8:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        break;
+      case 7:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 400);
+        this.ctx.lineTo(900, 400);
+        this.ctx.stroke();
+        break;
+      case 6:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 400);
+        this.ctx.lineTo(900, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 400);
+        this.ctx.lineTo(900, 475);
+        this.ctx.stroke();
+        break;
+      case 5:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 400);
+        this.ctx.lineTo(900, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 400);
+        this.ctx.lineTo(900, 475);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.arc(900, 500, 25, 0, 2 * Math.PI)
+        this.ctx.stroke();
+        break;
+      case 4:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 400);
+        this.ctx.lineTo(900, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 400);
+        this.ctx.lineTo(900, 475);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.arc(900, 500, 25, 0, 2 * Math.PI)
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 525);
+        this.ctx.lineTo(900, 600);
+        this.ctx.stroke();
+        break;
+      case 3:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 400);
+        this.ctx.lineTo(900, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 400);
+        this.ctx.lineTo(900, 475);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.arc(900, 500, 25, 0, 2 * Math.PI)
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 525);
+        this.ctx.lineTo(900, 600);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 530);
+        this.ctx.lineTo(870, 570);
+        this.ctx.stroke();
+        break;
+      case 2:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 400);
+        this.ctx.lineTo(900, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 400);
+        this.ctx.lineTo(900, 475);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.arc(900, 500, 25, 0, 2 * Math.PI)
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 525);
+        this.ctx.lineTo(900, 600);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 530);
+        this.ctx.lineTo(870, 570);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 530);
+        this.ctx.lineTo(930, 570);
+        this.ctx.stroke();
+        break;
+      case 1:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 400);
+        this.ctx.lineTo(900, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 400);
+        this.ctx.lineTo(900, 475);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.arc(900, 500, 25, 0, 2 * Math.PI)
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 525);
+        this.ctx.lineTo(900, 600);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 530);
+        this.ctx.lineTo(870, 570);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 530);
+        this.ctx.lineTo(930, 570);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 600);
+        this.ctx.lineTo(870, 640);
+        this.ctx.stroke();
+        break;
+      case 0:
+        this.ctx.beginPath();
+        this.ctx.moveTo(700, 700);
+        this.ctx.lineTo(1000, 700);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 700);
+        this.ctx.lineTo(800, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(800, 400);
+        this.ctx.lineTo(900, 400);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 400);
+        this.ctx.lineTo(900, 475);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.arc(900, 500, 25, 0, 2 * Math.PI)
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 525);
+        this.ctx.lineTo(900, 600);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 530);
+        this.ctx.lineTo(870, 570);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 530);
+        this.ctx.lineTo(930, 570);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 600);
+        this.ctx.lineTo(870, 640);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.moveTo(900, 600);
+        this.ctx.lineTo(930, 640);
+        this.ctx.stroke();
+        break;
+      
+    }
   }
 
-  gameOver() {
+  gameOver(errorsLeft) {
+    if(errorsLeft <= 0){
+    this.ctx.font = "30px Arial"
+    this.ctx.fillText('GAME OVER!', 400,500)
+    /* let imgGameOver = new Image()
+    imgGameOver.src = '../images/gameover.png'
+    this.ctx.drawImage(imgGameOver, 300,300,700,400) */
     // ... your code goes here
-  }
+    }
+  } 
 
   winner() {
     // ... your code goes here
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,66 @@
 class Hangman {
   constructor(words) {
-    this.words = words;
     // ... your code goes here
+
+    this.words = words;
+    this.secretWord;
+    this.letters = []
+    this.guessedLetters = ""
+    this.errorsLeft = 10
   }
 
   pickWord() {
     // ... your code goes here
+    let randomIndex = Math.floor(Math.random()*this.words.length)
+    this.secretWord = this.words[randomIndex]
+    return this.secretWord
+    
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if (keyCode >= 65 && keyCode <= 90){
+      return true
+    }
+
   }
 
   checkClickedLetters(letter) {
+    if(!this.letters.includes(letter) || !this.guessedLetters.includes(letters))
+    {
+      return true
+      }
     // ... your code goes here
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    if(this.secretWord.includes(letter) && this.checkClickedLetters(letter)){
+      this.guessedLetters += letter
+    }
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    if(!this.guessedLetters.includes(letter) && this.checkClickedLetters(letter)){
+      this.letters.push(letter)
+      this.errorsLeft -= 1
+    }
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft === 0){
+      console.log("It's Over Baby")
+      return true
+    }
   }
 
   checkWinner() {
     // ... your code goes here
+    if(this.secretWord.includes(this.guessedLetters)){
+      return true
+    }
   }
 }
 
@@ -41,15 +72,42 @@ if (startGameButton) {
   startGameButton.addEventListener('click', event => {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
-    // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    console.log(hangman.words)
+    console.log(hangman.secretWord)
+    hangmanCanvas.createBoard()
 
     // ... your code goes here
   });
 }
 
-document.addEventListener('keydown', event => {
+document.addEventListener('keyup', (e) => {
+  if(hangman.checkIfLetter(e.keyCode)){
+    let letter = String.fromCharCode(e.keyCode).toLowerCase()
+
+    hangman.checkClickedLetters(letter)
+    hangman.addCorrectLetter(letter)
+    
+    
+    hangman.addWrongLetter(letter)
+    hangman.checkGameOver()
+    hangmanCanvas.createBoard()
+    
+    for(elem of hangman.guessedLetters){
+    hangmanCanvas.getAndWriteIndex(elem)}
+    hangmanCanvas.drawHangman(hangman.errorsLeft)
+
+    hangmanCanvas.writeWrongLetter(hangman.letters, hangman.errorsLeft)
+    hangmanCanvas.gameOver(hangman.errorsLeft)
+
+  
+  }
+
+  console.log(hangman.letters)
+  console.log(hangman.guessedLetters)
+  console.log(hangman.errorsLeft)
   // React to user pressing a key
   // ... your code goes here
 });
