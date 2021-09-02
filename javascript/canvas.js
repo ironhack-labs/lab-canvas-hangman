b5ff4cb058505fd4cc7418a4f83cfe17c4faa9c2
Pool Ortega <poolortegas@gmail.com>
2021-09-02 11:59:13 -0500

    Intento1

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,19 +1,66 @@
+let lineaX=100
+let lineaY=500
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
+    this.secretWord= secretWord
     // ... your code goes here
   }
 
   createBoard() {
-    // ... your code goes here
+    console.log('limpio tablero')
+    this.context.clearRect(0,0,1200,800);
+    this.drawLines();
+
   }
 
   drawLines() {
-    // ... your code goes here
+    console.log(this.secretWord)
+   // console.log(this.secretWord.length)
+    //dependiendo la longitud de la palabra son las lineas que dibuja
+    for(let i = 0;i<this.secretWord.length;i++){
+      this.context.beginPath();
+      lineaX+=100
+//donde poner el lapiz
+this.context.moveTo(lineaX+5,lineaY)
+//indicar hacia donde se va mover pero no la pinta aun
+this.context.lineTo(lineaX+50,lineaY)
+//Stroke es dibujar la linea
+this.context.stroke()
+    }
+    lineaX=100
+    lineaY=500
   }
 
   writeCorrectLetter(index) {
-    // ... your code goes here
+    //llamo al string con las letras que se han tecleado y lo convierto en array
+    let acomodarPalabraAdivinada=hangman.guessedLetters.split("")
+    let palabraNormal=this.secretWord.split("");
+    /* METODOS QUE ESTABA INTENTANDO PARA ARREGLAR LA PALABRA EN PPOSISIOCON
+    let arrGuia=Array(palabraNormal.length)
+    arrGuia.push(acomodarPalabraAdivinada);
+
+    for(let i=0;i<palabraNormal.length;i++){
+      if(acomodarPalabraAdivinada[i]==palabraNormal[i]){
+      console.log("estan en la misma posicion")
+     
+
+
+    }
+    else{
+
+      console.log("NO estan en la misma posision")
+
+    }
+   
+   }
+   console.log("EL array guia ",arrGuia)
+   */
+   //VOLVI UNIR EL ARRAY NADAMAS PARA ESCRIBIRLO
+   acomodarPalabraAdivinada=acomodarPalabraAdivinada.join("")
+   this.context.font="48px arial"
+  this.contextfillStyle = "blue"
+  this.context.fillText(`${acomodarPalabraAdivinada}`,lineaX+100,lineaY-5)
   }
 
   writeWrongLetter(letter, errorsLeft) {
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,79 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord= this.pickWord(words)
+    this.letters=[];
+    this.guessedLetters="";
+    this.errorsLeft=10;
   }
 
   pickWord() {
     // ... your code goes here
+    //[hola,adios,perrro]
+    //multiplicamos el random por la longitud del arr y le quitamos los decimales
+    let random= Math.trunc(Math.random()*this.words.length)
+    //creamos una variable para pasarle la palabra random
+    let pickedWord =this.words[random]
+    //regresamos la palabra random
+    return pickedWord;
+
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    if(keyCode>=65 &&keyCode<=90){
+      
+      return true
+    }else{
+     return false
+    }
   }
-
   checkClickedLetters(letter) {
-    // ... your code goes here
+     //Si la palabra existe regresa un falso
+    if(this.letters.includes(letter)){
+      //console.log(this.letters)
+      return false
+      //SI la palabra no existe regresa un verdadero y la empuja al array de las palabras 
+    } else{
+      this.letters.push(letter)
+      console.log(this.letters)
+      return true
+    }
+    
+  
+  
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+  
+  
+    this.guessedLetters+=letter
+    //console.log('como va la palabra',this.guessedLetters)
+    
+   
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft--
+    //REVISAR SI FUNCIONA SI NO QUITARLO
+      this.letters.push(letter);
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if(this.errorsLeft>0){
+      return false
+    } else{
+      return true
+    }
+   //return !this.errorsLeft>0
   }
 
-  checkWinner() {
-    // ... your code goes here
+  checkWinner() { 
+    //SPLIT Separa las letras en arrays, despues  ordena ese array por orden alfabetico y por ultimo ese array lo junta en una palabra
+    let ordenarSecreta = this.secretWord.split("").sort().join("");
+    let ordenarAdivinar = this.guessedLetters.split("").sort().join("");
+    if (ordenarSecreta === ordenarAdivinar) return true;
+    else return false;
   }
 }
 
@@ -39,17 +84,52 @@ const startGameButton = document.getElementById('start-game-button');
 
 if (startGameButton) {
   startGameButton.addEventListener('click', event => {
+    //hangman queda activado con los valores que se pasan y es por eso que se puede acceder aestos valores en el addevent liseter de abajho
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
-
+    hangman.secretWord = hangman.pickWord();
+    
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    hangmanCanvas.createBoard()
     // ... your code goes here
   });
 }
 
 document.addEventListener('keydown', event => {
-  // React to user pressing a key
-  // ... your code goes here
+
+  //REVISA SI LA PALABRA HA SIDO APRETADA ANTES - Si no ha sido apretada y se agregan al array de palabras que han sido tecleadas, entra al if
+  if(hangman.checkClickedLetters(event.key)==true){
+    //SI la palabra secreta incluye la tecla que se esta metiendo
+    if(hangman.secretWord.includes(event.key)){
+      //console.log("Agregando Palabra buena")
+      //Agrega la letra que esta siendo presionada al string vacio de  this.guessedLetters=""
+      hangman.addCorrectLetter(event.key)
+      //Se va al canvas para pintar las letras
+      hangmanCanvas.writeCorrectLetter(event.key)
+      //Verifica si la palabra escrita corresponde a la palabra secrta si es asi se gana
+      if(hangman.checkWinner()){
+        console.log("GANASTE")
+      }
+
+      //Si la palabra secreta NO INCLUYE la tecla que se sta metiendo
+    } else{
+      //console.log("Agregando Palabra mala")
+      //Se agrega la palabra al array de palabras que ya han sido tecleadas
+      hangman.addWrongLetter(event.key)
+      console.log("Oportunidades: ", hangman.errorsLeft)
+      //SI erros.Left llega a 0 entonces se ha perdido 
+      if(hangman.errorsLeft==0){
+        console.log("Perdiste")
+      }
+
+      
+    }
+   
+
+ }
+ 
+  //voy en add correct event
+  //console.log(Hangman.checkClickedLetters(event.key))
+  //addCorrectLetter(letter)
 });
