6f1e6af3a7251d635eac84dbdfdc1242afed66de
Jorge LRangel <jorgelrgarduno@gmail.com>
2021-09-02 17:55:46 -0500

    lab dia 3 Semana 2 Hangman

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,15 +1,30 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
+    this.secretWord = secretWord;
     // ... your code goes here
   }
 
   createBoard() {
     // ... your code goes here
+    this.context.clearRect(0, 0, 1200, 800)
+    this.drawLines()
   }
 
   drawLines() {
-    // ... your code goes here
+    let space = 20 // asignamos un variable para determinar el spacion blanco entre las lineas con un tamaño de 20
+    let barraword = 50 // asignamos valor al tamaño de la barra donde se van a poner las letras
+    let positionx = 300 // posicion donde se va a poner en el eje X la barra
+    
+    for(let i = 0; i < this.secretWord.length; i++){ // aqui pusimos un for para que nos haga el recorrido de la palabra secreta para que nos pueda asignar los espacios que necesitamos caundo se llame el start
+      console.log(this.secretWord[i]); // i= 0 desde donde inicia, siguiente valor hasta donde va a ser y el i++ para que lo recorra
+      this.context.moveTo(positionx, 750) // los argumentos de moveTo siempre es X y Y  (inicio)
+      this.context.lineTo(positionx + barraword, 750 ) // los argumentos de linTo siempre es X y Y (fin)
+      this.context.stroke() // imprimir el movee y el line 
+      positionx += space + barraword
+    } 
+
+
   }
 
   writeCorrectLetter(index) {
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,54 @@
 class Hangman {
   constructor(words) {
-    this.words = words;
+    this.words = words; // asignamos la paralabra de la matriz
     // ... your code goes here
+    this.secretWord = this.pickWord(); // asignamos un valor a la palabra
+    this.letters = []; // asinar un array vacio
+    this.guessedLetters = ""; // asignar un string vacio
+    this.errorsLeft = 10; // asiganmos el numero de intentos que debería tener
+  }   // definimos todas nuestras de variables
+
+  pickWord() {  // redondea enteros pero para el valor menor math.florr
+    // ... your code goes here
+    return this.words[Math.floor(Math.random() * this.words.length)];
   }
 
-  pickWord() {
+  checkIfLetter(keyCode) { // funcion en la que valida las letras
     // ... your code goes here
+    if(keyCode >= 65 && keyCode <= 90) return true;
+    else return false;  
   }
 
-  checkIfLetter(keyCode) {
+  checkClickedLetters(letter) { // verificamos si ya se preciono la letra nos regresa un verdadero o falso
     // ... your code goes here
+    if (this.letters.includes(letter)) return false;
+    else return true; 
   }
 
-  checkClickedLetters(letter) {
+  addCorrectLetter(letter) {  // verificamos si la palabra 
     // ... your code goes here
-  }
-
-  addCorrectLetter(letter) {
-    // ... your code goes here
+        this.guessedLetters += letter;
+    
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft--;
+    this.letters.push(letter);
   }
 
-  checkGameOver() {
+  checkGameOver() { 
     // ... your code goes here
+    if(this.errorsLeft > 0) return false;
+    else return true;
   }
 
-  checkWinner() {
+  checkWinner() { // se asignaron nuevas funciones en las que tuvimos que verificar por emedio de .spli .sort y .join ordenos las letreas contenidas y despues comparamos esas dos funciones para que nos pudiera dar un booleano
     // ... your code goes here
+    let orderSecret = this.secretWord.split("").sort().join("");
+    let orderGuessed = this.guessedLetters.split("").sort().join("");
+    if (orderSecret === orderGuessed) return true;
+    else return false;
   }
 }
 
@@ -41,9 +60,12 @@ if (startGameButton) {
   startGameButton.addEventListener('click', event => {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
-    // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+   //HINT (uncomment when start working on the canvas portion of the lab)
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord); // variable hangmanCanvas estoy almacenando una nueva clase y le paso el argumento de secretword
+    hangmanCanvas.createBoard() // invocar la funcion de createBoard ejecuta todo lo que tiene dentro de ella
+     
+
 
     // ... your code goes here
   });
