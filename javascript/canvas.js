b6a78f203f0c1fa76cce2b169aea4a8f5feb4157
Diana Gomez <dianaegc094@gmail.com>
2021-09-03 09:59:31 -0400

    Lab hangman

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -2,18 +2,103 @@ class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
     // ... your code goes here
+    this.secretWord = secretWord
+    this.createBoard()
   }
 
   createBoard() {
-    // ... your code goes here
+    this.context.clearRect(0,0,1200,800)
+    this.drawLines()
+    
   }
 
   drawLines() {
     // ... your code goes here
-  }
+    this.context.beginPath();
+    this.context.moveTo(0,800)
+    this.context.lineTo(110,750) 
+    this.context.lineWidth=4
+    this.context.stroke()
+    this.context.closePath();
+    //
+    this.context.beginPath();
+    this.context.moveTo(110,750)
+    this.context.lineTo(200,800)
+    this.context.lineWidth= // ?
+    this.context.stroke()
+    this.context.closePath();
+    //
+    this.context.beginPath();
+    this.context.moveTo(0,800)
+    this.context.lineTo(200,800) //para la base
+    this.context.lineWidth= 
+    this.context.stroke()
+    this.context.closePath();
+    //
+    this.context.beginPath();
+    this.context.moveTo(110,750)
+    this.context.lineTo(110,0)
+    this.context.lineWidth= //
+    this.context.stroke()
+    this.context.closePath();
+  //
+    this.context.beginPath();
+    this.context.moveTo(110,0)
+    this.context.lineTo(400,0)
+    this.context.lineWidth= //
+    this.context.stroke()
+    this.context.closePath();
+    //
+    this.context.beginPath();
+    this.context.moveTo(400,0)
+    this.context.lineTo(400,50)
+    this.context.lineWidth= //
+    this.context.stroke()
+    this.context.closePath();
+    //lineas para las letras
+    // Raya 1
+    /*this.context.beginPath();
+    this.context.moveTo(230,800) // punto de inicio 
+    this.context.lineTo(290,800) // punto final 290 - 230 = 60 
+    this.context.lineWidth=7 //
+    this.context.stroke()
+    this.context.closePath();
+    //Raya 2
+    this.context.moveTo(300,800)  // punto de inicio 
+    this.context.lineTo(360,800) // punto final
+    this.context.lineWidth=7 //
+    this.context.stroke()
+    this.context.closePath();
+    //Raya 3
+    this.context.moveTo(370,800)  //punto de inicio 
+    this.context.lineTo(430,800)  // punto final
+    this.context.lineWidth=7 
+    this.context.stroke()
+    this.context.closePath();*/
+
+
+    let space=10//el espacio entre las lineas
+    let barrWord=60// valor a la tamaño de las barras
+    let positionX=230 // donde va a iniciar en el eje x
+
+    for(let i=0;i<this.secretWord.length;i++){
+      console.log(this.secretWord[i])
+      this.context.moveTo(positionX,750)
+      this.context.lineTo(positionX+barrWord,750)
+      this.context.stroke()
+      this.context.closePath()
+       positionX += space + barrWord
+    }
+    }
+    
+    
+    
+
+  
 
   writeCorrectLetter(index) {
     // ... your code goes here
+
   }
 
   writeWrongLetter(letter, errorsLeft) {
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,68 @@
 class Hangman {
   constructor(words) {
-    this.words = words;
+    this.words = words;//almacenar todas las palabras que se le pueden dar al jugador para que las adivine
+   this.secretWord=this.pickWord(words)//guardar la palabra que se generó random 
+    this.letters=[]//alamacenar las letras que el usuario ya ha elegido al intentar adivinar
+    this.guessedLetters="" // para almacenar las letras que el usuario eligio y adivino
+   this.errorsLeft=10 //debe disminuir cada vez que un usuario  elige una letra que no aparece en la palabra que necesita adivinar
     // ... your code goes here
   }
 
   pickWord() {
     // ... your code goes here
+    let randomWord = Math.trunc(Math.random()*(this.words.length));//trunca (corta) el punto y los dígitos a la derecha de él, sin importar si el argumento es un número positivo o negativo.
+    let result = this.words[randomWord]
+    return result 
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if(keyCode>=65&& keyCode<=90){ 
+      return true
+    }else{
+      return false
+    }
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    if(this.letters.includes(letter)){//si la letra pasada fue seleccionada(clicked)
+      return false 
+    }else {
+      return true
+    }
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+   this.guessedLetters+=letter // añadirle a guessedLetters la letra 
+    return this.checkWinner()
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+   this.errorsLeft -=1
+   if(this.letters.includes(letter)){
+   }else{
+     this.letters.push(letter)
+   }
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft===0){
+      return true
+   }else{
+      return false
+    }
   }
 
   checkWinner() {
     // ... your code goes here
+  let orderSecret = this.secretWord.split("").sort().join(""); //split hace array a la palabra que esta en secretWord , sort ordena los elementos del arreglo alfabeticamente, join hace el array de nuevo cadena
+  let orderGuessed = this.guessedLetters.split("").sort().join("");///split hace array a la palabra que esta en secretWord , sort ordena los elementos del arreglo alfabeticamente, join hace el array de nuevo cadena
+  if (orderSecret === orderGuessed) return true;
+  else return false;
   }
 }
 
@@ -41,15 +74,17 @@ if (startGameButton) {
   startGameButton.addEventListener('click', event => {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
-    // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  // HINT (uncomment when start working on the canvas portion of the lab)
+   hangman.secretWord = hangman.pickWord();
+  hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
     // ... your code goes here
+
   });
 }
 
 document.addEventListener('keydown', event => {
   // React to user pressing a key
   // ... your code goes here
+  
 });
