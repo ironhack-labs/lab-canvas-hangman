ccdcc287f4834fe88ca33dcf0adc273807a46c5f
Michael Seitz <michael.s.seitz@gmail.com>
2021-10-13 18:01:24 +0200

    logic checks out . still no painting of letters

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -15,19 +15,22 @@ class HangmanCanvas {
     drawLines() {
         // ... your code goes here
         let startPointX = 100
-        const startPointY = 500
-        const lineWidth = 50
+        const startPointY = 100
+        const lineWidth = 20
         for (let i = 0; i < this.secretWord.length; i++){
           this.context.beginPath();
           this.context.moveTo(startPointX, startPointY);
           this.context.lineTo(startPointX+lineWidth, startPointY);
           this.context.stroke();
           startPointX+=lineWidth+25
+          this.writeCorrectLetter(i)
         }
     }
 
     writeCorrectLetter(index) {
-        // ... your code goes here
+        // ... your code goes herevar canvas = document.getElementById("myCanvas");
+        this.context.font = "30px Arial";
+        this.context.fillText("Hello World", 10, 50);
     }
 
     writeWrongLetter(letter, errorsLeft) {
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -21,25 +21,39 @@ class Hangman {
         return pickedWord;
     }
     checkIfLetter(keyCode) {
-        //  ... your code goes here
-        // if (keyCode >= 65 && keyCode <= 90)
-        //   return true
-        // else
-        //   false
-        return keyCode >= 65 && keyCode <= 90
+        return keyCode >= 65 && keyCode <= 90;
     }
 
     checkClickedLetters(letter) {
+        // a method that should check if the letter passed as an 
+        // argument has already been pressed. It should return 
+        // true if it was not or false in the opposite case.
         // ... your code goes here
-        return !this.letters.includes(letter)
+        console.log(`${letter} has already been pressed?`, !this.letters.includes(letter))
+        return !this.letters.includes(letter);
+    }
+
+    addLetterToLetters (letter) {
+        this.isCorrectLetter(letter) ? this.letters.push(letter) : console.log(`${letter} alreade guessed`);
+    }
+
+    isCorrectLetter(letter) {
+        // checks if the letter is in the secret word, returns true or false
+        console.log(`${letter} is a correct letter?`, this.secretWord.includes(letter))
+        return this.secretWord.includes(letter)
     }
 
     addCorrectLetter(letter) {
+        /*method that should add the passed letter to the guessedLetters
+         property. This could be a good place to check if the user won.*/
         // ... your code goes here
         this.guessedLetters += letter;
     }
 
     addWrongLetter(letter) {
+        /*a method that should subtract one from the variable errorsLeft.
+        It also should push this letter in the array of letters if 
+        the letter is not there already.*/
         // ... your code goes here
         this.errorsLeft--;
         if (this.checkClickedLetters(letter)) {
@@ -48,23 +62,21 @@ class Hangman {
     }
 
     checkGameOver() {
-        return this.errorsLeft <= 0
+        return this.errorsLeft <= 0;
     }
 
     checkWinner() {
         // ... your code goes here
         // return this.secretWord === this.guessedLetters
-        this.secretWord
-        this.guessedLetters
+        this.secretWord;
+        this.guessedLetters;
 
-        let isComplete = []
+        let isComplete = [];
         for (let char of this.secretWord) {
-          if (this.guessedLetters.includes(char))
-            isComplete.push(true)
-          else
-          isComplete.push(false)
+            if (this.guessedLetters.includes(char)) isComplete.push(true);
+            else isComplete.push(false);
         }
-        return !isComplete.includes(false)
+        return !isComplete.includes(false);
     }
 }
 
@@ -89,11 +101,33 @@ if (startGameButton) {
         hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
         // // // ... your code goes here
-        hangmanCanvas.createBoard()
+        hangmanCanvas.createBoard();
     });
 }
 
 document.addEventListener("keydown", (event) => {
     // React to user pressing a key
     // ... your code goes here
+    hangman.checkIfLetter(event.keyCode);
+    // console.log(event);
+    // console.log(event.key);
+
+    if (hangman.checkIfLetter(event.keyCode)) {
+        if (hangman.checkClickedLetters(event.key)) {
+            hangman.addLetterToLetters(event.key)
+            if (hangman.isCorrectLetter(event.key)) {
+                hangman.addCorrectLetter(event.key)
+                if (hangman.checkWinner()){
+                    console.log("You WON!!!!");
+                }
+            }
+            else {
+                hangman.addWrongLetter(event.key)
+                if (hangman.checkGameOver()) {
+                    console.log("You LOST Bigtime!!!!");
+                }
+            }
+        }
+    }
+
 });
