dd16e0d1244ef016d530a8c3e9a2fd3730689b08
Brigi Horvath <brigi.horvath82@gmail.com>
2021-10-13 18:54:17 +0200

    done

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -5,16 +5,19 @@ class HangmanCanvas {
   }
 
   createBoard() {
+    // const img = new Image();
+    // img.src = '../images/gameover.png';
+    // this.ctx.drawImage(img, 100, 100, 570, 240);
     this.ctx.clearRect(0, 0, 1200, 800);
     this.drawLines();
   }
 
   drawLines() {
-    let startX = (1200 - this.secretWord.length * 70) / 2;
+    let startX = 300;
     this.secretWord.split('').forEach((el) => {
       this.ctx.beginPath();
-      this.ctx.moveTo(startX, 200);
-      this.ctx.lineTo(startX + 50, 200);
+      this.ctx.moveTo(startX, 700);
+      this.ctx.lineTo(startX + 50, 700);
       this.ctx.stroke();
       this.ctx.closePath();
       startX += 70;
@@ -22,25 +25,80 @@ class HangmanCanvas {
   }
 
   writeCorrectLetter(index) {
-    let startX = (1200 - this.secretWord.length * 70) / 2;
+    let startX = 300;
     this.ctx.font = '30px Indie Flower';
     this.ctx.fillStyle = 'black';
-    this.ctx.fillText(this.secretWord[index], startX + index * 70 + 20, 190);
+    this.ctx.fillText(
+      this.secretWord[index].toUpperCase(),
+      startX + index * 70 + 20,
+      690
+    );
   }
 
   writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
+    let startX = 800;
+    this.ctx.font = '30px Indie Flower';
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillText(letter, startX + (10 - errorsLeft) * 30 + 10, 300);
   }
 
   drawHangman(errorsLeft) {
-    // ... your code goes here
+    const hangmanDrawing = [
+      { moveToX: 10, moveToY: 700, lineToX: 250, lineToY: 700 },
+      { moveToX: 10, moveToY: 700, lineToX: 130, lineToY: 600 },
+      { moveToX: 130, moveToY: 600, lineToX: 250, lineToY: 700 },
+      { moveToX: 130, moveToY: 600, lineToX: 130, lineToY: 100 },
+      { moveToX: 130, moveToY: 100, lineToX: 500, lineToY: 100 },
+      { moveToX: 500, moveToY: 100, lineToX: 500, lineToY: 200 },
+      {},
+      { moveToX: 500, moveToY: 300, lineToX: 500, lineToY: 500 },
+      { moveToX: 500, moveToY: 400, lineToX: 400, lineToY: 300 },
+      { moveToX: 500, moveToY: 400, lineToX: 600, lineToY: 300 },
+      { moveToX: 500, moveToY: 500, lineToX: 400, lineToY: 600 },
+      { moveToX: 500, moveToY: 500, lineToX: 600, lineToY: 600 },
+    ];
+    let index = 9 - errorsLeft;
+    // console.log(hangmanDrawing[index].moveToY);
+    this.ctx.beginPath();
+    this.ctx.lineWidth = 3;
+    this.ctx.strokeStyle = 'black';
+    if (index === 6) {
+      this.ctx.arc(500, 250, 50, 0, Math.PI * 2);
+      this.ctx.stroke();
+      this.ctx.closePath();
+    }
+
+    this.ctx.moveTo(
+      hangmanDrawing[index].moveToX,
+      hangmanDrawing[index].moveToY
+    );
+    this.ctx.lineTo(
+      hangmanDrawing[index].lineToX,
+      hangmanDrawing[index].lineToY
+    );
+    this.ctx.stroke();
+    this.ctx.closePath();
   }
 
   gameOver() {
-    // ... your code goes here
+    this.ctx.clearRect(0, 0, 1200, 800);
+
+    const img = new Image();
+    // If you try to call drawImage() before the image has finished loading,
+    // it won't do anything (or, in older browsers, may even throw an exception).
+    // So you need to be sure to use the load event so you don't try this before the image has loaded
+    img.addEventListener('load', () => {
+      this.ctx.drawImage(img, 315, 100, 570, 240);
+    });
+    img.src = '../images/gameover.png';
   }
 
   winner() {
-    // ... your code goes here
+    this.ctx.clearRect(0, 0, 1200, 800);
+    const img = new Image();
+    img.addEventListener('load', () => {
+      this.ctx.drawImage(img, 100, 100, 872, 618);
+    });
+    img.src = '../images/awesome.png';
   }
 }
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -12,19 +12,20 @@ class Hangman {
   }
 
   checkIfLetter(keyCode) {
-    console.log('checkIfLetter');
     // keyCode is deprecated but here used bc of the tests
     return keyCode <= 90 && keyCode >= 65;
   }
 
   checkClickedLetters(letter) {
-    console.log(letter);
     return !this.letters.includes(letter);
+  }
+
+  checkIfLetterIsCorrect(letter) {
+    return this.secretWord.toLowerCase().split('').includes(letter);
   }
 
   addCorrectLetter(letter) {
     this.guessedLetters += letter;
-    console.log(this.guessedLetters);
   }
 
   addWrongLetter(letter) {
@@ -35,18 +36,10 @@ class Hangman {
   }
 
   checkGameOver() {
-    console.log(this.errorsLeft);
-    return this.errorsLeft <= 0;
+    return this.errorsLeft === 0;
   }
 
   checkWinner() {
-    console.log(
-      this.secretWord
-        .split('')
-        .every((el) => this.guessedLetters.split('').includes(el))
-        ? 'Win'
-        : 'not yet'
-    );
     return this.secretWord
       .split('')
       .every((el) => this.guessedLetters.split('').includes(el));
@@ -80,17 +73,30 @@ if (startGameButton) {
 
 document.addEventListener('keydown', (event) => {
   console.log(event);
+  const lowerKey = event.key.toLowerCase();
   // keyCode is deprecated????
   if (!hangman.checkIfLetter(event.keyCode)) return;
-  if (hangman.checkClickedLetters(event.key)) {
-    hangman.addCorrectLetter(event.key);
+  if (!hangman.checkClickedLetters(lowerKey)) {
+    alert('You have already tried this letter!');
+    return;
+  }
+  if (hangman.checkIfLetterIsCorrect(lowerKey)) {
+    hangman.addCorrectLetter(lowerKey);
     hangman.secretWord.split('').forEach((el, i) => {
-      if (el === event.key) hangmanCanvas.writeCorrectLetter(i);
+      if (el === lowerKey) hangmanCanvas.writeCorrectLetter(i);
     });
-    hangman.checkWinner();
+    if (hangman.checkWinner()) {
+      setTimeout(() => {
+        hangmanCanvas.winner();
+      }, 1500);
+    }
   } else {
-    hangman.addWrongLetter(event.key);
-    hangmanCanvas.writeWrongLetter(event.key, this.errorsLeft);
-    hangman.checkGameOver();
+    hangman.addWrongLetter(lowerKey);
+    console.log(lowerKey);
+    hangmanCanvas.writeWrongLetter(lowerKey, hangman.errorsLeft);
+    hangmanCanvas.drawHangman(hangman.errorsLeft);
+    if (hangman.checkGameOver()) {
+      hangmanCanvas.gameOver();
+    }
   }
 });
