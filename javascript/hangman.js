fc1c2a872f97390f198f1533057ec9328e0fca68
phimanim <aliker.imane@gmail.com>
2021-10-14 18:20:17 +0200

    half way done

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,57 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 10;
+
   }
 
   pickWord() {
     // ... your code goes here
+    return this.words[Math.floor(Math.random() * this.words.length)]
+
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    return keyCode > 64 && keyCode < 91
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    return !this.letters.includes(letter)
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    this.guessedLetters += letter
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+    if (!this.letters.includes(letter) && !this.secretWord.includes(letter)){
+      this.errorsLeft -- 
+      this.letters.push(letter)
+    }
   }
 
   checkGameOver() {
     // ... your code goes here
+    if (this.errorsLeft > 0){
+      return false
+    }
+    else {
+      return true
+    }
   }
 
   checkWinner() {
     // ... your code goes here
+    let secretWordSplit = [...this.secretWord]
+
+    return secretWordSplit.every((wrd) => this.guessedLetters.split('').includes(wrd));
   }
 }
 
@@ -42,8 +65,8 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
     // ... your code goes here
   });
