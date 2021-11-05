b2960653c72c5b3812a80dc7fe2072d2a9ef8cda
Martin Giura <martin.giura@gmail.com>
2021-11-05 09:40:41 -0600

    Logic on place DONE, pending DOM manipulation and user interaction

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,96 @@
 class Hangman {
-  constructor(words) {
+  constructor(words, secretWord, letters, guessedLetters, errorsLeft) {
     this.words = words;
+    this.secretWord = this.pickWord()
+    this.letters = []
+    this.guessedLetters = ""
+    this.errorsLeft = 10
     // ... your code goes here
   }
 
   pickWord() {
-    // ... your code goes here
+    let randomPick = this.words[Math.floor(Math.random() * this.words.length)]
+    
+    return randomPick
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+  
+    if (keyCode >=65 && keyCode <=90) {
+      return true
+    } else if (keyCode === 186) {
+      return true
+    } else {
+      return false
+    }
   }
 
+    // a = 65
+    // b = 66
+    // c = 67
+    // d = 68
+    // e = 69
+    // f = 70
+    // g = 71
+    // h = 72
+    // i = 73
+    // j = 74
+    // k = 75
+    // l = 76 
+    // m = 77
+    // n = 78
+    // ñ = 186
+    // o = 79
+    // p = 80
+    // q = 81
+    // r = 82
+    // s = 83
+    // t = 84
+    // u = 85
+    // v = 86
+    // w = 87
+    // x = 88
+    // y = 89
+    // z = 90
+
+
+
   checkClickedLetters(letter) {
-    // ... your code goes here
+    if (!this.letters.includes(letter)) {
+      return true
+    }
+    return false
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+  if(this.secretWord.indexOf(letter) > -1) {
+    this.guessedLetters += letter;
+  }
+  this.checkWinner()
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+   this.letters.push(letter)
+   this.errorsLeft -= 1
+   if(this.letters.includes(!letter)) {
+     letter.push(this.letters)
+   }
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if (this.errorsLeft > 0) {
+      return false
+    }
+    return true
   }
 
   checkWinner() {
-    // ... your code goes here
+    if(this.guessedLetters == this.secretWord) {
+      return true
+    }
+    if(this.guessedLetters != this.secretWord) {
+      return false
+    }
   }
 }
 
