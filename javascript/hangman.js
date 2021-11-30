842c4fb1c5677ea69024901211b45d3a86c2cdc8
GeorgeLR87 <jorgelrgarduno@gmail.com>
2021-11-30 15:21:52 -0600

    Logica de Juego Hangman

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,37 +1,68 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+    this.secretWord = this.pickWord(this.words);
+    this.Letters = [];
+    this.guessedLetters = '';
+    this.errorsLeft = 10;      
   }
+
+  
 
   pickWord() {
     // ... your code goes here
+    let randomValue = this.words[Math.floor(Math.random() * this.words.length)];
+    return randomValue;
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if (keyCode > 64 && keyCode < 91) {
+      this.guessedLetters = String.fromCharCode(keyCode);
+      return true;
+  }
+  return false;
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    if (this.letters.includes(letter)) return false;
+        else return true;
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    this.guessedLetters += letter;
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft--;
   }
 
   checkGameOver() {
     // ... your code goes here
+    if (this.errorsLeft === 0) {
+      return true;
+  } else {
+      return false;
+  }
   }
 
   checkWinner() {
     // ... your code goes here
+    let secArr = this.secretWord.split('');
+        let guessArr = this.guessedLetters.split('');
+        for (let i = 0; i < this.secretWord.length; i++) {
+            if (guessArr.includes(secArr[i])) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
   }
-}
+
 
 let hangman;
 
