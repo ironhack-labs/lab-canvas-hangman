775026bde4e0799436af5d5d909bc29d7f9854d7
joaonacho <joaonacho@gmail.com>
2021-12-02 16:37:46 +0000

    in drawlines

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -14,7 +14,21 @@ class HangmanCanvas {
   }
 
   drawLines() {
-    // ... your code goes here
+    //should have a loop to compute lines needed considering the secretWord length
+
+    this.context.beginPath();
+    this.context.moveTo(250, 790);
+    this.context.lineTo(300, 790);
+    this.context.moveTo(330, 790);
+    this.context.lineTo(380, 790);
+    this.context.moveTo(410, 790);
+    this.context.lineTo(460, 790);
+    this.context.moveTo(490, 790);
+    this.context.lineTo(540, 790);
+    this.context.moveTo(570, 790);
+    this.context.lineTo(620, 790);
+    this.context.stroke();
+    this.context.closePath();
   }
 
   writeCorrectLetter(index) {
