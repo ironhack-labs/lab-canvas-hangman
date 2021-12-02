9b5c6f31fb9adafcf562f5b6b856821149d1c8a4
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2021-12-02 10:03:13 +0000

    Update hangman.js

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,38 +1,63 @@
 class Hangman {
-  constructor(words) {
-    this.words = words;
-    // ... your code goes here
+    constructor(words, secretWord, letters, guessedLetters, errorsLeft) {
+      this.words = words;
+      this.secretWord = this.pickWord();
+      this.letters = [];
+      this.guessedLetters = "";
+      this.errorsLeft = 10;
   }
 
   pickWord() {
-    // ... your code goes here
+    let randomPick = this.words[Math.floor(Math.random() * this.words.length)]
+    return randomPick; 
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+    if (keyCode >=65 && keyCode <=90) {
+      return true
+    } else {
+      return false
+    }
   }
+  
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+    if (!this.letters.includes(letter)) {
+    return true
+    } else {
+    return false
+  }
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
-  }
+      this.guessedLetters += letter;
+    }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+    this.errorsLeft--;
+   
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if (this.errorsLeft > 0) {
+      return false
+    }else{
+    return true
+  }
   }
 
   checkWinner() {
-    // ... your code goes here
-  }
+    let newArr = this.secretWord.split('');
+    let guessedLett = this.guessedLetters.split('');
+        for (let i = 0; i < this.secretWord.length; i++) {
+            if (guessedLett.includes(newArr[i])) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
-
 let hangman;
 
 const startGameButton = document.getElementById('start-game-button');
