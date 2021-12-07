db4005162623555d59aaaf23649ac844e6482f58
Felipe Leite Mantovani <mantovani.devilz@gmail.com>
2021-12-07 17:20:16 -0300

    Fazendo o .drawHangman

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -30,15 +30,51 @@ class HangmanCanvas {
   }
 
   writeCorrectLetter(index) {
-    // ... your code goes here
+    let letter = this.secretWord[index].toUpperCase();
+    this.context.font = '40px Georgia';
+    for (let i = 0; i < this.secretWord.length; i += 1){
+      if (letter === this.secretWord[i].toUpperCase()){
+        this.context.fillText(letter, 310 + 50 * i + 10 * i, 680, 50)
+      }
+    }
   }
 
   writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
+    this.context.font = '40px Georgia';
+    let row = Math.floor (errorsLeft / 5);
+    this.context.fillText (
+      letter,
+      1000 - 60 * errorsLeft + 300 * row,
+      300 - 100 * row,
+      50,
+    );
   }
 
   drawHangman(errorsLeft) {
-    // ... your code goes here
+    switch(errorsLeft){
+      case 9:
+       this.context.beginPath();
+       this.context.arc(500, 300, 50, 0, Math.PI * 2)
+       this.context.stroke();
+       this.context.closePath();
+       break;
+      case 8:
+        break;
+      case 7:
+        break;
+      case 6:
+        break;
+      case 5:
+        break;
+      case 4:
+        break;
+      case 3:
+        break;
+      case 2:
+        break;
+      case 1:
+        break;
+    }
   }
 
   gameOver() {
