83bca5612abfe33ee00b7fec9ee5ff7aee345290
Joaquin Velazquez <joaquin.ve@outlook.com>
2022-01-19 16:04:08 -0600

    Hangman terminado

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,65 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 10;
   }
 
   pickWord() {
     // ... your code goes here
-  }
+    return this.words[Math.floor(Math.random() * this.words.length)];
+    }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if(keyCode < 65 || keyCode > 90) {
+      return false
+    } else {
+      return true
+    }
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    //this.letters.push(letter);
+    if(!this.letters.includes(letter)) {
+      return true
+    } else {
+      return false
+    }
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    return this.guessedLetters += letter
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    return this.errorsLeft -= 1
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft > 0) {
+      return false
+    } else {
+      return true
+    }
   }
 
   checkWinner() {
-    // ... your code goes here
+    for(let i=0; i<this.secretWord.length;i++){
+      if(this.guessedLetters.indexOf(this.secretWord[i]) === -1) {
+      return false
+    } else {
+      continue
+    }
+    
+  }
+    return true
   }
 }
 
