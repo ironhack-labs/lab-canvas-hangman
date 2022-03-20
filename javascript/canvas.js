63dad7205624754d5f3ebaeed4180603a1cfd27e
Cintia Mendes <cintiamendesx@gmail.com>
2022-03-20 16:23:52 -0300

    interactions 1

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -2,14 +2,23 @@ class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
     // ... your code goes here
+    this.secretWord = secretWord;
+    console.log(this.secretWord)
   }
 
   createBoard() {
     // ... your code goes here
+    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+    this.drawLines();
+    this.writeCorrectLetter();
   }
 
   drawLines() {
     // ... your code goes here
+    for(let i = 1; i <= this.secretWord.length; i++) {
+      this.context.fillStyle = "black";
+      this.context.fillRect(400 + i * 80, 700, 50, 5);
+    }
   }
 
   writeCorrectLetter(index) {
