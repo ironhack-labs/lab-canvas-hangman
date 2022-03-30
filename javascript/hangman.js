97ac4aebc6ba6770cb2c3419ec80f255a19f0e1e
Melissa Osorio Tavera <melissa.osorio.tavera@gmail.com>
2022-03-30 13:26:06 -0600

    Ejercicio interation 1 terminado

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,69 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters="";
+    this.errorsLeft= 10;
+
   }
 
   pickWord() {
     // ... your code goes here
+    let randomWord = this.words[Math.floor(Math.random()*this.words.length)];
+    return randomWord;
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if (keyCode>=65 && keyCode<=90){
+      return true;
+    } else{
+      return false;
+    }
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    if(this.letters.includes(letter)){
+      return false
+    } else {
+      return true
+    }
+
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    return this.guessedLetters+=letter
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft-=1
+    if(!this.letters.includes(letter)) {
+      this.letters.push(letter)
+    }
+    return
   }
 
   checkGameOver() {
     // ... your code goes here
+if(this.errorsLeft<=0){
+ return true
+}else{
+  return false
+}
   }
 
   checkWinner() {
     // ... your code goes here
+  for(let i=0; i<this.secretWord.length; i++){
+    if(this.guessedLetters.indexOf(this.secretWord[i])===-1){
+      return false
+    }
+  }
+  return true
   }
 }
 
