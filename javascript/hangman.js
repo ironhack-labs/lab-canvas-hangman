dabb1456905457436f2fe4229c3b7f42a2b2ae86
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-03-30 13:15:19 -0600

    Lab fase 1 terminado

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,68 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = this.pickWord()
+    // this.randomWord = Math.floor((Math.random()*10)+1);
+    // this.secretWord = `${words[this.randomWord]}`;
+    
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 10;
   }
 
   pickWord() {
     // ... your code goes here
+    const randomNumber = Math.random()* this.words.length;
+    const positionArr = Math.floor(randomNumber);
+    return this.words[positionArr];
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if (keyCode >=65 && keyCode<=90){
+      return true;
+    }
+    return false;
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    if (this.letters.includes(letter)){
+      return false;
+    }
+    return true;
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    return this.guessedLetters += letter;
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft -= 1;
+    if (!this.letters.includes(letter)){
+      this.letters.push(letter);
+    }
   }
 
   checkGameOver() {
     // ... your code goes here
+    if (this.errorsLeft >0){
+      return false;
+    }else{
+      return true;
+    }
   }
 
   checkWinner() {
     // ... your code goes here
+    let sortedSecretWord = this.secretWord.split("").sort().join("");
+    const orderedGuessedWord = this.guessedLetters.split("").sort().join("");
+    if (sortedSecretWord === orderedGuessedWord){
+      return true;
+    }
+    return false;
   }
 }
 
