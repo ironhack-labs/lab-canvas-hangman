59725dfdbdb35c3742d063b75f30ff7076db384d
David Garcia <metaphysicaless1489@gmail.com>
2022-03-30 17:29:02 -0700

    nuevos cambios

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -86,9 +86,9 @@ if (startGameButton) {
   startGameButton.addEventListener('click', event => {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
-    // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+    //HINT (uncomment when start working on the canvas portion of the lab)
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
     // ... your code goes here
   });
