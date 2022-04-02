fb713de6334388b126b8cbcef9aeebf592ff2b8c
Georgina Aceves <aceves.georgina@hotmail.com>
2022-04-02 16:47:42 +0200

    done canvas.js and hangman.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,34 +1,140 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
-    // ... your code goes here
+
+    this.secretWord = secretWord
+    this.gameOverImg = new Image()
+    this.gameOverImg.src = "../images/gameover.png"
+    this.winnerImg = new Image()
+    this.winnerImg.src = "../images/awesome.png"
+    this.createBoard()
+
   }
 
   createBoard() {
-    // ... your code goes here
+
+    this.context.clearRect(0, 0, 1200, 800)
+    this.drawLines()
+
   }
 
   drawLines() {
-    // ... your code goes here
+
+    for(let i=0; i< this.secretWord.length;i++){
+      this.context.beginPath()
+      this.context.moveTo(250+i*70,600)
+      this.context.lineTo(300+i*70,600)
+      this.context.stroke()
+      this.context.closePath()
+    }
+
   }
 
   writeCorrectLetter(index) {
-    // ... your code goes here
+
+    let letter = index
+    this.context.font = "50px Arial"
+    for(let i=0; i< this.secretWord.length;i++){
+      if (letter === this.secretWord[i]){
+        this.context.fillText(letter,265+i*70,600)
+      }
+    }
+
   }
 
   writeWrongLetter(letter, errorsLeft) {
-    // ... your code goes here
+
+    this.context.clearRect(410, 100, 900, 150)
+
+    this.context.font = "30px Arial"
+
+    const errorsString = `Errors Left: ${errorsLeft}`
+    this.context.fillText(errorsString,800,200)
+
+    for(let i=0; i < letter.length ; i++){
+      this.context.fillText(letter[i],500+i*70,300)
+    }
+
   }
 
   drawHangman(errorsLeft) {
-    // ... your code goes here
+
+    console.log(errorsLeft)
+    switch (errorsLeft){
+      case 9:
+        this.context.lineWidth = 5
+        this.context.beginPath();
+        this.context.moveTo(50, 600);
+        this.context.lineTo(200, 600);
+        this.context.lineTo(125, 525);
+        this.context.lineTo(50, 600);
+        this.context.stroke();
+        this.context.closePath();
+        break;
+      case 8:
+        this.context.moveTo(125, 525);
+        this.context.lineTo(125, 150);
+        this.context.stroke();
+        break;
+      case 7:
+        this.context.moveTo(125, 150);
+        this.context.lineTo(350, 150);
+        this.context.stroke();
+        break;
+      case 6:
+        this.context.moveTo(350, 150);
+        this.context.lineTo(350, 200);
+        this.context.stroke();
+        break;
+      case 5:
+        this.context.beginPath();
+        this.context.arc(350, 250, 50, 0, 2 * Math.PI);
+        this.context.stroke();
+        break;
+      case 4:
+        this.context.moveTo(350, 300);
+        this.context.lineTo(350, 450);
+        this.context.stroke();
+        break;
+      case 3:
+        this.context.moveTo(350, 350);
+        this.context.lineTo(300, 310);
+        this.context.stroke();
+        break;
+      case 2:
+        this.context.moveTo(350, 350);
+        this.context.lineTo(400, 310);
+        this.context.stroke();
+        break;
+      case 1:
+        this.context.moveTo(350, 450);
+        this.context.lineTo(300, 490);
+        this.context.stroke();
+        break;
+      case 0:
+        this.context.moveTo(350, 450);
+        this.context.lineTo(400, 490);
+        this.context.stroke();
+        break;
+    }
+
   }
 
   gameOver() {
-    // ... your code goes here
+
+    const img = new Image()
+    img.src = "../images/gameover.png"
+    img.onload = this.context.drawImage(img,200,100,400,400)
+  }
+
   }
 
   winner() {
-    // ... your code goes here
+
+    const img = new Image()
+    img.src = "../images/awesome.png"
+    img.onload = this.context.drawImage(img,200,100,400,400)
+  
   }
+  
 }
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,35 +1,78 @@
 class Hangman {
   constructor(words) {
     this.words = words;
-    // ... your code goes here
+
+    this.secretWord = this.pickWord();
+    this.letters = [];
+    this.guessedLetters = "";
+    this.errorsLeft = 10;
+
   }
 
   pickWord() {
-    // ... your code goes here
+
+    const randomWord = this.words[Math.floor(Math.random()*this.words.length)];
+    return randomWord;
+  
   }
 
   checkIfLetter(keyCode) {
-    // ... your code goes here
+
+    if (keyCode >= 65 && keyCode <=90 ){
+      return true
+    } else{
+      return false
+    }
+
   }
 
   checkClickedLetters(letter) {
-    // ... your code goes here
+
+    if (this.letters.includes(letter)){
+      return false
+    } else{
+      return true
+    }
+
   }
 
   addCorrectLetter(letter) {
-    // ... your code goes here
+
+    return this.guessedLetters += letter
+
   }
 
   addWrongLetter(letter) {
-    // ... your code goes here
+
+    this.errorsLeft -= 1
+    if (!this.letters.includes(letter)){
+      this.letters.push(letter)
+    }
+    return
+
   }
 
   checkGameOver() {
-    // ... your code goes here
+    if (this.errorsLeft > 0){
+      return false
+    } else {
+      return true
+    }
+
   }
 
   checkWinner() {
-    // ... your code goes here
+    if(this.guessedLetters.length === this.secretWord.length){
+      for (let i=0; i < this.guessedLetters.length; i++){
+        if (!this.secretWord.includes(this.guessedLetters[i])){
+          return false
+        } 
+      }
+      return true
+    } else{
+      return false
+    }
+
   }
 }
 
@@ -41,15 +84,41 @@ if (startGameButton) {
   startGameButton.addEventListener('click', event => {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
-    // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
-
-    // ... your code goes here
+    hangman.secretWord = hangman.pickWord();
+    hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+   
   });
 }
 
 document.addEventListener('keydown', event => {
-  // React to user pressing a key
-  // ... your code goes here
+  
+  let letter = event.key
+
+  if(hangman.checkIfLetter(letter.toUpperCase().charCodeAt(0))){
+    if (hangman.secretWord.includes(letter)){
+      if(!hangman.guessedLetters.includes(letter)){
+
+        for (let i = 0; i <hangman.secretWord.length; i++){
+          if (letter === hangman.secretWord[i]){
+            hangman.addCorrectLetter(letter)
+          }
+        }
+        hangmanCanvas.writeCorrectLetter(letter)
+        console.log(hangman.guessedLetters)
+      }
+      if(hangman.checkWinner()) {
+        hangmanCanvas.winner()
+      }
+    } else{
+      if (hangman.checkClickedLetters(letter)){
+        hangman.addWrongLetter(letter)
+        hangmanCanvas.writeWrongLetter(hangman.letters, hangman.errorsLeft)
+        hangmanCanvas.drawHangman(hangman.errorsLeft)
+
+      }
+      if(hangman.checkGameOver()){
+        hangmanCanvas.gameOver()
+      }
+    }
+  }
 });
