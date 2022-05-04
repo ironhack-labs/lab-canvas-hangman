40e76ec4520261de16420f38b6a736dc7389d335
Ulises-lab <yaftemiranda@gmail.com>
2022-05-04 18:39:57 -0500

    Me hicieron falta la mayoria de los metodos del archivo canvas.js :(

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -2,14 +2,22 @@ class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
     // ... your code goes here
+    this.secretWord = secretWord;
   }
 
   createBoard() {
     // ... your code goes here
+    const board = this.context;
+    board.clearRect(0,0,1200,800);
+    this.drawLines();
   }
 
   drawLines() {
     // ... your code goes here
+    for(i = 0; i< this.secretWord.length; i++){
+      this.context.beginPath();
+      this.context.moveTo()
+    }
   }
 
   writeCorrectLetter(index) {
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,71 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = this.words[Math.floor(Math.random() * words.length)];
+    this.errorsLeft = 10;
+    this.guessedLetters = '';
+    this.letters = [];
   }
 
   pickWord() {
     // ... your code goes here
+    return this.secretWord;
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    //let value_letter = letter.keyCode || letter.which;
+    if((keyCode>64 && keyCode<91)) {
+      return true;
+    }else {
+      return false;
+    }
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    // let array_letters = this.letters;
+    /* if(this.letters){
+      return false;
+    }else{
+      return true;
+    } */
+    if(this.letters.includes(letter)){
+      return false;
+    }else{
+      return true;
+    }
+    //array_letters.push(letter.uppperCase);    
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    return this.guessedLetters += letter;
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    this.errorsLeft -= 1;
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft <= 0){
+      return true;
+    }else{
+      return false;
+    }
   }
 
   checkWinner() {
     // ... your code goes here
+    const order_secret_word = this.secretWord.split('').sort().join();
+    const order_guessed_letters = this.guessedLetters.split('').sort().join();
+    if(order_guessed_letters === order_secret_word){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
 
@@ -42,8 +79,8 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+     hangman.secretWord = hangman.pickWord();
+     hangmanCanvas = new HangmanCanvas(hangman.secretWord);
 
     // ... your code goes here
   });
