cb4d92ff1123ace9b7e79ef71e6da55329df98f0
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-05-05 18:14:28 +0200

    few tasks in canvas.js and complete hangman.js done

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -2,33 +2,80 @@ class HangmanCanvas {
   constructor(secretWord) {
     this.context = document.getElementById('hangman').getContext('2d');
     // ... your code goes here
+    //this.canvas = document.getElementById('hangman');
+    //this.context = this.canvas.getContext('2d');
+    this.secretWord = secretWord;
   }
 
   createBoard() {
     // ... your code goes here
+    this.context.clearRect(0, 0, 1200, 800);
+    this.drawLines();
   }
 
   drawLines() {
     // ... your code goes here
+
+    for (let i = 0; i < this.secretWord.length; i++) {
+      this.context.beginPath();
+      this.context.moveTo(200 + i * 70, 600);
+      this.context.lineTo(250 + i * 70, 600);
+      this.context.stroke();
+      this.context.closePath();
+      
+    }
   }
 
   writeCorrectLetter(index) {
     // ... your code goes here
+   /* checkClickedLetters(letter);
+    if(this.secretWord.includes(letter)) {
+     context.fillText(letter, index*60 + 240, 590);
+   } */
+    
   }
 
   writeWrongLetter(letter, errorsLeft) {
     // ... your code goes here
-  }
+   /* if(!this.secretWord.includes(letter)) {
+      context.fillText(letter, index*60 + 400, 300);
+    } */
+  } 
 
   drawHangman(errorsLeft) {
     // ... your code goes here
+    /* switch (errorsLeft) {
+      /*case 0:
+      
+      this.context.beginPath();
+      this.context.moveTo(75, 50);
+      this.context.lineTo(100, 75);
+      this.context.lineTo(100, 25);
+      this.context.stroke();
+      this.context.beginPath();
+      ctx.arc(x, y, radius, startAngle, endAngle, true);
+      ctx.moveTo(290, 75)
+      ctx.arc(100, 75, 40, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.closePath(); 
+    } */
   }
 
   gameOver() {
     // ... your code goes here
+    const gameOverImg = new Image();
+    gameOverImg.src = './images/gameover.png';
+    let goX = 0;
+    let goY = 0;
+    this.context.drawImage(gameOverImg, goX, goY);
   }
 
   winner() {
     // ... your code goes here
+    const winnerImg = new Image();
+    winnerImg.src = './images/awesome.png';
+    let wX = 0;
+    let wY = 0;
+    this.context.drawImage(winnerImg, wX, wY);
   }
 }
--- javascript/hangman.js
+++ javascript/hangman.js
@@ -2,34 +2,53 @@ class Hangman {
   constructor(words) {
     this.words = words;
     // ... your code goes here
+    this.secretWord = this.pickWord();
+    console.log(this.secretWord);
+    this.letters = [];
+    this.guessedLetters = '';
+    this.errorsLeft = 10;
   }
 
   pickWord() {
     // ... your code goes here
+      const randomWord= this.words[Math.floor(Math.random()*this.words.length)];
+      return randomWord;
   }
 
   checkIfLetter(keyCode) {
     // ... your code goes here
+    if(keyCode >= 65 && keyCode <= 90) return true;
+      return false;
   }
 
   checkClickedLetters(letter) {
     // ... your code goes here
+    if(!this.letters.includes(letter)) return true;
+      return false;
   }
 
   addCorrectLetter(letter) {
     // ... your code goes here
+    this.guessedLetters += letter;
   }
 
   addWrongLetter(letter) {
     // ... your code goes here
+    if(this.checkClickedLetters(letter)) {
+    this.errorsLeft -= 1;
+    this.letters.push(letter);
+   }
   }
 
   checkGameOver() {
     // ... your code goes here
+    if(this.errorsLeft > 0) return false;
+    return true;
   }
 
   checkWinner() {
     // ... your code goes here
+    return this.secretWord.split('').every(letter => this.guessedLetters.includes(letter));
   }
 }
 
@@ -42,10 +61,11 @@ if (startGameButton) {
     hangman = new Hangman(['node', 'javascript', 'react', 'miami', 'paris', 'amsterdam', 'lisboa']);
 
     // HINT (uncomment when start working on the canvas portion of the lab)
-    // hangman.secretWord = hangman.pickWord();
-    // hangmanCanvas = new HangmanCanvas(hangman.secretWord);
-
+     hangman.secretWord = hangman.pickWord();
+     hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  
     // ... your code goes here
+    hangmanCanvas.createBoard();
   });
 }
 
